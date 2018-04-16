3b756eba872c2a25ac48b237e2199cc5860332ee
Richard Tabor <hello@themebeans.com>
2018-04-16 15:25:14 -0400

    Tidy the Spacer Block

--- src/blocks/spacer/index.js
+++ src/blocks/spacer/index.js
@@ -1,12 +1,9 @@
-// Import CSS
-import './editor.scss';
-import './style.scss';
+import './styles/editor.scss';
+import './styles/style.scss';
 
-// External Dependencies
 import classnames from 'classnames';
 import ResizableBox from 're-resizable';
 
-// Internationalization
 const { __ } = wp.i18n;
 
 const { RangeControl } = wp.components;
@@ -16,7 +13,6 @@ const {
 	InspectorControls
 } = wp.blocks;
 
-// Custom Icon.
 const icon = [
 	<svg aria-hidden role="img" focusable="false" className="dashicon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
 		<path d="M0,3 L20,3 L20,17 L0,17 L0,3 Z M2,5.03271484 L2,15.0327148 L18.001709,15.0327148 L18.001709,5.03271484 L2,5.03271484 Z"></path>
@@ -67,7 +63,6 @@ registerBlockType( 'coblocks/spacer', {
 		);
 
 		return [
-
 			inspectorControls,
 			<ResizableBox
 				className={ classnames( className, 'coblocks-spacer-control' ) }
