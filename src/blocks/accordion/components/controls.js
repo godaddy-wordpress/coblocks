8f123d7469dc5e3303c402bf1271ac80b3484610
Richard Tabor <hello@themebeans.com>
2018-04-26 17:05:00 -0400

    Update accordion open icon

--- src/blocks/accordion/components/controls.js
+++ src/blocks/accordion/components/controls.js
@@ -1,9 +1,15 @@
 /**
+ * Internal dependencies
+ */
+import icons from './icons';
+
+/**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { Component } = wp.element;
 const { AlignmentToolbar, BlockControls } = wp.blocks;
+const { Toolbar, IconButton } = wp.components;
 
 export default class Controls extends Component {
 
@@ -20,8 +26,18 @@ export default class Controls extends Component {
 
 		const { title, content, open, textAlign } = attributes;
 
+		const customControls = [
+			{
+				icon: icons.open,
+				title: __( 'Display open' ),
+				onClick: () => setAttributes( {  open: ! open } ),
+				isActive: open === true,
+			}
+		];
+
 		return (
 			<BlockControls key="controls">
+				<Toolbar controls={ customControls } />
 				<AlignmentToolbar
 					value={ textAlign }
 					onChange={ ( nextTextAlign ) => setAttributes( { textAlign: nextTextAlign } ) }
--- src/blocks/accordion/components/icons.js
+++ src/blocks/accordion/components/icons.js
@@ -12,4 +12,13 @@ icons.accordion =
     </g>
 </svg>;
 
+icons.open =
+<svg className="dashicon" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g transform="translate(7.000000, 3.000000)" fill="currentColor" fill-rule="nonzero">
+            <polygon points="0 12.4144941 2.9165 12.4144941 0 9.24610888 2.5 9.24610888 2.5 4.7525778 0 4.7525778 2.9165 1.5841926 0 1.5841926 0 6.10622664e-16 7 6.10622664e-16 7 1.5841926 4.0835 1.5841926 7 4.7525778 4.5 4.7525778 4.5 9.24610888 7 9.24610888 4.0835 12.4144941 7 12.4144941 7 13.9986867 0 13.9986867"></polygon>
+        </g>
+    </g>
+</svg>;
+
 export default icons;
