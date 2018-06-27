7e3e740077c2bbf2bdf57ab97bcf85d56e31fade
Rich Tabor <hi@richtabor.com>
2018-06-27 11:52:17 -0400

    buttonColor Click to Tweet fix

--- src/blocks/click-to-tweet/components/colors.js
+++ src/blocks/click-to-tweet/components/colors.js
@@ -9,6 +9,7 @@ const { withColors } = wp.editor;
 const applyWithColors = withColors(
 	'backgroundColor',
 	{ textColor: 'color' },
+	{ buttonColor: 'background-color' },
 );
 
 export default applyWithColors;
