db727b7b1da5a9a9650475e991e19e0710ea9178
Rich Tabor <hi@richtabor.com>
2018-06-27 17:43:43 -0400

    Map Pricing Table Columns

--- src/blocks/pricing-table/components/controls.js
+++ src/blocks/pricing-table/components/controls.js
@@ -6,7 +6,7 @@ import icons from './icons';
 /**
  * WordPress dependencies
  */
-const { __ } = wp.i18n;
+const { __, sprintf } = wp.i18n;
 const { Component } = wp.element;
 const { AlignmentToolbar, BlockControls } = wp.editor;
 const { Toolbar, IconButton } = wp.components;
@@ -26,24 +26,21 @@ export default class Controls extends Component {
 
 		const { columns } = attributes;
 
-		const customControls = [
-			{
-				icon: icons.col_1,
-				title: __( 'One Column' ),
-				onClick: () => setAttributes( { columns: 1 } ),
-				isActive: columns === 1,
-			},
-			{
-				icon: icons.col_2,
-				title: __( 'Two Column' ),
-				onClick: () => setAttributes( { columns: 2 } ),
-				isActive: columns === 2,
-			},
-		];
-
 		return (
-			<BlockControls key="controls">
-				<Toolbar controls={ customControls } />
+			<BlockControls>
+				<Toolbar
+					controls={ '12'.split( '' ).map( ( count ) => ( {
+						icon: icons.blank,
+						// translators: %s: columns count e.g: "1", "2", "3"
+						title: sprintf( __( '%s Columns' ), count ),
+						isActive: columns == count,
+						subscript: count,
+						onClick: () =>
+							setAttributes( {
+								columns: count
+							} )
+					} ) ) }
+				/>
 			</BlockControls>
 		);
 	}
