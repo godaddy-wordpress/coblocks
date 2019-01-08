929aaec9a8e588944810617b9517bc4d4b163789
Rich Tabor <hi@richtabor.com>
2019-01-08 14:04:35 -0500

    Move registration into a separate class

--- includes/class-coblocks-block-assets.php
+++ includes/class-coblocks-block-assets.php
@@ -38,13 +38,6 @@ class CoBlocks_Block_Assets {
 	}
 
 	/**
-	 * The base directory path (without trailing slash).
-	 *
-	 * @var string $_url
-	 */
-	private $_dir;
-
-	/**
 	 * The base URL path (without trailing slash).
 	 *
 	 * @var string $_url
@@ -71,85 +64,10 @@ class CoBlocks_Block_Assets {
 	private function __construct() {
 		$this->_version = COBLOCKS_VERSION;
 		$this->_slug    = 'coblocks';
-		$this->_dir     = untrailingslashit( plugin_dir_path( '/', __FILE__ ) );
 		$this->_url     = untrailingslashit( plugins_url( '/', dirname( __FILE__ ) ) );
 
-		add_action( 'init', array( $this, 'register_blocks' ), 99 );
 		add_action( 'init', array( $this, 'editor_assets' ) );
 		add_action( 'enqueue_block_assets', array( $this, 'block_assets' ) );
-	}
-
-	/**
-	 * Add actions to enqueue assets.
-	 *
-	 * @access public
-	 */
-	public function register_blocks() {
-
-		// Return early if this function does not exist.
-		if ( ! function_exists( 'register_block_type' ) ) {
-			return;
-		}
-
-		// Shortcut for the slug.
-		$slug = $this->_slug;
-
-		register_block_type(
-			$slug . '/accordion', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/alert', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/author', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/click-to-tweet', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/dynamic-separator', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/gif', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/gist', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
-		register_block_type(
-			$slug . '/highlight', array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
 	}
 
 	/**
