f23bb610921ad012c493f3e4a8da4d7192879545
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-01-10 19:44:16 +0800

    Apply Typography Control to CoBlocks Click to Tweet

--- src/extensions/typography/controls.js
+++ src/extensions/typography/controls.js
@@ -25,7 +25,7 @@ class Controls extends Component {
 			setAttributes,
 		} = this.props;
 
-		const allowedBlocks = [ 'core/paragraph', 'core/heading', 'core/cover', 'core/button', 'core/list', 'coblocks/row', 'coblocks/column', 'coblocks/accordion' ];
+		const allowedBlocks = [ 'core/paragraph', 'core/heading', 'core/cover', 'core/button', 'core/list', 'coblocks/row', 'coblocks/column', 'coblocks/accordion', 'coblocks/click-to-tweet' ];
 
 		this.props.attributes.textPanelHideColor = true;
 		this.props.attributes.textPanelShowSpacingControls = true;
