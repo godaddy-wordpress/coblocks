ea42fcbc6404d334b1d92874af1eb388b4dc9c6c
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-01-10 21:49:54 +0800

    Update the description text in the API Key PanelBody to: Add your Google Maps API key. Updating this API key will set all your maps to use the new key.

--- src/blocks/google-map/components/inspector.js
+++ src/blocks/google-map/components/inspector.js
@@ -189,7 +189,7 @@ class Inspector extends Component {
 						</PanelBody>
 					}
 					<PanelBody title={ __( 'API Key' ) } className='block-coblocks__inspector-block-settings-panel-body' initialOpen={ false }>
-						<p>{ __( 'Add your Google Maps API key to display a Google map. Updating this API key will set all your maps to use this new key.') }</p>
+						<p>{ __( 'Add your Google Maps API key. Updating this API key will set all your maps to use the new key.') }</p>
 						{ ( this.state.apiKey === ''  ) ?
 							<p><span><a href={ RETRIEVE_KEY_URL } target="_blank"> { __( 'Retrieve your key' ) }</a> | <a href={ HELP_URL } target="_blank">{ __( 'Need help?' ) }</a></span></p>
 						: null }
