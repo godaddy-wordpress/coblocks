9c1f62c2a8574eee5e9ad70f9e588fb0594f9fe7
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-01-18 19:34:17 +0800

    Improvements : Row blocks are not properly responsive within the editor

--- src/blocks/row/components/edit.js
+++ src/blocks/row/components/edit.js
@@ -151,6 +151,7 @@ class Edit extends Component {
 			columns,
 			layout,
 			gutter,
+			stacked,
 			paddingTop,
 			paddingRight,
 			paddingBottom,
@@ -288,6 +289,7 @@ class Edit extends Component {
 				[ `has-${ paddingSize }-padding` ] : paddingSize && paddingSize != 'advanced',
 				'has-margin': marginSize && marginSize != 'no',
 				[ `has-${ marginSize }-margin` ] : marginSize && marginSize != 'advanced',
+				'is-stacked-on-mobile': stacked,
 			}
 		);
 
