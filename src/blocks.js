00d40f552d1dd1c8703a00088eceb6487f6cba19
Rich Tabor <hi@richtabor.com>
2019-01-23 13:26:38 -0500

    Import shapeDivider

--- src/blocks.js
+++ src/blocks.js
@@ -45,6 +45,7 @@ import * as row from './blocks/row';
 import * as social from './blocks/social';
 import * as map from './blocks/map';
 import * as mediaCard from './blocks/media-card';
+import * as shapeDivider from './blocks/shape-divider';
 
 export function registerBlocks () {
 	[
@@ -64,6 +65,7 @@ export function registerBlocks () {
 		social,
 		map,
 		mediaCard,
+		shapeDivider,
 	].forEach( ( block ) => {
 
 		if ( ! block ) {
