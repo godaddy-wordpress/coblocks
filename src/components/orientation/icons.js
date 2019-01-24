ad2b03ab545585d42a28b0a23f4a3e502c7d3713
Rich Tabor <hi@richtabor.com>
2019-01-24 18:30:52 -0500

    Add new Orientation control

---
+++ src/components/orientation/icons.js
@@ -0,0 +1,12 @@
+/**
+ * Custom icons
+ */
+const icons = {};
+
+icons.flipX =
+<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m12 20h2v-2h-2zm4-12h2v-2h-2zm-16-4v14c0 1.1.9 2 2 2h4v-2h-4v-14h4v-2h-4c-1.1 0-2 .9-2 2zm16-2v2h2c0-1.1-.9-2-2-2zm-8 20h2v-22h-2zm8-6h2v-2h-2zm-4-12h2v-2h-2zm4 8h2v-2h-2zm0 8c1.1 0 2-.9 2-2h-2z" transform="translate(3 1)"/></svg>
+
+icons.flipY =
+<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m12 20h2v-2h-2zm4-12h2v-2h-2zm-16-4v14c0 1.1.9 2 2 2h4v-2h-4v-14h4v-2h-4c-1.1 0-2 .9-2 2zm16-2v2h2c0-1.1-.9-2-2-2zm-8 20h2v-22h-2zm8-6h2v-2h-2zm-4-12h2v-2h-2zm4 8h2v-2h-2zm0 8c1.1 0 2-.9 2-2h-2z" transform="matrix(0 1 -1 0 23 3)"/></svg>
+
+export default icons;
