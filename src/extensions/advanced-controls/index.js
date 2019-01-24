9122c5364d83e51e1fc771baa659d1987b3eae13
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-01-24 19:47:07 +0800

    Add coBlocksSpacing to Spacer Block

--- src/extensions/advanced-controls/index.js
+++ src/extensions/advanced-controls/index.js
@@ -21,7 +21,7 @@ const { InspectorAdvancedControls }	= wp.editor;
 const { compose, createHigherOrderComponent } = wp.compose;
 const { ToggleControl } = wp.components;
 
-const blocksWithSpacingSupport = [ 'core/image', 'core/gallery' ];
+const blocksWithSpacingSupport = [ 'core/image', 'core/gallery', 'core/spacer' ];
 
 /**
  * Filters registered block settings, extending attributes with settings
