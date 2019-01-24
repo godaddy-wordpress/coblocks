e499996b9350ffe7281501a07e2ca0b3cd467fce
Rich Tabor <hi@richtabor.com>
2019-01-24 14:14:26 -0500

    Cleanup classes

--- src/blocks/media-card/index.js
+++ src/blocks/media-card/index.js
@@ -175,10 +175,6 @@ const settings = {
 			backgroundImage: backgroundImg ? `url(${ backgroundImg })` : undefined,
 		};
 
-		const innerClasses = classnames(
-			'wp-block-coblocks-media-card__inner', {
-		} );
-
 		const innerStyles = {
 			gridTemplateColumns,
 			maxWidth: maxWidth ? ( 'full' == align || 'wide' == align ) && maxWidth : undefined,
@@ -196,7 +192,7 @@ const settings = {
 		return (
 			<div className={ classes }>
 				<div className={ wrapperClasses } style={ wrapperStyles } >
-					<div className={ innerClasses } style={ innerStyles }>
+					<div className="wp-block-coblocks-media-card__inner" style={ innerStyles }>
 						<figure className={ classnames(
 								'wp-block-coblocks-media-card__media', {
 									'has-shadow': hasImgShadow,
