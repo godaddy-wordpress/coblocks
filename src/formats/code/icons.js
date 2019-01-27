98e3b6f9ff02205e98bd619b2a69167ac54953f9
Rich Tabor <hi@richtabor.com>
2019-01-26 20:24:18 -0500

    Add code format

---
+++ src/formats/code/icons.js
@@ -0,0 +1,9 @@
+/**
+ * Formatting icons
+ */
+const formatIcons = {};
+
+formatIcons.code =
+<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="m0 0h20v20h-20z"/><path d="m7.83333333 13.8333333-3.83333333-3.8333333 3.83333333-3.83333333-1.16666666-1.16666667-5 5 5 5zm4.33333337 0 3.8333333-3.8333333-3.8333333-3.83333333 1.1666666-1.16666667 5 5-5 5z" fill="currentColor" fill-rule="nonzero"/></g></svg>
+
+export default formatIcons;
---
+++ src/formats/code/index.js
@@ -0,0 +1,46 @@
+/**
+ * Internal dependencies
+ */
+import formatIcons from './icons';
+
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { toggleFormat } = wp.richText;
+const { RichTextToolbarButton, RichTextShortcut } = wp.editor;
+
+/**
+ * Block constants
+ */
+const name = 'coblocks/code';
+
+export const code = {
+	name,
+	title: __( 'Code' ),
+	tagName: 'code',
+	className: 'code',
+	edit( { isActive, value, onChange } ) {
+		const onToggle = () => onChange( toggleFormat( value, { type: name } ) );
+
+		return (
+			<Fragment>
+				<RichTextShortcut
+					type="access"
+					character="u"
+					onUse={ onToggle }
+				/>
+				<RichTextToolbarButton
+					icon={ formatIcons.code }
+					title={ __( 'code' ) }
+					onClick={ onToggle }
+					isActive={ isActive }
+					shortcutType="access"
+					shortcutCharacter="x"
+				/>
+			</Fragment>
+		);
+
+	},
+};
