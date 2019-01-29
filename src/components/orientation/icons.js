e185c622f514c21f6fe34fcdd28823a1c1ad3da7
Rich Tabor <hi@richtabor.com>
2019-01-29 11:07:11 -0500

    Update older flipX and flipY icons

--- src/components/orientation/icons.js
+++ src/components/orientation/icons.js
@@ -4,9 +4,9 @@
 const icons = {};
 
 icons.flipX =
-<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m12 20h2v-2h-2zm4-12h2v-2h-2zm-16-4v14c0 1.1.9 2 2 2h4v-2h-4v-14h4v-2h-4c-1.1 0-2 .9-2 2zm16-2v2h2c0-1.1-.9-2-2-2zm-8 20h2v-22h-2zm8-6h2v-2h-2zm-4-12h2v-2h-2zm4 8h2v-2h-2zm0 8c1.1 0 2-.9 2-2h-2z" transform="translate(3 1)"/></svg>
+<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m11 18h3v-2h-3zm3-12h2v-2h-2zm-14-2v12c0 1.1.9 2 2 2h3v-2h-3v-12h3v-2h-3c-1.1 0-2 .9-2 2zm14-2v2h2c0-1.1-.9-2-2-2zm-7 18h2v-20h-2zm7-4h2v-2h-2zm-3-12h3v-2h-3zm3 8h2v-4h-2zm0 6c1.1 0 2-.9 2-2h-2z" transform="translate(2)"/></svg>
 
 icons.flipY =
-<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m12 20h2v-2h-2zm4-12h2v-2h-2zm-16-4v14c0 1.1.9 2 2 2h4v-2h-4v-14h4v-2h-4c-1.1 0-2 .9-2 2zm16-2v2h2c0-1.1-.9-2-2-2zm-8 20h2v-22h-2zm8-6h2v-2h-2zm-4-12h2v-2h-2zm4 8h2v-2h-2zm0 8c1.1 0 2-.9 2-2h-2z" transform="matrix(0 1 -1 0 23 3)"/></svg>
+<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="m13 16h3v-2h-3zm3-12h2v-2h-2zm-14-2v12c0 1.1.9 2 2 2h3v-2h-3v-12h3v-2h-3c-1.1 0-2 .9-2 2zm14-2v2h2c0-1.1-.9-2-2-2zm-7 18h2v-20h-2zm7-4h2v-2h-2zm-3-12h3v-2h-3zm3 8h2v-4h-2zm0 6c1.1 0 2-.9 2-2h-2z" transform="matrix(0 1 -1 0 18 0)"/></svg>
 
 export default icons;
