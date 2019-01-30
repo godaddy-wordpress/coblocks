bf12016691b342ee43141c27c115af2647bd4019
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-01-30 22:56:51 +0800

    Better icon keywording

--- src/blocks/icon/icons/icons.js
+++ src/blocks/icon/icons/icons.js
@@ -6,49 +6,57 @@ import materialIcons from './material';
  */
 const icons = {};
 
+const keywords = {
+	heart : 'plus add circle insert',
+	add_circle : 'plus add circle insert',
+	remove_circle : 'plus add circle insert',
+	gesture : 'plus add circle insert',
+	flag : 'plus add circle insert',
+};
+
 icons.filled = {
 	heart : {
 		icon: materialIcons.heart,
-		keywords : 'plus add circle insert',
+		keywords : keywords.heart,
 	},
 	add_circle : {
 		icon: materialIcons.add_circle,
-		keywords : 'plus add circle insert',
+		keywords : keywords.add_circle,
 	},
 	remove_circle : {
 		icon: materialIcons.remove_circle,
-		keywords : 'plus add circle insert',
+		keywords : keywords.remove_circle,
 	},
 	gesture : {
 		icon: materialIcons.gesture,
-		keywords : 'plus add circle insert',
+		keywords : keywords.gesture,
 	},
 	flag : {
 		icon: materialIcons.remove_circle,
-		keywords : 'plus add circle insert',
+		keywords : keywords.remove_circle,
 	},
 }
 
 icons.outlined = {
 	heart : {
 		icon: materialIcons.heart_outline,
-		keywords : 'plus add circle insert',
+		keywords : keywords.heart_outline,
 	},
 	add_circle : {
 		icon: materialIcons.add_circle_outline,
-		keywords : 'plus add circle insert',
+		keywords : keywords.add_circle_outline,
 	},
 	remove_circle : {
 		icon: materialIcons.remove_circle_outline,
-		keywords : 'plus add circle insert',
+		keywords : keywords.remove_circle_outline,
 	},
 	gesture : {
 		icon: materialIcons.gesture,
-		keywords : 'plus add circle insert',
+		keywords : keywords.gesture,
 	},
 	flag : {
 		icon: materialIcons.remove_circle_outline,
-		keywords : 'plus add circle insert',
+		keywords : keywords.remove_circle_outline,
 	},
 }
 
