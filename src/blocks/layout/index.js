c6c32f126c0ff6cf0cdfd6440e5033cfe95a7d24
Rich Tabor <hi@richtabor.com>
2019-02-08 11:46:16 -0500

    Revert "Remove layout block"
    This reverts commit 83814a4a74b85dafb84a8fdb19fa4c898a24169b.

---
+++ src/blocks/layout/index.js
@@ -0,0 +1,85 @@
+/**
+ * Internal dependencies
+ */
+import './styles/editor.scss';
+import brandAssets from '../../utils/brand-assets';
+import Edit from './components/edit';
+
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { createBlock } = wp.blocks;
+
+
+/**
+ * Block constants
+ */
+const name = 'layout';
+
+const title = __( 'Layout' );
+
+const icon = brandAssets.blockIcon;
+
+const keywords = [
+	__( 'coblocks' ),
+	__( 'sections' ),
+	__( 'templates' ),
+];
+
+const settings = {
+
+	title: title,
+
+	description : __( 'Add a beautifully designed layout from your theme or the CoBlocks Layout Libary.' ),
+
+	keywords: keywords,
+
+	transforms: {
+		from: [
+			{
+				type: 'prefix',
+				prefix: ':coblocks',
+				transform: function( content ) {
+					return createBlock( `coblocks/${ name }`, {
+						content,
+					} );
+				},
+			},
+			{
+				type: 'prefix',
+				prefix: ':section',
+				transform: function( content ) {
+					return createBlock( `coblocks/${ name }`, {
+						content,
+					} );
+				},
+			},
+			{
+				type: 'prefix',
+				prefix: ':template',
+				transform: function( content ) {
+					return createBlock( `coblocks/${ name }`, {
+						content,
+					} );
+				},
+			},
+		],
+	},
+
+	edit:  Edit,
+
+	save( { attributes } ) {
+
+		const { content } = attributes;
+
+		return (
+			<div>
+				{ content }
+			</div>
+		);
+	},
+};
+
+export { name, title, icon, settings };
