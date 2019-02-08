b73a013533080d01fcc743e9a43e46b69fff995c
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-02-08 23:56:06 +0800

    Remove CoBlocks post type

--- class-coblocks.php
+++ class-coblocks.php
@@ -139,7 +139,6 @@ if ( ! class_exists( 'CoBlocks' ) ) :
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-post-meta.php';
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-google-map.php';
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-template-loader.php';
-			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-post-type.php';
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-modal.php';
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/class-coblocks-accordion-ie-support.php';
 			require_once COBLOCKS_PLUGIN_DIR . 'includes/get-dynamic-blocks.php';
@@ -152,8 +151,6 @@ if ( ! class_exists( 'CoBlocks' ) ) :
 				require_once COBLOCKS_PLUGIN_DIR . 'includes/admin/class-coblocks-feedback.php';
 				require_once COBLOCKS_PLUGIN_DIR . 'includes/admin/class-coblocks-install.php';
 				require_once COBLOCKS_PLUGIN_DIR . 'includes/admin/class-coblocks-url-generator.php';
-				require_once COBLOCKS_PLUGIN_DIR . 'includes/admin/class-coblocks-dashboard-columns.php';
-				require_once COBLOCKS_PLUGIN_DIR . 'includes/admin/class-coblocks-duplicate-template.php';
 			}
 		}
 
