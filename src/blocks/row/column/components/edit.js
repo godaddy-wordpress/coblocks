44c14d2c100f584b8e90edf07e5da957c0c39827
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-02-13 18:12:23 +0800

    Add FocalPointPicker to Column Bloock

--- src/blocks/row/column/components/edit.js
+++ src/blocks/row/column/components/edit.js
@@ -71,6 +71,8 @@ class Edit extends Component {
 			marginSize,
 			paddingSize,
 			contentAlign,
+			focalPoint,
+			hasParallax,
 		} = attributes;
 
 		const dropZone = (
@@ -100,6 +102,7 @@ class Edit extends Component {
 		const innerStyles = {
 			backgroundColor: backgroundColor.color,
 			backgroundImage: backgroundImg ? `url(${ backgroundImg })` : undefined,
+			backgroundPosition: focalPoint && ! hasParallax ? `${ focalPoint.x * 100 }% ${ focalPoint.y * 100 }%` : undefined,
 			color: textColor.color,
 			paddingTop: paddingSize === 'advanced' && paddingTop ? paddingTop + paddingUnit : undefined,
 			paddingRight: paddingSize === 'advanced' && paddingRight ? paddingRight + paddingUnit : undefined,
