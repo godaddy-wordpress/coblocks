6499e3655af3821315b7f9ca5d9b9d7d70da60fc
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-02-13 20:25:32 +0800

    Add padding to Row

--- src/blocks/hero/components/edit.js
+++ src/blocks/hero/components/edit.js
@@ -35,7 +35,7 @@ const { Spinner } = wp.components;
 */
 const ALLOWED_BLOCKS = [ 'core/heading', 'core/paragraph', 'core/spacer', 'core/button', 'core/list', 'core/image', 'coblocks/alert', 'coblocks/gif', 'coblocks/social', 'coblocks/row' , 'coblocks/column', 'coblocks/buttons' ];
 const TEMPLATE = [
-	[ 'coblocks/row', { columns: 1, align: 'full', layout: '100', paddingSize: 'huge', hasMarginControl: false, hasStackedControl: false, hasAlignmentControls: false, customBackgroundColor: '#FFFFFF' }, [
+	[ 'coblocks/row', { columns: 1, align: 'full', layout: '100', paddingSize: 'advanced', paddingUnit: '%', paddingTop: 5, paddingRight: 40, paddingBottom: 5, paddingLeft: 5, hasMarginControl: false, hasStackedControl: false, hasAlignmentControls: false, customBackgroundColor: '#FFFFFF' }, [
         [ 'coblocks/column', { width: "100" },
         	[
         		[ 'core/heading', { placeholder: _x( 'Add heading...', 'content placeholder' ), content: _x( 'Hero Block', 'content placeholder' ) , level: 3 } ],
