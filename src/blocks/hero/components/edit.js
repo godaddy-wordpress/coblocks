166ab001827a17df19bae70e3912210dcc6789cc
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-02-13 20:11:00 +0800

    Replace InnerBlocks content

--- src/blocks/hero/components/edit.js
+++ src/blocks/hero/components/edit.js
@@ -33,13 +33,14 @@ const { Spinner } = wp.components;
  * @constant
  * @type {string[]}
 */
-const ALLOWED_BLOCKS = [ 'core/heading', 'core/paragraph', 'core/spacer', 'core/button', 'core/list', 'core/image', 'coblocks/alert', 'coblocks/gif', 'coblocks/social', 'coblocks/row' , 'coblocks/column' ];
+const ALLOWED_BLOCKS = [ 'core/heading', 'core/paragraph', 'core/spacer', 'core/button', 'core/list', 'core/image', 'coblocks/alert', 'coblocks/gif', 'coblocks/social', 'coblocks/row' , 'coblocks/column', 'coblocks/buttons' ];
 const TEMPLATE = [
 	[ 'coblocks/row', { columns: 1, align: 'full', layout: '100', paddingSize: 'huge', hasMarginControl: false, hasStackedControl: false, hasAlignmentControls: false, customBackgroundColor: '#FFFFFF' }, [
         [ 'coblocks/column', { width: "100" },
         	[
         		[ 'core/heading', { placeholder: _x( 'Add heading...', 'content placeholder' ), content: _x( 'Hero Block', 'content placeholder' ) , level: 3 } ],
-			[ 'core/paragraph', { placeholder: _x( 'Add content...', 'content placeholder' ), content: _x( 'An introductory area of a page accompanied by a small amount of text and a call to action.', 'content placeholder' ) } ],
+				[ 'core/paragraph', { placeholder: _x( 'Add content...', 'content placeholder' ), content: _x( 'An introductory area of a page accompanied by a small amount of text and a call to action.', 'content placeholder' ) } ],
+				[ 'coblocks/buttons', { contentAlign: 'left', items: 2, gutter: 'medium' }],
         	]
         ],
     ] ],
