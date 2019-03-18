63d68cbc7dcf7200dedef71233b2b52e3048be58
Rich Tabor <hi@richtabor.com>
2019-03-18 11:01:28 -0400

    Try using a more traditional menu for selection edit/remove

--- src/components/background/controls.js
+++ src/components/background/controls.js
@@ -56,7 +56,7 @@ function BackgroundImageToolbarControls( props, options ) {
 					{ backgroundImg ?
 						<Dropdown
 							className={ classnames( 'components-dropdown-menu', 'components-coblocks-icon-dropdown' ) }
-							contentClassName="components-dropdown-menu__popover components-coblocks-icon-dropdown__popover"
+							contentClassName="components-dropdown-menu__popover"
 							renderToggle={ ( { isOpen, onToggle } ) => {
 								const openOnArrowDown = ( event ) => {
 									if ( ! isOpen && event.keyCode === DOWN ) {
@@ -73,8 +73,8 @@ function BackgroundImageToolbarControls( props, options ) {
 										onKeyDown={ openOnArrowDown }
 										aria-haspopup="true"
 										aria-expanded={ isOpen }
-										label={ __( 'Edit background' ) }
-										tooltip={ __( 'Background' ) }
+										label={ 'Edit background' }
+										tooltip={ 'Edit background' }
 									>
 										<span className="components-dropdown-menu__indicator" />
 									</IconButton>
@@ -83,55 +83,55 @@ function BackgroundImageToolbarControls( props, options ) {
 							renderContent={ ( { onClose } ) => {
 								return (
 									<NavigableMenu
-										className="components-coblocks-icon-dropdown"
+										className="components-dropdown-menu__menu"
 										role="menu"
-										aria-label={ __( 'Edit Background' ) }
+										aria-label={ __( 'Edit background' ) }
 									>
-										<div className="components-button-group">
-											<MediaUpload
-												onSelect={ ( media ) => {
-													setAttributes( { backgroundImg: media.url } );
-													// Set padding when background image is added.
-													if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
-														if ( ! attributes.paddingSize || attributes.paddingSize == 'no' ) {
-															setAttributes( { paddingSize: 'medium' } );
-														}
+										<MediaUpload
+											onSelect={ ( media ) => {
+												setAttributes( { backgroundImg: media.url } );
+												// Set padding when background image is added.
+												if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
+													if ( ! attributes.paddingSize || attributes.paddingSize == 'no' ) {
+														setAttributes( { paddingSize: 'medium' } );
 													}
-												} }
-												allowedTypes={ ALLOWED_BG_MEDIA_TYPES }
-												value={ backgroundImg }
-												render={ ( { open } ) => (
-													<IconButton
-														className="components-toolbar__control"
-														label={ __( 'Edit' ) }
-														icon={ icons.edit }
-														onClick={ open }
-													/>
-												) }
-											/>
-											<IconButton
-												className="components-toolbar__control"
-												label={ __( 'Remove' ) }
-												icon={ icons.trash }
-												onClick={ () => {
-													setAttributes( {
-														backgroundImg: '',
-														backgroundOverlay: 0,
-														backgroundRepeat: 'no-repeat',
-														backgroundPosition: '',
-														backgroundSize: 'cover',
-														hasParallax: false,
-													} );
-													// Remove padding when background image is removed.
-													if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
-														if( attributes.paddingSize ){
-															setAttributes( { paddingSize: 'no' } );
-														}
+												}
+											} }
+											allowedTypes={ ALLOWED_BG_MEDIA_TYPES }
+											value={ backgroundImg }
+											render={ ( { open } ) => (
+												<IconButton
+													className="components-dropdown-menu__menu-item"
+													icon={ icons.edit }
+													role="menuitem"
+													onClick={ open } >
+														{ __( 'Edit Background' ) }
+												</IconButton>
+
+											) }
+										/>
+										<IconButton
+											className="components-dropdown-menu__menu-item"
+											icon={ icons.trash }
+											role="menuitem"
+											onClick={ () => {
+												setAttributes( {
+													backgroundImg: '',
+													backgroundOverlay: 0,
+													backgroundRepeat: 'no-repeat',
+													backgroundPosition: '',
+													backgroundSize: 'cover',
+													hasParallax: false,
+												} );
+												// Remove padding when background image is removed.
+												if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
+													if( attributes.paddingSize ){
+														setAttributes( { paddingSize: 'no' } );
 													}
-												} }
-											>
-											</IconButton>
-										</div>
+												}
+											} } >
+												{ __( 'Remove Background' ) }
+										</IconButton>
 									</NavigableMenu>
 								);
 							} }
@@ -152,7 +152,7 @@ function BackgroundImageToolbarControls( props, options ) {
 							render={ ( { open } ) => (
 								<IconButton
 									className="components-toolbar__control"
-									label={ __( 'Background' ) }
+									label={ __( 'Add background' ) }
 									icon={ icons.backgroundImage }
 									onClick={ open }
 								/>
