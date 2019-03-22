2770ab94ba72c4f1e5a7b9ba0ed93619cd6325cd
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-03-22 20:15:07 +0800

    Use Popover instead of Dropdown on background editing

--- src/components/background/controls.js
+++ src/components/background/controls.js
@@ -17,7 +17,7 @@ import { ALLOWED_BG_MEDIA_TYPES, BLOCKS_WITH_AUTOPADDING } from './';
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
 const { AlignmentToolbar, MediaUpload, MediaUploadCheck } = wp.editor;
-const { Toolbar, IconButton, Button, DropdownMenu, ButtonGroup, Dropdown, NavigableMenu } = wp.components;
+const { Toolbar, IconButton, Button, DropdownMenu, ButtonGroup, Dropdown, NavigableMenu, Popover } = wp.components;
 const { DOWN } = wp.keycodes;
 
 /**
@@ -32,95 +32,75 @@ function BackgroundImageToolbarControls( props, options ) {
 
 	const {
 		backgroundImg,
+		openPopover,
 	} = attributes;
 
 	return (
 		<Fragment>
 			<MediaUploadCheck>
 				<Toolbar>
-					{ backgroundImg ?
-						<Dropdown
-							className={ classnames( 'components-dropdown-menu', 'components-coblocks-icon-dropdown' ) }
-							contentClassName="components-dropdown-menu__popover"
-							renderToggle={ ( { isOpen, onToggle } ) => {
-								const openOnArrowDown = ( event ) => {
-									if ( ! isOpen && event.keyCode === DOWN ) {
-										event.preventDefault();
-										event.stopPropagation();
-										onToggle();
+					{ openPopover && (
+						<Popover
+							position="bottom center"
+						>
+							<MediaUpload
+								onSelect={ ( media ) => {
+									setAttributes( { backgroundImg: media.url, backgroundType: media.type, openPopover: !openPopover } );
+									// Set padding when background image is added.
+									if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
+										if ( ! attributes.paddingSize || attributes.paddingSize == 'no' ) {
+											setAttributes( { paddingSize: 'medium' } );
+										}
 									}
-								};
-								return (
+								} }
+								allowedTypes={ ALLOWED_BG_MEDIA_TYPES }
+								value={ backgroundImg }
+								render={ ( { open } ) => (
 									<IconButton
-										className="components-dropdown-menu__toggle"
-										icon={ icons.backgroundImage }
-										onClick={ onToggle }
-										onKeyDown={ openOnArrowDown }
-										aria-haspopup="true"
-										aria-expanded={ isOpen }
-										label={ 'Edit background' }
-										tooltip={ 'Edit background' }
-									>
-										<span className="components-dropdown-menu__indicator" />
+										className="components-dropdown-menu__menu-item"
+										icon={ icons.edit }
+										role="menuitem"
+										onClick={ open } >
+											{ __( 'Edit Background' ) }
 									</IconButton>
-								);
-							} }
-							renderContent={ ( { onClose } ) => {
-								return (
-									<NavigableMenu
-										className="components-dropdown-menu__menu"
-										role="menu"
-										aria-label={ __( 'Edit background' ) }
-									>
-										<MediaUpload
-											onSelect={ ( media ) => {
-												setAttributes( { backgroundImg: media.url, backgroundType: media.type } );
-												// Set padding when background image is added.
-												if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
-													if ( ! attributes.paddingSize || attributes.paddingSize == 'no' ) {
-														setAttributes( { paddingSize: 'medium' } );
-													}
-												}
-											} }
-											allowedTypes={ ALLOWED_BG_MEDIA_TYPES }
-											value={ backgroundImg }
-											render={ ( { open } ) => (
-												<IconButton
-													className="components-dropdown-menu__menu-item"
-													icon={ icons.edit }
-													role="menuitem"
-													onClick={ open } >
-														{ __( 'Edit Background' ) }
-												</IconButton>
 
-											) }
-										/>
-										<IconButton
-											className="components-dropdown-menu__menu-item"
-											icon={ icons.trash }
-											role="menuitem"
-											onClick={ () => {
-												setAttributes( {
-													backgroundImg: '',
-													backgroundOverlay: 0,
-													backgroundRepeat: 'no-repeat',
-													backgroundPosition: '',
-													backgroundSize: 'cover',
-													hasParallax: false,
-												} );
-												// Remove padding when background image is removed.
-												if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
-													if( attributes.paddingSize ){
-														setAttributes( { paddingSize: 'no' } );
-													}
-												}
-											} } >
-												{ __( 'Remove Background' ) }
-										</IconButton>
-									</NavigableMenu>
-								);
-							} }
-						/>
+								) }
+							/>
+							<IconButton
+								className="components-dropdown-menu__menu-item"
+								icon={ icons.trash }
+								role="menuitem"
+								onClick={ () => {
+									setAttributes( {
+										backgroundImg: '',
+										backgroundOverlay: 0,
+										backgroundRepeat: 'no-repeat',
+										backgroundPosition: '',
+										backgroundSize: 'cover',
+										hasParallax: false,
+										openPopover: !openPopover,
+									} );
+									// Remove padding when background image is removed.
+									if ( BLOCKS_WITH_AUTOPADDING.includes( props.name ) ){
+										if( attributes.paddingSize ){
+											setAttributes( { paddingSize: 'no' } );
+										}
+									}
+								} } >
+									{ __( 'Remove Background' ) }
+							</IconButton>
+						</Popover>
+					) }
+					{ backgroundImg ?
+						<IconButton
+							className="components-dropdown-menu__toggle"
+							icon={ icons.backgroundImage }
+							aria-haspopup="true"
+							label={ 'Edit background' }
+							tooltip={ 'Edit background' }
+							onClick={ () => setAttributes( { openPopover: !openPopover } ) }
+						>
+						</IconButton>
 					:
 						<MediaUpload
 							onSelect={ ( media ) => {
