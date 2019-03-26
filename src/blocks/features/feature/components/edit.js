09899b3736ea700dee3eb358e23bac93233700f4
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-03-26 18:41:03 +0800

    background preloader for Features and feature blocks  #421

--- src/blocks/features/feature/components/edit.js
+++ src/blocks/features/feature/components/edit.js
@@ -19,6 +19,7 @@ const { compose } = wp.compose;
 const { Component, Fragment } = wp.element;
 const { InnerBlocks } = wp.editor;
 const { Spinner } = wp.components;
+const { isBlobURL } = wp.blob;
 
 /**
  * Constants
@@ -117,6 +118,7 @@ class Edit extends Component {
 				) }
 				<div className={ classes }>
 					<div className={ innerClasses } style={ innerStyles }>
+						{ isBlobURL( backgroundImg ) && <Spinner /> }
 						<InnerBlocks
 							allowedBlocks={ ALLOWED_BLOCKS }
 							template={ TEMPLATE }
