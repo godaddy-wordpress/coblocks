2eac2e76cd0a8e890bca5420ef3f7c2550bbec15
Rich Tabor <hi@richtabor.com>
2019-03-27 11:04:36 -0400

    Remove scss and use our utility classes

--- src/components/background/video.js
+++ src/components/background/video.js
@@ -12,7 +12,7 @@ function BackgroundVideo( attributes ) {
 
 	return [
 		backgroundType == 'video' ?
-			<div className="coblocks-video-bg position-absolute overflow-hidden w-full h-full pin-t pin-b">
+			<div className="coblocks-video-bg position-absolute overflow-hidden w-full h-full pin-t pin-r pin-b pin-l">
 				<video className="w-full h-full bg-center-center object-cover object-position" playsinline="" autoplay="" muted={ videoMuted } loop={ videoLoop } src={ backgroundImg }></video>
 			</div>
 		: null
