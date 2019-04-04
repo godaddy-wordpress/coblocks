9bf8dd2e580bb546b23bbee7980c3f1e9168dbfa
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-04-04 20:59:16 +0800

    Add deprecated save function for Shape Divider minHeight changes

--- src/blocks/shape-divider/index.js
+++ src/blocks/shape-divider/index.js
@@ -390,6 +390,50 @@ const settings = {
 			</div>
 		);
 	},
+	deprecated: [
+		{
+			save( { attributes, className } ) {
+
+				const {
+					backgroundColor,
+					backgroundHeight,
+					coblocks,
+					color,
+					customBackgroundColor,
+					customColor,
+					horizontalFlip,
+					shapeHeight,
+					verticalFlip,
+				} = attributes;
+
+				const shapeClass = getColorClassName( 'color', color );
+				const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+				const classes = classnames(
+					className, {
+					[ `coblocks-shape-divider-${ coblocks.id }` ] : coblocks && ( typeof coblocks.id != 'undefined' ),
+					'is-vertically-flipped' : verticalFlip,
+					'is-horizontally-flipped' : horizontalFlip,
+					[ shapeClass ]: shapeClass,
+					[ backgroundClass ]: backgroundClass,
+				} );
+
+				const styles = {
+					backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+					color: shapeClass ? undefined : customColor,
+				};
+
+				return (
+					<div className={ classes } style={ styles } aria-hidden="true">
+						<div className="wp-block-coblocks-shape-divider__svg-wrapper" style={ { height: shapeHeight } }>
+							{ getDividerFromStyle( attributes.className ) }
+						</div>
+						<div className="wp-block-coblocks-shape-divider__alt-wrapper" style={ { height: backgroundHeight } }></div>
+					</div>
+				);
+			},
+		}
+	],
 };
 
 export { name, title, icon, settings };
