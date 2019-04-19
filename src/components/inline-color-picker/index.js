849aef483dd60b85b8327794e8d956d19ebf0be5
Evan Herman <evan.m.herman@gmail.com>
2019-04-18 23:04:04 -0700

    Remove tooltip text (alignment off) Tweak color-picker z-index

--- src/components/inline-color-picker/index.js
+++ src/components/inline-color-picker/index.js
@@ -28,17 +28,14 @@ export default function InlineColorPicker( { disableCustomColors = false, value,
 						className="components-color-palette__custom-color"
 						contentClassName="components-color-palette__picker"
 						renderToggle={ ( { isOpen, onToggle } ) => (
-							<Tooltip
-								text={ customColorPickerLabel }>
-								<Button
-									aria-expanded={ isOpen }
-									onClick={ onToggle }
-									aria-label={ customColorPickerLabel }
-									isLink
-								>
-									<span class="components-color-palette__custom-color-gradient"></span>
-								</Button>
-							</Tooltip>
+							<Button
+								aria-expanded={ isOpen }
+								onClick={ onToggle }
+								aria-label={ customColorPickerLabel }
+								isLink
+							>
+								<span class="components-color-palette__custom-color-gradient"></span>
+							</Button>
 						) }
 						renderContent={ () => (
 							<ColorPicker
