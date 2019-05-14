a975de139fa72fbcca415204bddf3d73dba8eccf
Rich Tabor <hi@richtabor.com>
2019-05-14 15:08:36 -0400

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,173 @@
+/* global module, require */
+
+module.exports = function( grunt ) {
+
+	'use strict';
+
+	var pkg = grunt.file.readJSON( 'package.json' );
+
+	grunt.initConfig( {
+
+		pkg: pkg,
+
+		devUpdate: {
+			packages: {
+				options: {
+					packageJson: null,
+					packages: {
+						devDependencies: true,
+						dependencies: false
+					},
+					reportOnlyPkgs: [],
+					reportUpdated: false,
+					semver: true,
+					updateType: 'force'
+				}
+			}
+		},
+
+		clean: {
+			build: [ 'build/' ]
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						src: [
+							'class-' + pkg.name + '.php',
+							'LICENSE',
+							'readme.txt',
+							'src/**',
+							'dist/**',
+							'includes/**',
+							'languages/*.{mo,pot}',
+							'!**/*.{ai,eps,psd}'
+						],
+						dest: 'build/'
+					}
+				]
+			}
+		},
+
+		cssmin: {
+			options: {
+				processImport: false,
+				roundingPrecision: -1,
+				shorthandCompacting: false
+			},
+			getting_started: {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/styles/',
+						src: 'coblocks-getting-started.scss',
+						dest: 'dist/css/',
+						ext: '.min.css'
+					}
+				]
+			},
+		},
+
+		uglify: {
+			options: {
+				ASCIIOnly: true
+			},
+			all: {
+				expand: true,
+				cwd: 'src/js/',
+				src: '**/*.js',
+				dest: 'dist/js/',
+				ext: '.min.js'
+			}
+		},
+
+		imagemin: {
+			options: {
+				optimizationLevel: 3
+			},
+			assets: {
+				expand: true,
+				cwd: 'dist/images/',
+				src: [ '**/*.{gif,jpeg,jpg,png,svg}' ],
+				dest: 'build/dist/images/'
+			},
+			wp_org_assets: {
+				expand: true,
+				cwd: 'wp-org-assets/',
+				src: [ '**/*.{gif,jpeg,jpg,png,svg}' ],
+				dest: 'wp-org-assets/'
+			}
+		},
+
+		replace: {
+			php: {
+				src: [
+					'class-' + pkg.name + '.php',
+					'includes/**/*.php'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /Version:(\s*?)[a-zA-Z0-9\.\-\+]+$/m,
+						to: 'Version:$1' + pkg.version
+					},
+					{
+						from: /@since(.*?)NEXT/mg,
+						to: '@since$1' + pkg.version
+					},
+					{
+						from: /VERSION(\s*?)=(\s*?['"])[a-zA-Z0-9\.\-\+]+/mg,
+						to: 'VERSION$1=$2' + pkg.version
+					}
+				]
+			},
+			readme: {
+				src: 'readme.*',
+				overwrite: true,
+				replacements: [
+					{
+						from: /^(\*\*|)Stable tag:(\*\*|)(\s*?)[a-zA-Z0-9.-]+(\s*?)$/mi,
+						to: '$1Stable tag:$2$3<%= pkg.version %>$4'
+					}
+				]
+			}
+		},
+
+		shell: {
+			cgb_start: [
+				'npm run start'
+			].join( ' && ' ),
+			cgb_build: [
+				'npm run build'
+			].join( ' && ' ),
+			translations: [
+				'npm run babel:build && rm languages/coblocks-js.pot'
+			].join( ' && ' ),
+		},
+
+		wp_deploy: {
+			plugin: {
+				options: {
+					assets_dir: 'wp-org-assets/',
+					build_dir: 'build/',
+					plugin_main_file: pkg.name + '.php',
+					plugin_slug: pkg.name,
+					svn_user: grunt.file.exists( 'svn-username' ) ? grunt.file.read( 'svn-username' ).trim() : false
+				}
+			}
+		},
+
+	} );
+
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+	grunt.registerTask( 'default',    [ 'cssmin', 'uglify', 'imagemin', 'shell:cgb_start' ] );
+	grunt.registerTask( 'check',      [ 'devUpdate' ] );
+	grunt.registerTask( 'build',      [ 'shell:cgb_build', 'cssmin', 'uglify', 'imagemin', 'update-pot', 'replace', 'clean:build', 'copy:build' ] );
+	grunt.registerTask( 'deploy',     [ 'build', 'wp_deploy', 'clean:build' ] );
+	grunt.registerTask( 'update-pot', [ 'shell:translations' ] );
+	grunt.registerTask( 'version',    [ 'replace' ] );
+
+};
