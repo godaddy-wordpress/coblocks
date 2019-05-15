10674e80fadef3d67383c11050dc26160842fe31
Evan Herman <evan.m.herman@gmail.com>
2019-05-15 18:24:29 -0400

    Copy over assets and add the files in the deploy script

--- .dev/bin/deploy-plugin.sh
+++ .dev/bin/deploy-plugin.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
+# Checkout Coblocks from WordPress.org
 svn co "http://svn.wp-plugins.org/coblocks" $HOME/coblocks
 
-# Clean trunk and copy over new files
+# Clean /trunk/ and copy over plugin files
 rm -rf $HOME/coblocks/trunk/*
 cp -a build/coblocks/* $HOME/coblocks/trunk/
 
-# Deploy the plugin
+# Copy the WordPress.org assets over
+rm -rf $HOME/coblocks/assets/*
+cp -a wp-org-assets/* $HOME/coblocks/assets/
+
+# Deploy Coblocks to WordPress.org
 cd $HOME/coblocks
+svn add * --force
 svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD -m "Deploy new version of Coblocks"
