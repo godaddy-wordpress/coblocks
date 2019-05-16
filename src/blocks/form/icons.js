45100b455c95c35a5fa331531c166bc2ab0d668b
Rich Tabor <hi@richtabor.com>
2019-05-16 11:24:41 -0400

    fill-rule to FillRule

--- src/blocks/form/icons.js
+++ src/blocks/form/icons.js
@@ -25,7 +25,7 @@ icons.email =
 
 icons.textarea =
 <SVG className="components-coblocks-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-	<G fill="none" fill-rule="evenodd"><path d="m0 0h24v24h-24z"/><path d="m4 11h16v2h-16zm0-4h16v2h-16zm0 8h10v2h-10z" fill="currentColor" fill-rule="nonzero"/></G>
+	<G fill="none" fillRule="evenodd"><path d="m0 0h24v24h-24z"/><path d="m4 11h16v2h-16zm0-4h16v2h-16zm0 8h10v2h-10z" fill="currentColor" fillRule="nonzero"/></G>
 </SVG>
 
 export default icons;
