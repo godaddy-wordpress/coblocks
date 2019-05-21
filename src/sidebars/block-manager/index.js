1688dc8f665d2b5caa25af4a0157e92b3beb68ca
Rich Tabor <hi@richtabor.com>
2019-05-21 18:28:44 -0400

    Add eslint formatting

--- src/sidebars/block-manager/index.js
+++ src/sidebars/block-manager/index.js
@@ -2,7 +2,6 @@
  * Internal dependencies
  */
 import './styles/editor.scss';
-import icons from './../../utils/icons';
 import ModalSettings from './components/modal';
 
 /**
@@ -10,13 +9,12 @@ import ModalSettings from './components/modal';
  */
 const { registerPlugin } = wp.plugins;
 const { dispatch } = wp.data;
-
 const hideBlockTypes = dispatch( 'core/edit-post' ).hideBlockTypes;
 
 /**
  * Register Plugin
  */
-if( typeof hideBlockTypes === 'undefined' ){
+if ( typeof hideBlockTypes === 'undefined' ) {
 	registerPlugin( 'coblocks-block-manager', {
 		icon: false,
 		render: ModalSettings,
