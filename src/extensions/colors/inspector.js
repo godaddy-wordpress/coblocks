257ca2233c4a4a895fe7460fa90e8090dc4986ac
Rich Tabor <hi@richtabor.com>
2019-05-29 15:06:04 -0400

    Remove custom Color Settings for the heading block

--- src/extensions/colors/inspector.js
+++ src/extensions/colors/inspector.js
@@ -19,7 +19,7 @@ const { compose, createHigherOrderComponent } = wp.compose;
  */
 const Inspector = props => {
  	const { attributes, setAttributes } = props;
- 	const allowedBlocks = [ 'core/heading', 'core/cover', 'core/button', 'core/list', 'core/quote' ];
+ 	const allowedBlocks = [ 'core/cover', 'core/button', 'core/list', 'core/quote' ];
 
 	// Display on the allowedBlocks only.
 	if ( ! allowedBlocks.includes( props.name ) ){
@@ -29,7 +29,7 @@ const Inspector = props => {
 	}
 
 	// Show line height on appropriate blocks.
-	if ( ! [ 'core/heading', 'core/paragraph', 'core/cover', 'core/button' ].includes( props.name ) ) {
+	if ( ! [ 'core/paragraph', 'core/cover', 'core/button' ].includes( props.name ) ) {
 		props.attributes.textPanelLineHeight = true;
 	}
 
@@ -50,7 +50,7 @@ const Inspector = props => {
  * @return {Object} Filtered block settings.
  */
 function addAttributes( settings ) {
-	const allowedBlocks = [ 'core/heading', 'core/list', 'core/quote' ];
+	const allowedBlocks = [ 'core/list', 'core/quote' ];
 	// Use Lodash's assign to gracefully handle if attributes are undefined
 	if( allowedBlocks.includes( settings.name ) ){
 		settings.attributes = Object.assign( settings.attributes, ColorSettingsAttributes );
@@ -67,7 +67,7 @@ function addAttributes( settings ) {
  */
 const withInspectorControl = createHigherOrderComponent( (BlockEdit) => {
 	return ( props ) => {
-		const allowedBlocks = [ 'core/heading', 'core/list', 'core/quote' ];
+		const allowedBlocks = [ 'core/list', 'core/quote' ];
 		return (
 			<Fragment>
 				<BlockEdit {...props} />
