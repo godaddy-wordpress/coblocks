33dd6c8a9b5f0295a2d01a2b82693345e713dfaa
JR Tashjian <jrtashjian@gmail.com>
2019-06-06 16:39:57 -0400

    Use RichText component for menu-item data

--- src/blocks/menu-item/edit.js
+++ src/blocks/menu-item/edit.js
@@ -4,34 +4,62 @@
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { PanelBody, ToggleControl } = wp.components;
-const { InspectorControls } = wp.editor;
+const { InspectorControls, RichText } = wp.editor;
 
 class MenuItem extends Component {
+	renderInspectorControls() {
+		const { attributes, setAttributes } = this.props;
+		return (
+			<InspectorControls>
+				<PanelBody title={ __( 'Menu Settings' ) } initialOpen={ true }>
+					<ToggleControl
+						label={ __( 'Images' ) }
+						help={
+							attributes.showImage ?
+								__( 'Showing image for this item' ) :
+								__( 'Toggle to show image for this item.' )
+						}
+						checked={ attributes.showImage }
+						onChange={ () => setAttributes( { showImage: ! attributes.showImage } ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		);
+	}
+
 	render() {
 		const { className, attributes, setAttributes } = this.props;
 
+		const richTextAttributes = {
+			keepPlaceholderOnFocus: true,
+			formattingControls: [ 'bold', 'italic' ],
+		};
+
 		return (
 			<Fragment>
-				<InspectorControls>
-					<PanelBody title={ __( 'Menu Settings' ) } initialOpen={ true }>
-						<ToggleControl
-							label={ __( 'Images' ) }
-							help={
-								attributes.showImage ?
-									__( 'Showing image for this item' ) :
-									__( 'Toggle to show image for this item.' )
-							}
-							checked={ attributes.showImage }
-							onChange={ () =>
-								setAttributes( { showImage: ! attributes.showImage } )
-							}
-						/>
-					</PanelBody>
-				</InspectorControls>
+				{ this.renderInspectorControls() }
 				<div className={ className }>
-					<pre style={ { fontSize: 12 } }>
-						{ JSON.stringify( this.props, null, 2 ) }
-					</pre>
+					<RichText
+						value={ attributes.itemName }
+						tagName="h4"
+						placeholder={ __( 'Add menu item...' ) }
+						onChange={ itemName => setAttributes( { itemName } ) }
+						{ ...richTextAttributes }
+					/>
+					<RichText
+						value={ attributes.itemDescription }
+						tagName="p"
+						placeholder={ __( 'Add description...' ) }
+						onChange={ itemDescription => setAttributes( { itemDescription } ) }
+						{ ...richTextAttributes }
+					/>
+					<RichText
+						value={ attributes.itemCost }
+						tagName="p"
+						placeholder={ __( '$0.00' ) }
+						onChange={ itemCost => setAttributes( { itemCost } ) }
+						{ ...richTextAttributes }
+					/>
 				</div>
 			</Fragment>
 		);
