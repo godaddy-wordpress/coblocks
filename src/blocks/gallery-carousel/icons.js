d7aa199fdcdb2b4e2403e9341cd3c4988c7caf34
Rich Tabor <hi@richtabor.com>
2019-06-07 16:33:44 -0400

    Update gallery block icons

--- src/blocks/gallery-carousel/icons.js
+++ src/blocks/gallery-carousel/icons.js
@@ -9,8 +9,6 @@ const { SVG, Path, G } = wp.components;
 const icons = {};
 
 icons.carousel =
-<SVG className="components-coblocks-svg" role="img" focusable="false" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-	<G transform="translate(3 3)"><Path d="m11 0h-4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h4c1.1 0 2-.9 2-2v-14c0-1.1-.9-2-2-2zm0 16h-4v-14h4z"/><Path d="m18 0h-1c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h1s0-.9 0-2v-14c0-1.1 0-2 0-2zm0 16h-1v-14h1z"/><Path d="m3 0h-1c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h1s0-.9 0-2v-14c0-1.1 0-2 0-2zm0 16h-1v-14h1z" transform="matrix(-1 0 0 1 3 0)"/></G>
-</SVG>;
+<SVG className="components-coblocks-svg" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><G fill="currentColor" fillRule="evenodd"><G fillRule="nonzero"><Path d="m18 4h-12c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-12c0-1.1-.9-2-2-2zm0 14h-12v-12h12z"/><Path d="m21 6v12c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2zm0 10v-8z"/><Path d="m1 6v12c1.1 0 2-.9 2-2v-8c0-1.1-.9-2-2-2zm0 10v-8z" transform="matrix(-1 0 0 -1 4 24)"/></G><Path d="m13.7857143 11-2.5 4.51-1.7857143-3.01-2.5 4.5h10z"/></G></SVG>;
 
 export default icons;
--- src/blocks/gallery-stacked/icons.js
+++ src/blocks/gallery-stacked/icons.js
@@ -9,8 +9,6 @@ const { SVG, Path, G } = wp.components;
 const icons = {};
 
 icons.stacked =
-<SVG className="components-coblocks-svg" role="img" focusable="false" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-	<G transform="translate(3 3)"><Path d="m16 0h-14c-1.1 0-2 .9-2 2v6c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-6c0-1.1-.9-2-2-2zm0 8h-14v-6h14z"/><Path d="m16 12h-14c-1.1 0-2 .9-2 2v2c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-2c0-1.1-.9-2-2-2zm0 4h-14v-2h14z"/></G>
-</SVG>
+<SVG className="components-coblocks-svg" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><G fill="currentColor" fillFule="nonzero"><Path d="m19 3c1.1 0 2 .9 2 2v9c0 1.1-.9 2-2 2h-14c-1.1 0-2-.9-2-2v-9c0-1.1.9-2 2-2zm0 11v-9h-14v9zm-4.3730469-6 3.3730469 5h-12l3.07006836-3.81713867 2.21564594 2.57547197z"/><Path d="m19 18h-14c-1.1 0-2 .9-2 2v1h18v-1c0-1.1-.9-2-2-2zm0 3h-14v-1h14z"/></G></SVG>
 
 export default icons;
