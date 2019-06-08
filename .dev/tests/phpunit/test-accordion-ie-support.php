4623250bb540bd2818bc1d18aa6f2d7862121e42
Evan Herman <evan.m.herman@gmail.com>
2019-06-08 11:38:11 -0400

    Update accordion ie support tests

--- .dev/tests/phpunit/test-accordion-ie-support.php
+++ .dev/tests/phpunit/test-accordion-ie-support.php
@@ -12,6 +12,8 @@ class Accordion_IE_Support_Tests extends WP_UnitTestCase {
 
 		parent::setUp();
 
+		set_current_screen( 'edit-post' );
+
 	}
 
 	public function tearDown() {
@@ -92,15 +94,28 @@ class Accordion_IE_Support_Tests extends WP_UnitTestCase {
 
 		$new_reflection = new CoBlocks_Accordion_IE_Support();
 
+		$post_id = wp_insert_post(
+			[
+				'post_author'  => 1,
+				'post_content' => '<!-- wp:coblocks/accordion --><div class="wp-block-coblocks-accordion"><!-- wp:coblocks/accordion-item {"title":"Accordion Title 1"} --><div class="wp-block-coblocks-accordion-item"><details><summary class="wp-block-coblocks-accordion-item__title">Accordion Title 1</summary><div class="wp-block-coblocks-accordion-item__content"><!-- wp:paragraph {"placeholder":"Add content..."} --><p>Accordion Content 1</p><!-- /wp:paragraph --></div></details></div><!-- /wp:coblocks/accordion-item --></div><!-- /wp:coblocks/accordion -->',
+				'post_title'   => 'CoBlocks Accordion',
+				'post_status'  => 'publish',
+			]
+		);
+
+		update_post_meta( $post_id, '_coblocks_accordion_ie_support', "'true'" );
+
 		global $post;
 
-		$post = new stdClass();
-
-		$post->ID = 1;
+		$post = get_post( $post_id );
 
 		$new_reflection->load_assets();
 
-		$this->assertTrue( true );
+		do_action( 'wp_enqueue_scripts' );
+
+		$wp_scripts = wp_scripts();
+
+		$this->assertContains( 'coblocks-accordion-polyfill', $wp_scripts->queue );
 
 	}
 }
