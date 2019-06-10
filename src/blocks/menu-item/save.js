7c8c51d7809f3a5c7efa10909aa519074d19ccb5
JR Tashjian <jrtashjian@gmail.com>
2019-06-10 18:37:13 -0400

    Menu-item: Only output html when not empty.

--- src/blocks/menu-item/save.js
+++ src/blocks/menu-item/save.js
@@ -4,16 +4,18 @@
 const { RichText } = wp.editor;
 
 export default function save( { attributes } ) {
-	return (
-		<div className={ attributes.className }>
-			{ attributes.showImage && attributes.itemImage && (
-				<figure>
-					<img src={ attributes.itemImage } alt={ '' } />
-				</figure>
-			) }
-			<RichText.Content tagName="h4" value={ attributes.itemName } />
-			<RichText.Content tagName="p" value={ attributes.itemDescription } />
-			<RichText.Content tagName="p" value={ attributes.itemCost } />
-		</div>
-	);
+	return attributes.itemName ||
+		attributes.itemDescription ||
+		attributes.itemCost ? (
+			<div className={ attributes.className }>
+				{ attributes.showImage && attributes.itemImage && (
+					<figure>
+						<img src={ attributes.itemImage } alt={ '' } />
+					</figure>
+				) }
+				<RichText.Content tagName="h4" value={ attributes.itemName } />
+				<RichText.Content tagName="p" value={ attributes.itemDescription } />
+				<RichText.Content tagName="p" value={ attributes.itemCost } />
+			</div>
+		) : null;
 }
