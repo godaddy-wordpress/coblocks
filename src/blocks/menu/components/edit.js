a887e9c84692536a062c4f49e32133bfe66fa24c
JR Tashjian <jrtashjian@gmail.com>
2019-06-14 15:20:33 -0400

    Move Inspector controls into a separate inspector.js file.

--- src/blocks/menu/components/edit.js
+++ src/blocks/menu/components/edit.js
@@ -2,6 +2,7 @@
  * Internal dependencies.
  */
 import MenuAppender from './menu-appender';
+import InspectorControls from './inspector';
 import icons from './icons';
 
 /**
@@ -15,11 +16,9 @@ import classnames from 'classnames';
  */
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
-const { PanelBody, ToggleControl } = wp.components;
 const { compose } = wp.compose;
 const { withSelect, dispatch, select } = wp.data;
-const { InspectorControls, InnerBlocks } = wp.editor;
-const { ENTER, SPACE } = wp.keycodes;
+const { InnerBlocks } = wp.editor;
 const TokenList = wp.tokenList;
 
 const ALLOWED_BLOCKS = [ 'coblocks/menu-item' ];
@@ -220,63 +219,14 @@ class Menu extends Component {
 
 		return (
 			<Fragment>
-				<InspectorControls>
-					<PanelBody title={ __( 'Styles' ) } initialOpen={ true }>
-						<div className="editor-block-styles block-editor-block-styles coblocks-editor-block-styles">
-							{ layoutOptions.map( style => (
-								<div
-									key={ `menu-style-${ style.name }` }
-									className={ classnames(
-										'editor-block-styles__item block-editor-block-styles__item',
-										{
-											'is-active': activeStyle === style,
-										}
-									) }
-									onClick={ () => this.updateStyle( style ) }
-									onKeyDown={ event => {
-										if ( ENTER === event.keyCode || SPACE === event.keyCode ) {
-											event.preventDefault();
-											this.updateStyle( style );
-										}
-									} }
-									role="button"
-									tabIndex="0"
-									aria-label={ style.label || style.name }
-								>
-									<div className="editor-block-styles__item-preview block-editor-block-styles__item-preview">
-										{ attributes.showImages ? style.iconWithImages : style.icon }
-									</div>
-									<div className="editor-block-styles__item-label block-editor-block-styles__item-label">
-										{ style.label || style.name }
-									</div>
-								</div>
-							) ) }
-						</div>
-					</PanelBody>
-
-					<PanelBody title={ __( 'Menu Settings' ) } initialOpen={ true }>
-						<ToggleControl
-							label={ __( 'Images' ) }
-							help={
-								attributes.showImages ?
-									__( 'Showing images for each item' ) :
-									__( 'Toggle to show images for each item.' )
-							}
-							checked={ attributes.showImages }
-							onChange={ this.toggleImages }
-						/>
-						<ToggleControl
-							label={ __( 'Prices' ) }
-							help={
-								attributes.showPrices ?
-									__( 'Showing the price of each item' ) :
-									__( 'Toggle to show the price of each item.' )
-							}
-							checked={ attributes.showPrices }
-							onChange={ this.togglePrices }
-						/>
-					</PanelBody>
-				</InspectorControls>
+				<InspectorControls
+					attributes={ attributes }
+					activeStyle={ activeStyle }
+					layoutOptions={ layoutOptions }
+					onToggleImages={ this.toggleImages }
+					onTogglePrices={ this.togglePrices }
+					onUpdateStyle={ this.updateStyle }
+				/>
 				<div
 					className={ classnames( className, {
 						'child-selected': isSelected || clientId === selectedParentClientId,
