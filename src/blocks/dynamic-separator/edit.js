9f5cc3f4c4663ebbd63b862626f6b4a77d73c634
Rich Tabor <hi@richtabor.com>
2019-06-14 14:55:34 -0400

    ESLint + breaking out block into separate files

--- src/blocks/dynamic-separator/edit.js
+++ src/blocks/dynamic-separator/edit.js
@@ -12,29 +12,17 @@ import applyWithColors from './colors';
 /**
  * WordPress dependencies
  */
-const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { compose } = wp.compose;
 const { ResizableBox } = wp.components;
 
-/**
- * Block edit function
- */
-class Edit extends Component {
-
-	constructor() {
-		super( ...arguments );
-	}
-
+class DynamicSeparatorEdit extends Component {
 	render() {
-
 		const {
 			attributes,
 			className,
 			isSelected,
 			setAttributes,
-			toggleSelection,
-			setColor,
 			color,
 		} = this.props;
 
@@ -42,7 +30,7 @@ class Edit extends Component {
 			height,
 		} = attributes;
 
-		return [
+		return (
 			<Fragment>
 				{ isSelected && (
 					<Inspector
@@ -83,10 +71,10 @@ class Edit extends Component {
 				>
 				</ResizableBox>
 			</Fragment>
-		];
+		);
 	}
-};
+}
 
 export default compose( [
 	applyWithColors,
-] )( Edit );
+] )( DynamicSeparatorEdit );
--- src/utils/icons.js
+++ src/utils/icons.js
@@ -107,11 +107,6 @@ icons.at =
 	</g>
 </svg>;
 
-icons.hr =
-<svg className="dashicon components-coblocks-svg" aria-hidden role="img" focusable="false" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-	<path d="m5.11338976 2.8941786 3.2418891 3.24188911 1.44197591-1.44197591-4.68386501-4.6940918-4.6940918 4.6940918 1.45220269 1.44197591zm0 12.6198459-3.24188911-3.2418891-1.44197591 1.4419759 4.68386502 4.6940918 4.69409179-4.6940918-1.45220269-1.4419759z" transform="translate(6.88661 2.795898)"/>
-</svg>;
-
 icons.gif =
 <svg aria-hidden className="dashicon components-coblocks-svg" role="img" focusable="false" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 	<path d="m16 11v7h-14v-14h5.02c.05-.71.22-1.38.48-2h-5.5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-5zm-1.5 5h-11l2.75-3.53 1.96 2.36 2.75-3.54zm2.8-9.11c.44-.7.7-1.51.7-2.39 0-2.49-2.01-4.5-4.5-4.5s-4.5 2.01-4.5 4.5 2.01 4.5 4.49 4.5c.88 0 1.7-.26 2.39-.7l3.12 3.12 1.42-1.42zm-3.8.11c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" transform="translate(3 2)"/>
