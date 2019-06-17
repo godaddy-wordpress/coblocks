be3796797211345876f3112538c7d1f582f83ead
JR Tashjian <jrtashjian@gmail.com>
2019-06-17 13:18:09 -0400

    Adding additional utility classes to menu-item

--- src/blocks/menu/menu-item/components/save.js
+++ src/blocks/menu/menu-item/components/save.js
@@ -12,13 +12,21 @@ export default function save( { attributes } ) {
 	return hasEmptyAttributes( attributes ) ? null : (
 		<div className={ attributes.className }>
 			{ attributes.showImage && attributes.imageUrl && (
-				<figure>
+				<figure className="wp-block-coblocks-menu-item__image">
 					<img src={ attributes.imageUrl } alt={ '' } />
 				</figure>
 			) }
 			<div className="wp-block-coblocks-menu-item__content">
-				<RichText.Content tagName="h4" value={ attributes.title } />
-				<RichText.Content tagName="p" value={ attributes.description } />
+				<RichText.Content
+					tagName="h4"
+					className="wp-block-coblocks-menu-item__title"
+					value={ attributes.title }
+				/>
+				<RichText.Content
+					tagName="p"
+					className="wp-block-coblocks-menu-item__description"
+					value={ attributes.description }
+				/>
 				{ attributes.showPrice && (
 					<RichText.Content
 						tagName="p"
