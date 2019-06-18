5675191a25eca696913d9d123c4c0f862534bb61
JR Tashjian <jrtashjian@gmail.com>
2019-06-18 16:12:02 -0400

    Start integrating Menu and MenuItem Schema microdata

--- src/blocks/menu/components/save.js
+++ src/blocks/menu/components/save.js
@@ -5,7 +5,7 @@ const { InnerBlocks } = wp.blockEditor;
 
 export default function save( { className } ) {
 	return (
-		<div className={ className }>
+		<div className={ className } itemScope itemType="http://schema.org/Menu">
 			<InnerBlocks.Content />
 		</div>
 	);
--- src/blocks/menu/menu-item/components/save.js
+++ src/blocks/menu/menu-item/components/save.js
@@ -8,9 +8,22 @@ import { hasEmptyAttributes } from '../../../../utils/block-helpers';
  */
 const { RichText } = wp.blockEditor;
 
+const isEmpty = attributes => {
+	const attributesToCheck = [ 'imageUrl', 'title', 'description', 'itemPrice' ];
+	const newAttributes = Object.entries( attributes ).filter( ( [ key ] ) =>
+		attributesToCheck.includes( key )
+	);
+
+	return hasEmptyAttributes( Object.fromEntries( newAttributes ) );
+};
+
 export default function save( { attributes } ) {
-	return hasEmptyAttributes( attributes ) ? null : (
-		<div className={ attributes.className }>
+	return isEmpty( attributes ) ? null : (
+		<div
+			className={ attributes.className }
+			itemScope
+			itemType="http://schema.org/MenuItem"
+		>
 			{ attributes.showImage && attributes.imageUrl && (
 				<figure className="wp-block-coblocks-menu-item__image">
 					<img src={ attributes.imageUrl } alt={ '' } />
@@ -21,18 +34,27 @@ export default function save( { attributes } ) {
 					tagName="h4"
 					className="wp-block-coblocks-menu-item__heading"
 					value={ attributes.title }
+					itemprop="name"
 				/>
 				<RichText.Content
 					tagName="p"
 					className="wp-block-coblocks-menu-item__description"
 					value={ attributes.description }
+					itemprop="description"
 				/>
 				{ attributes.showPrice && (
-					<RichText.Content
-						tagName="p"
+					<p
 						className="wp-block-coblocks-menu-item__price"
-						value={ attributes.itemPrice }
-					/>
+						itemProp="offers"
+						itemScope
+						itemType="http://schema.org/Offer"
+					>
+						<RichText.Content
+							tagName="span"
+							value={ attributes.itemPrice }
+							itemprop="price"
+						/>
+					</p>
 				) }
 			</div>
 		</div>
