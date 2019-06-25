7b13ba7b54a382858b843e70c135d174f5583a76
aledesma <aledesma@godaddy.com>
2019-06-25 05:44:31 -0700

    Removed extra spacing from original commit.

--- src/blocks/gist/components/gist.js
+++ src/blocks/gist/components/gist.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
@@ -18,15 +18,15 @@ const { Placeholder, Spinner } = wp.components;
 // -- MAIN --
 // Extending PureComponent allow us to prevent re-rendering when the props DONT change.
 export default class Gist extends Component {
-	constructor (props) {
-		super(props)
-		this.url = props.url
-		this.file = props.file
-		this.stylesheetAdded = false // Ensures we only add the Gist's stylesheet one time.
+	constructor(props) {
+		super(props);
+		this.url = props.url;
+		this.file = props.file;
+		this.stylesheetAdded = false; // Ensures we only add the Gist's stylesheet one time.
 		this.state = {
 			loading: true, // We have not fetched the Gist yet.
 			gistContent: '' // Raw HTML of the Gist.
-		}
+		};
 		this._handleScriptError = this._handleScriptError.bind(this);
 	}
 
@@ -36,102 +36,102 @@ export default class Gist extends Component {
 		return 0;
 	}
 
-	static __nextGist () {
-		return "embed_gist_callback_" + this.__gistCallbackId++;
+	static __nextGist() {
+		return 'embed_gist_callback_' + this.__gistCallbackId++;
 	}
 
 	// The Gist JSON data includes a stylesheet file.
 	// We ensure to add that file only one time in our page.
-	static __addStylesheet (href) {
+	static __addStylesheet(href) {
 		if (!this.stylesheetAdded) {
 			let link = document.createElement('link');
-			link.type = "text/css";
-			link.rel = "stylesheet";
+			link.type = 'text/css';
+			link.rel = 'stylesheet';
 			link.href = href;
 			document.head.appendChild(link);
 			this.stylesheetAdded = true;
 		}
 	}
 
-
-	componentDidMount () {
+	componentDidMount() {
 		// Request the Gist iframe.
-		this._buildGist()
+		this._buildGist();
 	}
 
-	_getID () {
+	_getID() {
 		// Extract a string in form `username/uniqueValue` from the provided Gist url.
-		return (this.url).match(/(\.com\/)(.*?)([^#]+)/).pop();
+		return this.url.match(/(\.com\/)(.*?)([^#]+)/).pop();
 	}
 
-	_getFile () {
+	_getFile() {
 		// If `file` prop was provided return that.
 		if (this.file != null) {
-			return `&file=${this.file}`
+			return `&file=${this.file}`;
 		}
 
 		// Else construct the file parameter from the `url` prop.
-		const file = (this.url).split('#').pop()
+		const file = this.url.split('#').pop();
 
 		// If the file parameter exist in Gist url return that file.
 		if (file.match(/file*/) != null) {
-			return `&file=${file.replace('file-', '').replace('-', '.')}`
+			return `&file=${file.replace('file-', '').replace('-', '.')}`;
 		}
 
 		// Else the user wants to link the whole Gist repository.
-		return ''
+		return '';
 	}
 
-	_tranformedURL (gistCallback) {
+	_tranformedURL(gistCallback) {
 		// Construct a gist url that will allow us to redner the Gist into our page.
-		const id = this._getID()
-		const file = this._getFile()
+		const id = this._getID();
+		const file = this._getFile();
 
-		return `https://gist.github.com/${id}.json?callback=${gistCallback}${file}`
+		return `https://gist.github.com/${id}.json?callback=${gistCallback}${file}`;
 	}
 
-	_buildGist () {
+	_buildGist() {
 		const { _handleScriptError } = this;
-
-		const gistCallback = Gist.__nextGist()
-			window[gistCallback] = (gist) => {
+		const gistCallback = Gist.__nextGist();
+		window[gistCallback] = gist => {
 			Gist.__addStylesheet(gist.stylesheet);
 			this.setState({
 				loading: false,
 				gistContent: gist.div
 			});
-		}
+		};
 		let gistScript = document.createElement('script');
 		gistScript.type = 'text/javascript';
 		gistScript.src = this._tranformedURL(gistCallback);
-		gistScript.onerror = function (err) {
+		gistScript.onerror = function(err) {
 			_handleScriptError();
-		}
-			document.head.appendChild(gistScript);
+		};
+		document.head.appendChild(gistScript);
 	}
 
-	_handleScriptError () {
+	_handleScriptError() {
 		this.setState({
 			loading: false,
 			gistContent: `Error Loading Gist! \nURL does not resolve to a valid Gist.`
-		})
+		});
 	}
 
-	render () {
-		if ( this.state.loading ) {
+	render() {
+		if (this.state.loading) {
 			return [
 				<Placeholder
 					key="placeholder"
-					icon={ icons.github }
-					label={ __( 'Loading Gist' ) }>
-					<Spinner/>
+					icon={icons.github}
+					label={__('Loading Gist')}
+				>
+					<Spinner />
 				</Placeholder>
-
 			];
 		} else {
 			// Render as html.
 			// https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
-			return <div dangerouslySetInnerHTML={{__html: this.state.gistContent}} />;
+			return (
+				<div dangerouslySetInnerHTML={{ __html: this.state.gistContent }} />
+			);
 		}
 	}
 }
@@ -140,4 +140,4 @@ export default class Gist extends Component {
 Gist.propTypes = {
 	url: PropTypes.string.isRequired,
 	file: PropTypes.string
-}
+};
