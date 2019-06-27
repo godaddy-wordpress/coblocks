a6b068967ba43e7bd6cffc70594ab941c1e56663
paranoia1906 <anthonymledesma@gmail.com>
2019-06-27 12:03:33 -0700

    Had to pull in from origin for match.

--- src/blocks/shape-divider/index.js
+++ src/blocks/shape-divider/index.js
@@ -1,28 +1,26 @@
 /**
  * External dependencies
  */
+import classnames from 'classnames';
 import omit from 'lodash/omit';
 import includes from 'lodash/includes';
 
 /**
  * Internal dependencies
  */
-import './editor.scss';
-import './style.scss';
-import Edit from './edit';
-import icons from './icons';
+import './styles/editor.scss';
+import './styles/style.scss';
+import Edit from './components/edit';
+import icons from './components/icons';
 import ResponsiveBaseControlAttributes from '../../components/responsive-base-control/attributes';
-import dividers from './dividers';
-import Transforms from './transformations';
-import Deprecated from './deprecations';
-import { Save } from './save';
-import metadata from './block.json';
+import dividers from './components/dividers';
 
 /**
  * WordPress dependencies
  */
 const { __, _x } = wp.i18n;
 const { createBlock } = wp.blocks;
+const { getColorClassName } = wp.blockEditor;
 
 /**
  * Return the appropriate SVG for the block style.
@@ -62,7 +60,7 @@ export function getDividerFromStyle( className ) {
 /**
  * Block constants
  */
-const { name, attributes } = metadata;
+const name = 'shape-divider';
 
 const title = __( 'Shape Divider' );
 
@@ -74,6 +72,74 @@ const keywords = [
 	__( 'svg' ),
 ];
 
+const blockAttributes = {
+	align: {
+		type: 'string',
+		default: 'full',
+	},
+	height: {
+		type: 'number',
+		default: 100,
+	},
+	shapeHeight: {
+		type: 'number',
+		default: 100,
+	},
+	shapeHeightMobile: {
+		type: 'number',
+	},
+	shapeHeightTablet: {
+		type: 'number',
+	},
+	backgroundHeight: {
+		type: 'number',
+		default: 50,
+	},
+	backgroundHeightMobile: {
+		type: 'number',
+	},
+	backgroundHeightTablet: {
+		type: 'number',
+	},
+	syncHeightAlt: {
+		type: 'boolean',
+		default: true,
+	},
+	verticalFlip: {
+		type: 'boolean',
+		default: false,
+	},
+	horizontalFlip: {
+		type: 'boolean',
+		default: false,
+	},
+	color: {
+		type: 'string',
+	},
+	customColor: {
+		type: 'string',
+		default: '#111',
+	},
+	backgroundColor: {
+		type: 'string',
+	},
+	customBackgroundColor: {
+		type: 'string',
+	},
+	noBottomMargin: {
+		type: 'boolean',
+		default: true,
+	},
+	noTopMargin: {
+		type: 'boolean',
+		default: true,
+	},
+	justAdded: {
+		type: 'boolean',
+		default: true,
+	},
+	...ResponsiveBaseControlAttributes,
+};
 
 const settings = {
 
@@ -83,7 +149,7 @@ const settings = {
 
 	keywords: keywords,
 
-	attributes: {attributes, ...ResponsiveBaseControlAttributes},
+	attributes: blockAttributes,
 
 	supports: {
 		align: [ 'wide', 'full' ],
@@ -101,14 +167,276 @@ const settings = {
 		{ name: 'pointed', label: _x( 'Pointed', 'block style' ) },
 	],
 
-	transforms: Transforms,
+	transforms: {
+		from: [
+			// Default.
+			...[ ':divider', ':top-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }` );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Waves.
+			...[ ':waves-divider', ':waves-angled-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-waves',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-waves-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-waves',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Sloped.
+			...[ ':sloped-divider', ':top-sloped-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-sloped',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-sloped-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-sloped',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Sloped.
+			...[ ':rounded-divider', ':top-rounded-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-rounded',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-rounded-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-rounded',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Angled.
+			...[ ':angled-divider', ':top-angled-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-angled',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-angled-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-angled',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Triangle.
+			...[ ':triangle-divider', ':top-triangle-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-triangle',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-triangle-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-triangle',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Pointed.
+			...[ ':pointed-divider', ':top-pointed-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-pointed',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-pointed-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-pointed',
+						verticalFlip: true,
+					} );
+				},
+			},
+			// Hills.
+			...[ ':hills-divider', ':top-hills-divider' ].map( ( prefix ) => ( {
+				type: 'prefix',
+				prefix,
+				transform() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-hills',
+					} );
+				},
+			} ) ),
+			{
+				type: 'prefix',
+				prefix: ':bottom-hills-divider',
+				transform: function() {
+					return createBlock( `coblocks/${ name }`, {
+						className: 'is-style-hills',
+						verticalFlip: true,
+					} );
+				},
+			},
+			{
+				type: 'block',
+				blocks: [ 'core/spacer' ],
+				transform: ( { height } ) => createBlock( `coblocks/${ name }`, {
+					height: height,
+				} ),
+			},
+			{
+				type: 'block',
+				blocks: [ 'coblocks/dynamic-separator' ],
+				transform: ( { height } ) => createBlock( `coblocks/${ name }`, {
+					height: height,
+				} ),
+			},
+			{
+				type: 'block',
+				blocks: [ 'core/separator' ],
+				transform: () => createBlock( `coblocks/${ name }` ),
+			},
+		],
+	},
 
 	edit: Edit,
 
-	save: Save,
+	save( { attributes, className } ) {
 
-	deprecated: Deprecated,
+		const {
+			backgroundColor,
+			backgroundHeight,
+			coblocks,
+			color,
+			customBackgroundColor,
+			customColor,
+			horizontalFlip,
+			shapeHeight,
+			verticalFlip,
+		} = attributes;
+
+		const shapeClass = getColorClassName( 'color', color );
+		const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+		const classes = classnames(
+			className, {
+			[ `coblocks-shape-divider-${ coblocks.id }` ] : coblocks && ( typeof coblocks.id != 'undefined' ),
+			'is-vertically-flipped' : verticalFlip,
+			'is-horizontally-flipped' : horizontalFlip,
+			[ shapeClass ]: shapeClass,
+			[ backgroundClass ]: backgroundClass,
+		} );
+
+		const styles = {
+			backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+			color: shapeClass ? undefined : customColor,
+		};
+
+		return (
+			<div className={ classes } style={ styles } aria-hidden="true">
+				<div className="wp-block-coblocks-shape-divider__svg-wrapper" style={ { minHeight: shapeHeight } }>
+					{ getDividerFromStyle( attributes.className ) }
+				</div>
+				<div className="wp-block-coblocks-shape-divider__alt-wrapper" style={ { minHeight: backgroundHeight } }></div>
+			</div>
+		);
+	},
+	deprecated: [
+		{
+			attributes: {
+				...blockAttributes,
+			},
+			save( { attributes, className } ) {
+
+				const {
+					backgroundColor,
+					backgroundHeight,
+					coblocks,
+					color,
+					customBackgroundColor,
+					customColor,
+					horizontalFlip,
+					shapeHeight,
+					verticalFlip,
+				} = attributes;
+
+				const shapeClass = getColorClassName( 'color', color );
+				const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+				const classes = classnames(
+					className, {
+					'is-vertically-flipped' : verticalFlip,
+					'is-horizontally-flipped' : horizontalFlip,
+					[ shapeClass ]: shapeClass,
+					[ backgroundClass ]: backgroundClass,
+				} );
+
+				const styles = {
+					backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+					color: shapeClass ? undefined : customColor,
+				};
+
+				return (
+					<div className={ classes } style={ styles } aria-hidden="true">
+						<div className="wp-block-coblocks-shape-divider__svg-wrapper" style={ { height: shapeHeight } }>
+							{ getDividerFromStyle( attributes.className ) }
+						</div>
+						<div className="wp-block-coblocks-shape-divider__alt-wrapper" style={ { height: backgroundHeight } }></div>
+					</div>
+				);
+			},
+		}
+	],
 };
 
-export { name, title, icon, settings, attributes, metadata };
+export { name, title, icon, settings };
 
