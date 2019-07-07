297444a47b256e298ab9310da89b3ac1c58460d7
paranoia1906 <anthonymledesma@gmail.com>
2019-07-07 12:47:32 -0700

    Transforms to and from share block. Attributes are lost.

--- src/blocks/social-profiles/transforms.js
+++ src/blocks/social-profiles/transforms.js
@@ -1,52 +1,51 @@
 const { createBlock } = wp.blocks;
 
-// import { name } from './';
-
 export const transforms = {
 	to: [
 		{
 			type: 'block',
 			blocks: [ 'coblocks/social' ],
-			transform: ( { value } ) => {
+			transform: ( { attributes } ) => {
+				console.log( attributes );
 				// transforming an empty social element
-				if ( ! value || ! value.length ) {
-					return createBlock( 'coblocks/social' );
-				}
+				// if ( ! value || ! value.length ) {
+				// 	return createBlock( 'coblocks/social' );
+				// }
 				// transforming an social element with content
 				return createBlock( 'coblocks/social', {
-					content: value,
+					content: attributes,
 				} );
+			},
+		},
+		{
+			type: 'raw',
+			selector: 'div.wp-block-coblocks-social-profiles',
+			schema: {
+				div: {
+					classes: [ 'wp-block-coblocks-social-profiles' ],
+				},
+			},
+		},
+	],
+	from: [
+		{
+			type: 'block',
+			blocks: [ 'coblocks/social' ],
+			transform: ( { attributes } ) => {
+				console.log( attributes );
+				return createBlock( 'coblocks/social-profiles', {
+					value: attributes,
+				} );
+			},
+		},
+		{
+			type: 'raw',
+			selector: 'div.wp-block-coblocks-social-profiles',
+			schema: {
+				div: {
+					classes: [ 'wp-block-coblocks-social-profiles' ],
+				},
 			},
 		},
 	],
 };
-
-// from: [
-// 	{
-// 		type: 'block',
-// 		blocks: [ `coblocks/${ name }` ],
-// 		transform: ( { content } ) => {
-// 			return createBlock( `coblocks/${ name }`, {
-// 				value: content,
-// 			} );
-// 		},
-// 	},
-// 	{
-// 		type: 'raw',
-// 		selector: 'div.coblocks-social-profiles',
-// 		schema: {
-// 			div: {
-// 				classes: [ 'coblocks-social-profiles' ],
-// 			},
-// 		},
-// 	},
-// 	{
-// 		type: 'prefix',
-// 		prefix: ':social-profiles',
-// 		transform: function( content ) {
-// 			return createBlock( `coblocks/${ name }`, {
-// 				content,
-// 			} );
-// 		},
-// 	},
-// ],
