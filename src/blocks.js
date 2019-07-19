414e7dcce4b42faf5f63cce8678ed821010bae6f
dzoniub <dzoniub@gmail.com>
2019-07-19 14:42:04 +0200

    Change blog name to blogroll, extracted inspector into separate file.

--- src/blocks.js
+++ src/blocks.js
@@ -43,7 +43,7 @@ import * as accordion from './blocks/accordion';
 import * as accordionItem from './blocks/accordion/accordion-item';
 import * as alert from './blocks/alert';
 import * as author from './blocks/author';
-import * as blog from './blocks/blog';
+import * as blogroll from './blocks/blogroll';
 import * as clickToTweet from './blocks/click-to-tweet';
 import * as column from './blocks/row/column';
 import * as dynamicSeparator from './blocks/dynamic-separator';
@@ -73,7 +73,7 @@ export function registerBlocks () {
 		accordionItem,
 		alert,
 		author,
-		blog,
+		blogroll,
 		clickToTweet,
 		column,
 		dynamicSeparator,
--- src/blocks/blogroll/index.js
+++ src/blocks/blogroll/index.js
@@ -9,9 +9,9 @@ import icons from './icons';
  */
 const { __, _x } = wp.i18n;
 
-const name = 'blog';
+const name = 'blogroll';
 
-const title = __( 'Blog' );
+const title = __( 'Blogroll' );
 
 const icon = icons.blog;
 
@@ -19,13 +19,7 @@ const settings = {
 	title: title,
 	description: __( 'Display a list of your most recent posts.' ),
 	icon,
-	category: 'widgets',
 	keywords: [ __( 'blog posts' ) ],
-	styles: [
-		{ name: 'grid', label: _x( 'Grid', 'block style' ), isDefault: true },
-		{ name: 'list', label: _x( 'List', 'block style' ) },
-		{ name: 'carousel', label: _x( 'Carousel', 'block style' ) },
-	],
 	supports: {
 		align: [ 'wide', 'full' ],
 		html: false,
