919c2a5e4eff84bfdf771bdd0905dc1c097dd6c7
paranoia1906 <anthonymledesma@gmail.com>
2019-07-19 14:30:47 -0700

    Updated comment for fromEntries and removed css comments

--- src/js/coblocks-fromEntries.js
+++ src/js/coblocks-fromEntries.js
@@ -1,4 +1,4 @@
-// Act as polyfill for Object.fromEntries for IE11.
+// Helper for Object.fromEntries for IE11, Edge, and Opera.
 export default function fromEntries( iterable ) {
 	return [ ...iterable ].reduce(
 		( obj, { 0: key, 1: val } ) => Object.assign( obj, { [ key ]: val } ),
