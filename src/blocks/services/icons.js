9085dba73298af266b5910dbde420208990b2204
Rich Tabor <hi@richtabor.com>
2019-07-22 17:13:05 -0700

    Update icon

--- src/blocks/services/icons.js
+++ src/blocks/services/icons.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-const { SVG, Path } = wp.components;
+const { SVG, Path, G } = wp.components;
 
 /**
  * Block user interface icons
@@ -9,16 +9,12 @@ const { SVG, Path } = wp.components;
 const icons = {};
 
 icons.services = (
-	<SVG
-		className="components-coblocks-svg"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<Path
-			d="m12 4v-2h-4v2zm-10 2v11h16v-11zm16-2c1.11 0 2 .89 2 2v11c0 1.11-.89 2-2 2h-16c-1.11 0-2-.89-2-2l.01-11c0-1.11.88-2 1.99-2h4v-2c0-1.11.89-2 2-2h4c1.11 0 2 .89 2 2v2z"
-			fill="currentColor"
-			transform="translate(2 2)"
-		/>
+	<SVG className="components-coblocks-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<G fill="currentColor" fillRule="nonzero">
+			<Path d="m3 3h18v10h-18z"/>
+			<Path d="m5 15h14v2h-14z"/>
+			<Path d="m5 19h14v2h-14z" />
+		</G>
 	</SVG>
 );
 
