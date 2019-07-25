7fe5c646fc7b77656145cbc4dcc45e19975ee2e0
Rich Tabor <hi@richtabor.com>
2019-07-25 11:16:02 -0700

    Alphabetize block.json

--- src/blocks.js
+++ src/blocks.js
@@ -40,31 +40,31 @@ import * as accordion from './blocks/accordion';
 import * as accordionItem from './blocks/accordion/accordion-item';
 import * as alert from './blocks/alert';
 import * as author from './blocks/author';
+import * as buttons from './blocks/buttons';
+import * as carousel from './blocks/gallery-carousel';
 import * as clickToTweet from './blocks/click-to-tweet';
 import * as column from './blocks/row/column';
 import * as dynamicSeparator from './blocks/dynamic-separator';
+import * as feature from './blocks/features/feature';
+import * as features from './blocks/features';
+import * as foodAndDrinks from './blocks/food-and-drinks';
+import * as foodItem from './blocks/food-and-drinks/food-item';
 import * as form from './blocks/form';
 import * as gif from './blocks/gif';
 import * as gist from './blocks/gist';
+import * as hero from './blocks/hero';
 import * as highlight from './blocks/highlight';
-import * as foodAndDrinks from './blocks/food-and-drinks';
-import * as foodItem from './blocks/food-and-drinks/food-item';
+import * as icon from './blocks/icon';
+import * as map from './blocks/map';
+import * as masonry from './blocks/gallery-masonry';
+import * as mediaCard from './blocks/media-card';
 import * as pricingTable from './blocks/pricing-table';
 import * as pricingTableItem from './blocks/pricing-table/pricing-table-item';
 import * as row from './blocks/row';
-import * as share from './blocks/share';
-import * as map from './blocks/map';
-import * as mediaCard from './blocks/media-card';
 import * as shapeDivider from './blocks/shape-divider';
-import * as icon from './blocks/icon';
-import * as feature from './blocks/features/feature';
-import * as features from './blocks/features';
-import * as buttons from './blocks/buttons';
-import * as hero from './blocks/hero';
-import * as stacked from './blocks/gallery-stacked';
-import * as masonry from './blocks/gallery-masonry';
-import * as carousel from './blocks/gallery-carousel';
+import * as share from './blocks/share';
 import * as socialProfiles from './blocks/social-profiles';
+import * as stacked from './blocks/gallery-stacked';
 
 export function registerBlocks() {
 	[
@@ -95,8 +95,8 @@ export function registerBlocks() {
 		row,
 		shapeDivider,
 		share,
-		stacked,
 		socialProfiles,
+		stacked,
 	].forEach( block => {
 		if ( ! block ) {
 			return;
