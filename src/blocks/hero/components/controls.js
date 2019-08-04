9fdcdb835b7a311e8b46a1812d66964eb1e2e39a
paranoia1906 <anthonymledesma@gmail.com>
2019-08-04 11:31:16 -0700

    add the colorpicker to the Hero block toolbar

--- src/blocks/hero/components/controls.js
+++ src/blocks/hero/components/controls.js
@@ -2,6 +2,17 @@
 /**
  * Internal dependencies
  */
+import applyWithColors from './colors';
+
+/**
+ * WordPress dependencies
+*/
+const { compose } = wp.compose;
+const { withFallbackStyles } = wp.components;
+
+/**
+ * Internal dependencies
+ */
 import icons from './icons';
 import { BackgroundControls } from '../../../components/background';
 import CSSGridToolbar from '../../../components/grid-control/toolbar';
@@ -9,19 +20,41 @@ import CSSGridToolbar from '../../../components/grid-control/toolbar';
 /**
  * WordPress dependencies
  */
+
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
-const { AlignmentToolbar, BlockControls } = wp.blockEditor;
-const { Toolbar } = wp.components;
+const { AlignmentToolbar, BlockControls, PanelColorSettings } = wp.blockEditor;
+const { Toolbar, Dropdown, IconButton } = wp.components;
+
+/**
+ * Fallback styles
+ */
+const { getComputedStyle } = window;
+const FallbackStyles = withFallbackStyles( ( node, ownProps ) => {
+	const { backgroundColor } = ownProps.attributes;
+
+	const editableNode = node.querySelector( '[contenteditable="true"]' );
+
+	//verify if editableNode is available, before using getComputedStyle.
+	const computedStyles = editableNode ? getComputedStyle( editableNode ) : null;
+
+	return {
+		fallbackBackgroundColor: backgroundColor || ! computedStyles ? undefined : computedStyles.backgroundColor,
+	};
+} );
 
 class Controls extends Component {
 	render() {
 		const {
 			attributes,
+			backgroundColor,
 			setAttributes,
+			setBackgroundColor,
+			setTextColor,
+			textColor,
 		} = this.props;
 
-		const { contentAlign } = attributes;
+		const { contentAlign, paddingSize } = attributes;
 
 		return (
 			<Fragment>
@@ -33,10 +66,56 @@ class Controls extends Component {
 							props={ this.props }
 						/>
 					</Toolbar>
+
 					<AlignmentToolbar
 						value={ contentAlign }
 						onChange={ ( nextContentAlign ) => setAttributes( { contentAlign: nextContentAlign } ) }
 					/>
+					<Toolbar>
+						<Dropdown
+							className="components-dropdown-menu"
+							position="bottom right"
+							renderToggle={ ( { onToggle } ) => (
+								<IconButton
+									className="components-dropdown-menu__toggle"
+									icon={ icons.colorpicker }
+									aria-haspopup="true"
+									label={ __( 'Color Settings' ) }
+									tooltip={ __( 'Color Settings' ) }
+									onClick={ onToggle }
+								>
+									<span className="components-dropdown-menu__indicator" />
+								</IconButton>
+							) }
+							renderContent={ () => (
+								<PanelColorSettings
+									title={ __( 'Color Settings' ) }
+									initialOpen={ true }
+									colorSettings={ [
+										{
+											value: backgroundColor.color,
+											onChange: ( nextBackgroundColor ) => {
+												setBackgroundColor( nextBackgroundColor );
+
+												// Add padding if there's none.
+												if ( ! paddingSize || paddingSize === 'no' ) {
+													setAttributes( { paddingSize: 'huge' } );
+												}
+											},
+											label: __( 'Background Color' ),
+										},
+										{
+											value: textColor.color,
+											onChange: setTextColor,
+											label: __( 'Text Color' ),
+										},
+									] }
+								>
+								</PanelColorSettings>
+							) }
+						/>
+					</Toolbar>
+
 					{ BackgroundControls( this.props ) }
 				</BlockControls>
 			</Fragment>
@@ -44,4 +123,7 @@ class Controls extends Component {
 	}
 }
 
-export default Controls;
+export default compose( [
+	applyWithColors,
+	FallbackStyles,
+] )( Controls );
