e38888452e528c0ab22e9bff17e6d0f79df64cfc
paranoia1906 <anthonymledesma@gmail.com>
2019-08-23 15:43:25 -0700

    handle styles for blocks

--- src/blocks/share/index.php
+++ src/blocks/share/index.php
@@ -92,7 +92,8 @@ function coblocks_render_share_block( $attributes ) {
 	$google_url    = apply_filters( 'coblocks_google_share_url', $google_url );
 
 	// Attributes.
-	$text_align    = is_array( $attributes ) && isset( $attributes['textAlign'] ) ? "style=text-align:{$attributes['textAlign']}" : '';
+	$text_align    = is_array( $attributes ) && isset( $attributes['textAlign'] ) ? "text-align:{$attributes['textAlign']};" : '';
+	$background_color    = is_array( $attributes ) && isset( $attributes['customBlockBackgroundColor'] ) ? "background-color:{$attributes['customBlockBackgroundColor']};" : '';
 	$border_radius = is_array( $attributes ) && isset( $attributes['borderRadius'] ) ? "border-radius: {$attributes['borderRadius']}px;" : '';
 	$has_padding   = is_array( $attributes ) && isset( $attributes['padding'] ) ? 'has-padding' : '';
 
@@ -222,12 +223,13 @@ function coblocks_render_share_block( $attributes ) {
 
 	// Render block content.
 	$block_content = sprintf(
-		'<div class="%1$s" %2$s><ul>%3$s</ul></div>',
+		'<div class="%1$s" style="%2$s %3$s"><ul>%4$s</ul></div>',
 		esc_attr( $class ),
 		esc_attr( $text_align ),
+		esc_attr( $background_color),
 		$markup
 	);
-
+	
 	return $block_content;
 }
 
