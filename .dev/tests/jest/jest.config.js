7c7600eb483b7a23c4d05bde3d3084c8ebb98b94
JR Tashjian <jrtashjian@gmail.com>
2019-09-06 19:42:03 -0400

    Mock scss imports

--- .dev/tests/jest/jest.config.js
+++ .dev/tests/jest/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
 	testMatch: ['**/__tests__/**/*.js', '**/?(*.)(spec|test).js', '**/test/*.js'],
 	testPathIgnorePatterns: ['/.git/', '/node_modules/', '<rootDir>/build/'],
 	setupFiles: [ '<rootDir>/.dev/tests/jest/setup-globals.js' ],
+	moduleNameMapper: {
+		'\\.(scss|css|less)$': '<rootDir>/.dev/tests/jest/styleMock.js',
+	},
 };
