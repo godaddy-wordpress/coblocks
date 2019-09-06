d838d9cc109cae18dd4f3e3b86613712e8c0dfab
JR Tashjian <jrtashjian@gmail.com>
2019-09-06 18:20:52 -0400

    Highlight: test that the save component renders

---
+++ src/blocks/highlight/test/highlight-save.spec.js
@@ -0,0 +1,31 @@
+/**
+ * External dependencies
+ */
+// import { create as createTestRenderer } from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+/**
+ * Internal dependencies.
+ */
+import SaveComponent from '../save';
+
+describe( 'coblocks/highlight/save', () => {
+	it( 'renders', () => {
+		const renderer = new ShallowRenderer();
+
+		const attributes = {
+			content: '',
+			align: '',
+			backgroundColor: '',
+			customBackgroundColor: '',
+			textColor: '',
+			customTextColor: '',
+			fontSize: '',
+			customFontSize: ''
+		};
+
+		renderer.render( <SaveComponent attributes={attributes} /> );
+
+		expect( renderer.getRenderOutput() ).toMatchSnapshot();
+	} );
+} );
