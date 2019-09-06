7bd06c86b3a009277c2800475b2d5491ee533524
JR Tashjian <jrtashjian@gmail.com>
2019-09-06 17:46:39 -0400

    Jest should ignore the build directory

--- .dev/tests/jest/jest.config.js
+++ .dev/tests/jest/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	rootDir: '../../../',
 	testMatch: ['**/__tests__/**/*.js', '**/?(*.)(spec|test).js', '**/test/*.js'],
-	testPathIgnorePatterns: ['/.git/', '/node_modules/', '<rootDir>/.*/build/'],
+	testPathIgnorePatterns: ['/.git/', '/node_modules/', '<rootDir>/build/'],
 };
