cd11e3e294bcabc92fa7b8994776a4f442084e68
JR Tashjian <jrtashjian@gmail.com>
2019-09-07 00:23:09 -0400

    highlight block: new behavior tests

--- src/blocks/highlight/test/index.spec.js
+++ src/blocks/highlight/test/index.spec.js
@@ -25,8 +25,78 @@ describe( 'coblocks/highlight', () => {
 
 	it( 'should not render without content', () => {
 		const block = createBlock( name );
+
+		expect( serialize( block ) ).toEqual( `<!-- wp:${ name } /-->` );
+	} );
+
+	it( 'should center align text with inline css', () => {
+		const block = createBlock( name, {
+			content: 'highlighted content',
+			align: 'center',
+		} );
 		const serializedBlock = serialize( block );
 
-		expect( serializedBlock ).toEqual( `<!-- wp:${ name } /-->` );
+		expect( serializedBlock ).toMatch( /<p.*style=".*text-align:center/ );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should apply has-background class if any background color is selected', () => {
+		const block = createBlock( name, { content: 'highlighted content' } );
+		let serializedBlock = '';
+
+		block.attributes.customBackgroundColor = '#000000';
+		serializedBlock = serialize( block );
+		expect( serializedBlock ).toMatch( /<mark.*class=".*has-background/ );
+		expect( serializedBlock ).toMatchSnapshot();
+
+		block.attributes.customBackgroundColor = undefined;
+		block.attributes.backgroundColor = 'primary';
+		serializedBlock = serialize( block );
+		expect( serializedBlock ).toMatch( /<mark.*class=".*has-background/ );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should apply has-primary-background-color class if the primary color is selected from the color palette', () => {
+		const block = createBlock( name, {
+			content: 'highlighted content',
+			backgroundColor: 'primary',
+		} );
+		const serializedBlock = serialize( block );
+
+		expect( serialize( block ) ).toMatch( /<mark.*class=".*has-primary-background-color/ );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should apply custom background color with inline css', () => {
+		const block = createBlock( name, {
+			content: 'highlighted content',
+			customBackgroundColor: '#000000',
+		} );
+		const serializedBlock = serialize( block );
+
+		expect( serialize( block ) ).toMatch( /<mark.*style=".*background-color:#000000/ );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should apply has-small-font-size class if the small font size is selected', () => {
+		const block = createBlock( name, {
+			content: 'highlighted content',
+			fontSize: 'small',
+		} );
+		const serializedBlock = serialize( block );
+
+		expect( serialize( block ) ).toMatch( /<p.*class=".*has-small-font-size/ );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should apply custom font size with inline css', () => {
+		const block = createBlock( name, {
+			content: 'highlighted content',
+			customFontSize: 50,
+		} );
+		const serializedBlock = serialize( block );
+
+		expect( serialize( block ) ).toMatch( /<mark.*style=".*font-size:50px/ );
+		expect( serializedBlock ).toMatchSnapshot();
 	} );
 } );
