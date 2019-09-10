c696f8b91dc7dd2b51ab1a5da9ece592359a66d1
JR Tashjian <jrtashjian@gmail.com>
2019-09-09 21:39:03 -0400

    Test all variations of a deprecated block at once.

--- src/blocks/highlight/test/index.spec.js
+++ src/blocks/highlight/test/index.spec.js
@@ -118,9 +118,56 @@ describe( 'coblocks/highlight', () => {
 			<!-- wp:coblocks/highlight -->
 			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content">highlighted content</mark></p>
 			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"align":"left"} -->
+			<p style="text-align:left" class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"align":"center"} -->
+			<p style="text-align:center" class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"align":"right"} -->
+			<p style="text-align:right" class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"fontSize":"small"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-small-font-size">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"fontSize":"large"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-large-font-size">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"fontSize":"huge"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-huge-font-size">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"customFontSize":100} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content" style="font-size:100px">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"backgroundColor":"primary"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-background has-primary-background-color">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"customBackgroundColor":"#87dbff"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-background" style="background-color:#87dbff">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"textColor":"primary"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-text-color has-primary-color">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
+
+			<!-- wp:coblocks/highlight {"customTextColor":"#87dbff"} -->
+			<p class="wp-block-coblocks-highlight"><mark class="wp-block-coblocks-highlight__content has-text-color" style="color:#87dbff">highlighted content</mark></p>
+			<!-- /wp:coblocks/highlight -->
 		`.trim();
 
-		const block = parse( postContent );
-		expect( block[ 0 ].isValid ).toBe( true );
+		const blocks = parse( postContent );
+
+		for ( let i = 0; i < blocks.length; i++ ) {
+			expect( blocks[ i ].isValid ).toBe( true );
+		}
 	} );
 } );
