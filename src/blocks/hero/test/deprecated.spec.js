040082ff47f6f9028426ae4cdd8627c110163964
JR Tashjian <jrtashjian@gmail.com>
2019-09-12 15:46:36 -0400

    Dynamically test all deprecations and variations of a block.

--- src/blocks/hero/test/deprecated.spec.js
+++ src/blocks/hero/test/deprecated.spec.js
@@ -10,68 +10,127 @@ import { registerBlockType, unregisterBlockType, createBlock, serialize, parse }
  */
 import { name, settings } from '../index';
 
-describe( 'coblocks/hero deprecation 1', () => {
-	// Make variables accessible for all tests.
-	let v1Block;
+const variations = {
+	layout: [],
+	fullscreen: [],
+	paddingTop: [],
+	paddingRight: [],
+	paddingBottom: [],
+	paddingLeft: [],
+	paddingTopTablet: [],
+	paddingRightTablet: [],
+	paddingBottomTablet: [],
+	paddingLeftTablet: [],
+	paddingTopMobile: [],
+	paddingRightMobile: [],
+	paddingBottomMobile: [],
+	paddingLeftMobile: [],
+	paddingUnit: [],
+	paddingSize: [],
+	paddingSyncUnits: [],
+	paddingSyncUnitsTablet: [],
+	paddingSyncUnitsMobile: [],
+	marginTop: [],
+	marginRight: [],
+	marginBottom: [],
+	marginLeft: [],
+	marginTopTablet: [],
+	marginRightTablet: [],
+	marginBottomTablet: [],
+	marginLeftTablet: [],
+	marginTopMobile: [],
+	marginRightMobile: [],
+	marginBottomMobile: [],
+	marginLeftMobile: [],
+	marginUnit: [],
+	marginSize: [],
+	marginSyncUnits: [],
+	marginSyncUnitsTablet: [],
+	marginSyncUnitsMobile: [],
+	hasMarginControl: [],
+	hasAlignmentControls: [],
+	hasStackedControl: [],
+	backgroundType: [],
+	backgroundImg: [],
+	backgroundPosition: [],
+	backgroundRepeat: [],
+	backgroundSize: [],
+	backgroundOverlay: [],
+	backgroundColor: [],
+	customBackgroundColor: [],
+	hasParallax: [],
+	focalPoint: [],
+	videoMuted: [],
+	videoLoop: [],
+	openPopover: [],
+	height: [],
+	heightTablet: [],
+	heightMobile: [],
+	syncHeight: [],
+	align: [],
+	contentAlign: [ undefined, 'left', 'center', 'right' ],
+	textColor: [ undefined, 'primary' ],
+	customTextColor: [ undefined, '#123456' ],
+	maxWidth: [ 750 ],
+	saveCoBlocksMeta: [],
+};
 
-	// Mapping of each attribute and value variation.
-	const variations = {
-		contentAlign: [ undefined, 'left', 'center', 'right' ],
-		textColor: [ undefined, 'primary' ],
-		customTextColor: [ undefined, '#123456' ],
-		maxWidth: [ 750 ],
-	};
+settings.deprecated.map( ( deprecated, index ) => {
+	describe( `coblocks/hero deprecation ${ index }`, () => {
+		// Make variables accessible for all tests.
+		let deprecatedBlock;
 
-	beforeEach( () => {
-		unregisterBlockType( name );
+		beforeEach( () => {
+			unregisterBlockType( name );
 
-		// Register the v1 block.
-		const v1Settings = Object.assign(
-			{}, omit( settings, [ 'attributes', 'save', 'deprecated' ] ),
-			{
-				attributes: settings.deprecated[ 0 ].attributes,
-				save: settings.deprecated[ 0 ].save,
-			}
-		);
-		registerBlockType( name, { category: 'common', ...v1Settings } );
-
-		// Create the block with the minimum attributes.
-		v1Block = createBlock( name );
-	} );
-
-	it( 'should deprecate old version', () => {
-		const v1Serialized = serialize( v1Block );
-
-		// Unregister the v1 block version.
-		unregisterBlockType( name );
-
-		// Register the current block version.
-		registerBlockType( name, { category: 'common', ...settings } );
-
-		const blocks = parse( v1Serialized );
-
-		for ( let i = 0; i < blocks.length; i++ ) {
-			expect( blocks[ i ].isValid ).toBe( true );
-		}
-	} );
-
-	Object.entries( variations ).map( ( [ attribute, variations ] ) => {
-		variations.map( variation => {
-			it( `should support attribute.${ attribute } set to '${ variation }'`, () => {
-				v1Block.attributes[ attribute ] = variation;
-				const v1Serialized = serialize( v1Block );
-
-				// Unregister the v1 block version.
-				unregisterBlockType( name );
-
-				// Register the current block version.
-				registerBlockType( name, { category: 'common', ...settings } );
-
-				const blocks = parse( v1Serialized );
-
-				for ( let i = 0; i < blocks.length; i++ ) {
-					expect( blocks[ i ].isValid ).toBe( true );
+			// Register the deprecated block.
+			const deprecatedSettings = Object.assign(
+				{}, omit( settings, [ 'attributes', 'save', 'deprecated' ] ),
+				{
+					attributes: deprecated.attributes,
+					save: deprecated.save,
 				}
+			);
+			registerBlockType( name, { category: 'common', ...deprecatedSettings } );
+
+			// Create the block with the minimum attributes.
+			deprecatedBlock = createBlock( name );
+		} );
+
+		it( 'should deprecate old version', () => {
+			const deprecatedSerialized = serialize( deprecatedBlock );
+
+			// Unregister the deprecated block version.
+			unregisterBlockType( name );
+
+			// Register the current block version.
+			registerBlockType( name, { category: 'common', ...settings } );
+
+			const blocks = parse( deprecatedSerialized );
+
+			for ( let i = 0; i < blocks.length; i++ ) {
+				expect( blocks[ i ].isValid ).toBe( true );
+			}
+		} );
+
+		Object.keys( deprecated.attributes ).map( ( attribute ) => {
+			variations[ attribute ].map( variation => {
+				it( `should support attribute.${ attribute } set to '${ variation }'`, () => {
+					deprecatedBlock.attributes[ attribute ] = variation;
+					const deprecatedSerialized = serialize( deprecatedBlock );
+
+					// Unregister the deprecated block version.
+					unregisterBlockType( name );
+
+					// Register the current block version.
+					registerBlockType( name, { category: 'common', ...settings } );
+
+					const blocks = parse( deprecatedSerialized );
+
+					for ( let i = 0; i < blocks.length; i++ ) {
+						expect( blocks[ i ].isValid ).toBe( true );
+					}
+				} );
 			} );
 		} );
 	} );
