bd2bfb0adaace4c6eaa129f58b5dfba436944533
JR Tashjian <jrtashjian@gmail.com>
2019-09-12 23:25:48 -0400

    A few more Highlight block Edit component tests

--- src/blocks/highlight/test/edit.spec.js
+++ src/blocks/highlight/test/edit.spec.js
@@ -24,4 +24,45 @@ describe( 'coblocks/highlight edit', () => {
 		expect( component ).toBeDefined();
 		expect( component ).toMatchSnapshot();
 	} );
+
+	it( 'should render with background color', () => {
+		const renderer = new ShallowRenderer();
+		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
+			backgroundColor: { color: '#123456' },
+		} ) } /> );
+		const component = renderer.getRenderOutput();
+
+		const markElement = component.props.children[ 2 ].props.children.props;
+
+		expect( component ).toBeDefined();
+		expect( markElement.className.includes( 'has-background' ) ).toBe( true );
+		expect( markElement.style.backgroundColor ).toBe( '#123456' );
+	} );
+
+	it( 'should render with text color', () => {
+		const renderer = new ShallowRenderer();
+		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
+			textColor: { color: '#123456' },
+		} ) } /> );
+		const component = renderer.getRenderOutput();
+
+		const markElement = component.props.children[ 2 ].props.children.props;
+
+		expect( component ).toBeDefined();
+		expect( markElement.className.includes( 'has-text-color' ) ).toBe( true );
+		expect( markElement.style.color ).toBe( '#123456' );
+	} );
+
+	it( 'should render with font size', () => {
+		const renderer = new ShallowRenderer();
+		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
+			fontSize: { size: 16 },
+		} ) } /> );
+		const component = renderer.getRenderOutput();
+
+		const markElement = component.props.children[ 2 ].props.children.props;
+
+		expect( component ).toBeDefined();
+		expect( markElement.style.fontSize ).toBe( '16px' );
+	} );
 } );
