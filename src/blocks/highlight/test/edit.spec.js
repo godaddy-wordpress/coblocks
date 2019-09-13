110cf837338e43947bbd9dfcaf0529102302d2ac
JR Tashjian <jrtashjian@gmail.com>
2019-09-13 16:57:14 -0400

    Mock and test RichText component prop functions

--- src/blocks/highlight/test/edit.spec.js
+++ src/blocks/highlight/test/edit.spec.js
@@ -3,6 +3,9 @@
  */
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { getBlockDefaultClassName } from '@wordpress/blocks';
+import { registerCoreBlocks } from '@wordpress/block-library';
+
+registerCoreBlocks();
 
 /**
  * Internal dependencies.
@@ -10,14 +13,27 @@ import { getBlockDefaultClassName } from '@wordpress/blocks';
 import { name } from '../index';
 import { Edit } from '../edit';
 
+let renderer;
+
 const defaultProps = {
 	className: getBlockDefaultClassName( name ),
 	attributes: {},
+	setAttributes: jest.fn( object => {
+		const instance = renderer.getMountedInstance();
+		return Object.assign( instance.props.attributes, object );
+	} ),
+	onReplace: jest.fn( value => value ),
 };
+
+// Mock only specific exports of a package.
+jest.mock( '@wordpress/block-editor', () => ( {
+	...require.requireActual( '@wordpress/block-editor' ),
+	RichText: () => <mock-richtext />,
+} ) );
 
 describe( 'coblocks/highlight edit', () => {
 	it( 'should render', () => {
-		const renderer = new ShallowRenderer();
+		renderer = new ShallowRenderer();
 		renderer.render( <Edit { ...defaultProps } /> );
 		const component = renderer.getRenderOutput();
 
@@ -26,7 +42,7 @@ describe( 'coblocks/highlight edit', () => {
 	} );
 
 	it( 'should render with background color', () => {
-		const renderer = new ShallowRenderer();
+		renderer = new ShallowRenderer();
 		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
 			backgroundColor: { color: '#123456' },
 		} ) } /> );
@@ -40,7 +56,7 @@ describe( 'coblocks/highlight edit', () => {
 	} );
 
 	it( 'should render with text color', () => {
-		const renderer = new ShallowRenderer();
+		renderer = new ShallowRenderer();
 		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
 			textColor: { color: '#123456' },
 		} ) } /> );
@@ -54,7 +70,7 @@ describe( 'coblocks/highlight edit', () => {
 	} );
 
 	it( 'should render with font size', () => {
-		const renderer = new ShallowRenderer();
+		renderer = new ShallowRenderer();
 		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
 			fontSize: { size: 16 },
 		} ) } /> );
@@ -64,5 +80,32 @@ describe( 'coblocks/highlight edit', () => {
 
 		expect( component ).toBeDefined();
 		expect( markElement.style.fontSize ).toBe( '16px' );
+	} );
+
+	it( 'RichText onChange', () => {
+		renderer = new ShallowRenderer();
+		renderer.render( <Edit { ...Object.assign( {}, defaultProps, {
+			attributes: Object.assign( {}, defaultProps.attributes, {
+				content: 'Lorem ipsum dolor sit amet.',
+			} ),
+		} ) } /> );
+
+		const component = renderer.getRenderOutput();
+		const markElement = component.props.children[ 2 ].props.children;
+
+		expect( markElement.props.value ).toBe( 'Lorem ipsum dolor sit amet.' );
+
+		markElement.props.onChange( 'onChange content' );
+		expect( renderer.getMountedInstance().props.attributes.content ).toBe( 'onChange content' );
+	} );
+
+	it( 'RichText onRemove', () => {
+		renderer = new ShallowRenderer();
+		renderer.render( <Edit { ...defaultProps } /> );
+
+		const component = renderer.getRenderOutput();
+		const markElement = component.props.children[ 2 ].props.children;
+
+		expect( markElement.props.onRemove() ).toEqual( [] );
 	} );
 } );
