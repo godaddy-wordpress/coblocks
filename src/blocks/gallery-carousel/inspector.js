6e16db4de185d0bed825b1a69232489f543e69fe
Nemanja Grabovac <ngrabovac@godaddy.com>
2019-09-18 17:28:43 +0200

    Inspector fix for toggles.

--- src/blocks/gallery-carousel/inspector.js
+++ src/blocks/gallery-carousel/inspector.js
@@ -12,7 +12,7 @@ import { BackgroundPanel } from '../../components/background';
 const { __ } = wp.i18n;
 const { Component, Fragment } = wp.element;
 const { InspectorControls, PanelColorSettings } = wp.blockEditor;
-const { PanelBody, RangeControl } = wp.components;
+const { PanelBody, RangeControl, ToggleControl } = wp.components;
 
 /**
  * Inspector controls
@@ -36,6 +36,15 @@ class Inspector extends Component {
 
 	setHeightTo( value ) {
 		this.props.setAttributes( { height: value } );
+	}
+
+	getThumbnailNavigationHelp( checked ) {
+		return checked ? __( 'Showing thumbnail navigation.' ) : __( 'Toggle to show thumbnails.' );
+	}
+
+
+	getResponsiveHeightHelp( checked ) {
+		return checked ? __( 'Percentage based height is activated.' ) : __( 'Toggle for percentage based height.' );
 	}
 
 	getColors() {
@@ -98,6 +107,7 @@ class Inspector extends Component {
 		const {
 			attributes,
 			isSelected,
+			setAttributes,
 		} = this.props;
 
 		const {
@@ -106,6 +116,8 @@ class Inspector extends Component {
 			gutter,
 			height,
 			radius,
+			thumbnails,
+			responsiveHeight,
 		} = attributes;
 
 		return (
@@ -132,14 +144,16 @@ class Inspector extends Component {
 								max={ 20 }
 							/>
 							}
-							<RangeControl
-								label={ __( 'Height in pixels' ) }
-								value={ height }
-								onChange={ this.setHeightTo }
-								min={ 200 }
-								max={ 1000 }
-								step={ 1 }
-							/>
+							{ !responsiveHeight &&
+								<RangeControl
+									label={ __( 'Height in pixels' ) }
+									value={ height }
+									onChange={ this.setHeightTo }
+									min={ 200 }
+									max={ 1000 }
+									step={ 1 }
+								/>
+							}
 							{ gutter > 0 && <RangeControl
 								label={ __( 'Rounded Corners' ) }
 								value={ radius }
@@ -148,6 +162,18 @@ class Inspector extends Component {
 								max={ 20 }
 								step={ 1 }
 							/> }
+							<ToggleControl
+								label={ __( 'Responsive Height' ) }
+								checked={ !! responsiveHeight }
+								onChange={ () => setAttributes( { responsiveHeight: ! responsiveHeight, height: 500, className: !responsiveHeight ? 'responsive-height' : '' } ) }
+								help={ this.getResponsiveHeightHelp }
+							/>
+							<ToggleControl
+								label={ __( 'Thumbnail Navigation' ) }
+								checked={ !! thumbnails }
+								onChange={ () => setAttributes( { thumbnails: ! thumbnails } ) }
+								help={ this.getThumbnailNavigationHelp }
+							/>
 						</PanelBody>
 						<SliderPanel { ...this.props } />
 						<BackgroundPanel { ...this.props }
