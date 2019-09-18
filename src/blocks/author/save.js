349d52d20b6a2e2aecbd682d718c2018d618ac79
Rich Tabor <hi@richtabor.com>
2019-09-18 17:45:45 -0400

    Improve Author block, adding colors/font sizes and cleanup (#808)
    * Remove AlignmentToolbar and textAlign support
    
    * Get deprecation passing
    
    * Check for attributes before rendering the block
    
    * Organize save.js
    
    * Clean up edit function
    
    * Deprecate optional heading
    
    * Clean styling
    
    * Update style.scss
    
    * Use author name for avatar alt text
    
    * Clean up editor styling
    
    * Add Color Settings with background and text color support
    
    * Add support for fontSize
    
    * Add fallback colors
    
    * Remove "Remove Image" toolbar control
    
    * Tweak dropzone language
    
    * Tweak avatar so it doesnt distort if not 1:1
    
    * Linting
    
    * …
    
    * Ensure biography inherits selected text color.
    
    * Ensure the avatar is not squished when editing

--- src/blocks/author/save.js
+++ src/blocks/author/save.js
@@ -1,57 +1,84 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import { hasEmptyAttributes } from '../../utils/block-helpers';
+
+/**
  * WordPress dependencies
  */
-const { RichText, InnerBlocks } = wp.blockEditor;
+const { RichText, InnerBlocks, getColorClassName, getFontSizeClass } = wp.blockEditor;
+
+const isEmpty = attributes => {
+	const attributesToCheck = [ 'name', 'imgUrl', 'biography' ];
+	const newAttributes = Object.entries( attributes ).filter( ( [ key ] ) =>
+		attributesToCheck.includes( key )
+	);
+
+	return hasEmptyAttributes( Object.fromEntries( newAttributes ) );
+};
 
 const save = ( { attributes } ) => {
 	const {
+		backgroundColor,
 		biography,
-		heading,
+		customBackgroundColor,
+		customTextColor,
 		imgUrl,
 		name,
-		textAlign,
+		textColor,
+		fontSize,
+		customFontSize,
 	} = attributes;
 
-	if ( name ) {
-		return (
-			<div style={ { textAlign: textAlign } }>
-				{ imgUrl && (
-					<div className={ 'wp-block-coblocks-author__avatar' }>
-						<img
-							className="wp-block-coblocks-author__avatar-img"
-							src={ imgUrl }
-							alt="avatar"
-						/>
-					</div>
+	const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+	const textClass = getColorClassName( 'color', textColor );
+	const fontSizeClass = getFontSizeClass( fontSize );
+
+	const classes = classnames( {
+		'has-text-color': textColor || customTextColor,
+		'has-background': backgroundColor || customBackgroundColor,
+		[ textClass ]: textClass,
+		[ backgroundClass ]: backgroundClass,
+		[ fontSizeClass ]: fontSizeClass,
+	} );
+
+	const styles = {
+		backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+		color: textClass ? undefined : customTextColor,
+		fontSize: fontSizeClass ? undefined : customFontSize,
+	};
+
+	return isEmpty( attributes ) ? null : (
+		<div className={ classes } style={ styles }>
+			{ imgUrl && (
+				<figure className={ 'wp-block-coblocks-author__avatar' }>
+					<img className="wp-block-coblocks-author__avatar-img" src={ imgUrl } alt={ name } />
+				</figure>
+			) }
+			<div className={ 'wp-block-coblocks-author__content' }>
+				{ ! RichText.isEmpty( name ) && (
+					<RichText.Content
+						tagName="span"
+						className="wp-block-coblocks-author__name"
+						value={ name }
+					/>
 				) }
-				<div className={ 'wp-block-coblocks-author__content' }>
-					{ ! RichText.isEmpty( heading ) && (
-						<RichText.Content
-							tagName="p"
-							className="wp-block-coblocks-author__heading"
-							value={ heading }
-						/>
-					) }
-					{ ! RichText.isEmpty( name ) && (
-						<RichText.Content
-							tagName="span"
-							className="wp-block-coblocks-author__name"
-							value={ name }
-						/>
-					) }
-					{ ! RichText.isEmpty( biography ) && (
-						<RichText.Content
-							tagName="p"
-							className="wp-block-coblocks-author__biography"
-							value={ biography }
-						/>
-					) }
-					<InnerBlocks.Content />
-				</div>
+				{ ! RichText.isEmpty( biography ) && (
+					<RichText.Content
+						tagName="p"
+						className="wp-block-coblocks-author__biography"
+						value={ biography }
+					/>
+				) }
+				<InnerBlocks.Content />
 			</div>
-		);
-	}
-	return null;
+		</div>
+	);
 };
 
 export default save;
