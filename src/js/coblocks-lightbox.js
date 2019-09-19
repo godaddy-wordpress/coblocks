704b8b010aa1b50f47e13b1445e7e7a8f5a4df26
Nemanja Grabovac <ngrabovac@godaddy.com>
2019-09-19 15:08:52 +0200

    Removed data-url attribute and adjusted lightbox script.

--- src/js/coblocks-lightbox.js
+++ src/js/coblocks-lightbox.js
@@ -32,14 +32,14 @@
 	$( '.has-lightbox ul li figure img' ).click( function( e ) {
 		$.each( images, function( i, img ) {
 			imagePreloader[ 'img-' + i ] = new window.Image();
-			imagePreloader[ 'img-' + i ].src = img.attributes[ 4 ].value;
+			imagePreloader[ 'img-' + i ].src = img.attributes[ 0 ].value;
 		} );
 
-		index = Number( e.target.attributes[ 5 ].value );
+		index = Number( e.target.attributes[ 4 ].value );
 		wrapper.css( 'display', 'flex' );
 		wrapperBackground.css( 'background-image', 'url(' + imagePreloader[ 'img-' + index ].src + ')' );
 		image.attr( 'src', imagePreloader[ 'img-' + index ].src );
-		counter.html( ( Number( e.target.attributes[ 5 ].value ) + 1 ) + ' / ' + images.length );
+		counter.html( ( Number( e.target.attributes[ 4 ].value ) + 1 ) + ' / ' + images.length );
 	} );
 
 	arrowLeftContainer.click( function() {
@@ -62,10 +62,10 @@
 
 	function changeImage( index ) {
 		$.each( images, function( i, img ) {
-			if ( parseInt( img.attributes[ 5 ].value ) === index ) {
+			if ( parseInt( img.attributes[ 4 ].value ) === index ) {
 				wrapperBackground.css( 'background-image', 'url(' + imagePreloader[ 'img-' + i ].src + ')' );
 				image.attr( 'src', imagePreloader[ 'img-' + i ].src );
-				counter.html( ( Number( img.attributes[ 5 ].value ) + 1 ) + ' / ' + images.length );
+				counter.html( ( Number( img.attributes[ 4 ].value ) + 1 ) + ' / ' + images.length );
 			}
 		} );
 	}
