6539e2e78c3c5689aa8985c3e5d8fc74b2d6a7ad
Rich Tabor <hi@richtabor.com>
2019-09-30 07:57:43 -0400

    Add carousel deprecation

--- src/blocks/gallery-carousel/deprecated.js
+++ src/blocks/gallery-carousel/deprecated.js
@@ -44,6 +44,192 @@ const deprecated =
 			responsiveHeight,
 		} = attributes;
 
+		const classes = classnames(
+			className, {
+				'has-responsive-height': responsiveHeight,
+			}
+		);
+
+		const innerClasses = classnames(
+			'is-cropped',
+			...GalleryClasses( attributes ),
+			...BackgroundClasses( attributes ), {
+				'has-horizontal-gutter': gutter > 0,
+
+			}
+		);
+
+		const innerStyles = {
+			...BackgroundStyles( attributes ),
+		};
+
+		const flickityClasses = classnames(
+			'has-carousel',
+			`has-carousel-${ gridSize }`, {
+				'has-aligned-cells': alignCells,
+				[ `has-margin-bottom-${ gutter }` ]: thumbnails && gutter > 0,
+				[ `has-margin-bottom-mobile-${ gutterMobile }` ]: thumbnails && gutterMobile > 0,
+			}
+		);
+
+		const flickityStyles = {
+			height: height ? height + 'px' : undefined,
+		};
+
+		const figureClasses = classnames(
+			'coblocks-gallery--figure', {
+				[ `has-margin-left-${ gutter }` ]: gutter > 0,
+				[ `has-margin-left-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+				[ `has-margin-right-${ gutter }` ]: gutter > 0,
+				[ `has-margin-right-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+			}
+		);
+
+		const flickityOptions = {
+			autoPlay: autoPlay && autoPlaySpeed ? parseFloat( autoPlaySpeed ) : false,
+			draggable: draggable,
+			pageDots: pageDots,
+			prevNextButtons: prevNextButtons,
+			wrapAround: true,
+			cellAlign: alignCells ? 'left' : 'center',
+			pauseAutoPlayOnHover: pauseHover,
+			freeScroll: freeScroll,
+			arrowShape: {
+				x0: 10,
+				x1: 60, y1: 50,
+				x2: 65, y2: 45,
+				x3: 20,
+			},
+			thumbnails: thumbnails,
+			responsiveHeight: responsiveHeight,
+		};
+
+		const captionColorClass = getColorClassName( 'color', captionColor );
+
+		const captionClasses = classnames(
+			'coblocks-gallery--caption',
+			'coblocks-gallery--primary-caption',
+			captionColorClass, {
+				'has-caption-color': captionColorClass,
+
+			}
+		);
+
+		const navClasses = classnames(
+			'carousel-nav', {
+				[ `has-margin-top-${ gutter }` ]: gutter > 0,
+				[ `has-margin-top-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+				[ `has-negative-margin-left-${ gutter }` ]: gutter > 0,
+				[ `has-negative-margin-left-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+				[ `has-negative-margin-right-${ gutter }` ]: gutter > 0,
+				[ `has-negative-margin-right-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+			}
+		);
+
+		const navFigureClasses = classnames(
+			'coblocks--figure', {
+				[ `has-margin-left-${ gutter }` ]: gutter > 0,
+				[ `has-margin-left-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+				[ `has-margin-right-${ gutter }` ]: gutter > 0,
+				[ `has-margin-right-mobile-${ gutterMobile }` ]: gutterMobile > 0,
+			}
+		);
+
+		const navOptions = {
+			asNavFor: '.has-carousel',
+			autoPlay: false,
+			contain: true,
+			cellAlign: 'left',
+			pageDots: false,
+			thumbnails: false,
+			draggable: draggable,
+			prevNextButtons: false,
+			wrapAround: false,
+		};
+
+		const captionStyles = {
+			color: captionColorClass ? undefined : customCaptionColor,
+		};
+
+		// Return early if there are no images.
+		if ( images.length <= 0 ) {
+			return;
+		}
+
+		return (
+			<div className={ classes }>
+				<div
+					className={ innerClasses }
+					style={ innerStyles }
+				>
+					<div
+						className={ flickityClasses }
+						style={ responsiveHeight ? undefined : flickityStyles }
+						data-flickity={ JSON.stringify( flickityOptions ) }
+					>
+						{ images.map( ( image ) => {
+							const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ image.id ? `wp-image-${ image.id }` : null } />;
+
+							return (
+								<div key={ image.id || image.url } className="coblocks-gallery--item">
+									<figure className={ figureClasses }>
+										{ img }
+									</figure>
+								</div>
+							);
+						} ) }
+					</div>
+					{ thumbnails ?
+						<div
+							className={ navClasses }
+							data-flickity={ JSON.stringify( navOptions ) }
+						>
+							{ images.map( ( image ) => {
+								const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } />;
+								return (
+									<div key={ image.id || image.url } className="coblocks--item-thumbnail">
+										<figure className={ navFigureClasses }>
+											{ img }
+										</figure>
+									</div>
+								);
+							} ) }
+						</div> : null
+					}
+				</div>
+				{ ! RichText.isEmpty( primaryCaption ) && <RichText.Content tagName="figcaption" className={ captionClasses } value={ primaryCaption } style={ captionStyles } /> }
+			</div>
+		);
+	},
+},
+{
+	attributes: {
+		...GalleryAttributes,
+		...BackgroundAttributes,
+		...metadata.attributes,
+	},
+	save( { attributes, className } ) {
+		const {
+			autoPlay,
+			autoPlaySpeed,
+			captionColor,
+			customCaptionColor,
+			draggable,
+			freeScroll,
+			gridSize,
+			gutter,
+			gutterMobile,
+			height,
+			images,
+			pageDots,
+			pauseHover,
+			prevNextButtons,
+			primaryCaption,
+			alignCells,
+			thumbnails,
+			responsiveHeight,
+		} = attributes;
+
 		const innerClasses = classnames(
 			'is-cropped',
 			...GalleryClasses( attributes ),
