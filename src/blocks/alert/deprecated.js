a0052dfed9eebd6ffbb94c246318e4f8e401eb8e
Anthony Ledesma <30462574+paranoia1906@users.noreply.github.com>
2019-09-30 13:28:39 -0700

    convert Alert block to use alignment classnames (#880)
    * switch alert block to use classnames for text alignment
    
    * fix deprecated for alert
    
    * set attributes correctly

--- src/blocks/alert/deprecated.js
+++ src/blocks/alert/deprecated.js
@@ -11,7 +11,7 @@ const { RichText, getColorClassName } = wp.blockEditor;
 /**
  * Internal dependencies
  */
-import { attributes } from './';
+import { attributes } from './block.json';
 
 const deprecated = [
 	{
@@ -107,6 +107,58 @@ const deprecated = [
 						className={ textClasses }
 						value={ value }
 						style={ textStyles }
+					/>
+					}
+				</div>
+			);
+		},
+	},
+	{
+		attributes,
+		save( { attributes } ) {
+			const {
+				backgroundColor,
+				customBackgroundColor,
+				customTextColor,
+				textAlign,
+				textColor,
+				title,
+				value,
+			} = attributes;
+
+			const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+			const textClass = getColorClassName( 'color', textColor );
+
+			const classes = classnames( {
+				'has-text-color': textColor || customTextColor,
+				[ textClass ]: textClass,
+				'has-background': backgroundColor || customBackgroundColor,
+				[ backgroundClass ]: backgroundClass,
+			} );
+
+			const styles = {
+				backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+				color: textClass ? undefined : customTextColor,
+				textAlign: textAlign ? textAlign : null,
+			};
+
+			return (
+				<div
+					className={ classes }
+					style={ styles }
+				>
+					{ ! RichText.isEmpty( title ) &&
+					<RichText.Content
+						tagName="p"
+						className="wp-block-coblocks-alert__title"
+						value={ title }
+					/>
+					}
+					{ ! RichText.isEmpty( value ) &&
+					<RichText.Content
+						tagName="p"
+						className="wp-block-coblocks-alert__text"
+						value={ value }
 					/>
 					}
 				</div>
