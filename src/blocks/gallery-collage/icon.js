7de68360807672c95dc6e92a5ca76934d9cc87b0
Rich Tabor <hi@richtabor.com>
2019-09-30 15:34:29 -0400

    Update icon.js

--- src/blocks/gallery-collage/icon.js
+++ src/blocks/gallery-collage/icon.js
@@ -3,4 +3,4 @@
  */
 import { Path, SVG, G } from '@wordpress/components';
 
-export default <SVG xmlns="http://www.w3.org/2000/svg"><G fill="currentColor" fillRule="evenodd"><G fillRule="nonzero"><Path d="m18 3c1.1 0 2 .9 2 2v14c0 1.1-.9 2-2 2h-12c-1.1 0-2-.9-2-2v-14c0-1.1.9-2 2-2zm0 16v-14h-12v14zm3-14c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2zm0 12v-10zm-18 2c-1.1 0-2-.9-2-2v-10c0-1.1.9-2 2-2zm0-12v10zm10.7857143 6 3.2142857 5h-10l2.5-3.75 1.7857143 2.5083333z" /></G></G></SVG>;
+export default <SVG><G fill="currentColor" fillRule="nonzero"><Path d="m21 7c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2h-12c-1.1 0-2-.9-2-2v-12c0-1.1.9-2 2-2zm0 14v-12h-12v12zm-8.5-4.33 1.69 2.26 2.48-3.1 3.33 4.17h-10z"/><path d="m1 3c0-1.1.9-2 2-2h8c1.1 0 2 .9 2 2v8c0 1.1-.9 2-2 2h-1.96984863c-.01000276 0-.02005334-.0000771-.03015137-.0002309v-2.0503229l.05102539.0505538h1.94897461v-8h-8v2h-2z" transform="matrix(0 -1 1 0 0 14)"/><G><Path d="m17 5c0-1.1.9-2 2-2h4v2h-4v2h-2z"/><Path d="m3 19c0-1.1.9-2 2-2h4v2h-4v4h-2z"/></G></G></SVG>;
