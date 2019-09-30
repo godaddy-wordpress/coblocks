870b6e1ece040bed6ec197e97e14adfd5163c3fb
Rich Tabor <hi@richtabor.com>
2019-09-30 07:52:51 -0400

    Update index.js

--- src/blocks/gallery-carousel/index.js
+++ src/blocks/gallery-carousel/index.js
@@ -12,6 +12,7 @@ import edit from './edit';
 import icon from './icon';
 import metadata from './block.json';
 import save from './save';
+import transforms from './transforms';
 import { GalleryAttributes } from '../../components/block-gallery/shared';
 
 /**
