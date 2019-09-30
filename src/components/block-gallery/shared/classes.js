33ec0108efcb4a246a2ba96fbe7ed3eb34bc62a6
Rich Tabor <hi@richtabor.com>
2019-09-30 08:01:36 -0400

    Update classes.js

--- src/components/block-gallery/shared/classes.js
+++ src/components/block-gallery/shared/classes.js
@@ -28,12 +28,6 @@ function GalleryClasses( attributes ) {
 		{ [ `has-border-radius-${ radius }` ]: radius > 0 },
 		{ [ `has-filter-${ filter }` ]: filter !== 'none' },
 		{ [ `has-caption-style-${ captionStyle }` ]: captionStyle !== undefined },
-		{ 'has-caption-color': captionColorClass || customCaptionColor },
-		captionColorClass,
-		{ [ `has-background-border-radius-${ attributes.backgroundRadius }` ]: attributes.backgroundRadius > 0 },
-		{ 'has-padding': backgroundPadding > 0 },
-		{ [ `has-padding-${ backgroundPadding }` ]: backgroundPadding > 0 },
-		{ [ `has-padding-mobile-${ backgroundPaddingMobile }` ]: backgroundPaddingMobile > 0 },
 	];
 }
 
