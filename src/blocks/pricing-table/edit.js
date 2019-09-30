2851359aa63a6f105c667265fd3a2765634cc5d6
Anthony Ledesma <30462574+paranoia1906@users.noreply.github.com>
2019-09-30 13:33:06 -0700

    convert Pricing Table block to use alignment classnames (#882)
    * deprecate pricing table to use alignment classnames
    
    * properly import attributes to deprecated
    
    * add styles to handle alignmnet
    
    * removed superfluous code
    
    * removed superfluous styles

--- src/blocks/pricing-table/edit.js
+++ src/blocks/pricing-table/edit.js
@@ -58,7 +58,7 @@ class Edit extends Component {
 		const classes = classnames(
 			className,
 			`has-${ count }-columns`,
-			`has-${ contentAlign }-content`,
+			{ [ `has-text-align-${ contentAlign }` ]: contentAlign }
 		);
 
 		return (
@@ -70,7 +70,6 @@ class Edit extends Component {
 				) }
 				<div
 					className={ classes }
-					style={ { textAlign: contentAlign } }
 				>
 					<div className={ `${ className }__inner` }>
 						<InnerBlocks
--- src/blocks/pricing-table/index.js
+++ src/blocks/pricing-table/index.js
@@ -12,6 +12,7 @@ import icon from './icon';
 import metadata from './block.json';
 import save from './save';
 import transforms from './transforms';
+import deprecated from './deprecated';
 
 /**
  * WordPress dependencies
@@ -39,6 +40,7 @@ const settings = {
 	},
 	attributes,
 	transforms,
+	deprecated,
 	edit,
 	save,
 };
