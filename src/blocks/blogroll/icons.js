af81eaf4dbf65953e6d29710c5dc7443d0a16b7c
Rich Tabor <hi@richtabor.com>
2019-10-07 14:53:03 -0400

    Remove icons we're not using

--- src/blocks/blogroll/icons.js
+++ src/blocks/blogroll/icons.js
@@ -11,7 +11,6 @@ const icons = {};
 icons.listPositionLeft  = <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><Path d="m11 15v-10h-10v10zm2-6h6v-2h-6zm0 4h6v-2h-6z" /></SVG>;
 icons.listPositionRight = <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><Path d="m19 15v-10h-10v10zm-18-6h6v-2h-6zm0 4h6v-2h-6z" /></SVG>;
 
-// TODO add the proper SVGs for the code below
 icons.layoutGridIcon = (
 	<SVG
 		height="26"
@@ -28,26 +27,6 @@ icons.layoutGridIcon = (
 			<Path d="m41 18h6v.8785807 1.1214193h-6z" />
 			<Path d="m33 10h23v.8785807 1.1214193h-23z" />
 			<Path d="m36 14h17v.8785807 1.1214193h-17z" />
-		</G>
-	</SVG>
-);
-
-icons.layoutGridIconWithImages = (
-	<SVG
-		height="26"
-		viewBox="0 0 56 26"
-		width="56"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<G fillRule="evenodd">
-			<Path d="m0 0h24v14h-24z" />
-			<Path d="m3 16h18v.8785807 1.1214193h-18z" />
-			<Path d="m1 20h22v.8785807 1.1214193h-22z" />
-			<Path d="m4 24h16v.8785807 1.1214193h-16z" />
-			<Path d="m32 0h24v14h-24z" />
-			<Path d="m35 16h18v.8785807 1.1214193h-18z" />
-			<Path d="m33 20h22v.8785807 1.1214193h-22z" />
-			<Path d="m36 24h16v.8785807 1.1214193h-16z" />
 		</G>
 	</SVG>
 );
@@ -70,26 +49,6 @@ icons.layoutListIcon = (
 	</SVG>
 );
 
-icons.layoutListIconWithImages = (
-	<SVG
-		height="26"
-		viewBox="0 0 56 26"
-		width="56"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<G fill="currentColor" fillRule="evenodd">
-			<Path d="m10 0h10v10h-10z" />
-			<Path d="m22 0h14v.87858073 1.12141927h-14z" />
-			<Path d="m22 4h22v.87858073 1.12141927h-22z" />
-			<Path d="m22 8h16v.87858073 1.12141927h-16z" />
-			<Path d="m10 16h10v10h-10z" />
-			<Path d="m22 16h14v.8785807 1.1214193h-14z" />
-			<Path d="m22 20h22v.8785807 1.1214193h-22z" />
-			<Path d="m22 24h16v.8785807 1.1214193h-16z" />
-		</G>
-	</SVG>
-);
-
 icons.layoutCarouselIcon = (
 	<SVG
 		height="26"
@@ -104,26 +63,6 @@ icons.layoutCarouselIcon = (
 			<Path d="m14 16h18v.8785807 1.1214193h-18z" />
 			<Path d="m14 20h28v.8785807 1.1214193h-28z" />
 			<Path d="m14 24h20v.8785807 1.1214193h-20z" />
-		</G>
-	</SVG>
-);
-
-icons.layoutCarouselIconWithImages = (
-	<SVG
-		height="26"
-		viewBox="0 0 56 26"
-		width="56"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<G fill="currentColor" fillRule="evenodd">
-			<Path d="m10 0h10v10h-10z" />
-			<Path d="m22 0h14v.87858073 1.12141927h-14z" />
-			<Path d="m22 4h22v.87858073 1.12141927h-22z" />
-			<Path d="m22 8h16v.87858073 1.12141927h-16z" />
-			<Path d="m10 16h10v10h-10z" />
-			<Path d="m22 16h14v.8785807 1.1214193h-14z" />
-			<Path d="m22 20h22v.8785807 1.1214193h-22z" />
-			<Path d="m22 24h16v.8785807 1.1214193h-16z" />
 		</G>
 	</SVG>
 );
