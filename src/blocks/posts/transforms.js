8c8e512a5bd35167025d02bfd408e1e5748444c9
Rich Tabor <hi@richtabor.com>
2019-10-09 12:27:01 -0400

    Update transforms.js

--- src/blocks/posts/transforms.js
+++ src/blocks/posts/transforms.js
@@ -11,6 +11,41 @@ import { createBlock } from '@wordpress/blocks';
 const transforms = {
 	from: [
 		{
+			type: 'block',
+			blocks: [ 'core/latest-posts' ],
+			transform: ( { displayPostDate, displayPostContent, order, orderBy, categories, postsToShow, excerptLength, align } ) => (
+				createBlock( metadata.name, {
+					displayPostDate,
+					displayPostContent,
+					order,
+					orderBy,
+					categories,
+					postsToShow,
+					align,
+					excerptLength,
+				} )
+			),
+		},
+		{
+			type: 'block',
+			blocks: [ 'coblocks/post-carousel' ],
+			transform: ( { displayPostDate, displayPostContent, columns, order, orderBy, categories, postsToShow, externalRssUrl, postFeedType, excerptLength, align } ) => (
+				createBlock( metadata.name, {
+					displayPostDate,
+					displayPostContent,
+					columns,
+					order,
+					orderBy,
+					categories,
+					postsToShow,
+					externalRssUrl,
+					postFeedType,
+					align,
+					excerptLength,
+				} )
+			),
+		},
+		{
 			type: 'prefix',
 			prefix: ':posts',
 			transform: () => {
@@ -26,6 +61,24 @@ const transforms = {
 				} );
 			},
 		} ) ),
+	],
+	to: [
+		{
+			type: 'block',
+			blocks: [ 'core/latest-posts' ],
+			transform: ( { displayPostDate, displayPostContent, order, orderBy, categories, postsToShow, excerptLength, align } ) => (
+				createBlock( 'core/latest-posts', {
+					displayPostDate,
+					displayPostContent,
+					order,
+					orderBy,
+					categories,
+					postsToShow,
+					align,
+					excerptLength,
+				} )
+			),
+		},
 	],
 };
 
