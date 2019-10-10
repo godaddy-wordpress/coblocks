27b771d914bcbabbb07f15e27a4344ea3bfa770d
dzoniub <dzoniub@gmail.com>
2019-10-10 12:19:19 +0200

    Remove ignore pattern for lint

--- .dev/tests/jest/jest.config.js
+++ .dev/tests/jest/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	rootDir: '../../../',
 	testMatch: ['**/__tests__/**/*.js', '**/?(*.)(spec|test).js', '**/test/*.js'],
-	testPathIgnorePatterns: ['/.git/', '/node_modules/', '<rootDir>/build/', '/src/js/vendors/slick.js'],
+	testPathIgnorePatterns: ['/.git/', '/node_modules/', '<rootDir>/build/'],
 	setupFiles: [ '<rootDir>/.dev/tests/jest/setup-globals.js' ],
 	moduleNameMapper: {
 		'\\.(scss|css|less)$': '<rootDir>/.dev/tests/jest/styleMock.js',
