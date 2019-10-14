c56572e51ffa780c6b39b4de3446f85194d7cd9f
paranoia1906 <anthonymledesma@gmail.com>
2019-10-14 10:52:13 -0700

    setup style previews to use static icons

--- src/blocks/icon/edit.js
+++ src/blocks/icon/edit.js
@@ -27,18 +27,19 @@ const MAX_ICON_SIZE = 400;
 
 export { MIN_ICON_SIZE, MAX_ICON_SIZE };
 
-/**
- * Block edit function
- */
 class Edit extends Component {
-	componentDidMount() {
-		// Randomized the default icon when first added.
-		const defaultIcons = [ 'aperture', 'gesture', 'scatter_plot', 'waves', 'blocks', 'coblocks', 'drafts', 'device_hub', 'marker' ];
-		const rand = defaultIcons[ Math.floor( Math.random() * defaultIcons.length ) ];
+	isStylePreview() {
+		return ( $( `#block-${ this.props.clientId }` ).parent().parent().hasClass( 'block-editor-block-preview__content' ) );
+	}
 
-		// CHeck if the icon is the default.
-		if ( this.props.attributes.iconRand === true ) {
-			this.props.setAttributes( { icon: rand, iconRand: false } );
+	componentDidMount() {
+		if ( this.isStylePreview() ) {
+			this.props.setAttributes( { icon: 'aperture' } );
+		} else {
+			// Randomized the default icon when first added.
+			const defaultIcons = [ 'aperture', 'gesture', 'scatter_plot', 'waves', 'blocks', 'coblocks', 'drafts', 'device_hub', 'marker' ];
+			const rand = defaultIcons[ Math.floor( Math.random() * defaultIcons.length ) ];
+			this.props.setAttributes( { icon: rand } );
 		}
 	}
 
