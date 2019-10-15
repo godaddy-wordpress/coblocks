9abb61119641962e871fd5d1237b9e3d6e694f89
JR Tashjian <jrtashjian@gmail.com>
2019-10-15 11:31:49 -0400

    Improve Gif block stability through block validation testing (#997)

---
+++ src/blocks/gif/test/save.spec.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { registerBlockType, createBlock, serialize } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies.
+ */
+import { name, settings } from '../index';
+
+// Make variables accessible for all tests.
+let block;
+let serializedBlock;
+
+describe( name, () => {
+	beforeAll( () => {
+		// Register the block.
+		registerBlockType( name, { category: 'common', ...settings } );
+	} );
+
+	beforeEach( () => {
+		// Create the block with the minimum attributes.
+		block = createBlock( name );
+
+		// Reset the reused variables.
+		serializedBlock = '';
+	} );
+
+	it( 'should render', () => {
+		block.attributes.url = 'https://wordpress.com/wp-content/uploads/1234/56/image-1.gif';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'https://wordpress.com/wp-content/uploads/1234/56/image-1.gif' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+} );
