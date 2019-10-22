f8564f2a9522dda924b981dddbbc8a677e4bd8b4
JR Tashjian <jrtashjian@gmail.com>
2019-10-22 16:16:10 -0400

    Adding save tests since we don't have deprecations here

---
+++ src/blocks/social-profiles/test/save.spec.js
@@ -0,0 +1,107 @@
+/**
+ * External dependencies
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { registerBlockType, createBlock, serialize } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies.
+ */
+import metadata from '../block.json';
+import { name, settings } from '../index';
+
+// Make variables accessible for all tests.
+let block;
+let serializedBlock;
+
+describe( name, () => {
+	beforeAll( () => {
+		// Register the block.
+		registerBlockType( name, { category: 'common', ...settings, attributes: metadata.attributes } );
+	} );
+
+	beforeEach( () => {
+		// Create the block with the minimum attributes.
+		block = createBlock( name );
+
+		// Reset the reused variables.
+		serializedBlock = '';
+	} );
+
+	it( 'should not render without links', () => {
+		serializedBlock = serialize( createBlock( name ) );
+		expect( serializedBlock ).toEqual( `<!-- wp:${ name } /-->` );
+	} );
+
+	it( 'should render with Facebook link', () => {
+		const url = 'https://facebook.com';
+		block.attributes.facebook = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { facebook: url } ) );
+	} );
+
+	it( 'should render with Twitter link', () => {
+		const url = 'https://twitter.com';
+		block.attributes.twitter = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { twitter: url } ) );
+	} );
+
+	it( 'should render with Instagram link', () => {
+		const url = 'https://instagram.com';
+		block.attributes.instagram = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { instagram: url } ) );
+	} );
+
+	it( 'should render with Pinterest link', () => {
+		const url = 'https://pinterest.com';
+		block.attributes.pinterest = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { pinterest: url } ) );
+	} );
+
+	it( 'should render with LinkedIn link', () => {
+		const url = 'https://linkedin.com';
+		block.attributes.linkedin = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { linkedin: url } ) );
+	} );
+
+	it( 'should render with YouTube link', () => {
+		const url = 'https://youtube.com';
+		block.attributes.youtube = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { youtube: url } ) );
+	} );
+
+	it( 'should render with Yelp link', () => {
+		const url = 'https://yelp.com';
+		block.attributes.yelp = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { yelp: url } ) );
+	} );
+
+	it( 'should render with Houzz link', () => {
+		const url = 'https://houzz.com';
+		block.attributes.houzz = url;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( JSON.stringify( { houzz: url } ) );
+	} );
+} );
