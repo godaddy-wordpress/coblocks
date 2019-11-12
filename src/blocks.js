f6aaf5ac48c094df7cfbfd6336ae2987e0d120b5
Rich Tabor <hi@richtabor.com>
2019-11-12 11:31:29 -0500

    Add top/bottom waves to cover block

--- src/blocks.js
+++ src/blocks.js
@@ -21,6 +21,7 @@ import './extensions/list-styles';
 import './extensions/button-styles';
 import './extensions/button-controls';
 import './extensions/image-styles';
+import './extensions/cover-styles';
 import './extensions/image-crop';
 
 // Formats
