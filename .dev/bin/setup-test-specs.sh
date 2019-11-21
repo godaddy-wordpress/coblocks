bd9c9ed8662c4c54db8161895fa4a562e1a2bce5
Evan Herman <evan.m.herman@gmail.com>
2019-11-21 16:27:23 -0500

    test

--- .dev/bin/setup-test-specs.sh
+++ .dev/bin/setup-test-specs.sh
@@ -12,9 +12,9 @@ do
   # Changed file matches /src/blocks/*
   if [[ $FILE == *"src/blocks/"* ]]; then
     testSpec=$(echo $FILE | cut -d'/' -f3)
-    foundwords=$(echo ${SPECS[@]} | grep -o "${testSpec}" | wc -w)
     # There is a test file for the altered block
     if [ -f "src/blocks/${testSpec}/test/${testSpec}.cypress.js" ]; then
+      foundwords=$(echo ${SPECS[@]} | grep -o "${testSpec}" | wc -w)
       # The test spec does not yet exist in the SPECS array
       if [[ "${foundwords}" -eq 0 ]]; then
         # Spec file string is empty, do not start string with a ,
@@ -23,7 +23,7 @@ do
         else
           SPECSTRING="${SPECSTRING},src/blocks/${testSpec}/test/${testSpec}.cypress.js"
         fi
-        SPECS=( "${SPECS[@]}" "src/blocks/${testSpec}/test/${testSpec}.cypress.js" )
+        SPECS=( "${SPECS[@]}" "${testSpec}" )
       fi
     fi
   fi
@@ -35,6 +35,6 @@ fi
 
 GREEN='\033[0;33m'
 NC='\033[0m'
-echo "\n${GREEN}Running the following Cypress spec files: ${SPECS[@]}${NC}\n"
+printf "\n${GREEN}Running the following Cypress spec files: ${SPECS[@]}${NC}\n"
 
 source $BASH_ENV
