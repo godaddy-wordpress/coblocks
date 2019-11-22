5d8348e04e37278fda145375d4e7c4ca50c5f193
Rich Tabor <hi@richtabor.com>
2019-11-22 13:46:03 -0500

    Add icons

--- src/blocks/form/layouts.js
+++ src/blocks/form/layouts.js
@@ -1,11 +1,12 @@
 /**
+ * Internal dependencies
+ */
+import icons from './icons';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import {
-	SVG,
-	Path,
-} from '@wordpress/components';
 
 /**
  * Template option choices for predefined columns layouts.
@@ -16,7 +17,7 @@ import {
 export const TEMPLATE_OPTIONS = [
 	{
 		title: __( 'Contact', 'coblocks' ),
-		icon: <SVG width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><Path fillRule="evenodd" clipRule="evenodd" d="M39 12C40.1046 12 41 12.8954 41 14V34C41 35.1046 40.1046 36 39 36H9C7.89543 36 7 35.1046 7 34V14C7 12.8954 7.89543 12 9 12H39ZM39 34V14H25V34H39ZM23 34H9V14H23V34Z" /></SVG>,
+		icon: icons.layoutContact,
 		template: [
 			[ 'coblocks/field-name', { required: false } ],
 			[ 'coblocks/field-email', { required: true } ],
@@ -26,21 +27,21 @@ export const TEMPLATE_OPTIONS = [
 	},
 	{
 		title: __( 'RSVP', 'coblocks' ),
-		icon: <SVG width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><Path fillRule="evenodd" clipRule="evenodd" d="M39 12C40.1046 12 41 12.8954 41 14V34C41 35.1046 40.1046 36 39 36H9C7.89543 36 7 35.1046 7 34V14C7 12.8954 7.89543 12 9 12H39ZM39 34V14H20V34H39ZM18 34H9V14H18V34Z" /></SVG>,
+		icon: icons.layoutRSVP,
 		template: [
 			[ 'coblocks/field-name', { required: true, hasLastName: true } ],
-			/* translators: a person's guest at a social function. */
+			/* translators: an additional guest */
 			[ 'coblocks/field-name', { label: __( 'Plus one', 'coblocks' ), hasLastName: true } ],
 			[ 'coblocks/field-email', { required: true } ],
 			[ 'coblocks/field-radio', { label: __( 'Will you be attending?', 'coblocks' ), required: true, options: [ __( 'Yes', 'coblocks' ), __( 'No', 'coblocks' ) ], isInline: true } ],
-			[ 'coblocks/field-textarea', { label: __( 'Details', 'coblocks' ) } ],
+			[ 'coblocks/field-textarea', { label: __( 'Notes?', 'coblocks' ) } ],
 		],
-		/* translators: RSVP is an initialism derived from the French phrase Répondez s'il vous plaît, meaning "Please respond" to require confirmation of an invitation. */
+		/* translators: RSVP is an initialism derived from the French phrase Répondez s'il vous plaît, meaning "Please respond" to require confirmation of an invitation */
 		submitButtonText: __( 'RSVP', 'coblocks' ),
 	},
 	{
 		title: __( 'Appointment', 'coblocks' ),
-		icon: <SVG width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><Path fillRule="evenodd" clipRule="evenodd" d="M39 12C40.1046 12 41 12.8954 41 14V34C41 35.1046 40.1046 36 39 36H9C7.89543 36 7 35.1046 7 34V14C7 12.8954 7.89543 12 9 12H39ZM39 34V14H30V34H39ZM28 34H9V14H28V34Z" /></SVG>,
+		icon: icons.layoutAppointment,
 		template: [
 			[ 'coblocks/field-name', { required: true, hasLastName: true } ],
 			[ 'coblocks/field-phone', { required: true } ],
