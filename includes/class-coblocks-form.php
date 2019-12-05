4571b5ffd7d8a960de480a56ad51579b18660bd8
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2019-12-05 15:47:25 -0700

    Use descriptive element name for Form block radio buttons (#1023)
    * add  variable to use in markup
    
    * format the field name into markup
    
    * adjust label logic and pass slug from radio render
    
    * improve descriptive labels on radio buttons
    
    * fix radio field test to look for new radio label

--- includes/class-coblocks-form.php
+++ includes/class-coblocks-form.php
@@ -440,7 +440,8 @@ class CoBlocks_Form {
 		$the_options = array_filter( $atts['options'] );
 
 		$label      = isset( $atts['label'] ) ? $atts['label'] : __( 'Choose one', 'coblocks' );
-		$label_slug = $radio_count > 1 ? sanitize_title( $label . '-' . $radio_count ) : sanitize_title( $label );
+		$label_desc = sanitize_title( $label ) !== 'choose-one' ? sanitize_title( $label ) : 'radio';
+		$label_slug = $radio_count > 1 ? sanitize_title( $label_desc . '-' . $radio_count ) : sanitize_title( $label_desc );
 
 		ob_start();
 
