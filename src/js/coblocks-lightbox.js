676a2479bb1f2d51348d32ead3cd3c8fd21275ca
JR Tashjian <jrtashjian@gmail.com>
2019-12-05 14:52:17 -0500

    Prevent carousel nav images from being lightboxed.

--- src/js/coblocks-lightbox.js
+++ src/js/coblocks-lightbox.js
@@ -22,7 +22,7 @@
 		const arrowRight = $( '<div/>', { class: 'arrow-right' } );
 		const arrowLeft = $( '<div/>', { class: 'arrow-left' } );
 
-		const images = $( `.has-lightbox.lightbox-${ lightboxIndex } figure img` );
+		const images = $( `.has-lightbox.lightbox-${ lightboxIndex } > :not(.carousel-nav) figure img` );
 		let index;
 
 		modalHeading.append( counter, close );
