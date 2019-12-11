85b9deb501f8376e08e9c01e769186627db35251
AnthonyLedesma <anthonymledesma@gmail.com>
2019-12-11 10:45:20 -0700

    improve loop logic

--- src/blocks/accordion/test/transforms.spec.js
+++ src/blocks/accordion/test/transforms.spec.js
@@ -16,7 +16,7 @@ describe( 'coblocks/accordion transforms', () => {
 	} );
 
 	// Should allow transform when prefixed with 1-5 colons.
-	for ( let i = 1; i < 6; i++ ) {
+	for ( let i = 1; i <= 5; i++ ) {
 		const prefix = Array( i + 1 ).join( ':' ) + 'accordion';
 		it( `should transform when ${ prefix } prefix is seen`, () => {
 			const block = helpers.performPrefixTransformation( name, prefix, prefix );
