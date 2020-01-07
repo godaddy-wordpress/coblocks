e59b56b6528f1feb9062f2d5c340e3b92e32f5ca
AnthonyLedesma <anthonymledesma@gmail.com>
2020-01-07 08:56:00 -0700

    fix offset image links

--- src/blocks/gallery-offset/save.js
+++ src/blocks/gallery-offset/save.js
@@ -86,7 +86,7 @@ const save = ( { attributes, className } ) => {
 							break;
 					}
 
-					const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-link={ image.link } className={ imgClasses } />;
+					const img = <img src={ image.url } alt={ image.alt } data-id={ image.id } data-imglink={ image.imgLink } data-link={ image.link } className={ imgClasses } />;
 
 					return (
 						<li key={ image.id || image.url } className="coblocks-gallery--item">
