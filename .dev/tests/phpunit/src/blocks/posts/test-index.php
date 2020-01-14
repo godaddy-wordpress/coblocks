163d918757d4a801c56bbc1b3b90604fa02ef115
Evan Herman <evan.m.herman@gmail.com>
2020-01-14 10:08:03 -0500

    Fix test_coblocks_render_posts_block unit test

--- .dev/tests/phpunit/src/blocks/posts/test-index.php
+++ .dev/tests/phpunit/src/blocks/posts/test-index.php
@@ -63,7 +63,7 @@ class CoBlocks_Posts_Index_Tests extends WP_UnitTestCase {
 		global $post;
 		$post = get_post( $post_id );
 
-		$this->assertEquals( '<div class="wp-block-coblocks-posts test-class-name"><div class="wp-block-coblocks-posts__inner has-columns has-2-columns has-responsive-columns has-medium-gutter"></div></div>', coblocks_render_posts_block( $attributes ) );
+		$this->assertEquals( '<div class="test-class-name"><div class="wp-block-coblocks-posts__inner"></div></div>', coblocks_render_posts_block( $attributes ) );
 	}
 
 	/**
