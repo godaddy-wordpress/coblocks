a85c3e6645c24810674db8681b74dbbe2bb09d9c
Rich Tabor <hi@richtabor.com>
2020-01-22 06:29:09 -0500

    Update HeadingToolbar to work with G7.2 and core

--- src/components/heading-toolbar/index.js
+++ src/components/heading-toolbar/index.js
@@ -4,6 +4,11 @@
 import { range } from 'lodash';
 
 /**
+ * Internal dependencies.
+ */
+import HeadingLevelIcon from './icon';
+
+/**
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
@@ -12,16 +17,14 @@ import { Toolbar } from '@wordpress/components';
 
 class HeadingToolbar extends Component {
 	createLevelControl( targetLevel, selectedLevel, onChange ) {
+		const isActive = targetLevel === selectedLevel;
+
 		return {
-			icon: 'heading',
-			title: sprintf(
-				/* translators: %d: heading level e.g: "1", "2", "3" */
-				__( 'Heading %d', 'coblocks' ),
-				targetLevel
-			),
-			isActive: targetLevel === selectedLevel,
+			icon: <HeadingLevelIcon level={ targetLevel } isPressed={ isActive } />,
+			// translators: %s: heading level e.g: "1", "2", "3"
+			title: sprintf( __( 'Heading %d' ), targetLevel ),
+			isActive,
 			onClick: () => onChange( targetLevel ),
-			subscript: String( targetLevel ),
 		};
 	}
 
@@ -29,6 +32,8 @@ class HeadingToolbar extends Component {
 		const { minLevel, maxLevel, selectedLevel, onChange } = this.props;
 		return (
 			<Toolbar
+				// isCollapsed={ isCollapsed }
+				icon={ <HeadingLevelIcon level={ selectedLevel } /> }
 				controls={ range( minLevel, maxLevel ).map( index =>
 					this.createLevelControl( index, selectedLevel, onChange )
 				) }
