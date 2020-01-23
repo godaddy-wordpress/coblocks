d777f544ae0e3298935129f823f08fe7349b298e
Evan Herman <evan.m.herman@gmail.com>
2020-01-23 16:23:19 -0500

    Tweak

--- src/blocks/icon/test/icon.cypress.js
+++ src/blocks/icon/test/icon.cypress.js
@@ -108,7 +108,7 @@ describe( 'Test CoBlocks Icon Block', function() {
 
 		cy.get( '.wp-block-coblocks-icon__inner a' )
 			.should( 'have.attr', 'href', 'https://www.google.com' )
-			.and( 'have.attr', 'rel', 'link rel noopener noreferrer' )
+			.and( 'have.attr', 'rel', 'Link Rel noopener noreferrer' )
 			.and( 'have.attr', 'target', '_blank' );
 
 		helpers.editPage();
