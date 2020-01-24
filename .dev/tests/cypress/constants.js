b1dde8f0d14bcdc2f80d8e181901db34f9137889
JR Tashjian <jrtashjian@gmail.com>
2020-01-24 00:16:58 -0500

    Cypress should fallback to Docker environment config if wp_creds.json override file does not exist

--- .dev/tests/cypress/constants.js
+++ .dev/tests/cypress/constants.js
@@ -1,6 +1,18 @@
-var wpCreds     = require( './wp_creds.json' ),
-    wpUsername  = wpCreds.wpUsername,
-    wpPassword  = wpCreds.wpPassword,
-    testURL     = wpCreds.testURL;
+const fs = require( 'fs' );
+const path = require( 'path' );
 
-export { wpUsername, wpPassword, testURL };
+// Default login for Docker environment.
+let wpCreds = {
+	testURL: 'http://localhost:8889',
+	wpUsername: 'admin',
+	wpPassword: 'password',
+};
+
+try {
+	const configFilePath = path.resolve( process.cwd(), 'wp_creds.json' );
+
+	fs.accessSync( configFilePath, fs.constants.R_OK );
+	wpCreds = require( configFilePath );
+} catch ( err ) {}
+
+export default wpCreds;
