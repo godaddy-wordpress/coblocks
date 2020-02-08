57adf00d9c5c941668341a26b9cea07e34fd2d3e
JR Tashjian <jrtashjian@gmail.com>
2020-02-08 15:14:59 -0700

    Simplify install-dependencies script

--- .dev/bin/install-dependencies.sh
+++ .dev/bin/install-dependencies.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-sudo apt-get update && sudo apt-get install subversion
+sudo apt-get update
+sudo apt-get install subversion mysql-client-5.7
 sudo -E docker-php-ext-install mysqli
 sudo sh -c "printf '\ndeb http://ftp.us.debian.org/debian sid main\n' >> /etc/apt/sources.list"
-sudo apt-get update && sudo apt-get install mysql-client-5.7
-
-composer install
 
 .dev/bin/install-wp-tests.sh wordpress_test root '' 127.0.0.1 latest
