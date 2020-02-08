efe00f983ce079e53b23ffc9addae4589fff63ee
JR Tashjian <jrtashjian@gmail.com>
2020-02-08 15:25:50 -0700

    remove mysql-client version

--- .dev/bin/install-dependencies.sh
+++ .dev/bin/install-dependencies.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install subversion mysql-client-5.7
+sudo apt-get install subversion mysql-client
 sudo -E docker-php-ext-install mysqli
-sudo sh -c "printf '\ndeb http://ftp.us.debian.org/debian sid main\n' >> /etc/apt/sources.list"
 
 .dev/bin/install-wp-tests.sh wordpress_test root '' 127.0.0.1 latest
