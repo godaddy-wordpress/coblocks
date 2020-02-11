035334c9ad5904f6fa6c87b3bed05a03903c1a10
Evan Herman <evan.m.herman@gmail.com>
2020-02-11 17:02:28 -0500

    Add food-and-drink block save tests

--- src/blocks/food-and-drinks/test/save.spec.js
+++ src/blocks/food-and-drinks/test/save.spec.js
@@ -33,4 +33,31 @@ describe( name, () => {
 		expect( serializedBlock ).toBeDefined();
 		expect( serializedBlock ).toMatchSnapshot();
 	} );
+
+	it( 'should render hide images', () => {
+		block.attributes.showImages = 0;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{\"showImages\":0}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render hide prices', () => {
+		block.attributes.showPrices = 0;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{\"showPrices\":0}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render 3 columns', () => {
+		block.attributes.columns = 3;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'data-columns=\"3\"' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
 } );
