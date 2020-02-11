6a5505c8d90bb28f95e8d52d5a030bdb99568259
Evan Herman <evan.m.herman@gmail.com>
2020-02-11 16:46:58 -0500

    Add features save.spec tests

--- src/blocks/features/test/save.spec.js
+++ src/blocks/features/test/save.spec.js
@@ -33,4 +33,49 @@ describe( name, () => {
 		expect( serializedBlock ).toBeDefined();
 		expect( serializedBlock ).toMatchSnapshot();
 	} );
+
+	it( 'should render heading h3', () => {
+		block.attributes.headingLevel = 3;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{\"headingLevel\":3}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render gutter 3 columns', () => {
+		block.attributes.columns = 3;
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'has-3-columns' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render gutter large', () => {
+		block.attributes.gutter = 'large';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'has-large-gutter' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render padding large', () => {
+		block.attributes.paddingSize = 'large';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'has-large-padding' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render margin huge', () => {
+		block.attributes.marginSize = 'huge';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'has-huge-margin' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
 } );
