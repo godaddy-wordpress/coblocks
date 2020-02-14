2a060145a9980fd228d52d7af620e2950b37de7c
JR Tashjian <jrtashjian@gmail.com>
2020-02-14 16:27:57 -0500

    Move function back into test.

--- src/blocks/logos/test/logos.cypress.js
+++ src/blocks/logos/test/logos.cypress.js
@@ -13,19 +13,6 @@ describe( 'Test CoBlocks Logos Block', function () {
 		pathToFixtures: '../.dev/tests/cypress/fixtures/images/',
 	};
 
-	const selectFromMediaLibrary = () => {
-		cy.get( '@targetElement' ).contains( /media library/i ).click();
-
-		// 1. Select Media Library tab.
-		cy.get( '.media-modal-content' ).find( '#menu-item-browse' ).click();
-		// 2. Select first item in Media Library list.
-		cy.get( '.media-modal-content' ).find( 'li.attachment' ).first( 'li' ).click();
-		// 3. Click "Create New Gallery" or "Add to Gallery" button.
-		cy.get( '.media-toolbar-primary .media-button.button-primary' ).click();
-		// 4. Click "Insert Gallery" or "Update Gallery" button.
-		cy.get( '.media-toolbar-primary .media-button.button-primary' ).click();
-	};
-
 	before( () => {
 		helpers.addCoBlocksBlockToPage( true, 'logos' );
 	} );
@@ -71,7 +58,16 @@ describe( 'Test CoBlocks Logos Block', function () {
 	 * to successfully save the block without errors.
 	 */
 	it( 'Test logos block saves with image from media library.', function () {
-		selectFromMediaLibrary();
+		cy.get( '@targetElement' ).contains( /media library/i ).click();
+
+		// 1. Select Media Library tab.
+		cy.get( '.media-modal-content' ).find( '#menu-item-browse' ).click();
+		// 2. Select first item in Media Library list.
+		cy.get( '.media-modal-content' ).find( 'li.attachment' ).first( 'li' ).click();
+		// 3. Click "Create New Gallery" or "Add to Gallery" button.
+		cy.get( '.media-toolbar-primary .media-button.button-primary' ).click();
+		// 4. Click "Insert Gallery" or "Update Gallery" button.
+		cy.get( '.media-toolbar-primary .media-button.button-primary' ).click();
 
 		helpers.checkForBlockErrors( 'logos' );
 
