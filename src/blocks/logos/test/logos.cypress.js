769fdac0d97a5fd1f7fdebf8930a61e15b67c55a
JR Tashjian <jrtashjian@gmail.com>
2020-02-14 16:55:37 -0500

    Revert "Use original selector for clicking the first attachment"
    This reverts commit 80613aa92f37ca8d8eb873ef857288df904784a8.

--- src/blocks/logos/test/logos.cypress.js
+++ src/blocks/logos/test/logos.cypress.js
@@ -19,7 +19,7 @@ describe( 'Test CoBlocks Logos Block', function () {
 		// 1. Select Media Library tab.
 		cy.get( '.media-modal-content' ).find( '#menu-item-browse' ).click();
 		// 2. Select first item in Media Library list.
-		cy.get( '.media-modal-content' ).find( 'li.attachment' ).first( 'li' ).click();
+		cy.get( '.media-modal-content' ).find( '.attachment' ).first().click();
 		// 3. Click "Create New Gallery" or "Add to Gallery" button.
 		cy.get( '.media-toolbar-primary .media-button.button-primary' ).click();
 		// 4. Click "Insert Gallery" or "Update Gallery" button.
