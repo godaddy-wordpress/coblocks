e86d340b5780229f0a818fb29bbe5306515a9557
Rich Tabor <hi@richtabor.com>
2020-02-18 21:27:25 -0500

    Rename CoBlocks Galleries category to "Galleries"

--- src/utils/block-category.js
+++ src/utils/block-category.js
@@ -10,19 +10,9 @@ import { __, sprintf } from '@wordpress/i18n';
 import brandAssets from './brand-assets';
 
 setCategories( [
-	// {
-	// 	slug: 'coblocks',
-	// 	title: 'CoBlocks',
-	// 	icon: brandAssets.categoryIcon,
-	// },
 	{
 		slug: 'coblocks-galleries',
-		title: sprintf(
-			/* translators: %s: Plugin title "CoBlocks" */
-			__( '%s Galleries', 'coblocks' ),
-			'CoBlocks'
-		),
-		icon: brandAssets.categoryIcon,
+		title: __( 'Galleries', 'coblocks' ),
 	},
 	...getCategories().filter( ( { slug } ) => slug !== 'coblocks-galleries' ),
 ] );
