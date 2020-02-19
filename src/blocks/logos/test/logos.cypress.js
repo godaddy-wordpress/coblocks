e9f43c83b5acd9c958261e4468e2e2d409d6c8e6
JR Tashjian <jrtashjian@gmail.com>
2020-02-19 17:59:24 -0500

    Introduce Black & white and Grayscale Logos block styles (#1358)
    * Eslint fixes
    
    * stylelint fixes
    
    * Introduce black-and-white and grayscale block styles
    
    * Ensure className exists before indexOf()
    
    * Add deprecation tests
    
    * Add save tests for new styles
    
    * Adding new end to end tests
    
    * Improve end-to-end test
    
    * Modify attachment selector
    
    * Try without cy.within()
    
    * Wait for .media-modal-content
    
    * Use original selector for clicking the first attachment
    
    * Move function back into test.
    
    * Revert "Move function back into test."
    
    This reverts commit 2a060145a9980fd228d52d7af620e2950b37de7c.
    
    * Revert "Use original selector for clicking the first attachment"
    
    This reverts commit 80613aa92f37ca8d8eb873ef857288df904784a8.
    
    * Revert "Wait for .media-modal-content"
    
    This reverts commit 5e102b72a95fce98f5b012e1be3157553d0f2f15.
    
    * Revert "Try without cy.within()"
    
    This reverts commit 00b0493734e3a0f54b68727cf2afffa7358cf861.
    
    * Revert "Modify attachment selector"
    
    This reverts commit 3d6db20c093ace48012d3d364e4729e5e1ee2d2c.
    
    * Change order of styles
    
    * Revert "Move function back into test."
    
    This reverts commit 2a060145a9980fd228d52d7af620e2950b37de7c.
    
    * Revert "Use original selector for clicking the first attachment"
    
    This reverts commit 80613aa92f37ca8d8eb873ef857288df904784a8.
    
    * Revert "Wait for .media-modal-content"
    
    This reverts commit 5e102b72a95fce98f5b012e1be3157553d0f2f15.
    
    * Revert "Try without cy.within()"
    
    This reverts commit 00b0493734e3a0f54b68727cf2afffa7358cf861.
    
    * Revert "Modify attachment selector"
    
    This reverts commit 3d6db20c093ace48012d3d364e4729e5e1ee2d2c.
    
    * Revert "Improve end-to-end test"
    
    This reverts commit 2403fa6af2134a727d9c16f1d8e8e0e50017cf50.
    
    Co-authored-by: Rich Tabor <hello@themebeans.com>

--- src/blocks/logos/test/logos.cypress.js
+++ src/blocks/logos/test/logos.cypress.js
@@ -1,12 +1,12 @@
-/*
+/**
  * Include our constants
  */
 import * as helpers from '../../../../.dev/tests/cypress/helpers';
 
-describe( 'Test CoBlocks Logos Block', function() {
+describe( 'Test CoBlocks Logos Block', function () {
 	/**
-	   * Setup Logos data
-	   */
+	 * Setup Logos data
+	 */
 	const logosData = {
 		fileName: '150x150.png',
 		imageBase: '150x150',
@@ -14,10 +14,10 @@ describe( 'Test CoBlocks Logos Block', function() {
 	};
 
 	/**
-	   * Test that we can add a logos block to the content, not add any images or
-	   * alter any settings, and are able to successfully save the block without errors.
-	   */
-	it( 'Test logos block saves with empty values.', function() {
+	 * Test that we can add a logos block to the content, not add any images or
+	 * alter any settings, and are able to successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with empty values.', function () {
 		helpers.addCoBlocksBlockToPage( true, 'logos' );
 
 		helpers.savePage();
@@ -32,10 +32,10 @@ describe( 'Test CoBlocks Logos Block', function() {
 	} );
 
 	/**
-	   * Test that we can upload images to block and are able
-	   * to successfully save the block without errors.
-	   */
-	it( 'Test logos block saves with image upload.', function() {
+	 * Test that we can upload images to block and are able
+	 * to successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with image upload.', function () {
 		const { fileName, imageBase, pathToFixtures } = logosData;
 		helpers.addCoBlocksBlockToPage( true, 'logos' );
 
@@ -66,10 +66,10 @@ describe( 'Test CoBlocks Logos Block', function() {
 	} );
 
 	/**
-	   * Test that we can add image from library and are able
-	   * to successfully save the block without errors.
-	   */
-	it( 'Test logos block saves with image from media library.', function() {
+	 * Test that we can add image from library and are able
+	 * to successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with image from media library.', function () {
 		helpers.addCoBlocksBlockToPage( true, 'logos' );
 
 		cy.get( '.wp-block[data-type="coblocks/logos"]' )
@@ -100,15 +100,11 @@ describe( 'Test CoBlocks Logos Block', function() {
 	} );
 
 	/**
-       * Test that we can add image filter and
-       * successfully save the block without errors.
-       */
-	it( 'Test logos block saves with black and white filter.', function() {
+	 * Test that we can add "black and white" image filter and
+	 * successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with black and white filter.', function () {
 		helpers.addCoBlocksBlockToPage( true, 'logos' );
-
-		helpers.toggleSettingCheckbox( /black & white/i );
-
-		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'has-filter-grayscale' );
 
 		cy.get( '.wp-block[data-type="coblocks/logos"]' )
 			.click()
@@ -125,13 +121,91 @@ describe( 'Test CoBlocks Logos Block', function() {
 
 		cy.get( 'button' ).contains( /insert gallery/i ).click();
 
+		helpers.setBlockStyle( /black & white/i );
+
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-black-and-white' );
+
 		helpers.savePage();
 
 		helpers.checkForBlockErrors( 'logos' );
 
 		helpers.viewPage();
 
-		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'has-filter-grayscale' );
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-black-and-white' );
+
+		helpers.editPage();
+	} );
+
+	/**
+	 * Test that we can add image "grayscale" filter and
+	 * successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with grayscale filter.', function () {
+		helpers.addCoBlocksBlockToPage( true, 'logos' );
+
+		cy.get( '.wp-block[data-type="coblocks/logos"]' )
+			.click()
+			.contains( /media library/i )
+			.click();
+
+		cy.get( '.media-modal-content' ).contains( /media library/i ).click();
+
+		cy.get( '.media-modal-content' ).find( 'li.attachment' )
+			.first( 'li' )
+			.click();
+
+		cy.get( 'button' ).contains( /create a new gallery/i ).click();
+
+		cy.get( 'button' ).contains( /insert gallery/i ).click();
+
+		helpers.setBlockStyle( /grayscale/i );
+
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-grayscale' );
+
+		helpers.savePage();
+
+		helpers.checkForBlockErrors( 'logos' );
+
+		helpers.viewPage();
+
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-grayscale' );
+
+		helpers.editPage();
+	} );
+
+	/**
+	 * Test that we can add image "default" filter and
+	 * successfully save the block without errors.
+	 */
+	it( 'Test logos block saves with default filter.', function () {
+		helpers.addCoBlocksBlockToPage( true, 'logos' );
+
+		cy.get( '.wp-block[data-type="coblocks/logos"]' )
+			.click()
+			.contains( /media library/i )
+			.click();
+
+		cy.get( '.media-modal-content' ).contains( /media library/i ).click();
+
+		cy.get( '.media-modal-content' ).find( 'li.attachment' )
+			.first( 'li' )
+			.click();
+
+		cy.get( 'button' ).contains( /create a new gallery/i ).click();
+
+		cy.get( 'button' ).contains( /insert gallery/i ).click();
+
+		helpers.setBlockStyle( /default/i );
+
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-default' );
+
+		helpers.savePage();
+
+		helpers.checkForBlockErrors( 'logos' );
+
+		helpers.viewPage();
+
+		cy.get( '.wp-block-coblocks-logos' ).should( 'have.class', 'is-style-default' );
 
 		helpers.editPage();
 	} );
