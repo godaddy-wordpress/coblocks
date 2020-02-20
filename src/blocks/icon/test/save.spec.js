e0e8f9347f18c4fafabcd13fb0881dbde7770dad
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2020-02-20 10:45:11 -0700

    Introduce Jest tests [Phase 1] (#1350)
    * stage with changes from phase 4
    
    * add Author block tests
    
    * add tests for buttons block
    
    * add click-to-tweet tests
    
    * add dynamic-separator tests
    
    * add Form block tests
    
    * add icon block tests
    
    * add share block tests
    
    * add social-profiles tests
    
    * add tests for events block
    
    * lint all tests
    
    * tweak author tests
    
    * update author test names
    
    * cypress helper match master
    
    * update phase 1 tests

--- src/blocks/icon/test/save.spec.js
+++ src/blocks/icon/test/save.spec.js
@@ -13,7 +13,7 @@ import { name, settings } from '../index';
 let block;
 let serializedBlock;
 
-describe( name, () => {
+describe( 'coblocks/icon', () => {
 	beforeAll( () => {
 		// Register the block.
 		registerBlockType( name, { category: 'common', ...settings } );
@@ -33,6 +33,86 @@ describe( name, () => {
 
 		expect( serializedBlock ).toBeDefined();
 		expect( serializedBlock ).toContain( '"icon":"coblocks"' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with height and width', () => {
+		block.attributes.height = '200';
+		block.attributes.width = '200';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '"height":"200"' );
+		expect( serializedBlock ).toContain( '"width":"200"' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with borderRadius and padding', () => {
+		block.attributes.borderRadius = '200';
+		block.attributes.padding = '30';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '"borderRadius":"200"' );
+		expect( serializedBlock ).toContain( '"padding":"30"' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with href, rel, and linkTarget', () => {
+		block.attributes.href = 'https://wordpress.org';
+		block.attributes.rel = 'rel';
+		block.attributes.linkTarget = '_blank';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( 'href="https://wordpress.org"' );
+		expect( serializedBlock ).toContain( 'rel="rel"' );
+		expect( serializedBlock ).toContain( 'target="_blank"' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with iconColor', () => {
+		block.attributes.iconColor = 'primary';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{"iconColor":"primary"}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with customIconColor', () => {
+		block.attributes.customIconColor = '#da5d5d';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{"customIconColor":"#da5d5d"}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with backgroundColor', () => {
+		block.attributes.backgroundColor = 'primary';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{"backgroundColor":"primary"}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with customBackgroundColor', () => {
+		block.attributes.customBackgroundColor = '#da5d5d';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{"customBackgroundColor":"#da5d5d"}' );
+		expect( serializedBlock ).toMatchSnapshot();
+	} );
+
+	it( 'should render with className', () => {
+		block.attributes.className = 'my-custom-class';
+		serializedBlock = serialize( block );
+
+		expect( serializedBlock ).toBeDefined();
+		expect( serializedBlock ).toContain( '{"className":"my-custom-class"}' );
 		expect( serializedBlock ).toMatchSnapshot();
 	} );
 } );
