e0d9134552d3726ee8fcf73dcaf9cc4379435ca9
JR Tashjian <jrtashjian@gmail.com>
2020-02-26 08:59:35 -0500

    phpcs fix

--- includes/class-coblocks-block-assets.php
+++ includes/class-coblocks-block-assets.php
@@ -336,7 +336,7 @@ class CoBlocks_Block_Assets {
 		// Define where the vendor asset is loaded from.
 		$vendors_dir = CoBlocks()->asset_source( 'js', 'vendors' );
 
-		// Required by the events block
+		// Required by the events block.
 		wp_enqueue_script(
 			'coblocks-slick',
 			$vendors_dir . '/slick.js',
