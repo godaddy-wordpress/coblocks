fe4fd5f78ee17788eac3649dd51f41e51ebfe11b
JR Tashjian <jrtashjian@gmail.com>
2020-02-27 17:42:41 -0500

    Add Food and Drinks deprecations

---
+++ src/blocks/food-and-drinks/test/deprecated.spec.js
@@ -0,0 +1,16 @@
+/**
+ * Internal dependencies.
+ */
+import * as helpers from '../../../../.dev/tests/jest/helpers';
+import { name, settings } from '../index';
+
+const variations = {
+	align: [ undefined, 'wide' ],
+	className: [ undefined, '', 'random classes' ],
+	columns: [ 2, 3, 4 ],
+	gutter: [ 'small', 'medium', 'large', 'xlarge' ],
+	showImages: [ undefined, true, false ],
+	showPrices: [ undefined, true, false ],
+};
+
+helpers.testDeprecatedBlockVariations( name, settings, variations );
