198ee52c83373b7abcc75210b2eac924f2e945e6
JR Tashjian <jrtashjian@gmail.com>
2020-02-28 00:57:57 -0500

    Ensure the first row of food-item blocks are not hidden when parent is deselected

--- src/blocks/food-and-drinks/test/food-and-drinks.cypress.js
+++ src/blocks/food-and-drinks/test/food-and-drinks.cypress.js
@@ -89,7 +89,8 @@ describe( 'Block: Food and Drinks', function () {
 		helpers.addCustomBlockClass( 'my-custom-class', 'food-and-drinks' );
 
 		// Click "Add Menu Section" and verify two blocks exist on the page.
-		cy.get( '.wp-block-coblocks-food-and-drinks' ).find( '.coblocks-list-appender__toggle' ).click( { multiple: true } ).click();
+		cy.get( '.wp-block[data-type="coblocks/food-and-drinks"]' ).click();
+		cy.get( '.wp-block[data-type="coblocks/food-and-drinks"]' ).find( '.coblocks-list-appender__toggle' ).click();
 		cy.get( '.wp-block-coblocks-food-and-drinks' ).should( 'have.length', 2 );
 
 		cy.get( '.wp-block[data-type="coblocks/food-and-drinks"]' ).last().within( () => {
