a12757c914aef0f015f8afbf23280300402cfd17
JR Tashjian <jrtashjian@gmail.com>
2020-02-28 01:13:08 -0500

    Fix Events block E2E test failures

--- src/blocks/events/test/events.cypress.js
+++ src/blocks/events/test/events.cypress.js
@@ -1,9 +1,9 @@
-/*
+/**
  * Include our constants
  */
 import * as helpers from '../../../../.dev/tests/cypress/helpers';
 
-describe( 'Test CoBlocks Events Block', function() {
+describe( 'Block: Events', function () {
 	// Setup events data.
 	const eventsData = {
 		ical: 'https://calendar.google.com/calendar/ical/8hohgb8qv19fgvjbbkcehe0ce0%40group.calendar.google.com/public/basic.ics',
@@ -13,76 +13,43 @@ describe( 'Test CoBlocks Events Block', function() {
 		},
 	};
 
+	beforeEach( () => {
+		helpers.addCoBlocksBlockToPage( true, 'events' );
+	} );
+
 	/**
 	 * Test that we can add a events block to the content, not add any text or
 	 * alter any settings, and are able to successfully save the block without errors.
-	*/
-	it( 'Test events block saves without content.', function() {
-		helpers.addCoBlocksBlockToPage( true, 'events' );
-
+	 */
+	it( 'can be inserted without errors', function () {
 		cy.get( '.wp-block-coblocks-events' ).should( 'exist' );
-
-		helpers.savePage();
-
 		helpers.checkForBlockErrors( 'events' );
-
-		helpers.viewPage();
-
-		cy.get( '.wp-block-coblocks-events' ).should( 'exist' );
-
-		helpers.editPage();
 	} );
 
 	/**
 	 * Test that we can add a events block to the content, and
 	 * import a calendar from a public ICS Google calendar
-	*/
-	it( 'Test events block saves with imported ICS calendar.', function() {
+	 */
+	it( 'can import an ICS calendar', function () {
 		const { ical } = eventsData;
-		helpers.addCoBlocksBlockToPage( true, 'events' );
-
-		cy.get( '.wp-block-coblocks-events' ).click( { force: true } ).should( 'exist' );
 
 		helpers.toggleSettingCheckbox( /link a calendar/i );
 
-		cy.get( 'input[placeholder="Enter URL here…"]' ).click().type( ical );
+		cy.get( 'input[placeholder="Enter URL here…"]' ).type( ical );
 		cy.get( 'button' ).contains( 'Use URL' ).click();
 
 		cy.get( '.wp-block-coblocks-event-item', { timeout: 10000 } ).should( 'exist' );
 
-		helpers.savePage();
-
 		helpers.checkForBlockErrors( 'events' );
-
-		helpers.viewPage();
-
-		cy.get( '.wp-block-coblocks-event-item' ).should( 'be.visible' );
-
-		helpers.editPage();
 	} );
 
 	/**
 	* Test the events block saves with custom classes
 	*/
-	it( 'Test events block saves with custom classes.', function() {
-		helpers.addCoBlocksBlockToPage( true, 'events' );
-
-		cy.get( '.wp-block-coblocks-events' ).click( { force: true } );
-
+	it( 'can set custom classes', function () {
 		helpers.addCustomBlockClass( 'my-custom-class', 'events' );
-
-		helpers.savePage();
+		cy.get( '.wp-block-coblocks-event-item' ).should( 'have.class', 'my-custom-class' );
 
 		helpers.checkForBlockErrors( 'events' );
-
-		cy.get( '.wp-block-coblocks-events' )
-			.should( 'have.class', 'my-custom-class' );
-
-		helpers.viewPage();
-
-		cy.get( '.wp-block-coblocks-events' )
-			.should( 'have.class', 'my-custom-class' );
-
-		helpers.editPage();
 	} );
 } );
