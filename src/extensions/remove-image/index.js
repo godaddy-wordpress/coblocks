a54935c790387abe277d27756169c9b14b43d8d8
AnthonyLedesma <anthonymledesma@gmail.com>
2020-03-02 11:27:40 -0700

    use onSelectImage logic from core

--- src/extensions/remove-image/index.js
+++ src/extensions/remove-image/index.js
@@ -1,3 +1,7 @@
+/**
+ * External dependencies
+ */
+import { get, omit } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -5,11 +9,14 @@
 import { addFilter } from '@wordpress/hooks';
 import { MediaUpload, MediaUploadCheck, InspectorControls } from '@wordpress/block-editor';
 import { Button } from '@wordpress/components';
-
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { Fragment } from '@wordpress/element';
-
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import * as helper from './../../utils/helper';
 
 const supportedBlocks = [
 	'core/image',
@@ -20,10 +27,75 @@ const withReplaceImage = createHigherOrderComponent( ( BlockEdit ) => {
 		if ( props.name.includes( supportedBlocks ) && !! props.attributes.url ) {
 			const {
 				setAttributes,
-				url,
+				attributes,
 			} = props;
 
-			const onUploadImage = ( media ) => setAttributes( { url: media.url, id: media.id } );
+			const onSelectImage = ( media ) => {
+				if ( ! media || ! media.url ) {
+					this.props.setAttributes( {
+						url: undefined,
+						alt: undefined,
+						id: undefined,
+						title: undefined,
+						caption: undefined,
+					} );
+					return;
+				}
+
+				const {
+					id,
+					url,
+					alt,
+					caption,
+					linkDestination,
+				} = attributes;
+
+				let mediaAttributes = helper.pickRelevantMediaFiles( media );
+
+				// If the current image is temporary but an alt text was meanwhile written by the user,
+				// make sure the text is not overwritten.
+				if ( helper.isTemporaryImage( id, url ) ) {
+					if ( alt ) {
+						mediaAttributes = omit( mediaAttributes, [ 'alt' ] );
+					}
+				}
+
+				// If a caption text was meanwhile written by the user,
+				// make sure the text is not overwritten by empty captions
+				if ( caption && ! get( mediaAttributes, [ 'caption' ] ) ) {
+					mediaAttributes = omit( mediaAttributes, [ 'caption' ] );
+				}
+
+				let additionalAttributes;
+				// Reset the dimension attributes if changing to a different image.
+				if ( ! media.id || media.id !== id ) {
+					additionalAttributes = {
+						width: undefined,
+						height: undefined,
+						sizeSlug: 'large',
+					};
+				} else {
+					// Keep the same url when selecting the same file, so "Image Size" option is not changed.
+					additionalAttributes = { url };
+				}
+
+				// Check if the image is linked to it's media.
+				if ( linkDestination === 'media' ) {
+					// Update the media link.
+					mediaAttributes.href = media.url;
+				}
+
+				// Check if the image is linked to the attachment page.
+				if ( linkDestination === 'attachment' ) {
+					// Update the media link.
+					mediaAttributes.href = media.link;
+				}
+
+				setAttributes( {
+					...mediaAttributes,
+					...additionalAttributes,
+				} );
+			};
 
 			return (
 				<Fragment>
@@ -33,8 +105,8 @@ const withReplaceImage = createHigherOrderComponent( ( BlockEdit ) => {
 							<MediaUploadCheck>
 								<MediaUpload
 									allowedTypes={ [ 'image' ] }
-									onSelect={ onUploadImage }
-									value={ url }
+									onSelect={ onSelectImage }
+									value={ props.url }
 									render={ ( { open } ) => (
 										<Button className="is-secondary" onClick={ open }>
 											{ __( 'Replace Image', 'coblocks' ) }
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -5,6 +5,11 @@ import pick from 'lodash/pick';
 import get from 'lodash/get';
 import findIndex from 'lodash/findIndex';
 
+/**
+ * WordPress dependencies
+ */
+import { isBlobURL } from '@wordpress/blob';
+
 // Set dim ratio.
 export function overlayToClass( ratio ) {
 	return ( ratio === 0 || ratio === 50 ) ?
@@ -22,6 +27,17 @@ export const pickRelevantMediaFiles = ( image, images ) => {
 	imageProps.imgLink = imgKey >= 0 ? images[ imgKey ].imgLink : '';
 	return imageProps;
 };
+
+/**
+ * Is the URL a temporary blob URL? A blob URL is one that is used temporarily
+ * while the image is being uploaded and will not have an id yet allocated.
+ *
+ * @param {number=} id The id of the image.
+ * @param {string=} url The url of the image.
+ *
+ * @return {boolean} Is the URL a Blob URL
+ */
+export const isTemporaryImage = ( id, url ) => ! id && isBlobURL( url );
 
 // Define accepted media for gallery blocks.
 export const ALLOWED_GALLERY_MEDIA_TYPES = [ 'image' ];
