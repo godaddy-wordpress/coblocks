e1c4e4a6dff909d5eeb75c3be04347b45a507b45
AnthonyLedesma <anthonymledesma@gmail.com>
2020-03-03 07:20:53 -0700

    write more tests

--- src/extensions/coblocks-settings/coblocks-settings.cypress.js
+++ src/extensions/coblocks-settings/coblocks-settings.cypress.js
@@ -4,6 +4,22 @@
 import * as helpers from '../../../.dev/tests/cypress/helpers';
 
 describe( 'Extension: CoBlocks Settings', function() {
+	let supportsGradients = false;
+	beforeEach( function() {
+		cy.get( '.edit-post-more-menu' ).click();
+		cy.get( '.components-menu-group' ).find( 'button' ).contains( 'Editor settings' ).click();
+		cy.get( '.coblocks-modal__content' ).find( 'input[type="checkbox"]' ).each( ( checkbox ) => {
+			if ( ! Cypress.$( checkbox ).prop( 'checked' ) ) {
+				cy.get( checkbox ).click();
+			}
+
+			if ( Cypress.$( checkbox ).parents( '.components-base-control__field' ).text() === 'Gradient styles' ) {
+				supportsGradients = true;
+			}
+		} );
+		cy.get( '.components-modal__header' ).find( 'button[aria-label="Close dialog"]' ).click();
+	} );
+
 	// /**
 	//  * Test that the CoBlocks panel may be hidden
 	//  */
@@ -23,20 +39,52 @@ describe( 'Extension: CoBlocks Settings', function() {
 	// } );
 
 	/**
-	 * Test that the CoBlocks panel controls function as expected.
+	 * Test that the CoBlocks panel typography controls function as expected.
 	 */
-	it( 'Can control settings as expected.', function() {
+	it( 'Can control typography settings as expected.', function() {
 		helpers.addCoBlocksBlockToPage( true, 'row' );
 		cy.get( 'div[aria-label="Select Row Columns"]' ).find( 'div:nth-child(1) button' ).click( { force: true } );
 		cy.get( '.wp-block-coblocks-row' ).click( { force: true } );
-		helpers.openSettingsPanel( /color settings/i );
 
 		cy.get( '.edit-post-more-menu' ).click();
 		cy.get( '.components-menu-group' ).find( 'button' ).contains( 'Editor settings' ).click();
-		//TODO: get all buttons, look for checked status, and make sure all are enabled before testing.
 
+		// Typography Test
 		cy.get( 'button[aria-label="Change typography"]' ).should( 'exist' );
 		cy.get( '.coblocks-modal__content' ).contains( 'Typography controls' ).click();
 		cy.get( 'button[aria-label="Change typography"]' ).should( 'not.exist' );
+
+		cy.get( '.components-modal__header' ).find( 'button[aria-label="Close dialog"]' ).click();
+	} );
+
+	/**
+	 * Test that the CoBlocks panel colors controls function as expected.
+	 */
+	it( 'Can control color settings as expected.', function() {
+		helpers.addCoBlocksBlockToPage( true, 'hero' );
+		cy.get( '.wp-block-button' ).first().click( { force: true } );
+		helpers.openSettingsPanel( /background & text color/i );
+
+		cy.get( '.edit-post-more-menu' ).click();
+		cy.get( '.components-menu-group' ).find( 'button' ).contains( 'Editor settings' ).click();
+
+		if ( supportsGradients ) {
+			// Gradient Panels
+			cy.get( '.block-editor-color-gradient-control button' ).contains( 'Gradient' ).should( 'exist' );
+			cy.get( '.coblocks-modal__content' ).contains( 'Gradient styles' ).click();
+			cy.get( '.block-editor-color-gradient-control button' ).contains( 'Gradient' ).should( 'not.exist' );
+		}
+
+		// Custom Color Picker
+		cy.get( 'button[aria-label="Custom color picker"]' ).should( 'exist' );
+		cy.get( '.coblocks-modal__content' ).contains( 'Custom color pickers' ).click();
+		cy.get( 'button[aria-label="Custom color picker"]' ).should( 'not.exist' );
+
+		// Color Settings
+		cy.get( '.components-panel__body-title' ).contains( /background & text color/i ).should( 'exist' );
+		cy.get( '.coblocks-modal__content' ).contains( 'Color settings' ).click();
+		cy.get( '.components-panel__body-title' ).contains( /background & text color/i ).should( 'not.exist' );
+
+		cy.get( '.components-modal__header' ).find( 'button[aria-label="Close dialog"]' ).click();
 	} );
 } );
