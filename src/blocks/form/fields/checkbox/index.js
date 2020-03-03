e3cd3b47ccafd86a989fa73d771760331d6dc477
Rich Tabor <hi@richtabor.com>
2020-03-03 17:31:25 -0500

    Support new block collection functionality in WordPress 5.4 (#1367)
    * registerBlockCollection
    
    * Set categories
    
    * Rename CoBlocks Galleries category to "Galleries"
    
    * Update block-category.js
    
    * try conditional
    
    * fix highlight test
    
    Co-authored-by: Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
    Co-authored-by: JR Tashjian <jrtashjian@gmail.com>

--- src/blocks/form/fields/checkbox/index.js
+++ src/blocks/form/fields/checkbox/index.js
@@ -14,7 +14,7 @@ import { __ } from '@wordpress/i18n';
  */
 const metadata = {
 	name: 'coblocks/field-checkbox',
-	category: 'coblocks',
+	category: 'layout',
 	attributes: {
 		label: {
 			type: 'string',
