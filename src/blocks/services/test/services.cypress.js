83d27401ef2756e2bc746ce5a0c5366bb18f7570
Rich Tabor <hi@richtabor.com>
2020-03-09 17:33:54 -0400

    Update services.cypress.js

--- src/blocks/services/test/services.cypress.js
+++ src/blocks/services/test/services.cypress.js
@@ -101,7 +101,7 @@ describe( 'Test CoBlocks Services Block', function() {
 
 		cy.get( 'div.wp-block-button' ).should( 'not.exist' );
 
-		helpers.toggleSettingCheckbox( /action buttons/i );
+		helpers.toggleSettingCheckbox( /display buttons/i );
 
 		cy.get( 'div.wp-block-button' ).should( 'exist' );
 
