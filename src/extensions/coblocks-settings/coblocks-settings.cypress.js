fb42d4e1a2ad3574cfc0614a705d8d19726d5502
JR Tashjian <jrtashjian@gmail.com>
2020-03-19 14:10:33 -0400

    Improve regex for color panel selection

--- src/extensions/coblocks-settings/coblocks-settings.cypress.js
+++ src/extensions/coblocks-settings/coblocks-settings.cypress.js
@@ -25,7 +25,7 @@ describe( 'Extension: CoBlocks Settings', function() {
 	 */
 	it( 'Can control typography settings as expected.', function() {
 		helpers.addBlockToPost( 'coblocks/row', true );
-		cy.get( 'div[aria-label="Select Row Columns"]' ).find( 'div:nth-child(1) button' ).click( { force: true } );
+		cy.get( 'div[aria-label="Select row columns"]' ).find( 'div:nth-child(1) button' ).click( { force: true } );
 		cy.get( '.wp-block-coblocks-row' ).click( { force: true } );
 
 		cy.get( '.edit-post-more-menu' ).click();
@@ -44,8 +44,8 @@ describe( 'Extension: CoBlocks Settings', function() {
 	 */
 	it( 'Can control color settings as expected.', function() {
 		helpers.addBlockToPost( 'coblocks/hero', true );
-		cy.get( '.wp-block-button' ).first().click( { force: true } );
-		helpers.openSettingsPanel( /background & text color/i );
+		cy.get( '.wp-block-button' ).first().click();
+		helpers.openSettingsPanel( /(Background & Text Color|Color Settings)/i );
 
 		cy.get( '.edit-post-more-menu' ).click();
 		cy.get( '.components-menu-group' ).find( 'button' ).contains( 'Editor settings' ).click();
@@ -65,9 +65,9 @@ describe( 'Extension: CoBlocks Settings', function() {
 		cy.get( '.coblocks-modal__content' ).contains( 'Custom color pickers' ).click();// Re-enable
 
 		// Color Settings
-		cy.get( '.components-panel__body-title' ).contains( /background & text color/i ).should( 'exist' );
+		cy.get( '.components-panel__body-title' ).contains( /(Background & Text Color|Color Settings)/i ).should( 'exist' );
 		cy.get( '.coblocks-modal__content' ).contains( 'Color settings' ).click();
-		cy.get( '.components-panel__body-title' ).contains( /background & text color/i ).should( 'not.exist' );
+		cy.get( '.components-panel__body-title' ).contains( /(Background & Text Color|Color Settings)/i ).should( 'not.exist' );
 		cy.get( '.coblocks-modal__content' ).contains( 'Color settings' ).click(); // Re-enable
 
 		cy.get( '.components-modal__header' ).find( 'button[aria-label="Close dialog"]' ).click();
