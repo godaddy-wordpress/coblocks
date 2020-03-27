1abb747aea79779dff02b99e8d992d3b86d974ea
Rich Tabor <hi@richtabor.com>
2020-03-27 13:15:52 -0400

    Update deprecated.js

--- src/blocks/pricing-table/deprecated.js
+++ src/blocks/pricing-table/deprecated.js
@@ -40,6 +40,29 @@ const deprecated = [
 			);
 		},
 	},
+	{
+		attributes: currentBlockAttributes,
+		save( { attributes } ) {
+			const {
+				contentAlign,
+				count,
+			} = attributes;
+
+			const classes = classnames(
+				`has-${ count }-columns`,
+				{ [ `has-text-align-${ contentAlign }` ]: contentAlign }
+			);
+
+			return (
+
+				<div className={ classes }>
+					<div className="wp-block-coblocks-pricing-table__inner">
+						<InnerBlocks.Content />
+					</div>
+				</div>
+			);
+		}
+	},
 ];
 
 export default deprecated;
