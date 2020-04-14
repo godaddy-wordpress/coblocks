c1214493f3b7d09ee12e9e0ce19672b0948053e4
Rich Tabor <hi@richtabor.com>
2020-04-14 13:40:39 -0400

    Tweak example

--- src/blocks/pricing-table/example.js
+++ src/blocks/pricing-table/example.js
@@ -1,93 +1,67 @@
 /**
- * WordPress dependencies
- */
+* WordPress dependencies
+*/
 import { __ } from '@wordpress/i18n';
 
 const example = {
-    attributes: {
-        count: 3,
-        contentAlign: 'center',
-    },
-    innerBlocks: [
-        {
-            name: 'coblocks/pricing-table-item',
-            attributes: {
-                title: __( 'Basic', 'coblocks' ),
-                currency: '$',
-                amount: '29',
-                features: [
-                    { 	
-                        type: 'li',
-                        props: {
-                            children: [
-                                "Standard Support",
-                            ]
-                        }
-                    }
-                ]
-            },
-            innerBlocks: [ 
-                { 
-                    name: 'core/button',
-                    attributes: {
-                        text: 'Buy',
-                    }
-                }
-            ]
-        },
-        {
-            name: 'coblocks/pricing-table-item',
-            attributes: {
-                title: __( 'Pro', 'coblocks' ),
-                currency: '$',
-                amount: '39',
-                features: [
-                    { 	
-                        type: 'li',
-                        props: {
-                            children: [
-                                "Tailored Experience",
-                            ]
-                        }
-                    }
-                ]
-            },
-            innerBlocks: [ 
-                { 
-                    name: 'core/button',
-                    attributes: {
-                        text: 'Buy',
-                    }
-                }
-            ]
-        },
-        {
-            name: 'coblocks/pricing-table-item',
-            attributes: {
-                title: __( 'Gold', 'coblocks' ),
-                currency: '$',
-                amount: '99',
-                features: [
-                    { 	
-                        type: 'li',
-                        props: {
-                            children: [
-                                "Expert Guidence",
-                            ]
-                        }
-                    }
-                ]
-            },
-            innerBlocks: [ 
-                { 
-                    name: 'core/button',
-                    attributes: {
-                        text: 'Call',
-                    }
-                }
-            ]
-        },
-    ],
+	attributes: {
+		count: 2,
+		contentAlign: 'center',
+	},
+	innerBlocks: [
+	{
+		name: 'coblocks/pricing-table-item',
+		attributes: {
+			title: __( 'Basic', 'coblocks' ),
+			currency: '$',
+			amount: '19',
+			features: [
+			{
+				type: 'li',
+				props: {
+					children: [
+						__( 'Feature one', 'coblocks' ),
+					]
+				}
+			}
+			]
+		},
+		innerBlocks: [
+		{
+			name: 'core/button',
+			attributes: {
+				text: __( 'Buy now', 'coblocks' ),
+			}
+		}
+		]
+	},
+	{
+		name: 'coblocks/pricing-table-item',
+		attributes: {
+			title: __( 'Pro', 'coblocks' ),
+			currency: '$',
+			amount: '49',
+			features: [
+			{
+				type: 'li',
+				props: {
+					children: [
+						__( 'Feature one', 'coblocks' ),
+					]
+				}
+			}
+			]
+		},
+		innerBlocks: [
+		{
+			name: 'core/button',
+			attributes: {
+				text: __( 'Buy now', 'coblocks' ),
+			}
+		}
+		]
+	},
+	],
 };
 
 export default example;
