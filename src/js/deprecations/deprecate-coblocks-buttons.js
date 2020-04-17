7711cd85c2300d8057d2dfaf8f254e22d59fd2f9
AnthonyLedesma <anthonymledesma@gmail.com>
2020-04-17 10:13:52 -0700

    propagate alignment to children

--- src/js/deprecations/deprecate-coblocks-buttons.js
+++ src/js/deprecations/deprecate-coblocks-buttons.js
@@ -4,28 +4,43 @@
 import { getBlockType, registerBlockType, unregisterBlockType } from '@wordpress/blocks';
 import domReady from '@wordpress/dom-ready';
 
+
+
 /**
  * Prevents the CoBlocks Buttons block from being insertable.
  */
 function deprecateCoBlocksButtonsSettings() {
 	const coreButtons = getBlockType( 'core/buttons' );
+	const coBlocksButtons = getBlockType( 'coblocks/buttons' );	
+	if ( buttonsBlockDeprecated(coreButtons, coBlocksButtons) ) {
+		unregisterBlockType( 'coblocks/buttons' );
+		registerBlockType( 'coblocks/buttons', {
+			...coBlocksButtons,
+			supports: {
+				...coBlocksButtons.supports,
+				inserter: false,
+			},
+		} );
+	}
+}
+
+/**
+ * Check whether or not core/buttons is available for use.
+ *
+ * @param {Object} coreButtons The results of getBlockType.
+ * @param {Object} coBlocksButtons The results of getBlockType.
+ * 
+ * @return {boolean} True or false if block is deprecated.
+ */
+export function buttonsBlockDeprecated(coreButtons = getBlockType( 'core/buttons' ), coBlocksButtons = getBlockType( 'coblocks/buttons' )) {
 	if ( ! coreButtons ) {
-		return;
+		return false;
 	}
 
-	const coBlocksButtons = getBlockType( 'coblocks/buttons' );
 	if ( ! coBlocksButtons ) {
-		return;
+		return false;
 	}
-
-	unregisterBlockType( 'coblocks/buttons' );
-	registerBlockType( 'coblocks/buttons', {
-		...coBlocksButtons,
-		supports: {
-			...coBlocksButtons.supports,
-			inserter: false,
-		},
-	} );
+	return true;
 }
 
 domReady( deprecateCoBlocksButtonsSettings );
