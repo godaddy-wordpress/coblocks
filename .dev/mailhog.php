ef853e3b48de703abd00aeeab818dc3960b46f18
Evan Herman <evan.m.herman@gmail.com>
2020-04-23 16:55:11 -0400

    Update mailhog.php mu-plugin

--- .dev/mailhog.php
+++ .dev/mailhog.php
@@ -1,28 +1,13 @@
 <?php
 
 add_action( 'phpmailer_init', function( $phpmailer ) {
-	// Define that we are sending with SMTP
+	// Define that we are sending with SMTP.
 	$phpmailer->isSMTP();
 
-	// The hostname of the mail server
-	$phpmailer->Host = 'localhost';
-
-	// Use SMTP authentication (true|false)
+	// Set options.
+	$phpmailer->Host     = 'localhost';
 	$phpmailer->SMTPAuth = false;
-
-	// SMTP port number
-	// Mailhog normally run on port 1025
-	$phpmailer->Port = '1025';
-
-	// Username to use for SMTP authentication
-	// $phpmailer->Username = 'yourusername';
-
-	// Password to use for SMTP authentication
-	// $phpmailer->Password = 'yourpassword';
-
-	// The encryption system to use - ssl (deprecated) or tls
-	// $phpmailer->SMTPSecure = 'tls';
-
-	$phpmailer->From = 'admin@wp.dev';
+	$phpmailer->Port     = '1025';
+	$phpmailer->From     = 'admin@wp.dev';
 	$phpmailer->FromName = 'WP DEV';
 }, 10, 1 );
