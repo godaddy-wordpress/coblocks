0d2dd2ff50b803d2a9055fa7577fd757ab18b5d4
Evan Herman <evan.m.herman@gmail.com>
2020-04-23 17:09:37 -0400

    Fix Special Notes -> Special notes

--- src/blocks/form/test/form.cypress.js
+++ src/blocks/form/test/form.cypress.js
@@ -242,7 +242,7 @@ describe( 'Test CoBlocks Form Block', function() {
 					break;
 
 				case 5:
-					cy.get( $element ).contains( 'Special Notes' );
+					cy.get( $element ).contains( 'Special notes' );
 					break;
 			}
 		} );
