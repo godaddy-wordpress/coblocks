729daedebb77a836c4f3b7b61d345aac4bb21420
Evan Herman <evan.m.herman@gmail.com>
2020-04-29 10:55:38 -0400

    Add form block e2e test to confirm emails are being sent (#1468)
    * Introduce form block submission e2e tests
    
    * Update
    
    * Add mailhog to e2e firefox
    
    * Update path to mailhog.php mu-plugin
    
    * Set linuxbrew 
    
    * Set environment PATH
    
    * Updates for testing
    
    * Updates for testing
    
    * Test wp_mail
    
    * Update
    
    * Updates
    
    * Reenable tests
    
    * Update mailhog.php mu-plugin
    
    * Land back on edit screen after test run
    
    * Fix double spacing
    
    * Fix Special Notes -> Special notes
    
    * Add test for custom subject line, and keyword replacement [email]/[name]
    
    * Updates
    
    * Update
    
    * Update comment above test

---
+++ .dev/mailhog.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action( 'phpmailer_init', function( $phpmailer ) {
+	// Define that we are sending with SMTP.
+	$phpmailer->isSMTP();
+
+	// Set options.
+	$phpmailer->Host     = 'localhost';
+	$phpmailer->SMTPAuth = false;
+	$phpmailer->Port     = '1025';
+	$phpmailer->From     = 'admin@wp.dev';
+	$phpmailer->FromName = 'WP DEV';
+}, 10, 1 );
