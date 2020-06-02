1bb9bd9520f54deed741f622f344fe1259460ba4
JR Tashjian <jrtashjian@gmail.com>
2020-06-02 16:00:00 -0400

    Favor cypress-image-snapshot over cypress-plugin-snapshots

--- .dev/tests/cypress/plugins/index.js
+++ .dev/tests/cypress/plugins/index.js
@@ -1,14 +1,14 @@
-const { initPlugin } = require( 'cypress-plugin-snapshots/plugin' );
+const {
+	addMatchImageSnapshotPlugin,
+} = require( 'cypress-image-snapshot/plugin' );
 
 module.exports = ( on, config ) => {
-	initPlugin( on, config );
+	addMatchImageSnapshotPlugin( on, config );
 
-	on( 'task', {
-		log( message ) {
-			console.log( message );
-			return null
+	on( 'before:browser:launch', ( browser, launchOptions ) => {
+		if ( browser.family === 'chromium' && browser.name !== 'electron' && browser.isHeadless ) {
+			launchOptions.args.push( '--window-size=1920,1080' );
+			return launchOptions;
 		}
 	} );
-
-	return config;
-}
+};
--- .dev/tests/cypress/support/commands.js
+++ .dev/tests/cypress/support/commands.js
@@ -1,44 +1,49 @@
 import { loginToSite, disableGutenbergFeatures } from '../helpers';
 import 'cypress-file-upload';
-import 'cypress-plugin-snapshots/commands';
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+addMatchImageSnapshotCommand( {
+	failureThreshold: 0.25,
+	failureThresholdType: 'percent',
+} );
 
 before( function() {
-  loginToSite();
-  disableGutenbergFeatures();
+	loginToSite();
+	disableGutenbergFeatures();
 } );
 
 // Maintain WordPress logged in state
 Cypress.Cookies.defaults( {
-  whitelist: /wordpress_.*/
+	whitelist: /wordpress_.*/,
 } );
 
 // Custom uploadFile command
 Cypress.Commands.add( 'uploadFile', ( fileName, fileType, selector ) => {
-  cy.get( selector ).then( subject => {
-    cy.fixture( fileName, 'hex' ).then( ( fileHex ) => {
-      const fileBytes = hexStringToByte( fileHex );
-      const testFile = new File( [ fileBytes ], fileName, {
-          type: fileType
-      } );
-      const dataTransfer = new DataTransfer()
-      const el = subject[0]
+	cy.get( selector ).then( ( subject ) => {
+		cy.fixture( fileName, 'hex' ).then( ( fileHex ) => {
+			const fileBytes = hexStringToByte( fileHex );
+			const testFile = new File( [ fileBytes ], fileName, {
+				type: fileType,
+			} );
+			const dataTransfer = new DataTransfer();
+			const el = subject[ 0 ];
 
-      dataTransfer.items.add(testFile)
-      el.files = dataTransfer.files
-    } );
-  } );
+			dataTransfer.items.add( testFile );
+			el.files = dataTransfer.files;
+		} );
+	} );
 } );
 
 // Utilities
 function hexStringToByte( str ) {
-  if ( ! str ) {
-    return new Uint8Array();
-  }
+	if ( ! str ) {
+		return new Uint8Array();
+	}
 
-  var a = [];
-  for ( var i = 0, len = str.length; i < len; i += 2 ) {
-    a.push( parseInt( str.substr( i, 2 ), 16 ) );
-  }
+	const a = [];
+	for ( let i = 0, len = str.length; i < len; i += 2 ) {
+		a.push( parseInt( str.substr( i, 2 ), 16 ) );
+	}
 
-  return new Uint8Array( a );
+	return new Uint8Array( a );
 }
