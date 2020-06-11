51f04985ed6ed9576639c30f8ac565e1e67e51c3
JR Tashjian <jrtashjian@gmail.com>
2020-06-11 15:00:38 -0400

    E2E: Fix how blocks are found within the inserter

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -75,7 +75,8 @@ export function addBlockToPost( blockName, clearEditor = false ) {
 		blockID.split( '-' )[ 0 ]
 	);
 
-	cy.get( '.components-panel__body.is-opened .editor-block-list-item-coblocks-' + blockID ).first().click();
+	const targetClassName = ( blockCategory === 'core' ? '' : `-${ blockCategory }` ) + `-${ blockID }`;
+	cy.get( '.components-panel__body.is-opened .editor-block-list-item' + targetClassName ).first().click();
 
 	// Make sure the block was added to our page
 	cy.get( `div[data-type="${ blockName }"]` ).should( 'exist' );
