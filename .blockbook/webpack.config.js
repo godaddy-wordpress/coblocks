88322e7c5c3e4d3a277ea0c3c64f7c7e4f3e9fea
Riad Benguella <benguella@gmail.com>
2020-07-25 16:21:36 +0100

    Add a BlockBook to the repository

---
+++ .blockbook/webpack.config.js
@@ -0,0 +1,42 @@
+const nodeSassGlobImporter = require( 'node-sass-glob-importer' );
+const postcssConfig = require( '../.dev/config/postcss.config.js' );
+
+module.exports = ( baseConfig ) => {
+    return {
+        ...baseConfig,
+        module: {
+            ...baseConfig.module,
+            rules: [
+                ...baseConfig.module.rules,
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ...postcssConfig,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: {
+                                    importer: nodeSassGlobImporter(),
+                                }
+                            }
+                        }
+                    ],
+                },
+            ],
+        },
+    }
+}
