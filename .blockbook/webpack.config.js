a70158de40156bf5bd5728760812f2b0859645d7
JR Tashjian <jrtashjian@gmail.com>
2020-07-27 15:06:15 -0400

    eslint fixes

--- .blockbook/webpack.config.js
+++ .blockbook/webpack.config.js
@@ -2,41 +2,41 @@ const nodeSassGlobImporter = require( 'node-sass-glob-importer' );
 const postcssConfig = require( '../.dev/config/postcss.config.js' );
 
 module.exports = ( baseConfig ) => {
-    return {
-        ...baseConfig,
-        module: {
-            ...baseConfig.module,
-            rules: [
-                ...baseConfig.module.rules,
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                url: false,
-                            },
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                ...postcssConfig,
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sassOptions: {
-                                    importer: nodeSassGlobImporter(),
-                                }
-                            }
-                        }
-                    ],
-                },
-            ],
-        },
-    }
-}
+	return {
+		...baseConfig,
+		module: {
+			...baseConfig.module,
+			rules: [
+				...baseConfig.module.rules,
+				{
+					test: /\.scss$/,
+					use: [
+						{
+							loader: 'style-loader',
+						},
+						{
+							loader: 'css-loader',
+							options: {
+								url: false,
+							},
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								...postcssConfig,
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sassOptions: {
+									importer: nodeSassGlobImporter(),
+								},
+							},
+						},
+					],
+				},
+			],
+		},
+	};
+};
