0c359b2f7a9dbdfa2f49050ab8558bb24a261977
Rich Tabor <hi@richtabor.com>
2020-07-28 10:21:53 -0400

    Resolve invalid DOM elements

--- src/blocks/form/fields/submit-button/icon.js
+++ src/blocks/form/fields/submit-button/icon.js
@@ -3,4 +3,4 @@
  */
 import { Path, SVG } from '@wordpress/components';
 
-export default <SVG fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path d="m5 7.25h14c.6904 0 1.25.55964 1.25 1.25v7c0 .6904-.5596 1.25-1.25 1.25h-14c-.69036 0-1.25-.5596-1.25-1.25v-7c0-.69036.55964-1.25 1.25-1.25z" stroke="currentColor" stroke-width="1.5" fill="none" /><Path clip-rule="evenodd" d="m16 12.75h-8v-1.5h8z" fill="currentColor" fill-rule="evenodd" /></SVG>;
+export default <SVG fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path d="m5 7.25h14c.6904 0 1.25.55964 1.25 1.25v7c0 .6904-.5596 1.25-1.25 1.25h-14c-.69036 0-1.25-.5596-1.25-1.25v-7c0-.69036.55964-1.25 1.25-1.25z" stroke="currentColor" strokeWidth="1.5" fill="none" /><Path clipRule="evenodd" d="m16 12.75h-8v-1.5h8z" fill="currentColor" fillRule="evenodd" /></SVG>;
