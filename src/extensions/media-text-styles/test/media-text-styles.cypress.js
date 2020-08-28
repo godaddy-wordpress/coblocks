ff9c950c6fbe54465aca9a3d1b2b429466d14a25
AnthonyLedesma <anthonymledesma@gmail.com>
2020-08-28 08:11:42 -0700

    fix test spacing

--- src/extensions/media-text-styles/test/media-text-styles.cypress.js
+++ src/extensions/media-text-styles/test/media-text-styles.cypress.js
@@ -8,20 +8,20 @@ describe( 'Test CoBlocks Media Text styles extension', function() {
 	 * Test that we can add a media-text block as well as save and use alternate styles.
 	 */
 	it( 'Test core/media-text block extends with Media-Text styles extension.', function() {
-		const { imageBase } = helpers.upload.spec;
-		helpers.addBlockToPost( 'core/media-text', true );
+        const { imageBase } = helpers.upload.spec;
+        helpers.addBlockToPost( 'core/media-text', true );
         helpers.upload.imageToBlock( 'core/media-text' );
 
         const selectBlock = () => cy.get( `img[src*="${imageBase}"]` ).click();
 
-		cy.get( `img[src*="${imageBase}"]` );
+        cy.get( `img[src*="${imageBase}"]` );
         cy.get('.wp-block-media-text__content p').first().type( 'Content' );
-		selectBlock();
+        selectBlock();
 
         helpers.setBlockStyle( 'card' );
         cy.get( '.edit-post-visual-editor .wp-block-media-text.is-style-card' ).should('exist');
-		helpers.savePage();
-		helpers.checkForBlockErrors( 'core/media-text' );
+        helpers.savePage();
+        helpers.checkForBlockErrors( 'core/media-text' );
         helpers.viewPage();
         cy.get( '.wp-block-media-text.is-style-card' ).should('exist');
         helpers.editPage();
@@ -30,17 +30,17 @@ describe( 'Test CoBlocks Media Text styles extension', function() {
         helpers.setBlockStyle( 'overlap' );
         cy.get( '.edit-post-visual-editor .wp-block-media-text.is-style-overlap' ).should('exist');
         helpers.savePage();
-		helpers.checkForBlockErrors( 'core/media-text' );
+        helpers.checkForBlockErrors( 'core/media-text' );
         helpers.viewPage();
         cy.get( '.wp-block-media-text.is-style-overlap' ).should('exist');
         helpers.editPage();
         selectBlock();
-        
+                
 
         helpers.setBlockStyle( 'outline' );
         cy.get( '.edit-post-visual-editor .wp-block-media-text.is-style-outline' ).should('exist');
         helpers.savePage();
-		helpers.checkForBlockErrors( 'core/media-text' );
+        helpers.checkForBlockErrors( 'core/media-text' );
         helpers.viewPage();
         cy.get( '.wp-block-media-text.is-style-outline' ).should('exist');
         helpers.editPage();
