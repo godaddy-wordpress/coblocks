e65e9f83d2f9a20a3c145c7f2abd558fea7c4153
JR Tashjian <jrtashjian@gmail.com>
2020-09-03 13:50:46 -0400

    Introduce custom block patterns saved as a CPT. (#1659)
    * Create new layotu CPT and taxonomies
    
    * Load the layouts from the CPT into the layout selector
    
    * Remove Gutenberg requirement for layout selector
    
    * Update dependencies
    
    * Add new block patterns modal
    
    * Make it easier to link existing/non-existing terms to a pattern
    
    * Register block_pattern_category tax with CPT
    
    * Add excerpt support
    
    * Load block patterns
    
    * prefix CPT and taxonomies with "coblocks"
    
    * typo
    
    * Mistakenly removed gutter-control require
    
    * Mistakenly removed gutter-control require
    
    * Manually trigger click even on "Block Patterns" tab
    
    * Remove icon from block settings menu item
    
    * Minor copy updates
    
    * Update CPT prefix to coblocks_pattern
    
    * Update CPT prefix
    
    * Remove whitespace
    
    * Update index.js
    
    * Add required * to Name
    
    * Adding new singleton trait
    
    * Use singleton trait
    
    * Adding inline docs
    
    * Use constants for the slugs
    
    * Fix const usage
    
    * Add method visibility and missing inline docs
    
    Co-authored-by: Rich Tabor <hi@richtabor.com>

---
+++ src/extensions/block-patterns/index.js
@@ -0,0 +1,45 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { registerPlugin } from '@wordpress/plugins';
+import { Fragment, useState } from '@wordpress/element';
+import { BlockSettingsMenuControls } from '@wordpress/block-editor';
+import { MenuItem } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import CoBlocksBlockPatternsModal from './modal';
+import './styles/style.scss';
+
+const CoBlocksBlockPatterns = () => {
+	const [ isOpen, setOpen ] = useState( false );
+
+	const openModal = () => setOpen( true );
+	const closeModal = () => setOpen( false );
+
+	const props = { isOpen, openModal, closeModal };
+
+	return (
+		<Fragment>
+			<BlockSettingsMenuControls>
+				{ ( { onClose } ) => (
+					<MenuItem
+						onClick={ () => {
+							openModal();
+							onClose();
+						} }
+					>
+						{ __( 'Add Design Pattern', 'coblocks' ) }
+					</MenuItem>
+				) }
+			</BlockSettingsMenuControls>
+			<CoBlocksBlockPatternsModal { ...props } />
+		</Fragment>
+	);
+};
+
+registerPlugin( 'coblocks-block-patterns', {
+	render: CoBlocksBlockPatterns,
+} );
