15f8bec97c6e57a6c8f1942c30bf9e9426a7f7bc
Rich Tabor <hi@richtabor.com>
2020-09-30 12:20:04 -0400

    Revert "Remove control"
    This reverts commit 934660317f08c8405445bc68d97db1e1217c1a6b.

--- src/blocks/row/controls.js
+++ src/blocks/row/controls.js
@@ -6,6 +6,8 @@ import map from 'lodash/map';
 /**
  * Internal dependencies
  */
+import { layoutOptions } from './utilities';
+import rowIcons from './icons';
 import { BackgroundControls } from '../../components/background';
 
 /**
@@ -14,8 +16,31 @@ import { BackgroundControls } from '../../components/background';
 import { __ } from '@wordpress/i18n';
 import { Component, Fragment } from '@wordpress/element';
 import { BlockControls, BlockVerticalAlignmentToolbar } from '@wordpress/block-editor';
+import { ToolbarGroup } from '@wordpress/components';
 
 class Controls extends Component {
+	// Switches the icon based on the layout selected,
+	// Fallback is the default layout icon.
+	layoutIcon() {
+		const {
+			columns,
+			layout,
+		} = this.props.attributes;
+
+		let selectedRows = 1;
+
+		if ( columns ) {
+			selectedRows = parseInt( columns.toString().split( '-' ) );
+		}
+
+		if ( layout === undefined ) {
+			return rowIcons.layout;
+		}
+
+		return map( layoutOptions[ selectedRows ], ( { key, smallIcon } ) => (
+			( key === layout ) ? smallIcon : ''
+		) );
+	}
 
 	render() {
 		const {
@@ -27,9 +52,16 @@ class Controls extends Component {
 		} = this.props;
 
 		const {
+			columns,
 			layout,
 			verticalAlignment,
 		} = attributes;
+
+		let selectedRows = 1;
+
+		if ( columns ) {
+			selectedRows = parseInt( columns.toString().split( '-' ) );
+		}
 
 		if ( ! layout ) {
 			return null;
@@ -38,6 +70,34 @@ class Controls extends Component {
 		return (
 			<Fragment>
 				<BlockControls>
+					{ ( columns && selectedRows > 1 ) &&
+						<ToolbarGroup
+							isCollapsed={ true }
+							icon={ this.layoutIcon() }
+							label={ __( 'Change row block layout', 'coblocks' ) }
+							controls={ map( layoutOptions[ selectedRows ], ( { name, key, smallIcon } ) => {
+								return {
+									title: name,
+									key,
+									icon: smallIcon,
+									isActive: key === layout,
+									onClick: () => {
+										const selectedWidth = key.toString().split( '-' );
+										const children = getBlocksByClientId( clientId );
+										setAttributes( {
+											layout: key,
+										} );
+										if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
+											map( children[ 0 ].innerBlocks, ( blockProps, index ) => (
+												updateBlockAttributes( blockProps.clientId, { width: selectedWidth[ index ] } )
+											) );
+										}
+									},
+								};
+							} ) }
+						>
+						</ToolbarGroup>
+					}
 					<BlockVerticalAlignmentToolbar
 						onChange={ ( alignment ) => {
 							const children = getBlocksByClientId( clientId );
