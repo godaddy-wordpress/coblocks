b57a2f1f9aecc2cc607a5cc1e33eea571775ff5d
Evan Herman <evan.m.herman@gmail.com>
2020-10-05 19:27:58 -0400

    Fix import

--- src/components/media-filter-control/index.js
+++ src/components/media-filter-control/index.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import classnames from 'classnames';
 import {
 	FilterDarkIcon,
 	FilterGrayscaleIcon,
@@ -15,12 +16,23 @@ import {
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
+import { Component, Fragment } from '@wordpress/element';
 import {
 	Toolbar,
 	DropdownMenu,
 	Icon,
 } from '@wordpress/components';
+
+import { addFilter } from '@wordpress/hooks';
+
+import { createHigherOrderComponent } from '@wordpress/compose';
+
+import { BlockControls } from '@wordpress/block-editor';
+
+const allowedBlocks = [
+	'core/image',
+	'core/gallery',
+];
 
 class MediaFilterControl extends Component {
 	render() {
@@ -105,3 +117,98 @@ class MediaFilterControl extends Component {
 }
 
 export default MediaFilterControl;
+
+/**
+ * Add the MediaFilterControl component to the core/image block
+ */
+const coreImageFilter = createHigherOrderComponent( BlockEdit => {
+	return ( props ) => {
+		if ( ! allowedBlocks.includes( props.name ) ) {
+			return <BlockEdit { ...props } />;
+		}
+
+		return (
+			<Fragment>
+				<BlockEdit { ...props } />
+				<BlockControls>
+					<MediaFilterControl
+						{ ...props }
+					/>
+				</BlockControls>
+			</Fragment>
+		);
+	};
+}, 'withGalleryExtension' );
+
+addFilter( 'editor.BlockEdit', 'coblocks/coreImageFilter', coreImageFilter );
+
+/**
+ * Add custom `has-filter-${ filter }` class to the core/image block
+ */
+const coreImageEditorStyles = createHigherOrderComponent( ( BlockListBlock ) => {
+	return ( props ) => {
+		console.log( props.name );
+		if ( ! allowedBlocks.includes( props.name ) ) {
+			return <BlockListBlock { ...props } />
+		}
+
+		const {
+			filter,
+		} = props.attributes;
+
+		let className = classnames(
+			{
+				[ `has-filter-${ filter }` ]: filter !== 'none',
+			}
+		)
+
+		return <BlockListBlock { ...props } className={ className } />;
+	};
+}, 'withStyleClasses' );
+
+addFilter( 'editor.BlockListBlock', 'coblocks/with-style-classes', coreImageEditorStyles );
+
+/**
+ * Add custom attribute to the core/image block
+ *
+ * @param {Object} settings Settings for the block.
+ *
+ * @return {Object} settings Modified settings.
+ */
+function imageFilterAttributes( settings ) {
+	if ( allowedBlocks.includes( settings.name ) && typeof settings.attributes !== 'undefined' ) {
+		settings.attributes = Object.assign( settings.attributes, {
+			filter: {
+				type: 'strig',
+				default: 'none',
+			}
+		} );
+	}
+
+	return settings;
+}
+
+addFilter( 'blocks.registerBlockType', 'coblocks/imageFilterAttributes', imageFilterAttributes );
+
+/**
+ * Add custom class in save element.
+ *
+ * @param {Object} extraProps Block element.
+ * @param {Object} blockType  Blocks object.
+ * @param {Object} attributes Blocks attributes.
+ *
+ * @return {Object} extraProps Modified block element.
+ */
+function imageBlockClass( extraProps, blockType, attributes ) {
+	if ( allowedBlocks.includes( blockType.name ) && typeof attributes.filter !== 'undefined' ) {
+		extraProps.className = classnames(
+			extraProps.className,
+			{
+				[ `has-filter-${ attributes.filter }` ]: attributes.filter !== 'none',
+			}
+		);
+	}
+	return extraProps;
+}
+
+addFilter( 'blocks.getSaveContent.extraProps', 'coblocks/imageApplyExtraClass', imageBlockClass );
