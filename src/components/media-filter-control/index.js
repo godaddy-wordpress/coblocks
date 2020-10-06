453f5dfa3e84b38b74116f5ae5fae53ba18014e2
Evan Herman <evan.m.herman@gmail.com>
2020-10-06 15:32:51 -0400

    Revert "Add image filter preview to the media-filter-control component (#1721)"
    This reverts commit 277b558d17f2a6c02f1201a052b06518644666cd.

--- src/components/media-filter-control/index.js
+++ src/components/media-filter-control/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import _ from 'lodash';
+import classnames from 'classnames';
 import {
 	FilterDarkIcon,
 	FilterGrayscaleIcon,
@@ -17,80 +17,31 @@ import {
  */
 import { __ } from '@wordpress/i18n';
 import { Component, Fragment } from '@wordpress/element';
-import { BlockPreview } from '@wordpress/block-editor';
 import {
-	MenuItem,
-	MenuGroup,
-	Popover,
 	Toolbar,
 	DropdownMenu,
 	Icon,
 } from '@wordpress/components';
-
-function PreviewImageFilterPopover( { hoveredFilter, selected } ) {
-	if ( ! hoveredFilter ) return null;
-
-	const block = _.cloneDeep( selected );
-
-	block.attributes.filter = hoveredFilter;
-
-	return (
-		<div className="block-editor-block-switcher__popover__preview__parent">
-			<div className="block-editor-block-switcher__popover__preview__container">
-				<Popover
-					className="block-editor-block-switcher__preview__popover"
-					position="bottom right"
-					focusOnMount={ false }
-				>
-					<div className="block-editor-block-switcher__preview coblocks__preview">
-						<div className="block-editor-block-switcher__preview-title">
-							{ __( 'Preview filter' ) }
-						</div>
-						<BlockPreview
-							autoHeight
-							blocks={ { ...block } }
-						/>
-					</div>
-				</Popover>
-			</div>
-		</div>
-	);
-}
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { BlockControls } from '@wordpress/block-editor';
 
 class MediaFilterControl extends Component {
-	constructor() {
-		super( ...arguments );
-
-		this.state = {
-			hoveredFilter: null
-		};
-	}
-
-	onChangeHoveredFilter = ( filter ) => {
-		this.setState( {
-			hoveredFilter: filter
-		} );
-	}
-
 	render() {
 		const {
 			attributes,
 			setAttributes,
-			selected,
 		} = this.props;
 
 		const {
 			filter,
 		} = attributes;
 
-		const { hoveredFilter } = this.state;
-
 		const filterControls = [
 			{
 				icon: <Icon icon={ FilterNoneIcon } />,
 				/* translators: image style */
 				title: __( 'Original', 'coblocks' ),
-				slug: 'original',
 				onClick: () => {
 					setAttributes( { filter: 'none' } );
 				},
@@ -100,7 +51,6 @@ class MediaFilterControl extends Component {
 				icon: <Icon icon={ FilterGrayscaleIcon } />,
 				/* translators: image style */
 				title: __( 'Grayscale filter', 'coblocks' ),
-				slug: 'grayscale',
 				onClick: () => {
 					setAttributes( { filter: 'grayscale' } );
 				},
@@ -110,7 +60,6 @@ class MediaFilterControl extends Component {
 				icon: <Icon icon={ FilterSepiaIcon } />,
 				/* translators: image style */
 				title: __( 'Sepia filter', 'coblocks' ),
-				slug: 'sepia',
 				onClick: () => {
 					setAttributes( { filter: 'sepia' } );
 				},
@@ -120,7 +69,6 @@ class MediaFilterControl extends Component {
 				icon: <Icon icon={ FilterSaturationIcon } />,
 				/* translators: image style */
 				title: __( 'Saturation filter', 'coblocks' ),
-				slug: 'saturation',
 				onClick: () => {
 					setAttributes( { filter: 'saturation' } );
 				},
@@ -130,7 +78,6 @@ class MediaFilterControl extends Component {
 				icon: <Icon icon={ FilterDarkIcon } />,
 				/* translators: image style */
 				title: __( 'Dim filter', 'coblocks' ),
-				slug: 'dim',
 				onClick: () => {
 					setAttributes( { filter: 'dim' } );
 				},
@@ -140,7 +87,6 @@ class MediaFilterControl extends Component {
 				icon: <Icon icon={ FilterVintageIcon } />,
 				/* translators: image style */
 				title: __( 'Vintage filter', 'coblocks' ),
-				slug: 'vintage',
 				onClick: () => {
 					setAttributes( { filter: 'vintage' } );
 				},
@@ -154,31 +100,9 @@ class MediaFilterControl extends Component {
 					hasArrowIndicator
 					icon={ <Icon icon={ FilterMainIcon } /> }
 					label={ __( 'Apply filter', 'coblocks' ) }
+					controls={ filterControls }
 					className="components-coblocks-media-filter"
-				>
-				{ ( { onClose } ) => (
-						<Fragment>
-							<MenuGroup>
-								<PreviewImageFilterPopover hoveredFilter={ hoveredFilter } selected={ selected } />
-								{
-									filterControls.map( ( filter ) => (
-										<MenuItem
-											role="menuitemradio"
-											label={ filter.title }
-											onClick={ filter.onClick }
-											onMouseEnter={ () => this.onChangeHoveredFilter( filter.slug ) }
-											onMouseLeave={ () => this.onChangeHoveredFilter( null ) }
-											isSelected={ filter === filter.slug }
-											icon={ filter.icon }
-											key={ `coblocks-image-filter-${ filter.slug }` }>
-											{ filter.title }
-										</MenuItem>
-									) )
-								}
-							</MenuGroup>
-						</Fragment>
-					) }
-				</DropdownMenu>
+				/>
 			</Toolbar>
 		);
 	}
