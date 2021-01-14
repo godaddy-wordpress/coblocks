22075adb45f00dde1895894d64fe7c61c8c9d9ee
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2021-01-14 14:10:14 -0700

    Introduce custom padding controls to core/group block (#1790)
    * custom padding group
    
    * add tests
    
    * improve maxValue logic

---
+++ src/extensions/padding-controls/padding-wrapper.js
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { cloneElement, isValidElement } from '@wordpress/element';
+
+/**
+ * Return an element Wrapped with padding Properties.
+ */
+function PaddingWrapper( { children, padding, paddingCustom, className } ) {
+	if ( ! isValidElement( children ) ) {
+		return children;
+	}
+
+	const attributes = {
+		className: classnames(
+			className,
+			children.props.className,
+			{ [ `has-${ padding }-padding` ]: padding }
+		),
+	};
+
+	if ( children.props.style ) {
+		attributes.style = {
+			...children.props.style,
+		};
+	}
+
+	if ( 'custom' === padding ) {
+		attributes.style = {
+			...attributes.style,
+			'--coblocks-custom-padding': `${ paddingCustom }em`,
+		};
+	}
+
+	return cloneElement( children, attributes );
+}
+
+PaddingWrapper.propTypes = {
+	padding: PropTypes.string,
+	paddingCustom: PropTypes.oneOfType( [ PropTypes.string, PropTypes.number ] ),
+};
+
+export default PaddingWrapper;
--- src/extensions/padding-controls/test/padding-controls.cypress.js
+++ src/extensions/padding-controls/test/padding-controls.cypress.js
@@ -14,11 +14,16 @@ describe( 'Extension: CoBlocks Padding Controls', function() {
 		helpers.openSettingsPanel( /group settings/i );
 
 		cy.get( '.components-base-control' ).contains( /padding/i ).parent().find( `.components-button[aria-label="None"]` ).click();
+		cy.get( '[data-type="core/group"]' ).should( 'have.class', `has-no-padding` );
 
-		[ 'Small', 'Medium', 'Large', 'Huge' ].forEach( ( padding ) => {
+		[ 'Small', 'Medium', 'Large', 'Huge', 'Custom' ].forEach( ( padding ) => {
 			cy.get( '.components-base-control' ).contains( /padding/i ).parent().find( `.components-button[aria-label="${ padding }"]` ).click();
 			cy.get( '[data-type="core/group"]' ).should( 'have.class', `has-${ padding.toLowerCase() }-padding` );
 		} );
+
+		// Test custom padding
+		cy.get( '.components-base-control' ).contains( /padding/i ).closest( '.components-panel__body' ).find( 'input.components-input-control__input' ).click().type( 5 ); // Panel body.
+		cy.get( '[data-type="core/group"]' ).should( 'have.attr', 'style', '--coblocks-custom-padding:5em;' );
 
 		helpers.checkForBlockErrors( 'core/group' );
 	} );
