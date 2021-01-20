f0e305a23935e2b3e184cb5503ada0c19ad4dc32
JR Tashjian <jrtashjian@gmail.com>
2021-01-20 16:02:26 -0500

    Disable animations when IntersectionObserver is not detected (#1799)

--- src/js/coblocks-animation.js
+++ src/js/coblocks-animation.js
@@ -1,18 +1,26 @@
 const elems = document.querySelectorAll( '.coblocks-animate' );
 
-const observer = new IntersectionObserver( ( entries ) => {
-	entries.forEach( ( entry ) => {
-		if ( ! entry.isIntersecting ) {
-			return;
-		}
+if ( 'IntersectionObserver' in window ) {
+	const observer = new IntersectionObserver( ( entries ) => {
+		entries.forEach( ( entry ) => {
+			if ( ! entry.isIntersecting ) {
+				return;
+			}
 
-		entry.target.classList.add( entry.target.dataset.coblocksAnimation );
-		observer.unobserve( entry.target );
+			entry.target.classList.add( entry.target.dataset.coblocksAnimation );
+			observer.unobserve( entry.target );
+		} );
+	}, {
+		threshold: [ 0.15 ],
 	} );
-}, {
-	threshold: [ 0.15 ],
-} );
 
-elems.forEach( ( elem ) => {
-	observer.observe( elem );
-} );
+	elems.forEach( ( elem ) => {
+		observer.observe( elem );
+	} );
+} else {
+	// Disable animations if IntersectionObserver is not available.
+	elems.forEach( ( node ) => {
+		node.classList.remove( 'coblocks-animate' );
+		delete ( node.dataset.coblocksAnimation );
+	} );
+}
