e61b058956e90f27d080f728f749534813c048c6
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2021-02-15 15:19:43 -0700

    Fix Gallery captions focus (#1823)
    * remove excess listener
    
    * re-add key listener
    
    * add rich text options tests
    
    * update helpers
    
    * update helpers
    
    * revert helpers changes
    
    * alter test
    
    * tweak test

--- src/blocks/gallery-carousel/test/gallery-carousel.cypress.js
+++ src/blocks/gallery-carousel/test/gallery-carousel.cypress.js
@@ -137,4 +137,37 @@ describe( 'Test CoBlocks Gallery Carousel Block', function() {
 
 		helpers.editPage();
 	} );
+
+	/**
+	 * Test that we can add image captions with rich text options
+	 */
+	it( 'Test carousel captions allow rich text controls.', function() {
+		helpers.addBlockToPost( 'coblocks/gallery-carousel', true );
+
+		cy.get( '[data-type="coblocks/gallery-carousel"]' )
+			.click()
+			.contains( /media library/i )
+			.click();
+
+		cy.get( '.media-modal-content' ).contains( /media library/i ).click();
+
+		cy.get( '.media-modal-content' ).find( 'li.attachment' )
+			.first( 'li' )
+			.click();
+
+		cy.get( '.media-frame-toolbar .media-toolbar-primary' ).then( ( mediaToolbar ) => {
+			if ( mediaToolbar.prop( 'outerHTML' ).includes( 'Insert gallery' ) ) { // wp 5.4
+				cy.get( 'button' ).contains( /insert gallery/i ).click();
+			} else { // pre wp 5.4
+				cy.get( 'button' ).contains( /create a new gallery/i ).click();
+				cy.get( 'button' ).contains( /insert gallery/i ).click();
+			}
+		} );
+
+		cy.get( '.block-editor-format-toolbar' ).should( 'not.exist' );
+
+		cy.get( '[data-type="coblocks/gallery-carousel"]' ).find( 'figcaption' ).focus();
+
+		cy.get( '.block-editor-format-toolbar' );
+	} );
 } );
