3860c84df0cdd177673abfcf92c5bef2b3310081
Evan Herman <evan.m.herman@gmail.com>
2021-03-02 16:47:57 -0500

    Tweak after testing

--- includes/class-coblocks-form.php
+++ includes/class-coblocks-form.php
@@ -971,7 +971,7 @@ class CoBlocks_Form {
 		$email_content = (string) apply_filters( 'coblocks_form_email_content', $this->email_content, $_POST, $post_id );
 
 		$reply_to  = isset( $_POST[ $email_field_id ]['value'] ) ? esc_html( $_POST[ $email_field_id ]['value'] ) : esc_html( get_bloginfo( 'admin_email' ) );
-		$from_name = isset( $_POST[ $name_field_id ]['value'] ) ? esc_html( $_POST[ $name_field_id ]['value'] ) : $reply_to;
+		$from_name = ( isset( $_POST[ $name_field_id ]['value'] ) && ! empty( $_POST[ $name_field_id ]['value'] ) ) ? esc_html( $_POST[ $name_field_id ]['value'] ) . " <{$reply_to}>" : /* translators: 1. Reply to email address; eg: email@example.com */ sprintf( __( 'Form Submission <%s>', 'coblocks' ), $reply_to );
 
 		/**
 		 * Filter the form email headers.
