997df7e4f5279596b60f4d0d749903fe97f3e1fd
Olivier Lafleur <olivier.lafleur@gmail.com>
2021-03-15 21:45:41 -0400

    Failing test to reproduce problem and fix callback function name

--- src/blocks/gist/test/gist.cypress.js
+++ src/blocks/gist/test/gist.cypress.js
@@ -79,4 +79,22 @@ describe( 'Test CoBlocks Gist Block', function() {
 
 		helpers.editPage();
 	} );
+
+	it( 'Test two gists in the edit page should render properly', function() {
+		helpers.addBlockToPost( 'coblocks/gist', true );
+
+		cy.get( '.wp-block-coblocks-gist textarea' ).invoke( 'val', gistUrl ).type( '{enter}' );
+
+		helpers.addBlockToPost( 'coblocks/gist' );
+
+		cy.get( '.wp-block-coblocks-gist textarea' ).invoke( 'val', gistUrl ).type( '{enter}' );
+
+		helpers.savePage();
+
+		helpers.viewPage();
+
+		helpers.editPage();
+
+		cy.get( '.wp-block-coblocks-gist' ).find( '.gist-file' ).should( 'have.length', 2 );
+	} );
 } );
