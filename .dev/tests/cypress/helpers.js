7bfc2a3f2832f4e664f03bd87590e418e8cdf671
JR Tashjian <jrtashjian@gmail.com>
2021-03-19 13:51:16 -0400

    Focus input before altering value

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -287,7 +287,7 @@ export function setInputValue( panelName, settingName, value, ignoreCase = true 
 		.then( ( $settingSection ) => {
 			cy.get( Cypress.$( $settingSection ).parent() )
 				.find( 'input[type="number"]' )
-				.click()
+				.focus()
 				.type( `{selectall}${ value }` );
 		} );
 }
