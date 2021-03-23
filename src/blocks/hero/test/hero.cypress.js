648e56f9871b5a0dc481cd4f354e2c2cbba0a229
Stephen Novosel <snovosel@godaddy.com>
2021-03-23 15:54:46 -0400

    add test for hero implementation of media replace flow

--- src/blocks/hero/test/hero.cypress.js
+++ src/blocks/hero/test/hero.cypress.js
@@ -92,4 +92,34 @@ describe( 'Test CoBlocks Hero Block', function() {
 
 		helpers.editPage();
 	} );
+
+	it( 'should close media upload flow on media selection', function() {
+		helpers.addBlockToPost( 'coblocks/hero', true );
+
+		cy.get( '.wp-block-coblocks-hero__inner' ).click();
+
+		cy.get( '.block-editor-block-toolbar .components-toolbar__control[aria-label="Add background image"]' ).click();
+
+		const fileName = helpers.upload.spec.fileName;
+
+		// Disable reason: cy.fixture should not return a value.
+		// eslint-disable-next-line jest/valid-expect-in-promise
+		cy.fixture( helpers.upload.spec.pathToFixtures + fileName, 'base64' ).then( ( fileContent ) => {
+			cy.get( '.media-frame input[type="file"]' )
+				.upload(
+					{ fileContent, fileName, mimeType: 'image/png' },
+					{ force: true },
+				);
+		} );
+
+		cy.get( '.media-toolbar-primary > .button' ).click();
+
+		cy.get( '.media-replace-flow button' ).click();
+
+		cy.get( '.components-popover__content' ).should( 'be.visible' );
+
+		cy.get( '.block-editor-media-replace-flow__media-upload-menu .components-menu-item__button' ).contains( 'Open Media Library' ).click();
+
+		cy.get( '.components-popover__content' ).should( 'not.be.visible' );
+	} );
 } );
