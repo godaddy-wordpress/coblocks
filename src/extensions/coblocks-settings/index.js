6d4e4bcf3e5bcfbe701576fc86c9b34df375b9c1
JR Tashjian <jrtashjian@gmail.com>
2021-03-24 14:34:27 -0400

    Refactor settings modal to utilize slot-fills

--- src/extensions/coblocks-settings/index.js
+++ src/extensions/coblocks-settings/index.js
@@ -9,52 +9,71 @@ import './styles/style.scss';
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { applyFilters } from '@wordpress/hooks';
 import { Fragment, useState } from '@wordpress/element';
 import { PluginMoreMenuItem } from '@wordpress/edit-post';
 import { registerPlugin, getPlugin, unregisterPlugin } from '@wordpress/plugins';
+import { Modal } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import CoBlocksSettingsModal from './coblocks-settings-modal';
-import registerCoBlocksSettingsStore from './coblocks-settings-store';
+import CoBlocksSettingsModalControl from './coblocks-settings-slot';
 
-if ( typeof coblocksSettings !== 'undefined' && coblocksSettings.coblocksSettingsEnabled ) {
-	const CoBlocksSettingsMenuItem = () => {
-		const [
-			isOpen,
-			setOpen,
-		] = useState( false );
+export default function CoBlocksSettings() {
+	const [ isOpen, setOpen ] = useState( false );
 
-		const openModal = () => setOpen( true );
-		const closeModal = () => setOpen( false );
+	const openModal = () => setOpen( true );
+	const closeModal = () => setOpen( false );
 
-		const props = {
-			isOpen,
-			openModal,
-			closeModal,
-		};
+	return (
+		<Fragment>
+			<PluginMoreMenuItem onClick={ openModal }>
+				{ __( 'Editor settings', 'coblocks' ) }
+			</PluginMoreMenuItem>
 
-		return (
-			<Fragment>
-				<PluginMoreMenuItem onClick={ openModal }>
-					{
-						applyFilters( 'coblocks-settings-title', __( 'Editor settings', 'coblocks' ) )
-					}
-				</PluginMoreMenuItem>
-				<CoBlocksSettingsModal { ...props } />
-			</Fragment>
+			{ isOpen && (
+				<Modal
+					className="coblocks-settings-modal"
+					title={ __( 'Editor settings', 'coblocks' ) }
+					closeLabel={ __( 'Close', 'coblocks' ) }
+					onRequestClose={ closeModal }
+				>
+					<section className="edit-post-preferences-modal__section">
+						<h2 className="edit-post-preferences-modal__section-title">
+							{ __( 'General' ) }
+						</h2>
 
-		);
-	};
+						<CoBlocksSettingsModalControl.Slot>
+							{
+								/**
+								 * Override with your implementation:
+								 *
+								 * import { registerPlugin } from '@wordpress/plugins';
+								 * import { CoBlocksSettingsModalControl } from './coblocks-settings-slot';
+								 *
+								 * registerPlugin( 'your-settings-modal-control', {
+								 *     render: () => (
+								 *         <CoBlocksSettingsModalControl>
+								 *             <YourCustomComponent />
+								 *         </CoBlocksSettingsModalControl>
+								 *     ),
+								 * } );
+								 */
+							}
+							<div>CoBlocksSettingsModalControl</div>
+						</CoBlocksSettingsModalControl.Slot>
+					</section>
+				</Modal>
+			) }
+		</Fragment>
+	);
+}
 
+if ( typeof coblocksSettings !== 'undefined' && !! parseInt( coblocksSettings.coblocksSettingsEnabled ) ) {
 	registerPlugin( 'coblocks-settings', {
 		icon: '',
-		render: CoBlocksSettingsMenuItem,
+		render: CoBlocksSettings,
 	} );
-
-	registerCoBlocksSettingsStore();
 } else if ( getPlugin( 'coblocks-settings' ) ) {
 	unregisterPlugin( 'coblocks-settings' );
 }
