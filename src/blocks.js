d960d8a9f38c8965f7581b285a9a3e4a084e4b89
JR Tashjian <jrtashjian@gmail.com>
2021-03-26 01:38:07 -0400

    Load new extension

--- src/blocks.js
+++ src/blocks.js
@@ -31,6 +31,7 @@ import './extensions/image-filter';
 import './extensions/coblocks-settings/';
 import './extensions/layout-selector';
 import './extensions/block-patterns';
+import './extensions/block-editor-settings';
 
 // Internal Extensions / Components
 import './components/gutter-control';
