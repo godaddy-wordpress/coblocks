3c3382171043733dbaed8e1186a0301ce3caf5f0
JR Tashjian <jrtashjian@gmail.com>
2021-04-06 11:18:04 -0400

    Create new post for each collage block test

--- src/blocks/gallery-collage/test/gallery-collage.cypress.js
+++ src/blocks/gallery-collage/test/gallery-collage.cypress.js
@@ -12,12 +12,18 @@ describe( 'Test CoBlocks Gallery Collage Block', function() {
 		caption: 'Caption Here',
 	};
 
+	const createNewPost = () => {
+		helpers.goTo( '/wp-admin/post-new.php?post_type=post' );
+		helpers.disableGutenbergFeatures();
+		helpers.addBlockToPost( 'coblocks/gallery-collage' );
+	};
+
 	/**
 	 * Test that we can add a gallery-collage block to the content, not add any images or
 	 * alter any settings, and are able to successfully save the block without errors.
 	 */
 	it( 'Test collage block saves with empty values.', function() {
-		helpers.addBlockToPost( 'coblocks/gallery-collage', true );
+		helpers.addBlockToPost( 'coblocks/gallery-collage' );
 		helpers.checkForBlockErrors( 'coblocks/gallery-collage' );
 	} );
 
@@ -27,8 +33,7 @@ describe( 'Test CoBlocks Gallery Collage Block', function() {
 	 */
 	it( 'Test collage block saves with image upload.', function() {
 		const { imageBase } = helpers.upload.spec;
-		helpers.addBlockToPost( 'coblocks/gallery-collage', true );
-
+		createNewPost();
 		cy.get( '[data-type="coblocks/gallery-collage"]' ).first().click();
 
 		helpers.upload.imageToBlock( 'coblocks/gallery-collage' );
@@ -43,8 +48,7 @@ describe( 'Test CoBlocks Gallery Collage Block', function() {
 	 * to successfully save the block without errors.
 	 */
 	it( 'Test collage block saves with images from media library.', function() {
-		helpers.addBlockToPost( 'coblocks/gallery-collage', true );
-
+		createNewPost();
 		cy.get( '[data-type="coblocks/gallery-collage"]' )
 			.first()
 			.click()
@@ -68,8 +72,7 @@ describe( 'Test CoBlocks Gallery Collage Block', function() {
 	 */
 	it( 'Test collage block saves with images captions.', function() {
 		const { caption } = galleryData;
-		helpers.addBlockToPost( 'coblocks/gallery-collage', true );
-
+		createNewPost();
 		cy.get( '[data-type="coblocks/gallery-collage"]' )
 			.first()
 			.click()
@@ -98,8 +101,7 @@ describe( 'Test CoBlocks Gallery Collage Block', function() {
 	 * Collage block has always-focused rich text options.
 	 */
 	it( 'Test collage captions allow rich text controls.', function() {
-		helpers.addBlockToPost( 'coblocks/gallery-collage', true );
-
+		createNewPost();
 		cy.get( '[data-type="coblocks/gallery-collage"]' )
 			.first()
 			.click()
