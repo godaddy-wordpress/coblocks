6f2a05b228aef29acc21819495cc2dc7adc0ac8c
AnthonyLedesma <anthonymledesma@gmail.com>
2021-04-12 12:00:28 -0700

    try helpers update

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -436,7 +436,7 @@ export function doEditorRedo() {
 export function openEditorSettingsModal() {
 	// Open "more" menu.
 	cy.get( '.edit-post-more-menu button' ).click();
-	cy.get( '.components-menu-item__button' ).contains( 'Editor settings' ).click();
+	cy.get( '.components-menu-group' ).contains( 'Editor settings' ).click();
 
 	cy.get( '.components-modal__frame' ).contains( 'Editor settings' ).should( 'exist' );
 }
