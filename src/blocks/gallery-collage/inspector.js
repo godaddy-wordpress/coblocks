2e0a1c90887bcadfed1f13cc7f4d365ace551a59
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2021-05-05 14:19:15 -0700

    refactor to hooks and fix tiled spacing (#1897)

--- src/blocks/gallery-collage/inspector.js
+++ src/blocks/gallery-collage/inspector.js
@@ -10,120 +10,114 @@ import GutterControl from '../../components/gutter-control/gutter-control';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl, SelectControl } from '@wordpress/components';
 
-/**
- * Inspector controls
- */
-class Inspector extends Component {
+const Inspector = ( props ) => {
 	/**
 	 * setCaptionStyleTo
 	 *
 	 * @param {string} value
 	 */
-	setCaptionStyleTo = ( value ) => {
-		this.props.setAttributes( { captionStyle: value } );
-	}
+	const setCaptionStyleTo = ( value ) => {
+		props.setAttributes( { captionStyle: value } );
+	};
 
 	/**
 	 * getCaptionsHelp
 	 *
 	 * @param {boolean} checked
 	 */
-	getCaptionsHelp( checked ) {
+	const getCaptionsHelp = ( checked ) => {
 		return checked ? __( 'Showing captions for each media item.', 'coblocks' ) : __( 'Toggle to show media captions.', 'coblocks' );
-	}
+	};
 
 	/**
 	 * setShadowTo
 	 *
 	 * @param {boolean} value
 	 */
-	setShadowTo = ( value ) => {
-		this.props.setAttributes( { shadow: value } );
-	}
+	const setShadowTo = ( value ) => {
+		props.setAttributes( { shadow: value } );
+	};
 
 	/**
 	 * getLightboxHelp
 	 *
 	 * @param {boolean} checked
 	 */
-	getLightboxHelp( checked ) {
+	const getLightboxHelp = ( checked ) => {
 		return checked ? __( 'Image lightbox is enabled.', 'coblocks' ) : __( 'Toggle to enable the image lightbox.', 'coblocks' );
-	}
+	};
 
-	render() {
-		const {
-			attributes,
-			setAttributes,
-			enableGutter,
-			enableCaptions,
-		} = this.props;
+	const {
+		attributes,
+		setAttributes,
+		enableGutter,
+		enableCaptions,
+	} = props;
 
-		const {
-			shadow,
-			captions,
-			captionStyle,
-			lightbox,
-		} = attributes;
+	const {
+		shadow,
+		captions,
+		captionStyle,
+		lightbox,
+	} = attributes;
 
-		const shadowOptions = [
-			{
-				value: 'sm',
-				/* translators: abbreviation for small size */
-				label: __( 'S', 'coblocks' ),
-				tooltip: __( 'Small', 'coblocks' ),
-			},
-			{
-				value: 'md',
-				/* translators: abbreviation for medium size */
-				label: __( 'M', 'coblocks' ),
-				tooltip: __( 'Medium', 'coblocks' ),
-			},
-			{
-				value: 'lg',
-				/* translators: abbreviation for large size */
-				label: __( 'L', 'coblocks' ),
-				tooltip: __( 'Large', 'coblocks' ),
-			},
-		];
+	const shadowOptions = [
+		{
+			value: 'sm',
+			/* translators: abbreviation for small size */
+			label: __( 'S', 'coblocks' ),
+			tooltip: __( 'Small', 'coblocks' ),
+		},
+		{
+			value: 'md',
+			/* translators: abbreviation for medium size */
+			label: __( 'M', 'coblocks' ),
+			tooltip: __( 'Medium', 'coblocks' ),
+		},
+		{
+			value: 'lg',
+			/* translators: abbreviation for large size */
+			label: __( 'L', 'coblocks' ),
+			tooltip: __( 'Large', 'coblocks' ),
+		},
+	];
 
-		return (
-			<InspectorControls>
-				<PanelBody title={ __( 'Collage settings', 'coblocks' ) }>
-					{ enableGutter && <GutterControl { ...this.props } /> }
-					{ ! enableGutter && <OptionSelectorControl
-						label={ __( 'Shadow', 'coblocks' ) }
-						options={ shadowOptions }
-						currentOption={ shadow }
-						showNoneOption
-						onChange={ ( newShadow ) => setAttributes( { shadow: newShadow } ) }
-					/> }
-					<ToggleControl
-						label={ __( 'Lightbox', 'coblocks' ) }
-						checked={ !! lightbox }
-						onChange={ () => setAttributes( { lightbox: ! lightbox } ) }
-						help={ this.getLightboxHelp }
-					/>
-					{ enableCaptions && <ToggleControl
-						label={ __( 'Captions', 'coblocks' ) }
-						checked={ !! captions }
-						onChange={ () => setAttributes( { captions: ! captions } ) }
-						help={ this.getCaptionsHelp }
-					/> }
-					{ captions && <SelectControl
-						label={ __( 'Caption style', 'coblocks' ) }
-						value={ captionStyle }
-						onChange={ this.setCaptionStyleTo }
-						options={ captionOptions }
-					/> }
-				</PanelBody>
-				<GalleryLinkSettings { ...this.props } />
-			</InspectorControls>
-		);
-	}
-}
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Collage settings', 'coblocks' ) }>
+				{ enableGutter && <GutterControl { ...props } /> }
+				{ ! enableGutter && <OptionSelectorControl
+					label={ __( 'Shadow', 'coblocks' ) }
+					options={ shadowOptions }
+					currentOption={ shadow }
+					showNoneOption
+					onChange={ ( newShadow ) => setShadowTo( newShadow ) }
+				/> }
+				<ToggleControl
+					label={ __( 'Lightbox', 'coblocks' ) }
+					checked={ !! lightbox }
+					onChange={ () => setAttributes( { lightbox: ! lightbox } ) }
+					help={ getLightboxHelp }
+				/>
+				{ enableCaptions && <ToggleControl
+					label={ __( 'Captions', 'coblocks' ) }
+					checked={ !! captions }
+					onChange={ () => setAttributes( { captions: ! captions } ) }
+					help={ getCaptionsHelp }
+				/> }
+				{ captions && <SelectControl
+					label={ __( 'Caption style', 'coblocks' ) }
+					value={ captionStyle }
+					onChange={ setCaptionStyleTo }
+					options={ captionOptions }
+				/> }
+			</PanelBody>
+			<GalleryLinkSettings { ...props } />
+		</InspectorControls>
+	);
+};
 
 export default Inspector;
