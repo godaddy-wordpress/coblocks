f634a2b2bb1a9ec36a6b99a7f707d60ce143d74c
Stephen Novosel <novosel991@gmail.com>
2021-05-10 10:49:01 -0400

    remove test rtl

--- .dev/tests/phpunit/src/blocks/post-carousel/test-index.php
+++ .dev/tests/phpunit/src/blocks/post-carousel/test-index.php
@@ -42,41 +42,6 @@ class CoBlocks_Post_Carousel_Index_Tests extends WP_UnitTestCase {
 	/**
 	 * Test the social block markup returns correctly
 	 */
-	public function test_coblocks_render_post_carousel_block_rtl() {
-		switch_to_locale( 'ar' );
-
-		$attributes = [
-			'className' => 'test-class-name',
-			'columns' => 2,
-			'postsToShow' => 4,
-			'order' => 'date',
-			'orderBy' => 'desc',
-			'postFeedType' => 'internal',
-		];
-
-		$post_id = wp_insert_post(
-			[
-				'post_author'  => 1,
-				'post_content' => '',
-				'post_title'   => 'CoBlocks Post Carousel Block',
-				'post_status'  => 'publish',
-			]
-		);
-
-		global $post;
-		$post = get_post( $post_id );
-
-
-
-		$this->assertEquals(
-			'<div class="wp-block-coblocks-post-carousel test-class-name"><div class="coblocks-slick pb-8" data-slick="{&quot;slidesToScroll&quot;:1,&quot;arrow&quot;:true,&quot;slidesToShow&quot;:2,&quot;infinite&quot;:true,&quot;adaptiveHeight&quot;:false,&quot;draggable&quot;:true,&quot;rtl&quot;:true,&quot;responsive&quot;:[{&quot;breakpoint&quot;:1024,&quot;settings&quot;:{&quot;slidesToShow&quot;:3}},{&quot;breakpoint&quot;:600,&quot;settings&quot;:{&quot;slidesToShow&quot;:2}},{&quot;breakpoint&quot;:480,&quot;settings&quot;:{&quot;slidesToShow&quot;:1}}]}"></div></div>',
-			coblocks_render_post_carousel_block( $attributes )
-		);
-	}
-
-	/**
-	 * Test the social block markup returns correctly
-	 */
 	public function test_coblocks_render_post_carousel_block() {
 		$attributes = [
 			'className' => 'test-class-name',
