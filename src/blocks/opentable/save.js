149b48a5036b056c0fd07237c48060b0e87f27fa
Emile Hamwey <ehamwey@procads.net>
2021-06-29 14:43:39 -0400

    Block markup saving, block type changed to layout

--- src/blocks/opentable/save.js
+++ src/blocks/opentable/save.js
@@ -1,41 +1,42 @@
 
 export default function save( { attributes, className } ) {
-	console.log( className );
+	if ( attributes.className === undefined || attributes.className === null ) {
+		return null;
+	}
 	return (
-		<div></div>
-		// <div className={ className }>
-		// 	<div className="iframe__overflow-wrapper">
-		// 		{ { wide: (
-		// 			<iframe
-		// 				frameBorder="0"
-		// 				style={ { width: '840px', height: '200px', display: 'block', margin: 'auto' } }
-		// 				title="open table frame"
-		// 				src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=wide&overlay=false&insideiframe=true` }
-		// 			/>
-		// 		), tall: (
-		// 			<iframe
-		// 				frameBorder="0"
-		// 				style={ { width: '288px', height: '490px', display: 'block', margin: 'auto' } }
-		// 				title="open table frame"
-		// 				src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=tall&overlay=false&insideiframe=true` }
-		// 			/>
-		// 		), standard: (
-		// 			<iframe
-		// 				frameBorder="0"
-		// 				style={ { width: '224px', height: '301px', display: 'block', margin: 'auto' } }
-		// 				title="open table frame"
-		// 				src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=standard&overlay=false&insideiframe=true` }
-		// 			/>
-		// 		), button: (
-		// 			<iframe
-		// 				frameBorder="0"
-		// 				style={ { width: '210px', height: '115px', display: 'block', margin: 'auto' } }
-		// 				title="open table frame"
-		// 				src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=button&theme=standard&overlay=false&insideiframe=true` }
-		// 			/>
-		// 		) }[ className.substring( className.lastIndexOf( '-' ) + 1 ) ]
-		// 		}
-		// 	</div>
-		// </div>
+		<div className={ className }>
+			<div className="iframe__overflow-wrapper">
+				{ { wide: (
+					<iframe
+						frameBorder="0"
+						style={ { width: '840px', height: '200px', display: 'block', margin: 'auto' } }
+						title="open table frame"
+						src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=wide&overlay=false&insideiframe=true` }
+					/>
+				), tall: (
+					<iframe
+						frameBorder="0"
+						style={ { width: '288px', height: '490px', display: 'block', margin: 'auto' } }
+						title="open table frame"
+						src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=tall&overlay=false&insideiframe=true` }
+					/>
+				), standard: (
+					<iframe
+						frameBorder="0"
+						style={ { width: '224px', height: '301px', display: 'block', margin: 'auto' } }
+						title="open table frame"
+						src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=standard&theme=standard&overlay=false&insideiframe=true` }
+					/>
+				), button: (
+					<iframe
+						frameBorder="0"
+						style={ { width: '210px', height: '115px', display: 'block', margin: 'auto' } }
+						title="open table frame"
+						src={ `//www.opentable.com/widget/reservation/canvas?rid=${ attributes.restaurantID }$&domain=com&type=button&theme=standard&overlay=false&insideiframe=true` }
+					/>
+				) }[ attributes.className.substring( attributes.className.lastIndexOf( '-' ) + 1 ) ]
+				}
+			</div>
+		</div>
 	);
 }
