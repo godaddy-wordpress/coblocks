d01e8aa350f0380688b2370f9ed3461ce76772ce
Olivier Lafleur <olafleur@godaddy.com>
2021-07-22 14:03:22 -0400

    Convert Author Inspector Component to React hooks

--- src/blocks/author/inspector.js
+++ src/blocks/author/inspector.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import { InspectorControls, ContrastChecker, PanelColorSettings, withColors, FontSizePicker, withFontSizes } from '@wordpress/block-editor';
 import { PanelBody, withFallbackStyles } from '@wordpress/components';
@@ -21,62 +21,60 @@ const applyFallbackStyles = withFallbackStyles( ( node, ownProps ) => {
 	};
 } );
 
-class Inspector extends Component {
-	render() {
-		const {
-			backgroundColor,
-			setBackgroundColor,
-			setTextColor,
-			textColor,
-			setFontSize,
-			fallbackFontSize,
-			fallbackTextColor,
-			fallbackBackgroundColor,
-			fontSize,
-		} = this.props;
+const Inspector = ( props ) => {
+	const {
+		backgroundColor,
+		setBackgroundColor,
+		setTextColor,
+		textColor,
+		setFontSize,
+		fallbackFontSize,
+		fallbackTextColor,
+		fallbackBackgroundColor,
+		fontSize,
+	} = props;
 
-		return (
-			<Fragment>
-				<InspectorControls>
-					<PanelBody title={ __( 'Author settings', 'coblocks' ) } className="blocks-font-size">
-						<FontSizePicker
-							label={ 'test' }
-							fallbackFontSize={ fallbackFontSize }
-							value={ fontSize.size }
-							onChange={ setFontSize }
-						/>
-					</PanelBody>
-					<PanelColorSettings
-						title={ __( 'Color settings', 'coblocks' ) }
-						initialOpen={ false }
-						colorSettings={ [
-							{
-								value: backgroundColor.color,
-								onChange: setBackgroundColor,
-								label: __( 'Background color', 'coblocks' ),
-							},
-							{
-								value: textColor.color,
-								onChange: setTextColor,
-								label: __( 'Text color', 'coblocks' ),
-							},
-						] }
-					>
-						<ContrastChecker
-							{ ...{
-								textColor: textColor.color,
-								backgroundColor: backgroundColor.color,
-								fallbackTextColor,
-								fallbackBackgroundColor,
-							} }
-							fontSize={ fontSize.size }
-						/>
-					</PanelColorSettings>
-				</InspectorControls>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={ __( 'Author settings', 'coblocks' ) } className="blocks-font-size">
+					<FontSizePicker
+						label={ 'test' }
+						fallbackFontSize={ fallbackFontSize }
+						value={ fontSize.size }
+						onChange={ setFontSize }
+					/>
+				</PanelBody>
+				<PanelColorSettings
+					title={ __( 'Color settings', 'coblocks' ) }
+					initialOpen={ false }
+					colorSettings={ [
+						{
+							value: backgroundColor.color,
+							onChange: setBackgroundColor,
+							label: __( 'Background color', 'coblocks' ),
+						},
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: __( 'Text color', 'coblocks' ),
+						},
+					] }
+				>
+					<ContrastChecker
+						{ ...{
+							textColor: textColor.color,
+							backgroundColor: backgroundColor.color,
+							fallbackTextColor,
+							fallbackBackgroundColor,
+						} }
+						fontSize={ fontSize.size }
+					/>
+				</PanelColorSettings>
+			</InspectorControls>
+		</Fragment>
+	);
+};
 
 export default compose( [
 	withColors( 'backgroundColor', { textColor: 'color' } ),
