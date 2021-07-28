bd48e7741a89e573c2bcf3b98aa06af1d76d9528
Olivier Lafleur <olivier.lafleur@gmail.com>
2021-07-28 14:04:06 -0400

    Convert Author block to React hooks (#1947)
    * Convert Author Edit component to React hooks
    
    * Convert Author Inspector Component to React hooks
    
    * remove fragments
    
    Co-authored-by: Olivier Lafleur <olafleur@godaddy.com>
    Co-authored-by: AnthonyLedesma <anthonymledesma@gmail.com>

--- src/blocks/author/controls.js
+++ src/blocks/author/controls.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 import { BlockControls, MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
 import { Toolbar, Button } from '@wordpress/components';
 import { edit } from '@wordpress/icons';
@@ -22,7 +22,7 @@ class Controls extends Component {
 		const onSelectImage = ( media ) => setAttributes( { imgUrl: media.url, imgId: media.id } );
 
 		return (
-			<Fragment>
+			<>
 				<BlockControls>
 					{ imgUrl &&
 						<MediaUploadCheck>
@@ -44,7 +44,7 @@ class Controls extends Component {
 						</MediaUploadCheck>
 					}
 				</BlockControls>
-			</Fragment>
+			</>
 		);
 	}
 }
