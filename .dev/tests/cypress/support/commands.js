dd7b7769461ff99789975d3297c741385fee3d38
AnthonyLedesma <anthonymledesma@gmail.com>
2021-07-30 11:04:02 -0700

    discard unhandled exceptions in Cypress

--- .dev/tests/cypress/support/commands.js
+++ .dev/tests/cypress/support/commands.js
@@ -41,3 +41,13 @@ function hexStringToByte( str ) {
 
 	return new Uint8Array( a );
 }
+
+/**
+ * Starting in Cypress 8.1.0 Unhandled Exceptions now cause tests to fail.
+ * Sometimes unhandled exceptions occur in Core that do not effect the UX created by CoBlocks.
+ * We discard unhandled exceptions and pass the test as long as assertions continue expectedly.
+ */
+Cypress.on( 'uncaught:exception', () => {
+	// returning false here prevents Cypress from failing the test.
+	return false;
+} );
