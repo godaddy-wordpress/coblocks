9268ec0a24f34b1b47f6c2bedb014e1fc598d41b
Emile Hamwey <ehamwey@procads.net>
2021-08-05 15:26:06 -0400

    add more e2e tests

--- src/blocks/opentable/test/opentable.cypress.js
+++ src/blocks/opentable/test/opentable.cypress.js
@@ -8,25 +8,25 @@ describe( 'Test CoBlocks OpenTable Block', function() {
 	 * Test that we can add an OpenTable block to the content, not add anything and save the block
 	 * without errors.
 	 */
-	// it( 'Test OpenTable block saves with empty values.', function() {
-	// 	helpers.addBlockToPost( 'coblocks/opentable', true );
+	it( 'Test OpenTable block saves with empty values.', function() {
+		helpers.addBlockToPost( 'coblocks/opentable', true );
 
-	// 	helpers.savePage();
+		helpers.savePage();
 
-	// 	helpers.checkForBlockErrors( 'coblocks/opentable' );
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
 
-	// 	helpers.viewPage();
+		helpers.viewPage();
 
-	// 	cy.get( '.wp-block-coblocks-opentable' ).should( 'not.exist' );
+		cy.get( '.wp-block-coblocks-opentable' ).should( 'not.exist' );
 
-	// 	helpers.editPage();
-	// } );
+		helpers.editPage();
+	} );
 
 	/**
 	 * Test that we can add an OpenTable block to the content, add a restaurant and then
-	 * successfully save the block without errors. Then test going back to the block's edit state.
+	 * successfully save the block without errors.
 	 */
-	it( 'Test OpenTable block saves with single restaurant selection then test going back to the block edit state.', function() {
+	it( 'Test OpenTable block saves with single restaurant selection.', function() {
 		helpers.addBlockToPost( 'coblocks/opentable', true );
 
 		cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
@@ -44,12 +44,35 @@ describe( 'Test CoBlocks OpenTable Block', function() {
 		helpers.viewPage();
 
 		helpers.editPage();
+	} );
+
+	it( 'Test OpenTable changing to Tall style.', function() {
+		helpers.addBlockToPost( 'coblocks/opentable', true );
+
+		cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
+
+		cy.wait( 1000 );
+
+		cy.get( '.components-form-token-field__suggestion' ).first().click();
+
+		cy.get( 'button[type="submit"]' ).click();
 
 		helpers.selectBlock( 'opentable' );
 
-		cy.get( 'button[id="id-aseal6-6"]' ).should( 'exist' ).click();
-	} );
+		cy.get( 'button[aria-label="Edit Restaurant"]' ).should( 'exist' ).click();
 
+		cy.get( '.block-editor-block-styles__item[aria-label="Tall"]' ).should( 'exist' ).click();
+
+		helpers.savePage();
+
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
+
+		helpers.viewPage();
+
+		cy.get( 'div[class="wp-block-coblocks-opentable iframe__overflow-wrapper is-style-tall"]' ).should( 'exist' );
+
+		helpers.editPage();
+	} );
 	/**
 	 * Test that the No results found message is displayed when no restaurants are found.
 	 */
@@ -61,5 +84,13 @@ describe( 'Test CoBlocks OpenTable Block', function() {
 		cy.wait( 1000 );
 
 		cy.get( '.components-notice__content' ).should( 'contain', 'No results found' );
+
+		helpers.savePage();
+
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
+
+		helpers.viewPage();
+
+		helpers.editPage();
 	} );
 } );
