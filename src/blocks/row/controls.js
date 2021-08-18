12949dfacb5921f5d966f9ce966216db3904bed4
Stephen Novosel <novosel991@gmail.com>
2021-08-18 15:45:36 -0400

    refactor row block and related components to use react hook architecture

--- src/blocks/row/controls.js
+++ src/blocks/row/controls.js
@@ -14,19 +14,27 @@ import { BackgroundControls } from '../../components/background';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
 import { BlockControls, BlockVerticalAlignmentToolbar } from '@wordpress/block-editor';
 import { ToolbarGroup } from '@wordpress/components';
 
-class Controls extends Component {
+const Controls = ( props ) => {
+	const {
+		clientId,
+		attributes,
+		setAttributes,
+		getBlocksByClientId,
+		updateBlockAttributes,
+	} = props;
+
+	const {
+		columns,
+		layout,
+		verticalAlignment,
+	} = attributes;
+
 	// Switches the icon based on the layout selected,
 	// Fallback is the default layout icon.
-	layoutIcon() {
-		const {
-			columns,
-			layout,
-		} = this.props.attributes;
-
+	const layoutIcon = () => {
 		let selectedRows = 1;
 
 		if ( columns ) {
@@ -40,83 +48,67 @@ class Controls extends Component {
 		return map( layoutOptions[ selectedRows ], ( { key, smallIcon } ) => (
 			( key === layout ) ? smallIcon : ''
 		) );
+	};
+
+	let selectedRows = 1;
+
+	if ( columns ) {
+		selectedRows = parseInt( columns.toString().split( '-' ) );
 	}
 
-	render() {
-		const {
-			clientId,
-			attributes,
-			setAttributes,
-			getBlocksByClientId,
-			updateBlockAttributes,
-		} = this.props;
-
-		const {
-			columns,
-			layout,
-			verticalAlignment,
-		} = attributes;
-
-		let selectedRows = 1;
-
-		if ( columns ) {
-			selectedRows = parseInt( columns.toString().split( '-' ) );
-		}
-
-		if ( ! layout ) {
-			return null;
-		}
-
-		return (
-			<Fragment>
-				<BlockControls>
-					{ ( columns && selectedRows > 1 ) &&
-						<ToolbarGroup
-							isCollapsed={ true }
-							icon={ this.layoutIcon() }
-							label={ __( 'Change row block layout', 'coblocks' ) }
-							controls={ map( layoutOptions[ selectedRows ], ( { name, key, smallIcon } ) => {
-								return {
-									title: name,
-									key,
-									icon: smallIcon,
-									isActive: key === layout,
-									onClick: () => {
-										const selectedWidth = key.toString().split( '-' );
-										const children = getBlocksByClientId( clientId );
-										setAttributes( {
-											layout: key,
-										} );
-										if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
-											map( children[ 0 ].innerBlocks, ( blockProps, index ) => (
-												updateBlockAttributes( blockProps.clientId, { width: selectedWidth[ index ] } )
-											) );
-										}
-									},
-								};
-							} ) }
-						>
-						</ToolbarGroup>
-					}
-					<BlockVerticalAlignmentToolbar
-						onChange={ ( alignment ) => {
-							const children = getBlocksByClientId( clientId );
-							setAttributes( { verticalAlignment: alignment } );
-							if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
-								map( children[ 0 ].innerBlocks, ( blockProps ) => (
-									updateBlockAttributes( blockProps.clientId, { verticalAlignment: alignment } )
-								) );
-							}
-						} }
-						value={ verticalAlignment }
-					/>
-					{ layout &&
-						BackgroundControls( this.props )
-					}
-				</BlockControls>
-			</Fragment>
-		);
+	if ( ! layout ) {
+		return null;
 	}
-}
+
+	return (
+		<>
+			<BlockControls>
+				{ ( columns && selectedRows > 1 ) &&
+					<ToolbarGroup
+						isCollapsed={ true }
+						icon={ layoutIcon() }
+						label={ __( 'Change row block layout', 'coblocks' ) }
+						controls={ map( layoutOptions[ selectedRows ], ( { name, key, smallIcon } ) => {
+							return {
+								title: name,
+								key,
+								icon: smallIcon,
+								isActive: key === layout,
+								onClick: () => {
+									const selectedWidth = key.toString().split( '-' );
+									const children = getBlocksByClientId( clientId );
+									setAttributes( {
+										layout: key,
+									} );
+									if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
+										map( children[ 0 ].innerBlocks, ( blockProps, index ) => (
+											updateBlockAttributes( blockProps.clientId, { width: selectedWidth[ index ] } )
+										) );
+									}
+								},
+							};
+						} ) }
+					>
+					</ToolbarGroup>
+				}
+				<BlockVerticalAlignmentToolbar
+					onChange={ ( alignment ) => {
+						const children = getBlocksByClientId( clientId );
+						setAttributes( { verticalAlignment: alignment } );
+						if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
+							map( children[ 0 ].innerBlocks, ( blockProps ) => (
+								updateBlockAttributes( blockProps.clientId, { verticalAlignment: alignment } )
+							) );
+						}
+					} }
+					value={ verticalAlignment }
+				/>
+				{ layout &&
+					BackgroundControls( props )
+				}
+			</BlockControls>
+		</>
+	);
+};
 
 export default Controls;
--- src/blocks/row/inspector.js
+++ src/blocks/row/inspector.js
@@ -17,7 +17,6 @@ import GutterControl from '../../components/gutter-control/gutter-control';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import { InspectorControls, PanelColorSettings } from '@wordpress/block-editor';
 import { PanelBody, withFallbackStyles } from '@wordpress/components';
@@ -42,207 +41,207 @@ const FallbackStyles = withFallbackStyles( ( node, ownProps ) => {
 
 /**
  * Inspector controls
+ *
+ * @param {Object} props
  */
-class Inspector extends Component {
-	render() {
-		const {
-			attributes,
-			backgroundColor,
-			clientId,
-			setAttributes,
-			setBackgroundColor,
-			setTextColor,
-			textColor,
-			updateBlockAttributes,
-			getBlocksByClientId,
-		} = this.props;
+const Inspector = ( props ) => {
+	const {
+		attributes,
+		backgroundColor,
+		clientId,
+		setAttributes,
+		setBackgroundColor,
+		setTextColor,
+		textColor,
+		updateBlockAttributes,
+		getBlocksByClientId,
+	} = props;
 
-		const {
-			columns,
-			layout,
-			marginBottom,
-			marginLeft,
-			marginRight,
-			marginSize,
-			marginTop,
-			marginBottomTablet,
-			marginLeftTablet,
-			marginRightTablet,
-			marginTopTablet,
-			marginBottomMobile,
-			marginLeftMobile,
-			marginRightMobile,
-			marginTopMobile,
-			marginSyncUnits,
-			marginSyncUnitsTablet,
-			marginSyncUnitsMobile,
-			marginUnit,
-			paddingBottom,
-			paddingLeft,
-			paddingRight,
-			paddingSize,
-			paddingTop,
-			paddingBottomTablet,
-			paddingLeftTablet,
-			paddingRightTablet,
-			paddingTopTablet,
-			paddingBottomMobile,
-			paddingLeftMobile,
-			paddingRightMobile,
-			paddingTopMobile,
-			paddingSyncUnits,
-			paddingSyncUnitsTablet,
-			paddingSyncUnitsMobile,
-			paddingUnit,
-			hasMarginControl,
-		} = attributes;
+	const {
+		columns,
+		layout,
+		marginBottom,
+		marginLeft,
+		marginRight,
+		marginSize,
+		marginTop,
+		marginBottomTablet,
+		marginLeftTablet,
+		marginRightTablet,
+		marginTopTablet,
+		marginBottomMobile,
+		marginLeftMobile,
+		marginRightMobile,
+		marginTopMobile,
+		marginSyncUnits,
+		marginSyncUnitsTablet,
+		marginSyncUnitsMobile,
+		marginUnit,
+		paddingBottom,
+		paddingLeft,
+		paddingRight,
+		paddingSize,
+		paddingTop,
+		paddingBottomTablet,
+		paddingLeftTablet,
+		paddingRightTablet,
+		paddingTopTablet,
+		paddingBottomMobile,
+		paddingLeftMobile,
+		paddingRightMobile,
+		paddingTopMobile,
+		paddingSyncUnits,
+		paddingSyncUnitsTablet,
+		paddingSyncUnitsMobile,
+		paddingUnit,
+		hasMarginControl,
+	} = attributes;
 
-		let selectedRows = 1;
+	let selectedRows = 1;
 
-		if ( columns ) {
-			selectedRows = parseInt( columns.toString().split( '-' ) );
-		}
-
-		const handleEvent = ( key ) => {
-			const selectedWidth = key.toString().split( '-' );
-			const children = getBlocksByClientId( clientId );
-			setAttributes( {
-				layout: key,
-			} );
-
-			if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
-				map( children[ 0 ].innerBlocks, ( blockProps, index ) => (
-					updateBlockAttributes( blockProps.clientId, { width: selectedWidth[ index ] } )
-				) );
-			}
-		};
-
-		return (
-			<Fragment>
-				<InspectorControls>
-					{ ( columns && selectedRows >= 1 ) &&
-					<Fragment>
-						{ layout &&
-						<Fragment>
-							{ selectedRows > 1 &&
-								<PanelBody title={ __( 'Styles', 'coblocks' ) } initialOpen={ false }>
-									<div className={ classnames(
-										'block-editor-block-styles',
-										'coblocks-editor-block-styles',
-									) } >
-										{ map( layoutOptions[ selectedRows ], ( { name, key, icon } ) => (
-											<div
-												key={ `style-${ name }` }
-												className={ classnames(
-													'block-editor-block-styles__item',
-													{ 'is-active': layout === key },
-												) }
-												onClick={ () => handleEvent( key ) }
-												onKeyPress={ () => handleEvent( key ) }
-												role="button"
-												tabIndex="0"
-												aria-label={ name }
-											>
-												<div className="block-editor-block-styles__item-preview">
-													{ icon }
-												</div>
-											</div>
-										) ) }
-									</div>
-								</PanelBody>
-							}
-							{ layout &&
-								<Fragment>
-									<PanelBody title={ __( 'Row settings', 'coblocks' ) }>
-										{ selectedRows >= 2 && <GutterControl { ...this.props } /> }
-										<DimensionsControl { ...this.props }
-											type={ 'padding' }
-											label={ __( 'Padding', 'coblocks' ) }
-											help={ __( 'Space inside of the container.', 'coblocks' ) }
-											valueTop={ paddingTop }
-											valueRight={ paddingRight }
-											valueBottom={ paddingBottom }
-											valueLeft={ paddingLeft }
-											valueTopTablet={ paddingTopTablet }
-											valueRightTablet={ paddingRightTablet }
-											valueBottomTablet={ paddingBottomTablet }
-											valueLeftTablet={ paddingLeftTablet }
-											valueTopMobile={ paddingTopMobile }
-											valueRightMobile={ paddingRightMobile }
-											valueBottomMobile={ paddingBottomMobile }
-											valueLeftMobile={ paddingLeftMobile }
-											unit={ paddingUnit }
-											syncUnits={ paddingSyncUnits }
-											syncUnitsTablet={ paddingSyncUnitsTablet }
-											syncUnitsMobile={ paddingSyncUnitsMobile }
-											dimensionSize={ paddingSize }
-										/>
-										{ hasMarginControl &&
-											<DimensionsControl { ...this.props }
-												type={ 'margin' }
-												label={ __( 'Margin', 'coblocks' ) }
-												help={ __( 'Space around the container.', 'coblocks' ) }
-												valueTop={ marginTop }
-												valueRight={ marginRight }
-												valueBottom={ marginBottom }
-												valueLeft={ marginLeft }
-												valueTopTablet={ marginTopTablet }
-												valueRightTablet={ marginRightTablet }
-												valueBottomTablet={ marginBottomTablet }
-												valueLeftTablet={ marginLeftTablet }
-												valueTopMobile={ marginTopMobile }
-												valueRightMobile={ marginRightMobile }
-												valueBottomMobile={ marginBottomMobile }
-												valueLeftMobile={ marginLeftMobile }
-												unit={ marginUnit }
-												syncUnits={ marginSyncUnits }
-												syncUnitsTablet={ marginSyncUnitsTablet }
-												syncUnitsMobile={ marginSyncUnitsMobile }
-												dimensionSize={ marginSize }
-											/>
-										}
-									</PanelBody>
-									<PanelColorSettings
-										title={ __( 'Color settings', 'coblocks' ) }
-										initialOpen={ false }
-										colorSettings={ [
-											{
-												value: backgroundColor.color,
-												onChange: ( nextBackgroundColor ) => {
-													setBackgroundColor( nextBackgroundColor );
-
-													if ( ! paddingSize || paddingSize === 'no' ) {
-														setAttributes( { paddingSize: 'medium' } );
-													}
-
-													if ( ! nextBackgroundColor ) {
-														setAttributes( { paddingSize: 'no' } );
-													}
-												},
-												label: __( 'Background color', 'coblocks' ),
-											},
-											{
-												value: textColor.color,
-												onChange: setTextColor,
-												label: __( 'Text color', 'coblocks' ),
-											},
-										] }
-									>
-									</PanelColorSettings>
-									<BackgroundPanel { ...this.props }
-										hasOverlay={ true }
-									/>
-								</Fragment>
-							}
-						</Fragment>
-						}
-					</Fragment>
-					}
-				</InspectorControls>
-			</Fragment>
-		);
+	if ( columns ) {
+		selectedRows = parseInt( columns.toString().split( '-' ) );
 	}
-}
+
+	const handleEvent = ( key ) => {
+		const selectedWidth = key.toString().split( '-' );
+		const children = getBlocksByClientId( clientId );
+		setAttributes( {
+			layout: key,
+		} );
+
+		if ( typeof children[ 0 ].innerBlocks !== 'undefined' ) {
+			map( children[ 0 ].innerBlocks, ( blockProps, index ) => (
+				updateBlockAttributes( blockProps.clientId, { width: selectedWidth[ index ] } )
+			) );
+		}
+	};
+
+	return (
+		<>
+			<InspectorControls>
+				{ ( columns && selectedRows >= 1 ) &&
+				<>
+					{ layout &&
+					<>
+						{ selectedRows > 1 &&
+							<PanelBody title={ __( 'Styles', 'coblocks' ) } initialOpen={ false }>
+								<div className={ classnames(
+									'block-editor-block-styles',
+									'coblocks-editor-block-styles',
+								) } >
+									{ map( layoutOptions[ selectedRows ], ( { name, key, icon } ) => (
+										<div
+											key={ `style-${ name }` }
+											className={ classnames(
+												'block-editor-block-styles__item',
+												{ 'is-active': layout === key },
+											) }
+											onClick={ () => handleEvent( key ) }
+											onKeyPress={ () => handleEvent( key ) }
+											role="button"
+											tabIndex="0"
+											aria-label={ name }
+										>
+											<div className="block-editor-block-styles__item-preview">
+												{ icon }
+											</div>
+										</div>
+									) ) }
+								</div>
+							</PanelBody>
+						}
+						{ layout &&
+							<>
+								<PanelBody title={ __( 'Row settings', 'coblocks' ) }>
+									{ selectedRows >= 2 && <GutterControl { ...props } /> }
+									<DimensionsControl { ...props }
+										type={ 'padding' }
+										label={ __( 'Padding', 'coblocks' ) }
+										help={ __( 'Space inside of the container.', 'coblocks' ) }
+										valueTop={ paddingTop }
+										valueRight={ paddingRight }
+										valueBottom={ paddingBottom }
+										valueLeft={ paddingLeft }
+										valueTopTablet={ paddingTopTablet }
+										valueRightTablet={ paddingRightTablet }
+										valueBottomTablet={ paddingBottomTablet }
+										valueLeftTablet={ paddingLeftTablet }
+										valueTopMobile={ paddingTopMobile }
+										valueRightMobile={ paddingRightMobile }
+										valueBottomMobile={ paddingBottomMobile }
+										valueLeftMobile={ paddingLeftMobile }
+										unit={ paddingUnit }
+										syncUnits={ paddingSyncUnits }
+										syncUnitsTablet={ paddingSyncUnitsTablet }
+										syncUnitsMobile={ paddingSyncUnitsMobile }
+										dimensionSize={ paddingSize }
+									/>
+									{ hasMarginControl &&
+										<DimensionsControl { ...props }
+											type={ 'margin' }
+											label={ __( 'Margin', 'coblocks' ) }
+											help={ __( 'Space around the container.', 'coblocks' ) }
+											valueTop={ marginTop }
+											valueRight={ marginRight }
+											valueBottom={ marginBottom }
+											valueLeft={ marginLeft }
+											valueTopTablet={ marginTopTablet }
+											valueRightTablet={ marginRightTablet }
+											valueBottomTablet={ marginBottomTablet }
+											valueLeftTablet={ marginLeftTablet }
+											valueTopMobile={ marginTopMobile }
+											valueRightMobile={ marginRightMobile }
+											valueBottomMobile={ marginBottomMobile }
+											valueLeftMobile={ marginLeftMobile }
+											unit={ marginUnit }
+											syncUnits={ marginSyncUnits }
+											syncUnitsTablet={ marginSyncUnitsTablet }
+											syncUnitsMobile={ marginSyncUnitsMobile }
+											dimensionSize={ marginSize }
+										/>
+									}
+								</PanelBody>
+								<PanelColorSettings
+									title={ __( 'Color settings', 'coblocks' ) }
+									initialOpen={ false }
+									colorSettings={ [
+										{
+											value: backgroundColor.color,
+											onChange: ( nextBackgroundColor ) => {
+												setBackgroundColor( nextBackgroundColor );
+
+												if ( ! paddingSize || paddingSize === 'no' ) {
+													setAttributes( { paddingSize: 'medium' } );
+												}
+
+												if ( ! nextBackgroundColor ) {
+													setAttributes( { paddingSize: 'no' } );
+												}
+											},
+											label: __( 'Background color', 'coblocks' ),
+										},
+										{
+											value: textColor.color,
+											onChange: setTextColor,
+											label: __( 'Text color', 'coblocks' ),
+										},
+									] }
+								>
+								</PanelColorSettings>
+								<BackgroundPanel { ...props }
+									hasOverlay={ true }
+								/>
+							</>
+						}
+					</>
+					}
+				</>
+				}
+			</InspectorControls>
+		</>
+	);
+};
 
 export default compose( [
 	applyWithColors,
