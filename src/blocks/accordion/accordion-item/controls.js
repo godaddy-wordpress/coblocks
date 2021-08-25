b4d6ec07d2bf061b444c39fa0dfda8486e7156ca
Steve Novosel <novosel991@gmail.com>
2021-08-25 11:02:15 -0400

    refactor Accordion block to use hooks (#1979)
    * refactor Accordion block to use hooks
    
    * use react hooks for all accordion block components

--- src/blocks/accordion/accordion-item/controls.js
+++ src/blocks/accordion/accordion-item/controls.js
@@ -7,39 +7,36 @@ import { OpenIcon as icon } from '@godaddy-wordpress/coblocks-icons';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
 import { BlockControls } from '@wordpress/block-editor';
 import { Toolbar, Icon } from '@wordpress/components';
 
-class Controls extends Component {
-	render() {
-		const {
-			attributes,
-			setAttributes,
-		} = this.props;
+const Controls = ( props ) => {
+	const {
+		attributes,
+		setAttributes,
+	} = props;
 
-		const {
-			open,
-		} = attributes;
+	const {
+		open,
+	} = attributes;
 
-		const customControls = [
-			{
-				icon: <Icon icon={ icon } />,
-				/* translators: toggle label to display the accordion open */
-				title: __( 'Display as open', 'coblocks' ),
-				onClick: () => setAttributes( { open: ! open } ),
-				isActive: open === true,
-			},
-		];
+	const customControls = [
+		{
+			icon: <Icon icon={ icon } />,
+			/* translators: toggle label to display the accordion open */
+			title: __( 'Display as open', 'coblocks' ),
+			onClick: () => setAttributes( { open: ! open } ),
+			isActive: open === true,
+		},
+	];
 
-		return (
-			<Fragment>
-				<BlockControls>
-					<Toolbar controls={ customControls } />
-				</BlockControls>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<>
+			<BlockControls>
+				<Toolbar controls={ customControls } />
+			</BlockControls>
+		</>
+	);
+};
 
 export default Controls;
--- src/blocks/accordion/inspector.js
+++ src/blocks/accordion/inspector.js
@@ -3,47 +3,46 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
 
 /**
  * Inspector controls
+ *
+ * @param {Object} props
  */
-class Inspector extends Component {
-	render() {
-		const {
-			attributes,
-			setAttributes,
-		} = this.props;
+const Inspector = ( props ) => {
+	const {
+		attributes,
+		setAttributes,
+	} = props;
 
-		const {
-			polyfill,
-		} = attributes;
+	const {
+		polyfill,
+	} = attributes;
 
-		return (
-			<Fragment>
-				<InspectorControls>
-					<PanelBody title={ __( 'Accordion settings', 'coblocks' ) }>
-						<ToggleControl
-							label={ __( 'Internet Explorer support', 'coblocks' ) }
-							checked={ !! polyfill }
-							help={ ! polyfill ? __( 'Add support for Internet Explorer by loading a JavaScript polyfill.', 'coblocks' ) : __( 'Supporting Internet Explorer by loading a JavaScript polyfill on this page.', 'coblocks' ) }
-							onChange={ () => {
-								setAttributes( { polyfill: ! polyfill } );
-								// Save values to metadata.
-								wp.data.dispatch( 'core/editor' ).editPost( {
-									meta: {
-										_coblocks_accordion_ie_support: '\'' + ! polyfill + '\'',
-									},
-								} );
-							} }
-						/>
-					</PanelBody>
-				</InspectorControls>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Accordion settings', 'coblocks' ) }>
+					<ToggleControl
+						label={ __( 'Internet Explorer support', 'coblocks' ) }
+						checked={ !! polyfill }
+						help={ ! polyfill ? __( 'Add support for Internet Explorer by loading a JavaScript polyfill.', 'coblocks' ) : __( 'Supporting Internet Explorer by loading a JavaScript polyfill on this page.', 'coblocks' ) }
+						onChange={ () => {
+							setAttributes( { polyfill: ! polyfill } );
+							// Save values to metadata.
+							wp.data.dispatch( 'core/editor' ).editPost( {
+								meta: {
+									_coblocks_accordion_ie_support: '\'' + ! polyfill + '\'',
+								},
+							} );
+						} }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		</>
+	);
+};
 
 export default Inspector;
