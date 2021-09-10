2c24c6258eabd4f7635a8b1b51775acfc252463c
Steve Novosel <novosel991@gmail.com>
2021-09-10 15:16:56 -0400

    refactor food and drink to a functional component (#2003)
    Co-authored-by: Olivier Lafleur <olafleur@godaddy.com>

--- src/blocks/food-and-drinks/controls.js
+++ src/blocks/food-and-drinks/controls.js
@@ -6,33 +6,28 @@ import HeadingToolbar from '../../components/heading-toolbar';
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
 import { BlockControls } from '@wordpress/block-editor';
 
-class Controls extends Component {
-	render() {
-		const {
-			attributes,
-			onChangeHeadingLevel,
-		} = this.props;
+const Controls = ( props ) => {
+	const {
+		attributes,
+		onChangeHeadingLevel,
+	} = props;
 
-		const {
-			headingLevel,
-		} = attributes;
+	const {
+		headingLevel,
+	} = attributes;
 
-		return (
-			<Fragment>
-				<BlockControls>
-					<HeadingToolbar
-						minLevel={ 2 }
-						maxLevel={ 5 }
-						selectedLevel={ headingLevel }
-						onChange={ onChangeHeadingLevel }
-					/>
-				</BlockControls>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<BlockControls>
+			<HeadingToolbar
+				minLevel={ 2 }
+				maxLevel={ 5 }
+				selectedLevel={ headingLevel }
+				onChange={ onChangeHeadingLevel }
+			/>
+		</BlockControls>
+	);
+};
 
 export default Controls;
