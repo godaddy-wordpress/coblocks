f7d76bb8cca6828be9a0007d26c8a8dbded7de52
Olivier Lafleur <olafleur@godaddy.com>
2021-09-14 15:33:03 -0400

    controls.js to hooks

--- src/blocks/buttons/controls.js
+++ src/blocks/buttons/controls.js
@@ -1,29 +1,26 @@
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
 import { BlockControls, AlignmentToolbar } from '@wordpress/block-editor';
 
-class Controls extends Component {
-	render() {
-		const {
-			attributes,
-			setAttributes,
-		} = this.props;
+const Controls = ( props ) => {
+	const {
+		attributes,
+		setAttributes,
+	} = props;
 
-		const { contentAlign } = attributes;
+	const { contentAlign } = attributes;
 
-		return (
-			<Fragment>
-				<BlockControls>
-					<AlignmentToolbar
-						value={ contentAlign }
-						onChange={ ( nextContentAlign ) => setAttributes( { contentAlign: nextContentAlign } ) }
-					/>
-				</BlockControls>
-			</Fragment>
-		);
-	}
-}
+	return (
+		<>
+			<BlockControls>
+				<AlignmentToolbar
+					value={ contentAlign }
+					onChange={ ( nextContentAlign ) => setAttributes( { contentAlign: nextContentAlign } ) }
+				/>
+			</BlockControls>
+		</>
+	);
+};
 
 export default Controls;
