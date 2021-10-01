5ea05c98bd33005b7e0a25a8b76422e42900cae0
Olivier Lafleur <olafleur@godaddy.com>
2021-10-01 10:37:51 -0400

    update snapshots

--- src/blocks/gallery-collage/save.js
+++ src/blocks/gallery-collage/save.js
@@ -33,8 +33,7 @@ const save = ( { attributes } ) => {
 	} );
 
 	const getAriaLabel = () => {
-		// eslint-disable-next-line no-restricted-syntax
-		return __( `Gallery with ${ images.length } pictures - collage`, 'coblocks' );
+		return __( `Gallery Collage`, 'coblocks' );
 	};
 
 	return (
@@ -86,7 +85,7 @@ const save = ( { attributes } ) => {
 									<figure className={ classes }>
 										{ href ? <a href={ href } target={ target } rel={ rel }>{ img }</a> : img }
 										{ captions && image.caption && (
-											<RichText.Content tagName="figcaption" className="wp-block-coblocks-gallery-collage__caption" value={ image.caption } aria-label={ 'Caption' } />
+											<RichText.Content tagName="figcaption" className="wp-block-coblocks-gallery-collage__caption" value={ image.caption } />
 										) }
 									</figure>
 									}
