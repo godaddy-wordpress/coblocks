5b4658fb7cb76d4dd842e7ceb632eece750b89af
Olivier Lafleur <olafleur@godaddy.com>
2021-10-05 23:09:11 -0400

    deprecation for gallery collage

--- src/blocks/gallery-collage/deprecated.js
+++ src/blocks/gallery-collage/deprecated.js
@@ -42,6 +42,109 @@ const deprecated =
 	},
 	save( { attributes } ) {
 		const {
+			animation,
+			captions,
+			captionStyle,
+			filter,
+			images,
+			lightbox,
+			linkTo,
+			rel,
+			shadow,
+			target,
+		} = attributes;
+
+		const classes = classnames( 'wp-block-coblocks-gallery-collage__figure', {
+			[ `shadow-${ shadow }` ]: shadow && shadow !== 'none',
+		} );
+
+		return (
+			<GutterWrapper { ...attributes }>
+				<div className={ classnames( {
+					[ `has-filter-${ filter }` ]: filter !== 'none',
+					[ `has-caption-style-${ captionStyle }` ]: captions && captionStyle !== undefined,
+					'has-lightbox': lightbox,
+				} ) }>
+					<ul>
+						{ images.sort( ( a, b ) => parseInt( a.index ) - parseInt( b.index ) )
+							// Limit images output based on he selector style.
+							.filter( ( image ) => parseInt( image.index ) < ( [ 'is-style-tiled', 'is-style-layered' ].includes( attributes.className ) ? 4 : 5 ) )
+							.map( ( image, index ) => {
+								let href;
+
+								switch ( linkTo ) {
+									case 'media':
+										href = image.url;
+										break;
+									case 'attachment':
+										href = image.link;
+										break;
+								}
+
+								// If an image has a custom link, override the linkTo selection.
+								if ( image.imgLink ) {
+									href = image.imgLink;
+								}
+
+								const itemClasses = classnames(
+									'wp-block-coblocks-gallery-collage__item',
+									`item-${ index + 1 }`,
+									{
+										[ `coblocks-animate` ]: animation,
+									}
+								);
+								const imgClasses = classnames( image.id && [ `wp-image-${ image.id }` ] );
+								const img = typeof image.url === 'undefined' ? null : ( <img src={ image.url } alt={ image.alt } data-index={ image.index } data-id={ image.id } data-imglink={ image.imgLink } data-link={ image.link } className={ imgClasses } /> );
+
+								return (
+									<li
+										key={ `image-${ index }` }
+										className={ itemClasses }
+										data-coblocks-animation={ animation }
+									>
+										{ img &&
+										<figure className={ classes }>
+											{ href ? <a href={ href } target={ target } rel={ rel }>{ img }</a> : img }
+											{ captions && image.caption && (
+												<RichText.Content tagName="figcaption" className="wp-block-coblocks-gallery-collage__caption" value={ image.caption } />
+											) }
+										</figure>
+										}
+									</li>
+								);
+							} )
+						}
+					</ul>
+				</div>
+			</GutterWrapper>
+		);
+	},
+},
+{
+	attributes: {
+		...GalleryAttributes,
+		...metadata.attributes,
+		images: {
+			...metadata.attributes.images,
+			query: {
+				...metadata.attributes.images.query,
+				caption: {
+					...metadata.attributes.images.query.caption,
+					type: 'array',
+				},
+			},
+		},
+		gutter: {
+			type: 'string',
+			default: 'small',
+		},
+		gutterCustom: {
+			type: 'string',
+			default: '3',
+		},
+	},
+	save( { attributes } ) {
+		const {
 			captions,
 			captionStyle,
 			filter,
