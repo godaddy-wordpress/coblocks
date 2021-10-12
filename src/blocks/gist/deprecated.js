68ed220fa3036af4992a206ba876615ba3ae0af6
JR Tashjian <jrtashjian@gmail.com>
2021-10-12 18:54:44 -0400

    Deprecate Gist block.

--- src/blocks/gist/deprecated.js
+++ src/blocks/gist/deprecated.js
@@ -12,11 +12,12 @@ import metadata from './block.json';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { RichText } from '@wordpress/block-editor';
 
 const deprecated = [
 	{
 		attributes: metadata.attributes,
-		save( { attributes } ) {
+		save: ( { attributes } ) => {
 			const { url, file, meta } = attributes;
 
 			const classes = classnames( {
@@ -35,6 +36,44 @@ const deprecated = [
 					<noscript>
 						<a href={ noscriptSrc }>{ __( 'View this gist on GitHub', 'coblocks' ) }</a>
 					</noscript>
+				</div>
+			);
+		},
+	},
+	{
+		attributes: Object.assign( {}, metadata.attributes, {
+			caption: {
+				type: 'string',
+				source: 'html',
+				selector: 'figcaption',
+			},
+		} ),
+		save: ( { attributes } ) => {
+			const { url, file, meta, caption } = attributes;
+
+			if ( 'undefined' === typeof url ) {
+				return;
+			}
+
+			const classes = classnames( {
+				'no-meta': ! meta,
+			} );
+
+			const src = file ? `${ url }.js?file=${ file }` : `${ url }.js`;
+
+			const noscriptSrc = file
+				? `${ url }#file-${ file.replace( '.', '-' ) }`
+				: `${ url }`;
+
+			return (
+				<div className={ classes }>
+					<script src={ src } />
+					<noscript>
+						<a href={ noscriptSrc }>{ __( 'View this gist on GitHub', 'coblocks' ) }</a>
+					</noscript>
+					{ ! RichText.isEmpty( caption ) && (
+						<RichText.Content tagName="figcaption" value={ caption } />
+					) }
 				</div>
 			);
 		},
