3bdbd41badf08738ca15e8067039617a21415eeb
olafleur-godaddy <85255688+olafleur-godaddy@users.noreply.github.com>
2021-10-14 09:31:43 -0400

    Be explicit in our use of WordPress Unsafe APIs (#2083)
    * Be explicit in our use of WordPress Unsafe APIs
    
    * remove parameter which is there by default
    
    * Add disable reason comment

--- src/blocks/posts/edit.js
+++ src/blocks/posts/edit.js
@@ -20,6 +20,8 @@ import { __ } from '@wordpress/i18n';
 import { compose, usePrevious } from '@wordpress/compose';
 import { RawHTML, useState, useEffect, useRef } from '@wordpress/element';
 import { addQueryArgs } from '@wordpress/url';
+// Disable reason: We choose to use unsafe APIs in our codebase.
+// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 import { dateI18n, format, __experimentalGetSettings } from '@wordpress/date';
 import { withSelect } from '@wordpress/data';
 import { BlockControls, RichText } from '@wordpress/block-editor';
--- src/blocks/services/service/edit.js
+++ src/blocks/services/service/edit.js
@@ -15,6 +15,8 @@ import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import { useEffect } from '@wordpress/element';
 import {
+	// Disable reason: We choose to use unsafe APIs in our codebase.
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalImageURLInputUI as ImageURLInputUI,
 	BlockControls,
 	InnerBlocks,
