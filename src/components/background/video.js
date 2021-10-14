7013b1abd917a09349f7b63c07e0164cf58fa992
Olivier Lafleur <olafleur@godaddy.com>
2021-10-14 15:49:23 -0400

    fix unique key for media card

--- src/components/background/video.js
+++ src/components/background/video.js
@@ -14,7 +14,7 @@ function BackgroundVideo( attributes ) {
 
 	return [
 		backgroundType === 'video'
-			? <div className="coblocks-video-bg position-absolute overflow-hidden w-full h-full pin-t pin-r pin-b pin-l">
+			? <div key={ backgroundImg } className="coblocks-video-bg position-absolute overflow-hidden w-full h-full pin-t pin-r pin-b pin-l">
 				<video className="w-full h-full bg-center-center object-cover object-position" playsinline="" autoPlay="" muted={ videoMuted } loop={ videoLoop } src={ backgroundImg }></video>
 			</div>
 			: null,
