37d51ff025b29a4d2bc1c4dcde05fb537a5325c0
JR Tashjian <jrtashjian@gmail.com>
2021-10-15 13:03:01 -0400

    Start deprecating for server-rendered solution

--- src/blocks/shape-divider/deprecated.js
+++ src/blocks/shape-divider/deprecated.js
@@ -119,6 +119,60 @@ const deprecated = [
 		},
 		save: deprecatedSVGs,
 	},
+	{
+		attributes: {
+			...metadata.attributes,
+			coblocks: {
+				type: 'object',
+			},
+			justAdded: {
+				type: 'boolean',
+				default: 'true',
+			},
+		},
+		save: ( { attributes, className } ) => {
+			const {
+				backgroundColor,
+				backgroundHeight,
+				coblocks,
+				color,
+				customBackgroundColor,
+				customColor,
+				horizontalFlip,
+				shapeHeight,
+				verticalFlip,
+			} = attributes;
+
+			const shapeClass = getColorClassName( 'color', color );
+			const backgroundClass = getColorClassName( 'background-color', backgroundColor );
+
+			let classes = classnames(
+				className, {
+					'is-vertically-flipped': verticalFlip,
+					'is-horizontally-flipped': horizontalFlip,
+					[ shapeClass ]: shapeClass,
+					[ backgroundClass ]: backgroundClass,
+				} );
+
+			if ( coblocks && ( typeof coblocks.id !== 'undefined' ) ) {
+				classes = classnames( classes, `coblocks-shape-divider-${ coblocks.id }` );
+			}
+
+			const styles = {
+				backgroundColor: backgroundClass ? undefined : customBackgroundColor,
+				color: shapeClass ? undefined : customColor,
+			};
+
+			return (
+				<div className={ classes } style={ styles } aria-hidden="true">
+					<div className="wp-block-coblocks-shape-divider__svg-wrapper" style={ { minHeight: shapeHeight } }>
+						{ getDividerFromStyle( attributes.className ) }
+					</div>
+					<div className="wp-block-coblocks-shape-divider__alt-wrapper" style={ { minHeight: backgroundHeight } }></div>
+				</div>
+			);
+		},
+	},
 ];
 
 // Deprecations for Advanced Spacing controls.
