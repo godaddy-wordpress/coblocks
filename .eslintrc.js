9fce8fd2747c9b2a5f6a0ca2a6c80b04317befc7
Jason Lemay <jlemay@godaddy.com>
2021-10-20 10:19:21 -0400

    Use GoDaddy ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,53 @@
+/* eslint-disable sort-keys */
 module.exports = {
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
-	],
+	extends: [ '@godaddy-wordpress/eslint-config' ],
+
+	// CoBlocks config
 	env: {
 		browser: true,
 		'cypress/globals': true,
 		jest: true,
 	},
-	globals: {
-		page: true,
-	},
 	plugins: [
 		'cypress',
 		'chai-friendly',
 	],
-	rules: {
-		'jest/expect-expect': [
-			'error',
-			{
-				assertFunctionNames: [ 'expect', 'cy.get', 'helpers.checkForBlockErrors' ],
-			},
-		],
+
+	// Specific Globals used in CoBlocks
+	globals: {
+		page: true,
 	},
+
+	// Specific Rules for CoBlocks
+	// - Before adding a new rule here, see if it could be
+	// - added in '@godaddy-wordpress/eslint-config' instead
+	rules: {
+		// Jest plugin rules
+		'jest/expect-expect': [ 'error', {
+			assertFunctionNames: [ 'expect', 'cy.get', 'helpers.checkForBlockErrors' ],
+		} ],
+
+		// Temp override to slowly change everything
+		'import/no-extraneous-dependencies': 0,
+		'sort-imports': [ 'warn', {
+			allowSeparatedGroups: true,
+			ignoreCase: true,
+			memberSyntaxSortOrder: [ 'all', 'single', 'multiple', 'none' ],
+		} ],
+		'sort-keys': [ 'warn', 'asc', { natural: true } ],
+		'sort-vars': [ 'warn', { ignoreCase: true } ],
+		'react/destructuring-assignment': [ 'warn', 'always' ],
+		'react/jsx-no-target-blank': [ 'warn' ],
+		'react/jsx-no-undef': [ 'warn' ],
+		'react/jsx-sort-default-props': [ 'warn', { ignoreCase: true } ],
+		'react/jsx-sort-props': [ 'warn', { ignoreCase: true } ],
+		'react/no-deprecated': [ 'warn' ],
+		'react/sort-prop-types': [ 'warn', { ignoreCase: true } ],
+	},
+	overrides: [
+		{
+			files: [ 'deprecated.js', 'save.js' ],
+			rules: { 'sort-keys': 'warn' },
+		},
+	],
 };
