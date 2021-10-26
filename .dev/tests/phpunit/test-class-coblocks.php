5ec6ab19ff52a6798fb68e0d95dea410ee01b156
JR Tashjian <jrtashjian@gmail.com>
2021-10-26 15:35:33 -0400

    Fix phpunit asset test

--- .dev/tests/phpunit/test-class-coblocks.php
+++ .dev/tests/phpunit/test-class-coblocks.php
@@ -207,8 +207,8 @@ class CoBlocks_Tests extends WP_UnitTestCase {
 				'src/js/coblocks-slick-initializer.js',
 			],
 			'css' => [
-				'dist/coblocks-editor.css',
-				'dist/coblocks-style.css',
+				'dist/coblocks.css',
+				'dist/style-coblocks.css',
 			],
 		];
 
