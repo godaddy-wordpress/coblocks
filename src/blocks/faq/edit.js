c7745ddef81c72eff447b6cb89700ca139205208
Jason Lemay <jlemay@godaddy.com>
2021-10-28 11:12:08 -0400

    Update

--- src/blocks/faq/edit.js
+++ src/blocks/faq/edit.js
@@ -1,14 +1,7 @@
 /**
- * External dependencies
- */
- import classnames from 'classnames';
-
-/**
  * Internal dependencies.
  */
 import CustomAppender from './appender';
-import GutterWrapper from './../../components/gutter-control/gutter-wrapper';
-import Inspector from './inspector';
 
 /**
  * WordPress dependencies.
@@ -17,9 +10,9 @@ import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { createBlock } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/block-editor';
-import { useDispatch, useSelect, withDispatch, withSelect } from '@wordpress/data';
+import { useSelect, withDispatch, withSelect } from '@wordpress/data';
 
-const ALLOWED_BLOCKS = [ 
+const ALLOWED_BLOCKS = [
 	'coblocks/faq-item',
 	'core/heading',
 ];
@@ -27,6 +20,7 @@ const ALLOWED_BLOCKS = [
 const TEMPLATE = [
 	[ 'core/heading', {
 		align: 'center',
+		className: 'wp-block-coblocks-faq__title',
 		level: 3,
 		placeholder: __( 'FAQ title', 'coblocks' ),
 	} ],
@@ -35,34 +29,14 @@ const TEMPLATE = [
 
 const Edit = ( props ) => {
 	const {
-		attributes,
 		className,
 		clientId,
 		insertBlock,
-		isSelected,
-		setAttributes,
 	} = props;
-
-	const {
-		columns,
-		gutter,
-	} = attributes;
 
 	const { innerBlocks } = useSelect( ( select ) => ( {
 		innerBlocks: select( 'core/block-editor' ).getBlocks( clientId ),
 	} ) );
-
-	const {
-		selectBlock,
-	} = useDispatch( 'core/block-editor' );
-
-	const setColumns = ( value ) => {
-		setAttributes( { columns: parseInt( value ) } );
-	};
-
-	const setGutter = ( value ) => {
-		setAttributes( { gutter: value } );
-	};
 
 	const insertNewItem = () => {
 		const newItem = createBlock( 'coblocks/faq-item' );
@@ -78,31 +52,15 @@ const Edit = ( props ) => {
 		insertBlock( newHeading, innerBlocks.length, clientId );
 	};
 
-	const innerClasses = classnames(
-		'wp-block-coblocks-faq__inner', 
-		{
-			'has-columns': columns > 1,
-			[ `has-${ columns }-columns` ]: columns,
-			'has-responsive-columns': columns > 1,
-		}
-	);
-
 	return (
 		<>
-			{ isSelected && (
-				<Inspector selectBlock={ selectBlock } { ...props } />
-			) }
 			<div className={ className }>
-				<div className={ innerClasses }>
-					<GutterWrapper { ...attributes }>
-						<InnerBlocks
-							allowedBlocks={ ALLOWED_BLOCKS }
-							renderAppender={ () => <CustomAppender onAddNewItem={ insertNewItem } onAddNewHeading={ insertNewHeading } /> }
-							template={ TEMPLATE }
-							templateInsertUpdatesSelection={ false }
-						/>
-					</GutterWrapper>
-				</div>
+				<InnerBlocks
+					allowedBlocks={ ALLOWED_BLOCKS }
+					renderAppender={ () => <CustomAppender onAddNewItem={ insertNewItem } onAddNewHeading={ insertNewHeading } /> }
+					template={ TEMPLATE }
+					templateInsertUpdatesSelection={ false }
+				/>
 			</div>
 		</>
 	);
@@ -112,19 +70,15 @@ export default compose( [
 
 	withSelect( ( select, props ) => {
 		const {
-			getBlockOrder,
+			getBlocks,
 			getBlockRootClientId,
 			getBlockSelectionStart,
-			getBlocks,
-			getBlocksByClientId,
 		} = select( 'core/block-editor' );
 
 		// Get clientId of the parent block.
 		const parentClientId = getBlockRootClientId( getBlockSelectionStart() );
 
 		return {
-			getBlockOrder,
-			getBlocksByClientId,
 			innerBlocks: getBlocks( props.clientId ),
 			isSelected: props.isSelected || props.clientId === parentClientId,
 		};
@@ -133,39 +87,11 @@ export default compose( [
 	withDispatch( ( dispatch ) => {
 		const {
 			insertBlock,
-			insertBlocks,
-			updateBlockAttributes,
 		} = dispatch( 'core/block-editor' );
 
 		return {
 			insertBlock,
-			insertBlocks,
-			updateBlockAttributes,
 		};
 	} ),
-
-	// Ensure there is a minimum of one coblocks/food-item innerBlock per column set.
-	/*( WrappedComponent ) => ( ownProps ) => {
-		// This is a newly added block if we have zero innerBlocks. We want the TEMPLATE definition to be used in this case.
-		if ( ownProps.innerBlocks.length > 0 ) {
-			const foodItemBlocksCount = ownProps.innerBlocks.reduce( ( acc, cur ) => acc + ( cur.name === 'coblocks/food-item' ), 0 );
-
-			// Add a new block if the count is less than the columns set.
-			// We don't need a loop here because this will trigger a component update as soon as we insert a block (triggering this HOC again).
-			if ( foodItemBlocksCount < ownProps.attributes.columns ) {
-				ownProps.insertBlock(
-					createBlock( 'coblocks/food-item', {
-						showImage: ownProps.attributes.showImages,
-						showPrice: ownProps.attributes.showPrices,
-					} ),
-					ownProps.innerBlocks.length,
-					ownProps.clientId,
-					false
-				);
-			}
-		}
-
-		return <WrappedComponent { ...ownProps } />;
-	},*/
 
 ] )( Edit );
