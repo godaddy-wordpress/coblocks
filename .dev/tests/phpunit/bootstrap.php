fa867cbc75eff625ea975efac72b77806dd3b0bd
JR Tashjian <jrtashjian@gmail.com>
2021-11-11 15:45:47 -0500

    Use WP_PHPUNIT only within wp-env.

--- .dev/tests/phpunit/bootstrap.php
+++ .dev/tests/phpunit/bootstrap.php
@@ -17,8 +17,8 @@ if ( 'build' === getenv( 'LOCAL_DIR' ) ) {
 // Try the WP_TESTS_DIR environment variable first.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-// Next, try the WP_PHPUNIT composer package.
-if ( ! $_tests_dir ) {
+// Next, try the WP_PHPUNIT composer package if we're inside wp-env.
+if ( ! $_tests_dir && 'tests-mysql' === getenv( 'WORDPRESS_DB_HOST' ) ) {
 	$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
 }
 
@@ -31,7 +31,12 @@ if ( ! $_tests_dir ) {
 }
 // Fallback.
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run .dev/bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
 }
 
 // Give access to tests_add_filter() function.
