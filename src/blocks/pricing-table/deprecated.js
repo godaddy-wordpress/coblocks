499f6973aad2a3084c93d74976a5ba597d0f272f
Jason Lemay <jlemay@godaddy.com>
2021-11-17 10:23:04 -0500

    Revert pricing table

--- src/blocks/pricing-table/deprecated.js
+++ src/blocks/pricing-table/deprecated.js
@@ -63,40 +63,6 @@ const deprecated = [
 			);
 		},
 	},
-	{
-		attributes: currentBlock.attributes,
-		save( { attributes } ) {
-			const {
-				contentAlign,
-				count,
-				gutter,
-			} = attributes;
-
-			const classes = classnames(
-				{
-					[ `has-text-align-${ contentAlign }` ]: contentAlign,
-				}
-			);
-
-			const innerClasses = classnames( 'wp-block-coblocks-pricing-table__inner',
-				{
-					'has-columns': count > 1,
-					[ `has-${ count }-columns` ]: count,
-					'has-responsive-columns': count > 1,
-					[ `has-${ gutter }-gutter` ]: gutter,
-				}
-			);
-
-			return (
-
-				<div className={ classes }>
-					<div className={ innerClasses }>
-						<InnerBlocks.Content />
-					</div>
-				</div>
-			);
-		},
-	},
 ];
 
 export default deprecated;
