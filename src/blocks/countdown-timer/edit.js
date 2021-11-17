6c143a87913bc09877c87380ef96483e130fe2af
Olivier Lafleur <olafleur@godaddy.com>
2021-11-17 14:50:53 -0500

    wip

--- src/blocks/countdown-timer/edit.js
+++ src/blocks/countdown-timer/edit.js
@@ -1,5 +1,31 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+import { Icon, Placeholder } from '@wordpress/components';
+// import { TimerIcon } from '@godaddy-wordpress/coblocks-icons';
+
 const Edit = () => {
-	return <div>This is the Edit countdown</div>;
+	const blockProps = useBlockProps( {
+		className: 'asdf',
+		style: { color: 'blue' },
+	} );
+	//return <div { ...blockProps }>This is the Edit countdown</div>;
+
+	return (
+		<>
+			<Placeholder
+				instructions={ __( 'todo', 'coblocks' ) }
+				key="placeholder"
+				label="Countdown Timer"
+				// icon={ <Icon icon={ TimerIcon } /> }
+				{ ...blockProps }
+			>
+
+			</Placeholder>
+		</>
+	);
 };
 
 export default Edit;
