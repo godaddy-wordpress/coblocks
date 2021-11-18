ecf14dc078e9141eba515d45b8cd1b99b03cc294
Evan Herman <evan.m.herman@gmail.com>
2021-11-18 12:29:31 -0500

    Define results

--- .dev/performance/index.js
+++ .dev/performance/index.js
@@ -316,70 +316,14 @@ async function runPerformanceTests( branches, options ) {
 
 			// Format results as times.
 			results[ testSuite ][ branch ] = mapValues( medians, formatTime );
+
+			// Write the results to a file for use in the aggregator build
+			const mediansFilename = branch + '-median-results.json';
+			fs.writeFileSync(
+				path.resolve( __dirname, '../../../', mediansFilename ),
+				JSON.stringify( results, null, 2 )
+			);
 		}
-
-		const getDifference = ( key ) => {
-			const valueArray = Object.keys( results[ testSuite ] );
-
-			const x = results[ testSuite ][ valueArray[ 0 ] ][ key ],
-				y = results[ testSuite ][ valueArray[ 1 ] ][ key ];
-
-			return parseFloat( ( ( ( y - x ) / x ) * 100 ).toFixed( 2 ) );
-		};
-
-		// Computing difference.
-		const difference = mapValues(
-			{
-				load: getDifference( 'load' ),
-				type: getDifference( 'type' ),
-				minType: getDifference( 'minType' ),
-				maxType: getDifference( 'maxType' ),
-				focus: getDifference( 'focus' ),
-				minFocus: getDifference( 'minFocus' ),
-				maxFocus: getDifference( 'maxFocus' ),
-				inserterOpen: getDifference( 'inserterOpen' ),
-				minInserterOpen: getDifference( 'minInserterOpen' ),
-				maxInserterOpen: getDifference( 'maxInserterOpen' ),
-				inserterSearch: getDifference( 'inserterSearch' ),
-				minInserterSearch: getDifference( 'minInserterSearch' ),
-				maxInserterSearch: getDifference( 'maxInserterSearch' ),
-				inserterHover: getDifference( 'inserterHover' ),
-				minInserterHover: getDifference( 'minInserterHover' ),
-				maxInserterHover: getDifference( 'maxInserterHover' ),
-			}
-		);
-
-		results[ testSuite ][ 'change %' ] = difference;
-	}
-
-	// 5- Formatting the results.
-	log( '\n>> 🎉 Results.\n' );
-	for ( const testSuite of testSuites ) {
-		/** @type {Record<string, Record<string, string>>} */
-		const invertedResult = {};
-		Object.entries( results[ testSuite ] ).reduce(
-			( acc, [ key, val ] ) => {
-				for ( const entry of Object.keys( val ) ) {
-					const suffix = key === 'change %' ? ' %' : ' ms';
-					if ( ! acc[ entry ] && isFinite( val[ entry ] ) ) {
-						acc[ entry ] = {};
-					}
-					if ( isFinite( val[ entry ] ) ) {
-						acc[ entry ][ key ] = val[ entry ] + suffix;
-					}
-				}
-				return acc;
-			},
-			invertedResult
-		);
-		// eslint-disable-next-line no-console
-		console.table( invertedResult );
-
-		const resultsFilename = testSuite + '-performance-results.json';
-		fs.writeFileSync(
-			path.resolve( __dirname, '../../../', resultsFilename ),
-			JSON.stringify( results[ testSuite ], null, 2 )
-		);
 	}
 }
 
---
+++ .dev/performance/results.js
@@ -0,0 +1,72 @@
+// Read the test results from master and current branch at ~/project/[branch]-median-results.json files
+// combine them into a single results array with structure results[ testSuite ][ branch ] = results.json
+
+const testSuites = [ 'post-editor' ];
+const results = [];
+
+for ( const testSuite of testSuites ) {
+	results[ testSuite ] = {};
+
+	const getDifference = ( key ) => {
+		const valueArray = Object.keys( results[ testSuite ] );
+
+		const x = results[ testSuite ][ valueArray[ 0 ] ][ key ],
+			y = results[ testSuite ][ valueArray[ 1 ] ][ key ];
+
+		return parseFloat( ( ( ( y - x ) / x ) * 100 ).toFixed( 2 ) );
+	};
+
+	// Computing difference.
+	const difference = mapValues(
+		{
+			load: getDifference( 'load' ),
+			type: getDifference( 'type' ),
+			minType: getDifference( 'minType' ),
+			maxType: getDifference( 'maxType' ),
+			focus: getDifference( 'focus' ),
+			minFocus: getDifference( 'minFocus' ),
+			maxFocus: getDifference( 'maxFocus' ),
+			inserterOpen: getDifference( 'inserterOpen' ),
+			minInserterOpen: getDifference( 'minInserterOpen' ),
+			maxInserterOpen: getDifference( 'maxInserterOpen' ),
+			inserterSearch: getDifference( 'inserterSearch' ),
+			minInserterSearch: getDifference( 'minInserterSearch' ),
+			maxInserterSearch: getDifference( 'maxInserterSearch' ),
+			inserterHover: getDifference( 'inserterHover' ),
+			minInserterHover: getDifference( 'minInserterHover' ),
+			maxInserterHover: getDifference( 'maxInserterHover' ),
+		}
+	);
+
+	results[ testSuite ][ 'change %' ] = difference;
+}
+
+// 5- Formatting the results.
+log( '\n>> 🎉 Results.\n' );
+for ( const testSuite of testSuites ) {
+	/** @type {Record<string, Record<string, string>>} */
+	const invertedResult = {};
+	Object.entries( results[ testSuite ] ).reduce(
+		( acc, [ key, val ] ) => {
+			for ( const entry of Object.keys( val ) ) {
+				const suffix = key === 'change %' ? ' %' : ' ms';
+				if ( ! acc[ entry ] && isFinite( val[ entry ] ) ) {
+					acc[ entry ] = {};
+				}
+				if ( isFinite( val[ entry ] ) ) {
+					acc[ entry ][ key ] = val[ entry ] + suffix;
+				}
+			}
+			return acc;
+		},
+		invertedResult
+	);
+	// eslint-disable-next-line no-console
+	console.table( invertedResult );
+
+	const resultsFilename = testSuite + '-performance-results.json';
+	fs.writeFileSync(
+		path.resolve( __dirname, '../../../', resultsFilename ),
+		JSON.stringify( results[ testSuite ], null, 2 )
+	);
+}
