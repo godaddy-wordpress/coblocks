dffe58bba3118c9e976af4c7112c1622b81756b4
Jason Lemay <85305825+jlemay-godaddy@users.noreply.github.com>
2021-11-18 10:45:17 -0500

    Improving accessibility (#2158)
    * A11y for Logos block
    
    * A11y for Stacked Gallery block
    
    * A11y for Price Table block
    
    * Add missing deprecation
    
    * Update deprecations
    
    * Revert pricing table
    
    * Revert pricing table

--- src/blocks/logos/deprecated.js
+++ src/blocks/logos/deprecated.js
@@ -22,7 +22,7 @@ const deprecated = [
 		migrate: ( attributes ) => {
 			return {
 				...attributes,
-				className: attributes.className.replace( 'has-filter-grayscale', 'is-style-black-and-white' ),
+				className: attributes.className?.replace( 'has-filter-grayscale', 'is-style-black-and-white' ),
 			};
 		},
 		save: ( { attributes, className } ) => {
@@ -73,6 +73,61 @@ const deprecated = [
 							} ) }
 						</div>
 					) ) }
+				</div>
+			);
+		},
+	}, {
+		attributes: {
+			...metadata.attributes,
+		},
+		save: ( { attributes, className } ) => {
+			const { align } = attributes;
+
+			const hasImages = !! attributes.images.length;
+
+			if ( ! hasImages ) {
+				return null;
+			}
+
+			let count;
+
+			switch ( align ) {
+				case 'wide':
+					count = 4;
+					break;
+				case 'full':
+					count = 5;
+					break;
+				default:
+					count = 3;
+					break;
+			}
+
+			const imageChunks = chunk( attributes.images, count );
+
+			return (
+				<div className={ className }>
+					{ Object.keys( imageChunks ).map( ( keyOuter ) => {
+						const images = imageChunks[ keyOuter ];
+
+						return (
+							<div className="wp-block-coblocks-logos__row" key={ 'wrapper-' + keyOuter }>
+								{ images.map( ( img, index ) => {
+									return (
+										<div style={ { width: img.width || ( 100 / images.length ) + '%' } } key={ 'logo-' + keyOuter }>
+											<img
+												key={ 'img-' + index }
+												src={ img.url }
+												alt={ img.alt }
+												data-id={ img.id }
+												data-width={ img.width || ( 100 / images.length ) + '%' }
+											/>
+										</div>
+									);
+								} ) }
+							</div>
+						);
+					} ) }
 				</div>
 			);
 		},
