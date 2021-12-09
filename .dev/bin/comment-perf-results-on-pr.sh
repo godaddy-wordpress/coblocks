2603abddc3af96d13b6ad7ca496ff17374b6b609
Evan Herman <evan.m.herman@gmail.com>
2021-12-09 09:48:58 -0500

    Update scripts

--- .dev/bin/comment-perf-results-on-pr.sh
+++ .dev/bin/comment-perf-results-on-pr.sh
@@ -52,6 +52,7 @@ if [ 'null' == "$COMMENT" ]; then
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/issues/$PR_ID/comments" \
     -d "{\"body\": \"$COMMENT_BODY\"}")
+  success "Done."
   exit 0
 fi
 
