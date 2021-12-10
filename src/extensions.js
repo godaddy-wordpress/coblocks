2a322fadb9c01a163f65dec6792e02f5aeb8a915
AnthonyLedesma <anthonymledesma@gmail.com>
2021-12-10 08:31:10 -0700

    remove superfluous imports

--- src/extensions.js
+++ src/extensions.js
@@ -6,29 +6,14 @@ import './js/coblocks-color-fix';
 
 // Extensions
 import './extensions/apply-extensions';
-import './extensions/animation';
-import './extensions/colors/inspector';
-import './extensions/typography';
-import './extensions/attributes';
-import './extensions/advanced-controls';
-import './extensions/padding-controls';
-import './extensions/list-styles';
-import './extensions/button-styles';
-import './extensions/button-controls';
-import './extensions/image-styles';
-import './extensions/cover-styles';
-import './extensions/media-text-styles';
-import './extensions/lightbox-controls';
-import './extensions/replace-image';
-import './extensions/image-crop';
-import './extensions/image-filter';
-import './extensions/coblocks-settings';
-import './extensions/layout-selector';
 import './extensions/block-patterns';
-
-// Internal Extensions / Components
-import './components/gutter-control';
-import './components/form-label-colors';
+import './extensions/button-styles';
+import './extensions/coblocks-settings';
+import './extensions/cover-styles';
+import './extensions/image-styles';
+import './extensions/layout-selector';
+import './extensions/list-styles';
+import './extensions/media-text-styles';
 
 // Formats
 import './formats';
