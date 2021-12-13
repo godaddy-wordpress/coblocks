156d4732512f1da5897b46dcac378215e3d40e5e
Olivier Lafleur <olafleur@godaddy.com>
2021-12-13 13:49:26 -0500

    Fix Click to tweet tests in WP 5.9 nightly

--- src/blocks/click-to-tweet/test/click-to-tweet.cypress.js
+++ src/blocks/click-to-tweet/test/click-to-tweet.cypress.js
@@ -53,59 +53,64 @@ describe( 'Test CoBlocks Click to Tweet Block', function() {
 	 * Test the text sizes change as expected
 	 */
 	it( 'Test the text sizes change as expected.', function() {
+		const downshiftToggleButton = '#downshift-0-toggle-button, #downshift-1-toggle-button';
+		const downshiftMenuLi = '#downshift-0-menu li, #downshift-1-menu li';
+
 		helpers.addBlockToPost( 'coblocks/click-to-tweet', true );
 
 		cy.get( '.components-font-size-picker' ).then( () => {
-			if ( Cypress.$( '#toggle-group-control-0' ).length > 0 ) {
+			// WP 5.9
+			if ( Cypress.$( '.components-toggle-group-control-option' ).length > 0 ) {
 				cy.get( '.wp-block-coblocks-click-to-tweet' ).click( { force: true } );
 				cy.get( '.wp-block-coblocks-click-to-tweet__text' ).focus().type( 'Some custom data here.' );
 
-				cy.get( '#toggle-group-control-0' ).find( '[data-value="17px"]' ).click();
+				cy.get( '#toggle-group-control-0, #toggle-group-control-1' ).find( '[data-value="17px"]' ).click();
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /17px/ );
 
-				cy.get( '#toggle-group-control-0' ).find( '[data-value="24px"]' ).click();
+				cy.get( '#toggle-group-control-0, #toggle-group-control-1' ).find( '[data-value="24px"]' ).click();
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /24px/ );
+			// < WP 5.9
 			} else {
 				cy.get( '.wp-block-coblocks-click-to-tweet' ).click( { force: true } );
 				cy.get( '.wp-block-coblocks-click-to-tweet__text' ).focus().type( 'Some custom data here.' );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Small' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Small' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Small' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Small' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /17px/ );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Medium' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Medium' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Medium' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Medium' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /21px/ );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Large' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Large' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Large' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Large' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /24px/ );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Huge' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Huge' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Huge' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Huge' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /30px/ );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Custom' ).click();
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Custom' ).click();
 				cy.get( '.components-input-control__container input[aria-label="Custom"]' ).type( '5' );
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Custom' );
+				cy.get( downshiftToggleButton ).contains( 'Custom' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /5px/ );
 
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Default' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Default' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Default' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Default' );
 
 				// Test the reset button
-				cy.get( '#downshift-1-toggle-button' ).click();
-				cy.get( '#downshift-1-menu li' ).contains( 'Huge' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Huge' );
+				cy.get( downshiftToggleButton ).click();
+				cy.get( downshiftMenuLi ).contains( 'Huge' ).click();
+				cy.get( downshiftToggleButton ).contains( 'Huge' );
 				cy.get( '.wp-block-coblocks-click-to-tweet .block-editor-rich-text__editable' ).should( 'have.css', 'font-size' ).and( 'match', /30px/ );
 				cy.get( '.components-font-size-picker__controls button' ).contains( 'Reset' ).click();
-				cy.get( '#downshift-1-toggle-button' ).contains( 'Default' );
+				cy.get( downshiftToggleButton ).contains( 'Default' );
 			}
 		} );
 	} );
