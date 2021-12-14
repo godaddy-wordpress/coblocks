ec550caf2e4620b199717f030b91006f53871b3a
Olivier Lafleur <olafleur@godaddy.com>
2021-12-14 12:36:50 -0500

    Activate OpenTable tests on Chrome

--- src/blocks/opentable/test/opentable.cypress.js
+++ src/blocks/opentable/test/opentable.cypress.js
@@ -23,78 +23,73 @@ describe( 'Test CoBlocks OpenTable Block', function() {
 	} );
 
 	/**
-	 * Chrome does not seem to behave well with the network interactions we attempt with this block.
-	 * Tests are disabled for Chrome yet are enabled for Firefox.
-	 */
-	if ( Cypress.browser.name === 'firefox' ) {
-	/**
 	 * Test that we can add an OpenTable block to the content, add a restaurant and then
 	 * successfully save the block without errors.
 	 */
-		it( 'Test OpenTable block saves with single restaurant selection.', function() {
-			helpers.addBlockToPost( 'coblocks/opentable', true );
+	it( 'Test OpenTable block saves with single restaurant selection.', function() {
+		helpers.addBlockToPost( 'coblocks/opentable', true );
 
-			cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
+		cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
 
-			cy.get( '.components-form-token-field__suggestion:nth-child(1)' ).click();
+		cy.get( '.components-form-token-field__suggestion:nth-child(1)' ).click();
 
-			cy.get( 'button[type="submit"]' ).click();
+		cy.get( 'button[type="submit"]' ).click();
 
-			helpers.savePage();
+		helpers.savePage();
 
-			helpers.checkForBlockErrors( 'coblocks/opentable' );
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
 
-			helpers.viewPage();
+		helpers.viewPage();
 
-			helpers.editPage();
-		} );
+		helpers.editPage();
+	} );
 
-		/**
-		 * Test that we can add an OpenTable block to the content, add a restaurant and then
-		 * change the block style to tall and have it display correctly.
-		 */
-		it( 'Test OpenTable changing to Tall style.', function() {
-			helpers.addBlockToPost( 'coblocks/opentable', true );
+	/**
+	 * Test that we can add an OpenTable block to the content, add a restaurant and then
+	 * change the block style to tall and have it display correctly.
+	 */
+	it( 'Test OpenTable changing to Tall style.', function() {
+		helpers.addBlockToPost( 'coblocks/opentable', true );
 
-			cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
+		cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'test' );
 
-			cy.get( '.components-form-token-field__suggestion:nth-child(1)' ).click();
+		cy.get( '.components-form-token-field__suggestion:nth-child(1)' ).click();
 
-			cy.get( 'button[type="submit"]' ).click();
+		cy.get( 'button[type="submit"]' ).click();
 
-			helpers.selectBlock( 'opentable' );
+		helpers.selectBlock( 'opentable' );
 
-			cy.get( 'button[aria-label="Edit Restaurant"]' ).should( 'exist' ).click();
+		cy.get( 'button[aria-label="Edit Restaurant"]' ).should( 'exist' ).click();
 
-			cy.get( '.block-editor-block-styles__item[aria-label="Tall"]' ).should( 'exist' ).click();
+		cy.get( '.block-editor-block-styles__item[aria-label="Tall"]' ).should( 'exist' ).click();
 
-			helpers.savePage();
+		helpers.savePage();
 
-			helpers.checkForBlockErrors( 'coblocks/opentable' );
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
 
-			helpers.viewPage();
+		helpers.viewPage();
 
-			cy.get( 'div[class="wp-block-coblocks-opentable iframe__overflow-wrapper is-style-tall"]' ).should( 'exist' );
+		cy.get( 'div[class="wp-block-coblocks-opentable iframe__overflow-wrapper is-style-tall"]' ).should( 'exist' );
 
-			helpers.editPage();
-		} );
-		/**
-		 * Test that the No results found message is displayed when no restaurants are found.
-		 */
-		it( 'Test OpenTable block properly shows No results found on a superflouous query.', function() {
-			helpers.addBlockToPost( 'coblocks/opentable', true );
+		helpers.editPage();
+	} );
+	/**
+	 * Test that the No results found message is displayed when no restaurants are found.
+	 */
+	it( 'Test OpenTable block properly shows No results found on a superflouous query.', function() {
+		helpers.addBlockToPost( 'coblocks/opentable', true );
 
-			cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'thereisnowaythiswouldeverreturnarealrestaurant' );
+		cy.get( '.wp-block-coblocks-opentable .components-form-token-field__input' ).type( 'thereisnowaythiswouldeverreturnarealrestaurant' );
 
-			cy.get( '.components-notice__content' ).should( 'contain', 'No results found' );
+		cy.get( '.components-notice__content' ).should( 'contain', 'No results found' );
 
-			helpers.savePage();
+		helpers.savePage();
 
-			helpers.checkForBlockErrors( 'coblocks/opentable' );
+		helpers.checkForBlockErrors( 'coblocks/opentable' );
 
-			helpers.viewPage();
+		helpers.viewPage();
 
-			helpers.editPage();
-		} );
-	}
+		helpers.editPage();
+	} );
+	//}
 } );
