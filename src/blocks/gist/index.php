4c25f4e59da010810798f26ca009081ab3938261
AnthonyLedesma <anthonymledesma@gmail.com>
2022-01-07 13:53:49 -0700

    fix gist embed files

--- src/blocks/gist/index.php
+++ src/blocks/gist/index.php
@@ -24,6 +24,29 @@ function coblocks_block_gist_handler( $matches ) {
 	$gist_url  = empty( $matches[0] ) ? '' : $matches[0];
 	$gist_path = empty( $matches[1] ) ? '' : $matches[1];
 	$gist_file = empty( $matches[2] ) ? '' : $matches[2];
+	$gist_id   = explode( '/', $gist_path )[1];
+
+	// Fetch from API list of all files.
+	// in cases where only uppercase files are present.
+	$api_resp = wp_remote_get(
+		'https://api.github.com/gists/' . $gist_id,
+		array(
+			'headers' => array(
+				'Accept' => 'application/json',
+			),
+		)
+	);
+
+	$body  = json_decode( $api_resp['body'], true );
+	$files = empty( $body['files'] ) ? array() : $body['files'];
+
+	// Map all files into filenames array.
+	$file_list = array_map(
+		function( $file ) {
+			return $file['filename'];
+		},
+		$files
+	);
 
 	if (
 		empty( $gist_url ) ||
@@ -34,18 +57,20 @@ function coblocks_block_gist_handler( $matches ) {
 	}
 
 	$script_src = untrailingslashit( $gist_path );
-
-	if ( ! preg_match( '/\.js$/', $script_src ) ) {
-		$script_src .= '.js';
-	}
+	// .js suffix is needed after gist path for proper embed.
+	$script_src .= '.js';
 
 	if ( ! empty( $gist_file ) ) {
+		// We replace the fash to obtain true filename.
 		$dash_position = strrpos( $gist_file, '-' );
 		if ( false !== $dash_position ) {
 			$gist_file = substr_replace( $gist_file, '.', $dash_position, 1 );
 		}
 
-		$script_src .= '?file=' . $gist_file;
+		// Compare against files from API request. Return the first matching filename regardless of case.
+		$matched_file = array_search( $gist_file, array_map( 'strtolower', $file_list ), true );
+		$script_src  .= '?file=' . $matched_file;
+
 	}
 
 	return sprintf(
