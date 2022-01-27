f6e218e4a2a61f3e5e55e2a4eb726976be2cfe8f
Jonathan Bardo <info@jonathanbardo.com>
2022-01-27 11:24:16 -0500

    Go back to darker green

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -113,5 +113,5 @@ export const registerBlock = ( block ) => {
  * Returns the color used for Icon Color in the block editor.
  */
 export function getBlockIconColor() {
-	return '#1BDBDB';
+	return '#09757A';
 }
