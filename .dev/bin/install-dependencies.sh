419d05eb7403f7b731cfab6541e699f3f85805c6
Olivier Lafleur <85255688+olafleur-godaddy@users.noreply.github.com>
2022-02-10 11:03:56 -0500

    Drop PHP tests in 7.3 and add them in 8.1 (#2272)
    * test cimg
    
    * stuff
    
    * orb
    
    * install
    
    * rename
    
    * js with node only
    
    * Add unit testing PHP 8.1
    
    * try fix 8.1
    
    * fix tests
    
    * change deprecated function
    
    * PHP >= 7.3
    
    * coverage
    
    * Bump to 7.4
    
    * put back PHP 5.6
    
    * Use new PhpUnit for PHP 8.1
    
    * require-dev
    
    * install
    
    * update
    
    * should work ;)
    
    * another test
    
    * coverage
    
    * before

--- .dev/bin/install-dependencies.sh
+++ .dev/bin/install-dependencies.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 sudo apt-get update && sudo apt-get install -y subversion default-mysql-client
-sudo -E docker-php-ext-install mysqli
 
 .dev/bin/install-wp-tests.sh wordpress_test root '' 127.0.0.1 latest
