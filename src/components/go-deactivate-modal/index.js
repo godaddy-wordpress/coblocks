ca10633afc23cba0ec320135a47158c03d9cd996
Sindhu Balakrishnan <sbalakrishnan1@godaddy.com>
2022-02-22 15:21:59 -0800

    adding domain as a param

--- src/components/go-deactivate-modal/index.js
+++ src/components/go-deactivate-modal/index.js
@@ -16,16 +16,20 @@ const API_BASE_URL = 'https://wpnux.godaddy.com/v3/api';
 
 const GoDeactivateModal = () => {
 	const apiUrl = `${ API_BASE_URL }/feedback/go-theme-optout`;
+	const domain = goThemeDeactivateData.domain;
 
 	const isEvent = ( e ) => e.target.classList.contains( 'activate' );
 
-	if ( ! goThemeDeactivateData ) {
+	if ( ! goThemeDeactivateData || ! domain ) {
 		return null;
 	}
+
+	const getParams = { domain };
 
 	return (
 		<DeactivateModal
 			apiUrl={ apiUrl }
+			getParams={ getParams }
 			isEvent={ isEvent }
 			pageData={ goThemeDeactivateData }
 		/>
