19455b3bc8949fb517bbfcc50999b6636382581f
AnthonyLedesma <anthonymledesma@gmail.com>
2022-02-23 13:26:33 -0700

    move carousel to option selector

--- src/blocks/gallery-carousel/inspector.js
+++ src/blocks/gallery-carousel/inspector.js
@@ -1,8 +1,8 @@
 /**
  * Internal dependencies
  */
+import OptionSelectorControl from '../../components/option-selector-control';
 import ResponsiveTabsControl from '../../components/responsive-tabs-control';
-import SizeControl from '../../components/size-control';
 import SliderPanel from '../../components/slider-panel';
 
 /**
@@ -10,8 +10,8 @@ import SliderPanel from '../../components/slider-panel';
  */
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
-import { InspectorControls, InspectorAdvancedControls } from '@wordpress/block-editor';
-import { PanelBody, RangeControl, ToggleControl, BaseControl } from '@wordpress/components';
+import { BaseControl, PanelBody, RangeControl, ToggleControl } from '@wordpress/components';
+import { InspectorAdvancedControls, InspectorControls } from '@wordpress/block-editor';
 
 /**
  * Inspector controls
@@ -19,40 +19,29 @@ import { PanelBody, RangeControl, ToggleControl, BaseControl } from '@wordpress/
  * @param {Object} props passed from Edit functional component.
  */
 const Inspector = ( props ) => {
+	const { setAttributes } = props;
 	const [ temporaryInput, setTemporaryInput ] = useState( null );
 
-	const setRadiusTo = ( value ) => {
-		props.setAttributes( { radius: value } );
-	};
+	const setRadiusTo = ( value ) => setAttributes( { radius: value } );
+	const setGridSize = ( value ) => setAttributes( { gridSize: value } );
+	const setHeightTo = ( value ) => setAttributes( { height: value } );
+	const setTemporayInput = ( value ) => setTemporaryInput( value );
 
-	const setSizeControl = ( value ) => {
-		props.setAttributes( { gridSize: value } );
-	};
+	const getThumbnailNavigationHelp = ( checked ) => checked
+		? __( 'Showing thumbnail navigation.', 'coblocks' )
+		: __( 'Toggle to show thumbnails.', 'coblocks' );
 
-	const setHeightTo = ( value ) => {
-		props.setAttributes( { height: value } );
-	};
+	const getResponsiveHeightHelp = ( checked ) => checked
+		? __( 'Percentage based height is activated.', 'coblocks' )
+		: __( 'Toggle for percentage based height.', 'coblocks' );
 
-	const setTemporayInput = ( value ) => {
-		setTemporaryInput( value );
-	};
-
-	const getThumbnailNavigationHelp = ( checked ) => {
-		return checked ? __( 'Showing thumbnail navigation.', 'coblocks' ) : __( 'Toggle to show thumbnails.', 'coblocks' );
-	};
-
-	const getResponsiveHeightHelp = ( checked ) => {
-		return checked ? __( 'Percentage based height is activated.', 'coblocks' ) : __( 'Toggle for percentage based height.', 'coblocks' );
-	};
-
-	const getLightboxHelp = ( checked ) => {
-		return checked ? __( 'Image lightbox is enabled.', 'coblocks' ) : __( 'Toggle to enable the image lightbox.', 'coblocks' );
-	};
+	const getLightboxHelp = ( checked ) => checked
+		? __( 'Image lightbox is enabled.', 'coblocks' )
+		: __( 'Toggle to enable the image lightbox.', 'coblocks' );
 
 	const {
 		attributes,
 		isSelected,
-		setAttributes,
 	} = props;
 
 	const {
@@ -66,47 +55,84 @@ const Inspector = ( props ) => {
 		lightbox,
 	} = attributes;
 
+	/**
+	 * Grid constant.
+	 */
+	const gridOptions = () => {
+		const defaultOptions = [
+			{
+				/* translators: abbreviation for large size */
+				label: __( 'L', 'coblocks' ),
+				tooltip: __( 'Large', 'coblocks' ),
+				value: 'lrg',
+			},
+			{
+				/* translators: abbreviation for large size */
+				label: __( 'XL', 'coblocks' ),
+				tooltip: __( 'Extra Large', 'coblocks' ),
+				value: 'xlrg',
+			},
+		];
+
+		if ( !! align && align !== 'none' ) {
+			defaultOptions.unshift( {
+				/* translators: abbreviation for medium size */
+				label: __( 'M', 'coblocks' ),
+				tooltip: __( 'Medium', 'coblocks' ),
+				value: 'med',
+			} );
+		}
+		if ( align === 'full' ) {
+			defaultOptions.unshift( {
+				/* translators: abbreviation for small size */
+				label: __( 'S', 'coblocks' ),
+				tooltip: __( 'Small', 'coblocks' ),
+				value: 'sml',
+			} );
+		}
+		return defaultOptions;
+	};
+
 	return (
 		isSelected && (
 			<>
 				<InspectorControls>
 					<PanelBody title={ __( 'Carousel settings', 'coblocks' ) } >
-						<SizeControl { ...props }
-							type={ 'grid' }
+						<OptionSelectorControl
+							currentOption={ gridSize }
 							label={ __( 'Size', 'coblocks' ) }
-							onChange={ setSizeControl }
-							value={ gridSize }
-							resetValue={ 'xlrg' }
+							onChange={ setGridSize }
+							options={ gridOptions() }
 						/>
 						{ gridSize !== 'xlrg' && ( align === 'wide' || align === 'full' ) &&
-						<ResponsiveTabsControl { ...props }
-							max={ 20 }
-						/>
+							<ResponsiveTabsControl { ...props }
+								max={ 20 }
+							/>
 						}
 						{ gridSize !== 'xlrg' && ! align &&
-						<ResponsiveTabsControl { ...props }
-							max={ 20 }
-						/>
+							<ResponsiveTabsControl { ...props }
+								max={ 20 }
+							/>
 						}
 						{ gutter > 0 &&
-						<RangeControl
-							label={ __( 'Rounded corners', 'coblocks' ) }
-							value={ radius }
-							onChange={ setRadiusTo }
-							min={ 0 }
-							max={ 20 }
-							step={ 1 }
-						/>
+							<RangeControl
+								label={ __( 'Rounded corners', 'coblocks' ) }
+								max={ 20 }
+								min={ 0 }
+								onChange={ setRadiusTo }
+								step={ 1 }
+								value={ radius }
+							/>
 						}
 						{ ! responsiveHeight &&
 							<BaseControl
-								label={ __( 'Height in pixels', 'coblocks' ) }
 								className={ 'block-height-control' }
 								id="gallery-carousel-height-control"
+								label={ __( 'Height in pixels', 'coblocks' ) }
 							>
 								<input
-									type="number"
 									className={ 'block-height-control__input' }
+									min={ 0 }
 									onChange={ ( event ) => {
 										const unprocessedValue = event.target.value;
 										const inputValue = unprocessedValue !== ''
@@ -120,33 +146,33 @@ const Inspector = ( props ) => {
 										setTemporayInput( null );
 										setHeightTo( inputValue );
 									} }
-									value={ temporaryInput || height }
-									min={ 0 }
 									step="10"
+									type="number"
+									value={ temporaryInput || height }
 								/>
 							</BaseControl>
 						}
 						<ToggleControl
-							label={ __( 'Lightbox', 'coblocks' ) }
 							checked={ !! lightbox }
-							onChange={ () => setAttributes( { lightbox: ! lightbox } ) }
 							help={ getLightboxHelp }
+							label={ __( 'Lightbox', 'coblocks' ) }
+							onChange={ () => setAttributes( { lightbox: ! lightbox } ) }
 						/>
 						<ToggleControl
-							label={ __( 'Thumbnails', 'coblocks' ) }
 							checked={ !! thumbnails }
-							onChange={ () => setAttributes( { thumbnails: ! thumbnails } ) }
 							help={ getThumbnailNavigationHelp }
+							label={ __( 'Thumbnails', 'coblocks' ) }
+							onChange={ () => setAttributes( { thumbnails: ! thumbnails } ) }
 						/>
 					</PanelBody>
 					<SliderPanel { ...props } />
 				</InspectorControls>
 				<InspectorAdvancedControls>
 					<ToggleControl
-						label={ __( 'Responsive height', 'coblocks' ) }
 						checked={ !! responsiveHeight }
-						onChange={ () => setAttributes( { responsiveHeight: ! responsiveHeight } ) }
 						help={ getResponsiveHeightHelp }
+						label={ __( 'Responsive height', 'coblocks' ) }
+						onChange={ () => setAttributes( { responsiveHeight: ! responsiveHeight } ) }
 					/>
 				</InspectorAdvancedControls>
 			</>
