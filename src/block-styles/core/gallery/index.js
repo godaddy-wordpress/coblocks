e284b58aeae05235c3197528bea6e6fed6fb5a8c
JR Tashjian <jrtashjian@gmail.com>
2022-03-14 18:11:07 -0400

    Rename "stacked" to "compact" style

--- src/block-styles/core/gallery/index.js
+++ src/block-styles/core/gallery/index.js
@@ -4,9 +4,11 @@
 import { __ } from '@wordpress/i18n';
 import { registerBlockStyle } from '@wordpress/blocks';
 
+import './style.scss';
+
 [
 	{
-		label: __( 'Stacked', 'coblocks' ),
-		name: 'stacked',
+		label: __( 'Compact', 'coblocks' ),
+		name: 'compact',
 	},
 ].forEach( ( variation ) => registerBlockStyle( 'core/gallery', variation ) );
