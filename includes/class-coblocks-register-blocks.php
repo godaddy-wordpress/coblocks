c69dfd80f71814564e71f1a9a965157edb6e61e0
JR Tashjian <jrtashjian@gmail.com>
2022-03-16 11:40:26 -0400

    Remove extra register_block_type call for gallery-stacked

--- includes/class-coblocks-register-blocks.php
+++ includes/class-coblocks-register-blocks.php
@@ -141,14 +141,6 @@ class CoBlocks_Register_Blocks {
 				'style'         => $slug . '-frontend',
 			)
 		);
-		register_block_type(
-			$slug . '/gallery-stacked',
-			array(
-				'editor_script' => $slug . '-editor',
-				'editor_style'  => $slug . '-editor',
-				'style'         => $slug . '-frontend',
-			)
-		);
 	}
 }
 
