264210605de576dd43539a5fad097188f4ef5b04
Steve Novosel <novosel991@gmail.com>
2022-03-18 13:48:18 -0400

    remove tab index from accordion item content div on front end (#2310)

--- src/blocks/accordion/accordion-item/deprecated.js
+++ src/blocks/accordion/accordion-item/deprecated.js
@@ -55,12 +55,66 @@ const deprecated = [
 					{ ! RichText.isEmpty( title ) &&
 					<details open={ open }>
 						<RichText.Content
-							tagName="summary"
 							className={ titleClasses }
-							value={ title }
 							style={ titleStyles }
+							tagName="summary"
+							value={ title }
 						/>
 						<div className="wp-block-coblocks-accordion-item__content" style={ borderStyle }>
+							<InnerBlocks.Content />
+						</div>
+					</details>
+					}
+				</div>
+			);
+		},
+	},
+	{
+		attributes: {
+			...currentBlock.attributes,
+		},
+		save( { attributes } ) {
+			const {
+				backgroundColor,
+				customBackgroundColor,
+				customTextColor,
+				open,
+				textColor,
+				borderColor,
+				title,
+			} = attributes;
+
+			const backgroundColorClass = getColorClassName( 'background-color', backgroundColor );
+			const textColorClass = getColorClassName( 'color', textColor );
+
+			const titleClasses = classnames(
+				'wp-block-coblocks-accordion-item__title', {
+					'has-background': backgroundColor || customBackgroundColor,
+					[ backgroundColorClass ]: backgroundColorClass,
+					'has-text-color': textColor || customTextColor,
+					[ textColorClass ]: textColorClass,
+				} );
+
+			const titleStyles = {
+				backgroundColor: backgroundColorClass ? undefined : customBackgroundColor,
+				color: textColorClass ? undefined : customTextColor,
+			};
+
+			const borderStyle = {
+				borderColor: borderColor ? borderColor : customBackgroundColor,
+			};
+
+			return (
+				<div>
+					{ ! RichText.isEmpty( title ) &&
+					<details open={ open }>
+						<RichText.Content
+							className={ titleClasses }
+							style={ titleStyles }
+							tagName="summary"
+							value={ title }
+						/>
+						<div className="wp-block-coblocks-accordion-item__content" style={ borderStyle } tabIndex="0">
 							<InnerBlocks.Content />
 						</div>
 					</details>
