6c6f8bfce1f6f371d2ec94944e2b15ce1494a3a0
Olivier Lafleur <olafleur@godaddy.com>
2022-04-06 00:08:09 -0400

    Revert "remove stuff"
    This reverts commit 980b848cd9122d31a90802b8060ec1dd8660f21e.

--- src/extensions/site-content/index.js
+++ src/extensions/site-content/index.js
@@ -2,18 +2,37 @@
  * External dependencies
  */
 import { ContentIcon as icon } from '@godaddy-wordpress/coblocks-icons';
+import { sortBy } from 'lodash';
 
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
+import { ComplementaryArea } from '@wordpress/interface';
+import { compose } from '@wordpress/compose';
 import { registerPlugin } from '@wordpress/plugins';
+import { useEffect } from '@wordpress/element';
+import { withDispatch, withSelect } from '@wordpress/data';
 
 /**
  * Local dependencies
  */
+import { CoBlocksMenuIcon } from '../../components/common';
+import PostTypePanel from './post-type-panel';
 import { PLUGIN_NAME, SIDEBAR_NAME } from './constant';
+import './site-content-control';
 
 export const CoBlocksSiteContent = ( props ) => {
+	useEffect( () => {
+		const { currentPageMeta, editPost } = props;
+	}, [] );
+
+	const loadPostIntoEditor = ( postType, postId ) => {
+		window.location.href = `/wp-admin/post.php?post=${ postId }&action=edit`;
+	};
+
+	const { postTypes } = props;
+
 	return null;
 };
 
