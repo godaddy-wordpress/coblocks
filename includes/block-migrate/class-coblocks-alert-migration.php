e0d5a5ffa1fb5cd358a114489ed965e0cf157dff
Evan Herman <evan.m.herman@gmail.com>
2022-04-13 16:28:24 -0400

    Delete old files, remove alert

---
+++ includes/block-migrate/class-coblocks-alert-migration.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * CoBlocks_Gallery_Collage_Migration
+ *
+ * @package CoBlocks
+ */
+
+/**
+ * CoBlocks_Gallery_Collage_Migration
+ *
+ * Define how a coblocks/gallery-collage block should migrate into a core/gallery block.
+ */
+class CoBlocks_Alert_Migration extends CoBlocks_Block_Migration {
+	/**
+	 * Returns the name of the block.
+	 *
+	 * @inheritDoc
+	 */
+	protected function block_name() {
+		return 'coblocks/alert';
+	}
+
+	/**
+	 * Produce new attributes from the migrated block.
+	 *
+	 * @inheritDoc
+	 */
+	protected function migrate_attributes() {
+		return array_filter(
+			$this->paragraph_attributes()
+		);
+	}
+
+	/**
+	 * Calculate attributes applied to the paragraph.
+	 *
+	 * @return array attributes found and their values.
+	 */
+	private function paragraph_attributes() {
+		return array(
+			'style' => array(
+				'color' => array(
+					'background' => $this->get_alert_background_color_from_classname( $this->block_wrapper() ),
+				)
+			)
+		);
+	}
+
+	private function get_alert_background_color_from_classname( DOMElement $element ) {
+		return '#d0eac4';
+	}
+}
--- src/blocks/alert/index.js
+++ src/blocks/alert/index.js
@@ -1,80 +1,34 @@
 /**
- * External dependencies
- */
-import { AlertIcon as icon } from '@godaddy-wordpress/coblocks-icons';
-
-/**
  * Internal dependencies
  */
-import deprecated from './deprecated';
-import edit from './edit';
 import metadata from './block.json';
-import save from './save';
-import transforms from './transforms';
 
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { Icon } from '@wordpress/components';
+import { dispatch } from '@wordpress/data';
+import { createBlock, switchToBlockType } from '@wordpress/blocks';
 
 /**
  * Block constants
  */
-const { name, category, attributes } = metadata;
+const { name } = metadata;
+
+alert( name );
+console.log( metadata );
 
 const settings = {
-	/* translators: block name */
-	title: __( 'Alert', 'coblocks' ),
-	/* translators: block description */
-	description: __( 'Provide contextual feedback messages or notices.', 'coblocks' ),
-	icon: <Icon icon={ icon } />,
-	keywords: [
-		'coblocks',
-		/* translators: block keyword */
-		__( 'notice', 'coblocks' ),
-		/* translators: block keyword */
-		__( 'message', 'coblocks' ),
-	],
-	styles: [
-		{
-			name: 'info',
-			/* translators: block style */
-			label: __( 'Info', 'coblocks' ),
-			isDefault: true,
-		},
-		{
-			name: 'success',
-			/* translators: block style */
-			label: __( 'Success', 'coblocks' ),
-		},
-		{
-			name: 'warning',
-			/* translators: block style */
-			label: __( 'Warning', 'coblocks' ),
-		},
-		{
-			name: 'error',
-			/* translators: block style */
-			label: __( 'Error', 'coblocks' ),
-		},
-	],
-	supports: {
-		align: true,
-		alignWide: false,
-		alignFull: false,
+	title: 'Alert',
+	edit: ( props ) => {
+		const { replaceBlocks } = dispatch( 'core/block-editor' );
+		replaceBlocks(
+			[ props.clientId ],
+			switchToBlockType( props, 'core/paragraph' )
+		);
+		return null;
 	},
-	example: {
-		attributes: {
-			title: __( 'This is an alert block', 'coblocks' ),
-			value: __( 'An alert is a message that displays outside the flow of typical content. Alerts provide contextual feedback, typically asking readers to take an action.', 'coblocks' ),
-		},
-	},
-	attributes,
-	transforms,
-	edit,
-	save,
-	deprecated,
+	parent: [],
+	save: () => null
 };
 
-export { name, category, metadata, settings, attributes };
+export { name, metadata, settings };
