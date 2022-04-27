6c8e574e53eddfe56dbfd406d37a197eb5715447
Evan Herman <evan.m.herman@gmail.com>
2022-04-27 15:11:38 -0400

    Map custom colors

--- includes/block-migrate/class-coblocks-alert-migration.php
+++ includes/block-migrate/class-coblocks-alert-migration.php
@@ -26,12 +26,40 @@ class CoBlocks_Alert_Migration extends CoBlocks_Block_Migration {
 	 * @inheritDoc
 	 */
 	protected function migrate_attributes() {
+		$alert_wrapper = $this->query_selector( '//div[contains(@class,"wp-block-coblocks-alert")]' );
+
 		$result = array(
 			'content' => implode( "<br />", array_filter( array(
 				$this->query_selector( '//p[contains(@class,"wp-block-coblocks-alert__title")]' )->textContent,
 				$this->query_selector( '//p[contains(@class,"wp-block-coblocks-alert__text")]' )->textContent,
 			) ) ),
 		);
+
+		if ( ! $this->get_attribute_from_classname( 'is-style-', $alert_wrapper ) ) {
+			$result['className'] = 'is-style-info';
+		}
+
+		$result = $this->get_alert_colors( $this->block_attributes, $result );
+
+		return array_merge( $this->block_attributes, $result );
+	}
+
+	/**
+	 * Map the alert block custom colors to new attributes.
+	 *
+	 * @param  array $attributes Alert block attributes.
+	 * @param  array $result     Paragraph block attributes.
+	 *
+	 * @return array New paragraph block attributes.
+	 */
+	private function get_alert_colors( $attributes, $result ) {
+		if ( array_key_exists( 'customTextColor', $attributes ) ) {
+			$result['style']['color']['text'] = $attributes['customTextColor'];
+		}
+
+		if ( array_key_exists( 'customBackgroundColor', $attributes ) ) {
+			$result['style']['color']['background'] = $attributes['customBackgroundColor'];
+		}
 
 		return $result;
 	}
