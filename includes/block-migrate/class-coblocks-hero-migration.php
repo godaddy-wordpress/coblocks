472dc2300bf6ae309a6969c85a5548ea430445e6
Evan Herman <evan.m.herman@gmail.com>
2022-05-10 15:22:20 -0400

    Migrate remaining blocks

--- includes/block-migrate/class-coblocks-hero-migration.php
+++ includes/block-migrate/class-coblocks-hero-migration.php
@@ -30,6 +30,29 @@ class CoBlocks_Hero_Migration extends CoBlocks_Block_Migration {
 			$this->block_attributes['customOverlayColor'] = '#ffffff';
 		}
 
+		$hero_wrapper = $this->query_selector( '//div[contains(@class,"wp-block-coblocks-hero__inner")]' );
+		$padding_size = $test = $this->get_attribute_from_classname( '-padding', $hero_wrapper );
+
+		if ( $padding_size ) {
+			$this->block_attributes['className'] = "${padding_size}-padding";
+		}
+
+		if ( array_key_exists( 'backgroundImg', $this->block_attributes ) ) {
+			$this->block_attributes['url'] = $this->block_attributes['backgroundImg'];
+		}
+
+		if ( array_key_exists( 'backgroundRepeat', $this->block_attributes ) ) {
+			$this->block_attributes['isRepeated'] = true;
+		}
+
+		if ( array_key_exists( 'backgroundOverlay', $this->block_attributes ) ) {
+			$this->block_attributes['dimRatio'] = $this->block_attributes['backgroundOverlay'];
+		}
+
+		if ( array_key_exists( 'layout', $this->block_attributes ) ) {
+			$this->block_attributes['contentPosition'] = str_replace( '-', ' ', $this->block_attributes['layout'] );
+		}
+
 		if ( array_key_exists( 'backgroundColor', $this->block_attributes ) ) {
 			$this->block_attributes['overlayColor'] = $this->block_attributes['backgroundColor'];
 		}
