df0568a801747d4241771b542a046d7983ba4688
Evan Herman <evan.m.herman@gmail.com>
2022-06-01 14:54:09 -0400

    Refactor

---
+++ includes/block-migrate/class-coblocks-column-migration.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * CoBlocks_Column_Migration
+ *
+ * @package CoBlocks
+ */
+
+/**
+ * CoBlocks_Column_Migration
+ *
+ * Define how a coblocks/column block should migrate into a core/column block.
+ */
+class CoBlocks_Column_Migration extends CoBlocks_Block_Migration {
+	/**
+	 * Returns the name of the block.
+	 *
+	 * @inheritDoc
+	 */
+	protected function block_name() {
+		return 'coblocks/column';
+	}
+
+	/**
+	 * Produce new attributes from the migrated block.
+	 *
+	 * @inheritDoc
+	 */
+	protected function migrate_attributes() {
+		return array_filter( $this->block_attributes );
+	}
+}
