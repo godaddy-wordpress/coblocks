f82be5a9899f4cda700eb39a3c45d3315aff0568
Evan Herman <evan.m.herman@gmail.com>
2022-06-02 17:36:58 -0400

    Tweaks

--- src/blocks/author/index.js
+++ src/blocks/author/index.js
@@ -19,18 +19,13 @@ const settings = {
 		const { replaceBlocks } = dispatch( 'core/block-editor' );
 		const parentBlock = wp.data.select( 'core/editor' ).getBlocksByClientId( props.clientId )[ 0 ];
 
-		const authorName = props.attributes.name;
-		const biography = props.attributes.biography;
-		// const buttonUrl = parentBlock.innerBlocks[ 0 ].attributes.url;
-		// const buttonText = parentBlock.innerBlocks[ 0 ].attributes.text;
-
-		const authorNameBlock = createBlock( 'core/paragraph', { content: authorName, fontSize: 'medium' } );
-		const authorBioBlock = createBlock( 'core/paragraph', { content: biography } );
+		const authorNameBlock = createBlock( 'core/paragraph', { content: props.attributes.name, fontSize: 'medium' } );
+		const authorBioBlock = createBlock( 'core/paragraph', { content: props.attributes.biography } );
 		const buttonBlock = createBlock( 'core/buttons', {}, [ createBlock( 'core/button', parentBlock.innerBlocks[ 0 ].attributes ) ] );
 		const imageBlock = createBlock( 'core/image', { className: 'is-style-rounded', id: props.attributes.imgId, url: props.attributes.imgUrl } );
 
-		const leftColumn = createBlock( 'core/column', { width: '33.33%' }, [ imageBlock ] );
-		const rightColumn = createBlock( 'core/column', { width: '66.66%' }, [ authorNameBlock, authorBioBlock, buttonBlock ] );;
+		const leftColumn = createBlock( 'core/column', { width: '25%' }, [ imageBlock ] );
+		const rightColumn = createBlock( 'core/column', { width: '75%' }, [ authorNameBlock, authorBioBlock, buttonBlock ] );;
 
 		const columnsBlock = createBlock( 'core/columns', { style: { color: { background: '#F3F3F4' } } }, [ leftColumn, rightColumn ] );
 
