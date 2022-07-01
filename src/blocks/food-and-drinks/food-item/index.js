3b896cddb217ae7515cd361226724c861ee96c18
snovosel <snovosel@godaddy.com>
2022-06-30 21:57:53 -0400

    add paragraph to food item

--- src/blocks/food-and-drinks/food-item/index.js
+++ src/blocks/food-and-drinks/food-item/index.js
@@ -6,6 +6,7 @@ import metadata from './block.json';
 /**
  * WordPress dependencies.
  */
+import { __ } from '@wordpress/i18n/build-types';
 import { createBlock } from '@wordpress/blocks';
 
 /**
@@ -27,7 +28,19 @@ const settings = {
 						attributes,
 						innerBlocks,
 					} );
-					return createBlock( 'core/column', {}, [] );
+
+					const paragraphBlock = createBlock(
+						'core/paragraph',
+						{
+							content: attributes.title,
+							placeholder: __( 'Add title…', 'coblocks' ),
+						},
+						[]
+					);
+
+					return createBlock( 'core/column', {}, [
+						paragraphBlock,
+					] );
 				},
 				type: 'block',
 			},
