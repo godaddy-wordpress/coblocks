8343c6f3153b0fca458d60664cd3bfe32c097ac5
snovosel-godaddy <85255746+snovosel-godaddy@users.noreply.github.com>
2022-07-19 12:20:53 -0400

    WPEX 1978  migrate food and drinks to pattern (#2406)
    * add migration for food and drink block
    
    * migrate food and drink item inner block
    
    * handle food item attributes
    
    * add paragraph to food item
    
    * few fixes for food and drink
    
    * handle the rest of the food and drink attributes
    
    * migrate attributes of food item
    
    * carry over class  name for migration
    
    * carry over heading size and attributes to icons
    
    * carry over food item attributes using coblocks icons block
    
    * organize spacing of header and food attributes
    
    * persist horizontal layout
    
    * spacing for food item columns
    
    * handle show image attribute
    
    * carry over show image attribute
    
    * carry over image url
    
    * carry over grid type
    
    * carry over grid view
    
    * fix text align for price and description
    
    * remove legacy styles
    
    * remove legacy styles for food and drink
    
    * fix icon size
    
    * remove block migration unneeded code
    
    * simplify pattern for food item block
    
    * simplify the grid version of food item block migration
    
    * simplify list view of foood item:
    
    * revert removal of front end styles for food and drink block and food item block

---
+++ includes/block-migrate/class-coblocks-food-item-migration.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * CoBlocks_Food_And_Drink_Item_Migration
+ *
+ * @package CoBlocks
+ */
+
+ /**
+ * CoBlocks_Food_And_Drink_Item_Migration
+ *
+ * Define how a coblocks/food-item block should migrate into a block pattern.
+ */
+class CoBlocks_Food_Item_Migration extends CoBlocks_Block_Migration {
+	/**
+	 * Returns the name of the block.
+	 *
+	 * @inheritDoc
+	 */
+	public static function block_name() {
+		return 'coblocks/food-item';
+	}
+
+	/**
+	 * Produce new attributes from the migrated block.
+	 *
+	 * @inheritDoc
+	 */
+	protected function migrate_attributes() {
+		$food_item_content_container = $this->query_selector( '//div[contains(@class, "wp-block-coblocks-food-item__content")]' );
+		$food_item_header = $this->query_selector( '//div[contains(@class, "wp-block-coblocks-food-item__content")]//div[contains(@class, "wp-block-coblocks-food-item__heading")]//h4' );
+
+		if ( $food_item_header ) {
+			$this->block_attributes["title"] = $food_item_header->textContent;
+		}
+
+		$food_item_price = $this->query_selector('//p[contains(@class, "wp-block-coblocks-food-item__price")]');
+
+		if ( $food_item_price ) {
+			$this->block_attributes["price"] = $food_item_price->textContent;
+		}
+
+		$food_item_description = $this->query_selector('//p[contains(@class, "wp-block-coblocks-food-item__description")]');
+
+		if ( $food_item_description ) {
+			$this->block_attributes["description"] = $food_item_description->textContent;
+		}
+
+		$food_item_image_container = $this->query_selector( '//div[contains(@class, "wp-block-coblocks-food-item")]//img' );
+
+		if ( $food_item_image_container ) {
+			$food_item_image_src = $this->get_element_attribute( $food_item_image_container, 'src' );
+
+			$this->block_attributes['url'] = $food_item_image_src;
+		}
+
+		return $this->block_attributes;
+	}
+
+}
