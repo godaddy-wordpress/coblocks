bb5f0e544e86e34ae9642071f58ce9746211f1b6
Evan Herman <evan.m.herman@gmail.com>
2022-10-21 14:04:14 -0400

    Update service block test

--- src/blocks/services/service/test/service.cypress.js
+++ src/blocks/services/service/test/service.cypress.js
@@ -8,7 +8,7 @@ describe( 'Test CoBlocks Service Block', function() {
 	 * Test that we can add a service block to the content, not alter
 	 * any settings, and are able to successfully save the block without errors.
 	 */
-	it( 'Test service block saves with empty values.', function() {
+	it( 'Test service block does not render on ront of site with empty values.', function() {
 		helpers.addBlockToPost( 'coblocks/services', true );
 
 		helpers.savePage();
@@ -17,7 +17,7 @@ describe( 'Test CoBlocks Service Block', function() {
 
 		helpers.viewPage();
 
-		cy.get( '.wp-block-coblocks-service' ).should( 'exist' );
+		cy.get( '.wp-block-coblocks-service' ).should( 'not.exist' );
 
 		helpers.editPage();
 	} );
@@ -29,6 +29,9 @@ describe( 'Test CoBlocks Service Block', function() {
 		helpers.addBlockToPost( 'coblocks/services', true );
 
 		helpers.addCustomBlockClass( 'my-custom-class', 'service' );
+
+		cy.get( '.wp-block-coblocks-services [data-type="coblocks/service"]:first-child h3' ).type( 'Title 1' );
+		cy.get( '.wp-block-coblocks-services [data-type="coblocks/service"]:last-child h3' ).type( 'Title 2' );
 
 		helpers.savePage();
 
