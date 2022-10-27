f7c40f15456642c6a3d3171488f176d16873dc55
Olivier Lafleur <olafleur@godaddy.com>
2022-10-26 23:36:53 -0400

    Bump 'Tested up to' to WP 6.1

--- class-coblocks.php
+++ class-coblocks.php
@@ -7,7 +7,7 @@
  * Version: 2.24.4
  * Text Domain: coblocks
  * Domain Path: /languages
- * Tested up to: 6.0
+ * Tested up to: 6.1
  *
  * CoBlocks is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
