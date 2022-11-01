51339993153a00fd2ce525c26e32907c8e441d3a
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2022-10-31 18:50:15 -0700

    Disable color controls for WordPress 6.1+ (#2440)
    * disable color settings controls in 6.1+ WP
    
    * Skip Cypress tests for Color Controls for WP 6.1+
    
    Co-authored-by: Anthony M Ledesma <aledesma@LM-H7V42D79VD.lan>
    Co-authored-by: Olivier Lafleur <olafleur@godaddy.com>

--- src/extensions/colors/test/settings-modal-control.cypress.js
+++ src/extensions/colors/test/settings-modal-control.cypress.js
@@ -12,76 +12,86 @@ before( () => {
 
 describe( 'Settings Modal: Colors feature', () => {
 	beforeEach( () => {
-		// Reset settings.
-		helpers.getWindowObject().then( ( win ) => {
-			win.wp.data.dispatch( 'core' ).saveEntityRecord( 'root', 'site', {
-				[ COLORS_CUSTOM_FEATURE_ENABLED_KEY ]: true,
-				[ COLORS_FEATURE_ENABLED_KEY ]: true,
-				[ COLORS_GRADIENT_FEATURE_ENABLED_KEY ]: true,
+		if ( ! helpers.isWP61AtLeast() ) {
+			// Reset settings.
+			helpers.getWindowObject().then( ( win ) => {
+				win.wp.data.dispatch( 'core' ).saveEntityRecord( 'root', 'site', {
+					[ COLORS_CUSTOM_FEATURE_ENABLED_KEY ]: true,
+					[ COLORS_FEATURE_ENABLED_KEY ]: true,
+					[ COLORS_GRADIENT_FEATURE_ENABLED_KEY ]: true,
+				} );
+
+				win.wp.data.dispatch( 'core/block-editor' ).updateSettings( {
+					colors: [
+						{
+							color: 'rgb(255,255,255)',
+							name: 'Primary',
+							slug: 'primary',
+						},
+					],
+					gradients: [
+						{
+							gradient: 'linear-gradient(135deg, rgb(0,0,0) 0%, rgb(255,255,255) 100%)',
+							name: 'Primary to Secondary',
+							slug: 'primary-to-secondary',
+						},
+					],
+				} );
 			} );
 
-			win.wp.data.dispatch( 'core/block-editor' ).updateSettings( {
-				colors: [
-					{
-						color: 'rgb(255,255,255)',
-						name: 'Primary',
-						slug: 'primary',
-					},
-				],
-				gradients: [
-					{
-						gradient: 'linear-gradient(135deg, rgb(0,0,0) 0%, rgb(255,255,255) 100%)',
-						name: 'Primary to Secondary',
-						slug: 'primary-to-secondary',
-					},
-				],
-			} );
-		} );
+			// Open overlay settings on cover block where the color panel resides.
+			cy.get( '.edit-post-visual-editor .wp-block[data-type="core/cover"]' ).first().click();
+			helpers.openSettingsPanel( /Overlay/i );
 
-		// Open overlay settings on cover block where the color panel resides.
-		cy.get( '.edit-post-visual-editor .wp-block[data-type="core/cover"]' ).first().click();
-		helpers.openSettingsPanel( /Overlay/i );
-
-		// Open settings modal.
-		cy.get( '.interface-interface-skeleton__header .edit-post-more-menu .components-button' +
-			', .interface-interface-skeleton__header .interface-more-menu-dropdown .components-button' ).click();
-		cy.get( '.components-menu-item__button,.components-button' ).contains( 'Editor settings' ).click();
+			// Open settings modal.
+			cy.get( '.interface-interface-skeleton__header .edit-post-more-menu .components-button' +
+				', .interface-interface-skeleton__header .interface-more-menu-dropdown .components-button' ).click();
+			cy.get( '.components-menu-item__button,.components-button' ).contains( 'Editor settings' ).click();
+		}
 	} );
 
 	afterEach( () => {
-		cy.get( '.components-modal__header button[aria-label="Close dialog"]' ).click();
+		if ( ! helpers.isWP61AtLeast() ) {
+			cy.get( '.components-modal__header button[aria-label="Close dialog"]' ).click();
+		}
 	} );
 
 	it( 'can turn off all color settings', () => {
-		cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
+		if ( ! helpers.isWP61AtLeast() ) {
+			cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Color settings' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Color settings' ).click();
 
-		cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'not.exist' );
-		cy.get( '.components-circular-option-picker__dropdown-link-action, .components-color-palette__custom-color' ).should( 'not.exist' );
-		cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
+			cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'not.exist' );
+			cy.get( '.components-circular-option-picker__dropdown-link-action, .components-color-palette__custom-color' ).should( 'not.exist' );
+			cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Color settings' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Color settings' ).click();
+		}
 	} );
 
 	it( 'can turn off custom color settings', () => {
-		cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
+		if ( ! helpers.isWP61AtLeast() ) {
+			cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Custom color pickers' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Custom color pickers' ).click();
 
-		cy.get( '.components-circular-option-picker__dropdown-link-action, .components-color-palette__custom-color' ).should( 'not.exist' );
-		cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
+			cy.get( '.components-circular-option-picker__dropdown-link-action, .components-color-palette__custom-color' ).should( 'not.exist' );
+			cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Custom color pickers' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Custom color pickers' ).click();
+		}
 	} );
 
 	it( 'can turn off gradient settings', () => {
-		cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
+		if ( ! helpers.isWP61AtLeast() ) {
+			cy.get( '.components-panel__body-title' ).contains( /Overlay/i ).should( 'exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Gradient styles' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Gradient styles' ).click();
 
-		cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
+			cy.get( '.block-editor-color-gradient-control__button-tabs button, .components-toggle-group-control' ).should( 'not.exist' );
 
-		cy.get( '.coblocks-settings-modal' ).contains( 'Gradient styles' ).click();
+			cy.get( '.coblocks-settings-modal' ).contains( 'Gradient styles' ).click();
+		}
 	} );
 } );
