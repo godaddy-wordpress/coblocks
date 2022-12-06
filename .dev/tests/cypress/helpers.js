f1099c52a1747e743d0310b663bc5166f4e7916e
Justin Kopepasah <justin@kopepasah.com>
2022-12-06 17:11:41 -0600

    Switch back to old method

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -154,9 +154,9 @@ export function addNewGroupToPost() {
  * From inside the WordPress editor open the CoBlocks Gutenberg editor panel
  */
 export function savePage() {
-	cy.get( '.editor-post-publish-button__button' ).click();
+	cy.get( '.edit-post-header__settings button.is-primary' ).click();
 
-	cy.wait( 5000 );
+	cy.get( '.components-editor-notices__snackbar', { timeout: 120000 } ).should( 'not.be.empty' );
 
 	// Reload the page to ensure that we're not hitting any block errors
 	cy.reload();
