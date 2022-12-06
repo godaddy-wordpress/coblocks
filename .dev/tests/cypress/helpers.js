a8e2ed16213bdd753e2d07ed40f9215f754dadcb
Justin Kopepasah <justin@kopepasah.com>
2022-12-06 00:19:08 -0600

    Try toggling on block clear.

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -228,6 +228,10 @@ export function clearBlocks() {
 		safeWin.wp.data.dispatch( 'core/block-editor' ).removeBlocks(
 			safeWin.wp.data.select( 'core/block-editor' ).getBlocks().map( ( block ) => block.clientId )
 		);
+
+		if ( safeWin.wp.data.select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) ) {
+			safeWin.wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
+		}
 	} );
 }
 
