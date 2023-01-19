500fef95647cf68af80153486ecf1b4a2010529f
AnthonyLedesma <anthonymledesma@gmail.com>
2023-01-19 10:37:22 -0700

    allow custom colors to migrate

--- src/blocks/author/index.js
+++ src/blocks/author/index.js
@@ -21,26 +21,89 @@ const settings = {
 			return null;
 		}
 
+		// Used to set text color props
+		const hasTextColor = ( props.attributes.hasOwnProperty( 'textColor' ) );
+		const hasCustomTextColor = ( props.attributes.hasOwnProperty( 'style' ) &&
+			props.attributes.style.hasOwnProperty( 'color' ) && props.attributes.style.color.hasOwnProperty( 'text' ) );
+
+		// Used to set background color props
+		const hasBackgroundColor = ( props.attributes.hasOwnProperty( 'backgroundColor' ) );
+		const hasCustomBackgroundColor = ( props.attributes.hasOwnProperty( 'style' ) &&
+			props.attributes.style.hasOwnProperty( 'color' ) && props.attributes.style.color.hasOwnProperty( 'background' ) );
+
+		// Used to set font props
+		const hasFontSize = ( props.attributes.hasOwnProperty( 'fontSize' ) );
+		const hasCustomFont = ( props.attributes.hasOwnProperty( 'style' ) &&
+			props.attributes.style.hasOwnProperty( 'typography' ) );
+
 		const { replaceBlocks } = dispatch( 'core/block-editor' );
 		const isRTL = select( 'core/editor' ).getEditorSettings().isRTL;
 
-		const authorNameBlock = createBlock( 'core/heading', {
-			content: props.attributes.name,
-			level: 4,
+		const authorNameBlock = () => {
+			let authorNameBlockProps = {
+				content: props.attributes.name,
+				level: 4,
+			};
+
 			// Font controls - default is fontSize and style is used for custom setting.
-			...( props.attributes.hasOwnProperty( 'fontSize' ) && { fontSize: props.attributes.fontSize } ),
-			...( props.attributes.hasOwnProperty( 'style' ) && { style: props.attributes.style } ),
+			// All font controls should apply to the bio and name.
+			if ( hasFontSize ) {
+				authorNameBlockProps = { ...authorNameBlockProps,
+					fontSize: props.attributes.fontSize,
+				};
+			}
+			if ( hasCustomFont ) {
+				authorNameBlockProps = { ...authorNameBlockProps,
+					style: { ...authorNameBlockProps?.style,
+						typography: props.attributes.style.typography,
+					},
+				};
+			}
 
 			// Headings do not inherit color from parent - we descend the style here for initial match.
-			...( props.attributes.hasOwnProperty( 'textColor' ) && { textColor: props.attributes.textColor } ),
-		} );
+			if ( hasCustomTextColor ) {
+				authorNameBlockProps = { ...authorNameBlockProps,
+					style: { ...authorNameBlockProps?.style,
+						color: { ...authorNameBlockProps?.style?.color,
+							text: props.attributes.style.color.text,
+						},
+					},
+				};
+			}
+			if ( hasTextColor ) {
+				authorNameBlockProps = { ...authorNameBlockProps,
+					textColor: props.attributes.textColor,
+				};
+			}
 
-		const authorBioBlock = createBlock( 'core/paragraph', {
-			content: props.attributes.biography,
+			return createBlock( 'core/heading', authorNameBlockProps );
+		};
+
+		const authorBioBlock = () => {
+			let authorBioBlockProps = {
+				content: props.attributes.biography,
+			};
+
 			// Font controls - default is fontSize and style is used for custom setting.
-			...( props.attributes.hasOwnProperty( 'fontSize' ) && { fontSize: props.attributes.fontSize } ),
-			...( props.attributes.hasOwnProperty( 'style' ) && { style: props.attributes.style } ),
-		} );
+			// All font controls should apply to the bio and name.
+			if ( hasFontSize ) {
+				authorBioBlockProps = {
+					...authorBioBlockProps,
+					fontSize: props.attributes.fontSize,
+				};
+			}
+			if ( hasCustomFont ) {
+				authorBioBlockProps = {
+					...authorBioBlockProps,
+					style: {
+						...authorBioBlockProps?.style,
+						typography: props.attributes.style.typography,
+					},
+				};
+			}
+
+			return createBlock( 'core/paragraph', authorBioBlockProps );
+		};
 
 		const buttonBlock = parentBlock.innerBlocks;
 		const imageBlock = createBlock( 'core/image', { className: 'is-style-rounded', id: props.attributes.imgId, url: props.attributes.imgUrl } );
@@ -51,63 +114,76 @@ const settings = {
 		} );
 
 		const leftColumn = createBlock( 'core/column', { width: '25%' }, [ imageBlock ] );
-		const rightColumn = createBlock( 'core/column', { width: '75%' }, [ authorNameBlock, authorBioBlock, ...buttonBlock ] );
-		const columnsStyleProps = {
-			style: {
-				spacing: {
-					padding: {
-						bottom: '2.5rem',
-						left: '2.5rem',
-						right: '2.5rem',
-						top: '2.5rem',
-					},
-				},
-			},
-		};
+		const rightColumn = createBlock( 'core/column', { width: '75%' }, [ authorNameBlock(), authorBioBlock(), ...buttonBlock ] );
 
-		let animationProps = {};
-		if ( typeof props.attributes.animation !== 'undefined' ) {
-			animationProps = {
-				animation: props.attributes.animation,
-			};
-		}
-
-		let fontSizeProps = {};
-		if ( typeof props.attributes.fontSize !== 'undefined' ) {
-			fontSizeProps = {
-				fontSize: props.attributes.fontSize,
-			};
-		}
-
-		let columnsBlockProps = {
-			className: props.attributes?.className ?? '',
-			content: props.attributes.biography,
-			...animationProps,
-			...columnsStyleProps,
-			...fontSizeProps,
-			...( props.attributes.hasOwnProperty( 'textColor' ) && { textColor: props.attributes.textColor } ),
-			...( props.attributes.hasOwnProperty( 'backgroundColor' ) && { backgroundColor: props.attributes.backgroundColor } ),
-		};
-
-		// Does not have background color set. Use default block color from CoBlocks.
-		if ( ! props.attributes.hasOwnProperty( 'backgroundColor' ) ) {
-			// $dark-opacity-light-200 computed #8C8C971A - no variable access in root.
-			columnsBlockProps = {
-				...columnsBlockProps,
+		const columnsBlock = () => {
+			let columnsBlockProps = {
+				...( props.attributes.hasOwnProperty( 'className' ) && { className: props.attributes.className } ),
+				...( props.attributes.hasOwnProperty( 'animation' ) && { animation: props.attributes.animation } ),
 				style: {
-					...columnsBlockProps?.style,
-					color: {
-						background: '#8C8C971A',
+					spacing: {
+						padding: {
+							bottom: '2.5rem',
+							left: '2.5rem',
+							right: '2.5rem',
+							top: '2.5rem',
+						},
 					},
 				},
 			};
-		}
 
-		const columnsBlock = createBlock( 'core/columns', columnsBlockProps, [ leftColumn, rightColumn ] );
+			// Headings do not inherit color from parent - we descend the style here for initial match.
+			if ( hasCustomTextColor ) {
+				columnsBlockProps = { ...columnsBlockProps,
+					style: { ...columnsBlockProps?.style,
+						color: { ...columnsBlockProps?.style?.color,
+							text: props.attributes.style.color.text,
+						},
+					},
+				};
+			}
+			if ( hasTextColor ) {
+				columnsBlockProps = { ...columnsBlockProps,
+					textColor: props.attributes.textColor,
+				};
+			}
+
+			// Headings do not inherit color from parent - we descend the style here for initial match.
+			if ( hasCustomBackgroundColor ) {
+				columnsBlockProps = { ...columnsBlockProps,
+					style: { ...columnsBlockProps?.style,
+						color: { ...columnsBlockProps?.style?.color,
+							background: props.attributes.style.color.background,
+						},
+					},
+				};
+			}
+			if ( hasBackgroundColor ) {
+				columnsBlockProps = { ...columnsBlockProps,
+					backgroundColor: props.attributes.backgroundColor,
+				};
+			}
+
+			const hasNoBackgroundColorSet = ! props.attributes.hasOwnProperty( 'backgroundColor' ) &&
+				! props.attributes?.style?.color?.hasOwnProperty( 'background' );
+			// Does not have background color set. Use default block color from CoBlocks.
+			if ( hasNoBackgroundColorSet ) {
+				// $dark-opacity-light-200 computed #8C8C971A - no variable access in root.
+				columnsBlockProps = { ...columnsBlockProps,
+					style: { ...columnsBlockProps?.style,
+						color: { ...columnsBlockProps?.style?.color,
+							background: '#8C8C971A',
+						},
+					},
+				};
+			}
+
+			return createBlock( 'core/columns', columnsBlockProps, [ leftColumn, rightColumn ] );
+		};
 
 		replaceBlocks(
 			[ props.clientId ],
-			columnsBlock
+			columnsBlock()
 		);
 
 		return null;
