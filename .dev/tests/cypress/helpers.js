f1f0f87e4e0a95536f08336f965da3b9c4db06fa
Justin Kopepasah <justin@kopepasah.com>
2023-01-23 15:58:24 -0500

    Readd to block check in Cypress.

--- .dev/tests/cypress/helpers.js
+++ .dev/tests/cypress/helpers.js
@@ -175,6 +175,8 @@ export function savePage() {
  */
 
 export function checkForBlockErrors( blockName ) {
+	disableGutenbergFeatures();
+
 	cy.get( '.block-editor-warning' ).should( 'not.exist' );
 
 	cy.get( 'body.php-error' ).should( 'not.exist' );
