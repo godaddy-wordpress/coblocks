5b80fc3b6e96a3778a17ba187f0f486013313e1c
AnthonyLedesma <anthonymledesma@gmail.com>
2023-01-25 21:56:17 -0700

    add a variation for the author columns

--- src/block-variations/core/paragraph/index.js
+++ src/block-variations/core/paragraph/index.js
@@ -53,9 +53,33 @@ export const BLOCK_VARIATION_PARAGRAPH_ALERT_SUCCESS = {
 	title: __( 'Success alert', 'coblocks' ),
 };
 
+export const BLOCK_VARIATION_COLUMNS_AUTHOR_LAYOUT = {
+	/* translators: block variation description */
+	description: __( 'Success alert.', 'coblocks' ),
+	icon: paragraph,
+	innerBlocks: [
+		[ 'core/column', { width: '25%' }, [
+			[ 'core/image', {} ],
+		] ],
+		[ 'core/column', { width: '75%' }, [
+			[ 'core/heading', {} ],
+			[ 'core/paragraph', {} ],
+			[ 'core/buttons', {} ],
+
+		] ],
+	],
+	name: 'alert-success',
+	/* translators: block variation name */
+	title: __( 'Success alert', 'coblocks' ),
+};
+
 [
 	BLOCK_VARIATION_PARAGRAPH_ALERT_INFO,
 	BLOCK_VARIATION_PARAGRAPH_ALERT_WARNING,
 	BLOCK_VARIATION_PARAGRAPH_ALERT_ERROR,
 	BLOCK_VARIATION_PARAGRAPH_ALERT_SUCCESS,
 ].forEach( ( variation ) => registerBlockVariation( 'core/paragraph', variation ) );
+
+[
+	BLOCK_VARIATION_COLUMNS_AUTHOR_LAYOUT,
+].forEach( ( variation ) => registerBlockVariation( 'core/columns', variation ) );
