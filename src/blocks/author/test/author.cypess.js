0b9e28cbf1ff7da835a322e7eb756efa560c7117
AnthonyLedesma <anthonymledesma@gmail.com>
2023-02-23 10:30:31 -0700

    test more things

--- src/blocks/author/test/author.cypess.js
+++ src/blocks/author/test/author.cypess.js
@@ -3,7 +3,7 @@
  */
 import * as helpers from '../../../../.dev/tests/cypress/helpers';
 
-describe( 'Test CoBlocks Alert Block', function() {
+describe( 'Test CoBlocks Author Block', function() {
 	let postId;
 	before( () => {
 		cy.fixture( `../.dev/tests/cypress/fixtures/${ Cypress.spec.name }.json` )
@@ -12,11 +12,11 @@ describe( 'Test CoBlocks Alert Block', function() {
 			} );
 	} );
 
-	it( 'Test Alert block migrates into core blocks.', function() {
+	it( 'Test Author block migrates into core blocks.', function() {
 		helpers.goTo( `/wp-admin/post.php?post=${ postId }&action=edit` );
 
 		helpers.openBlockNavigator();
-		cy.get( '[data-type="core/paragraph"]' ).should( 'have.length', 3 );
-		helpers.checkForBlockErrors( 'core/paragraph' );
+		cy.get( '[data-type="core/columns"]' ).should( 'have.length', 3 );
+		helpers.checkForBlockErrors( 'core/columns' );
 	} );
 } );
