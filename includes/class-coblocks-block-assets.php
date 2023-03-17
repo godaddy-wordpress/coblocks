29483fe7f34049866d5e076bb201754120d35435
Evan Herman <evan.m.herman@gmail.com>
2023-03-17 16:11:42 -0400

    Add comment referencing initial report of the bug

--- includes/class-coblocks-block-assets.php
+++ includes/class-coblocks-block-assets.php
@@ -606,6 +606,10 @@ class CoBlocks_Block_Assets {
 
 		global $post;
 
+		/**
+		 * Resolves a fatal error bug on PHP 8+ with Timber.
+		 * @see https://wordpress.org/support/topic/the-method-has_masonry_v1_block-produces-a-fatal-error-on-php-8-0-22-and-above/
+		 */
 		$post_content = ! empty( $post ) ? $post->post_content : get_the_content();
 
 		preg_match_all( $v1_regex, $post_content, $matches );
