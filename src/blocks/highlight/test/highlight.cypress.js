336938728c7441bb1eeed732ee94d8af2ea11666
Olivier Lafleur <olafleur@godaddy.com>
2023-03-19 00:32:57 -0400

    Fix highlight test

--- src/blocks/highlight/test/highlight.cypress.js
+++ src/blocks/highlight/test/highlight.cypress.js
@@ -48,9 +48,6 @@ describe( 'Block: Highlight', function() {
 	 * Test the highlight block custom classes
 	 */
 	it( 'Test the Font size changes as expected.', function() {
-		// Styles tab introduced in 6.2.
-		helpers.selectStylesTabIfExists();
-
 		cy.get( '.components-toggle-group-control-option, .components-toggle-group-control-option-base' ).then( ( elems ) => {
 			let dataValue = Cypress.$( '.wp-block-coblocks-highlight' ).css( 'font-size' );
 			Array.from( elems ).forEach( ( elem ) => {
