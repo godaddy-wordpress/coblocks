227038c17b888350cb929849e51429a6a40235ea
Evan Herman <evan.m.herman@gmail.com>
2023-03-24 15:19:46 -0400

    Remove glboal

--- includes/class-coblocks-block-assets.php
+++ includes/class-coblocks-block-assets.php
@@ -662,16 +662,7 @@ class CoBlocks_Block_Assets {
 	 * @return boolean True when an element on the page has .coblocks-animate class, else false.
 	 */
 	public function has_coblocks_animation() {
-		global $post;
-
-		/**
-		 * Resolves a fatal error bug on PHP 8+ with Timber.
-		 *
-		 * @see https://wordpress.org/support/topic/the-method-has_masonry_v1_block-produces-a-fatal-error-on-php-8-0-22-and-above/
-		 */
-		$post_content = ! empty( $post ) ? $post->post_content : get_the_content();
-
-		return false !== strpos( $post_content, 'coblocks-animate' );
+		return false !== strpos( get_the_content(), 'coblocks-animate' );
 	}
 }
 
