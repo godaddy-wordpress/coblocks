273921da7c657570206b589c2fd411031f76342b
AnthonyLedesma <anthonymledesma@gmail.com>
2023-03-28 08:11:05 -0700

    apply phpcs

--- includes/block-migrate/loader.php
+++ includes/block-migrate/loader.php
@@ -27,10 +27,10 @@ require_once COBLOCKS_PLUGIN_DIR . 'includes/block-migrate/class-coblocks-author
 add_action(
 	'the_post',
 	function( WP_Post &$post ) {
-		if ( function_exists('is_admin') && ! is_admin() ) {
+		if ( function_exists( 'is_admin' ) && ! is_admin() ) {
 			return;
 		}
-		
+
 		if ( function_exists( 'get_current_screen' ) && ! get_current_screen()->is_block_editor ) {
 			return;
 		}
