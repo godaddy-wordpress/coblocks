b849b73617297b52d82b0573ebc8d690d03f4a04
Olivier Lafleur <olafleur@godaddy.com>
2023-07-20 09:56:49 -0400

    Revert highlight changes

--- src/blocks/highlight/save.js
+++ src/blocks/highlight/save.js
@@ -14,19 +14,22 @@ import { getColorClassnames, getColorStyles } from '../../utils/helper.js';
 import classnames from 'classnames';
 
 const save = ( { attributes } ) => {
-	const { content } = attributes;
+	const { align, content } = attributes;
 
 	const saveBlockProps = useBlockProps.save();
+	saveBlockProps.style.textAlign = align;
 
 	/**
 	 * In the Highlight block we descend only the `color` and `backgroundColor` styles and classnames but keep all others on the parent.
 	 */
 	const highlightClasses = getColorClassnames( saveBlockProps );
+	const highlightStyles = getColorStyles( saveBlockProps );
 
 	return RichText.isEmpty( content ) ? null : (
 		<p { ...saveBlockProps }>
 			<RichText.Content
 				className={ classnames( 'wp-block-coblocks-highlight__content', highlightClasses ) }
+				style={ highlightStyles }
 				tagName="mark"
 				value={ content }
 			/>
