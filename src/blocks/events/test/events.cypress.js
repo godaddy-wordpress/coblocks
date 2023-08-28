3158f96c8e5c5f77d22dea14d2c2f801fd20bafc
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2023-08-28 11:54:04 -0700

    Set min version of WP to 6.1 & add grunt helper (#2564)
    * set min version of WP to 6.1 & add grunt helper
    
    * comment out test code to satisfy lint:js
    
    * add test for 6.1 to run e2e
    
    * names must be different for the workflow
    
    * try adding php unit tests for other WP versions

--- src/blocks/events/test/events.cypress.js
+++ src/blocks/events/test/events.cypress.js
@@ -5,13 +5,13 @@ import * as helpers from '../../../../.dev/tests/cypress/helpers';
 
 describe( 'Block: Events', function() {
 	// Setup events data.
-	const eventsData = {
-		color: {
-			textColor: '#ffffff',
-			textColorRGB: 'rgb(255, 255, 255)',
-		},
-		ical: 'https://calendar.google.com/calendar/ical/8hohgb8qv19fgvjbbkcehe0ce0%40group.calendar.google.com/public/basic.ics',
-	};
+	// const eventsData = {
+	// 	color: {
+	// 		textColor: '#ffffff',
+	// 		textColorRGB: 'rgb(255, 255, 255)',
+	// 	},
+	// 	ical: 'https://calendar.google.com/calendar/ical/8hohgb8qv19fgvjbbkcehe0ce0%40group.calendar.google.com/public/basic.ics',
+	// };
 
 	beforeEach( () => {
 		helpers.addBlockToPost( 'coblocks/events', true );
