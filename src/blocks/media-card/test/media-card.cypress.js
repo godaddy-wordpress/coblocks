9c358fa8be8932279546f16069f3f0b64491bffb
Anthony Ledesma <30462574+AnthonyLedesma@users.noreply.github.com>
2024-03-12 08:01:40 -0700

    Bump workflow for 6.5 version (#2590)
    * bump the workflow for wp next
    
    * RC1
    
    * Start fixes for WP 6.5
    
    * More fixes
    
    * Fix Form block test
    
    * Fix padding controls test
    
    * Fix media filter control test
    
    * Remove exhaustive deps eslint warning
    
    * revert date to not trigger test-wp-next flow on main
    
    ---------
    
    Co-authored-by: aledesma-godaddy <aledesma@godaddy.com>
    Co-authored-by: Olivier Lafleur <olafleur@godaddy.com>

--- src/blocks/media-card/test/media-card.cypress.js
+++ src/blocks/media-card/test/media-card.cypress.js
@@ -1,14 +1,24 @@
+import { isWP65AtLeast } from '../../../../.dev/tests/cypress/helpers';
+
 describe( 'Test CoBlocks Media Card Block', function() {
 	/**
 	 * Test that we can not insert a media-card block into the page.
 	 * Media-card blocks is deprecated and should not be usable.
 	 */
 	it( 'Test media-card block in not insertable.', function() {
-		cy.get( '.edit-post-header-toolbar' ).find( '.edit-post-header-toolbar__inserter-toggle' ).then( ( inserterButton ) => {
-			if ( ! Cypress.$( inserterButton ).hasClass( 'is-pressed' ) ) {
-				cy.get( inserterButton ).click();
-			}
-		} );
+		if ( isWP65AtLeast() ) {
+			cy.get( '.edit-post-header-toolbar' ).find( '.editor-document-tools__inserter-toggle' ).then( ( inserterButton ) => {
+				if ( ! Cypress.$( inserterButton ).hasClass( 'is-pressed' ) ) {
+					cy.get( inserterButton ).click();
+				}
+			} );
+		} else {
+			cy.get( '.edit-post-header-toolbar' ).find( '.edit-post-header-toolbar__inserter-toggle' ).then( ( inserterButton ) => {
+				if ( ! Cypress.$( inserterButton ).hasClass( 'is-pressed' ) ) {
+					cy.get( inserterButton ).click();
+				}
+			} );
+		}
 
 		cy.get( '.block-editor-inserter__search' ).find( 'input' ).clear();
 		cy.get( '.block-editor-inserter__search' ).click().type( 'media-card' );
