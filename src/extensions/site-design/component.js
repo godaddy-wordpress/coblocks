bbce1c64353e8e8c5b595592a6ab3d5808c80af4
Jonathan Bardo <jbardo@godaddy.com>
2024-04-10 14:27:18 -0400

    Add null coalescing on design style after argument

--- src/extensions/site-design/component.js
+++ src/extensions/site-design/component.js
@@ -74,7 +74,7 @@ export function SiteDesignStyles() {
 		// Here we move the style element so that we can continue to target and change on user action.
 		// Only move element if it is not yet under the desktop preview div.
 		if ( ! stylesElement.closest( 'div.is-desktop-preview' ) ) {
-			document.querySelectorAll( '.is-desktop-preview style:last-of-type' )?.[ 0 ].after( stylesElement );
+			document.querySelectorAll( '.is-desktop-preview style:last-of-type' )?.[ 0 ]?.after?.( stylesElement );
 		}
 	}, [ isUpdating, designResp ] );
 
