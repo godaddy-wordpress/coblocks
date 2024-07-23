4a4e73cc77e30f253cd030fa6de487cb41cb0689
Evan Herman <evan.m.herman@gmail.com>
2024-07-23 15:45:22 -0400

    Bump tested up to values to 6.6 (#2616)

--- class-coblocks.php
+++ class-coblocks.php
@@ -7,7 +7,7 @@
  * Version: 3.1.12
  * Text Domain: coblocks
  * Domain Path: /languages
- * Tested up to: 6.5
+ * Tested up to: 6.6
  * Requires at least: 6.3
  *
  * CoBlocks is free software: you can redistribute it and/or modify
